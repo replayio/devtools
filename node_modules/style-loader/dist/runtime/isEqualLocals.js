19c7e7e3daed1c11e308cd83f65d409f7d5ae61c
Brian Hackett <bhackett1024@gmail.com>
2020-05-07 15:45:48 -1000

    add node modules

---
+++ node_modules/style-loader/dist/runtime/isEqualLocals.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function isEqualLocals(a, b) {
+  if (!a && b || a && !b) {
+    return false;
+  }
+
+  var p;
+
+  for (p in a) {
+    if (a[p] !== b[p]) {
+      return false;
+    }
+  }
+
+  for (p in b) {
+    if (!a[p]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = isEqualLocals;
