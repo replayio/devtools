079a23244779f1b79b11d79f791f72d9d8e3d306
Brian Hackett <bhackett1024@gmail.com>
2020-06-03 09:03:02 -1000

    Add missing file

---
+++ src/devtools/client/debugger/src/actions/sources/reloadLocations.js
@@ -0,0 +1,40 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+
+import {
+  getContext,
+  getSelectedLocation,
+  getSourceActorsForSource,
+  getSourceByActorId,
+} from "../../selectors";
+
+import {
+  selectSource,
+} from "./select";
+
+import { ThreadFront } from "protocol/thread";
+
+export function reloadLocations() {
+  return async ({ dispatch, getState }) => {
+    const cx = getContext(getState());
+    const selectedLocation = getSelectedLocation(getState());
+
+    const actors = getSourceActorsForSource(getState(), selectedLocation.sourceId);
+    if (actors.length != 1) {
+      // Not handling HTML files yet.
+      return;
+    }
+
+    const scriptId = actors[0].actor;
+    const preferredScriptId = ThreadFront.getPreferredScriptId(scriptId);
+    if (preferredScriptId != scriptId) {
+      const newSource = getSourceByActorId(getState(), preferredScriptId);
+      if (newSource) {
+        await dispatch(selectSource(cx, newSource.id));
+      }
+    }
+
+    // FIXME breakpoints should be reloaded as well...
+  };
+}
