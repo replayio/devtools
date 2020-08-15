cedb053fcbc87d075c99bb0f405ec6f89db0c4ad
Jason Laster <jason.laster.11@gmail.com>
2020-08-15 16:57:09 -0700

    Fix tooltip rendering

--- src/ui/components/Tooltip.js
+++ src/ui/components/Tooltip.js
@@ -1,17 +1,24 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
 
+let screen;
+
 export default function Tooltip({ tooltip }) {
   if (!tooltip) {
     return null;
   }
+  if (tooltip.screen) {
+    screen = tooltip.screen;
+  }
 
+  const currentScreen = tooltip.screen || screen;
   return (
     <div className="timeline-tooltip" style={{ left: tooltip.left }}>
-      {tooltip.screen && (
+      {currentScreen && (
         <img
+          style={{ opacity: tooltip.screen ? 1 : 0.8 }}
           className="timeline-tooltip-image"
-          src={`data:${tooltip.screen.mimeType};base64,${tooltip.screen.data}`}
+          src={`data:${currentScreen.mimeType};base64,${currentScreen.data}`}
         />
       )}
     </div>
