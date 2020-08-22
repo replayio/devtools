d1ef8ef8d3e587a3c6a431f3bb9597e0f12057e7
Jason Laster <jason.laster.11@gmail.com>
2020-08-21 20:56:09 -0700

    Rename event-timelne to right-sidebar (part 3)

--- src/ui/components/RisghtSidebar/index.js
+++ src/ui/components/RisghtSidebar/index.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import EventsTimeline from "./EventsTimeline";
 
+import "./RightSidebar.css";
+
 export default function RightSidebar({}) {
   const [expanded, setExpanded] = useState(true);
   return (
-    <div className="events-timeline">
+    <div className="right-sidebar">
       <EventsTimeline expanded={expanded} />
-      <div className="right-sidebar">
+      <div className="drawer">
         <button className="comment-button-container" onClick={() => setExpanded(!expanded)}>
           <div className="img comment-icon"></div>
         </button>
