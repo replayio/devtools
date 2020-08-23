8dce2c11bf371f7fc6f978efcb7f6ad28e1c33d9
Jason Laster <jlaster@mozilla.com>
2020-08-23 09:40:41 -0700

    Use npm version of lodash (#470)

--- src/devtools/client/debugger/src/utils/build-query.js
+++ src/devtools/client/debugger/src/utils/build-query.js
@@ -3,7 +3,7 @@
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
 // @flow
-import escapeRegExp from "lodash/escapeRegExp";
+import { escapeRegExp } from "lodash";
 
 import type { SearchModifiers } from "../types";
 
