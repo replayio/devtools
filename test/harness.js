82ff8c9f6f4c46f868577519dd5173f489e694cb
Brian Hackett <bhackett1024@gmail.com>
2020-08-26 11:06:25 -1000

    Add some logging to test harness

--- test/harness.js
+++ test/harness.js
@@ -2,6 +2,8 @@
 
 // Harness used for local testing.
 
+dump(`TestHarnessStart\n`);
+
 #include "header.js"
 
 // env is declared in the scope this is evaluated in.
@@ -9,6 +11,11 @@ const url = env.get("RECORD_REPLAY_TEST_URL");
 
 (async function() {
   await startRecordingTab(url, "localhost");
+  dump(`TestHarnessRecordingTabStarted\n`);
+
   await waitForMessage("RecordingFinished");
+  dump(`TestHarnessRecordingFinished\n`);
+
   await stopRecordingAndLoadDevtools();
+  dump(`TestHarnessFinished\n`);
 })();
