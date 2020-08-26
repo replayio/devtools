3ac01dc2942a2c5dad0f5e0bf195e890dc0365a6
Brian Hackett <bhackett1024@gmail.com>
2020-08-26 06:12:37 -1000

    Add test that events within iframes work, for #474

--- test/scripts/inspector-02.js
+++ test/scripts/inspector-02.js
@@ -1,5 +1,9 @@
-// Test that the element highlighter works.
+// Test that the element highlighter works, and iframe behavior.
 (async function() {
+  // Events within the iframe should show up.
+  await Test.addEventListenerLogpoints(["event.mouse.click"]);
+  await Test.waitForMessage("click { target: div#iframediv, clientX: 0, clientY: 0, layerX: 0, layerY: 0 }");
+
   await Test.addBreakpoint("doc_inspector_basic.html", 9);
   await Test.rewindToLine(9);
 
