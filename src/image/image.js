30f3639e4833f8b5838aff49ec078b9c406d51a5
Jason Laster <jason.laster.11@gmail.com>
2020-09-14 11:56:00 -0700

    add welcome page

--- src/image/image.js
+++ src/image/image.js
@@ -60,6 +60,7 @@ const gBackgroundImages = {
   ".invoke-confirm .close-confirm-dialog-button::before": require("devtools/client/debugger/images/close.svg"),
   ".invoke-confirm .learn-more-link::after": require("devtools/client/themes/images/help.svg"),
   "#header .logo": require("./images/logo.svg"),
+  ".computer-work": require("./images/computer-work.svg"),
   ".comment-close": require("devtools/client/themes/images/close.svg"),
   ".comment-write": require("devtools/client/themes/images/pen.svg"),
   ".comment-confirm": require("devtools/client/themes/images/check.svg"),
--- src/ui/components/Recordings/index.js
+++ src/ui/components/Recordings/index.js
@@ -2,15 +2,30 @@ const React = require("react");
 import { connect } from "react-redux";
 import { selectors } from "../../reducers";
 import { Recording } from "./Recording";
+import { useAuth0 } from "@auth0/auth0-react";
 
 import "./Recordings.css";
 
-const Recordings = props => (
-  <div className="recordings">
-    {props.recordings &&
-      props.recordings.map((recording, i) => <Recording data={recording} key={i} />)}
-  </div>
-);
+const Recordings = props => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  if (!isAuthenticated) {
+    return (
+      <div className="welcome-screen">
+        <div className="welcome-panel">
+          <img className="logo" src="images/logo.svg" />
+          <img className="atwork" src="images/computer-work.svg" />
+          <button onClick={() => loginWithRedirect()}>Log In</button>
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="recordings">
+      {props.recordings &&
+        props.recordings.map((recording, i) => <Recording data={recording} key={i} />)}
+    </div>
+  );
+};
 
 export default connect(
   state => ({
