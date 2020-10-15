6f5e637f2aac1f7c1c76644d9cec983a129ad645
Holger Benl <hbenl@evandor.de>
2020-10-15 17:56:39 +0200

    add typescript

--- src/devtools/client/debugger/babel.config.js
+++ src/devtools/client/debugger/babel.config.js
@@ -18,6 +18,7 @@ module.exports = {
         /[/\\]node_modules[/\\]react-aria-components[/\\]/,
       ],
       presets: [
+        "@babel/preset-typescript",
         "@babel/preset-react",
         [
           "@babel/preset-env",
