595101e0a8f5864d5cb094a6f778067bd26807fd
Jaril Valenciano <jarilvalenciano@gmail.com>
2020-10-21 17:44:01 -0400

    Add conditional logpoints

--- src/devtools/client/debugger/src/components/Editor/Panel.js
+++ src/devtools/client/debugger/src/components/Editor/Panel.js
@@ -30,6 +30,12 @@ export class Panel extends PureComponent {
     this.initialValue = breakpoint.options.logValue;
   }
 
+  getConditionValue() {
+    const { breakpoint } = this.props;
+    const options = breakpoint?.options;
+    return options?.condition;
+  }
+
   keepFocusOnInput() {
     if (this.input) {
       this.input.focus();
@@ -51,26 +57,41 @@ export class Panel extends PureComponent {
   toggleActionsPanel() {
     const value = this.codeMirror.getValue();
     if (value == this.initialValue) {
-      this.setViewing();
+      this.hideLogpointEditor();
     } else {
-      this.setEditing();
+      this.showLogpointEditor();
     }
   }
 
   save = () => {
+    if (this.conditionCodeMirror.getValue() === "") {
+      this.deleteCondition();
+      this.hideConditionalEditor();
+    }
+
+    console.log(this.conditionInput.value);
     const value = this.codeMirror.getValue();
     this.initialValue = value;
     this.toggleActionsPanel();
     this.setBreakpoint(value);
+    this.conditionInput.blur();
+    this.input.blur();
   };
 
   cancel = () => {
+    if (this.conditionCodeMirror.getValue() === "") {
+      this.hideConditionalEditor();
+    }
+
     this.codeMirror.doc.setValue(this.initialValue);
-    this.setViewing();
+    this.hideLogpointEditor();
+    this.conditionInput.blur();
+    this.input.blur();
   };
 
   delete = () => {
-    this.removeLogpoint();
+    this.deleteCondition();
+    this.deleteLogpoint();
   };
 
   setBreakpoint(value) {
@@ -81,10 +102,26 @@ export class Panel extends PureComponent {
     return this.props.setBreakpointOptions(cx, location, {
       ...options,
       logValue: value,
+      condition: this.conditionInput.value.trim(),
     });
   }
 
-  removeLogpoint() {
+  removeCondition = () => {
+    this.hideConditionalEditor();
+    this.deleteCondition();
+  };
+
+  deleteCondition = () => {
+    const { cx, breakpoint } = this.props;
+    const location = breakpoint.location;
+    const options = { ...breakpoint?.options } || {};
+    delete options.condition;
+    console.log(options);
+
+    return this.props.setBreakpointOptions(cx, location, options);
+  };
+
+  deleteLogpoint() {
     const { cx, breakpoint } = this.props;
     const location = breakpoint.location;
     const options = { ...breakpoint?.options } || {};
@@ -110,14 +147,52 @@ export class Panel extends PureComponent {
     }
   };
 
-  setEditing = () => {
-    this.editActionsNode.classList.remove("hidden");
+  showLogpointEditor = () => {
+    if (this.hasConditionalOpen) {
+      this.fullEditActionsNode.classList.remove("hidden");
+    } else {
+      this.compactEditActionsNode.classList.remove("hidden");
+    }
+
+    this.conditionTinyLabelNode.classList.add("hidden");
+    this.logpointTinyLabelNode.classList.add("hidden");
+
+    this.conditionalLabelNode.classList.remove("hidden");
     this.labelNode.classList.remove("hidden");
+    this.addConditionNode.classList.remove("hidden");
+    this.removeConditionNode.classList.remove("hidden");
   };
 
-  setViewing = () => {
-    this.editActionsNode.classList.add("hidden");
+  hideLogpointEditor = () => {
+    this.conditionTinyLabelNode.classList.remove("hidden");
+    this.logpointTinyLabelNode.classList.remove("hidden");
+
+    this.compactEditActionsNode.classList.add("hidden");
+    this.fullEditActionsNode.classList.add("hidden");
+    this.conditionalLabelNode.classList.add("hidden");
     this.labelNode.classList.add("hidden");
+    this.addConditionNode.classList.add("hidden");
+    this.removeConditionNode.classList.add("hidden");
+  };
+
+  showConditionalEditor = () => {
+    this.hasConditionalOpen = true;
+
+    this.conditionalNode.classList.remove("hidden");
+    this.conditionalLogpointHeader.classList.remove("hidden");
+    this.logpointHeader.classList.add("hidden");
+    this.fullEditActionsNode.classList.remove("hidden");
+    this.compactEditActionsNode.classList.add("hidden");
+  };
+
+  hideConditionalEditor = () => {
+    this.hasConditionalOpen = false;
+
+    this.conditionalNode.classList.add("hidden");
+    this.conditionalLogpointHeader.classList.add("hidden");
+    this.logpointHeader.classList.remove("hidden");
+    this.fullEditActionsNode.classList.add("hidden");
+    this.compactEditActionsNode.classList.remove("hidden");
   };
 
   UNSAFE_componentWillMount() {
@@ -145,15 +220,6 @@ export class Panel extends PureComponent {
     });
 
     if (this.input) {
-      let parent = this.input.parentNode;
-      while (parent) {
-        if (parent instanceof HTMLElement && parent.classList.contains("CodeMirror-scroll")) {
-          this.scrollParent = parent;
-          break;
-        }
-        parent = parent.parentNode;
-      }
-
       if (this.scrollParent) {
         this.scrollParent.addEventListener("scroll", this.repositionOnScroll);
         this.repositionOnScroll();
@@ -161,16 +227,16 @@ export class Panel extends PureComponent {
     }
   }
 
-  createEditor = input => {
+  createConditionEditor = input => {
     const { log, editor } = this.props;
     const codeMirror = editor.CodeMirror.fromTextArea(input, {
       mode: "javascript",
       theme: "mozilla",
-      placeholder: "Log message, e.g. displayName",
+      placeholder: L10N.getStr("editor.conditionalPanel.logPoint.placeholder2"),
     });
 
     codeMirror.on("keydown", (cm, e) => {
-      if (e.key === "Enter") {
+      if (e.key.length > 1 && e.key != "Backspace") {
         e.codemirrorIgnore = true;
         e.preventDefault();
       }
@@ -179,12 +245,51 @@ export class Panel extends PureComponent {
     const codeMirrorWrapper = codeMirror.getWrapperElement();
 
     codeMirrorWrapper.addEventListener("keyup", e => {
+      console.log(e);
+      if (e.key.length > 1 && e.key != "Enter" && e.key != "Backspace") {
+        return;
+      } else if (e.key == "Enter") {
+        this.save();
+      }
+      codeMirror.save();
+    });
+
+    codeMirrorWrapper.addEventListener("click", e => {
+      this.showLogpointEditor();
+    });
+
+    this.conditionInput = input;
+    this.conditionCodeMirror = codeMirror;
+  };
+
+  createLogpointEditor = input => {
+    const { editor } = this.props;
+    const codeMirror = editor.CodeMirror.fromTextArea(input, {
+      mode: "javascript",
+      theme: "mozilla",
+      placeholder: "Log message, e.g. displayName",
+    });
+
+    codeMirror.on("keydown", (cm, e) => {
+      if (e.key.length > 1 && e.key != "Backspace") {
+        e.codemirrorIgnore = true;
+        e.preventDefault();
+      }
+    });
+
+    const codeMirrorWrapper = codeMirror.getWrapperElement();
+
+    codeMirrorWrapper.addEventListener("keyup", e => {
+      if (e.key.length > 1 && e.key != "Enter" && e.key != "Backspace") {
+        return;
+      }
+
       codeMirror.save();
       this.onInput(e);
     });
 
     codeMirrorWrapper.addEventListener("click", e => {
-      this.setEditing();
+      this.showLogpointEditor();
     });
 
     this.input = input;
@@ -195,6 +300,7 @@ export class Panel extends PureComponent {
 
   renderPanel(props) {
     const { breakpoint } = props;
+    const line = breakpoint.location.line;
     const defaultValue = breakpoint?.options.logValue;
 
     const panel = document.createElement("div");
@@ -204,13 +310,62 @@ export class Panel extends PureComponent {
         onClick={() => this.keepFocusOnInput()}
         ref={node => (this.panelNode = node)}
       >
-        <div className="text title">Logpoint: Line {breakpoint.location.line}</div>
-        <div className="text label hidden" ref={node => (this.labelNode = node)}>
+        <div className="row header logpoint" ref={node => (this.logpointHeader = node)}>
+          <div className="text title">Logpoint: Line {line}</div>
+          <div
+            className="text add-condition hidden"
+            ref={node => (this.addConditionNode = node)}
+            onClick={this.showConditionalEditor}
+          >
+            Add a condition
+          </div>
+        </div>
+        <div
+          className="row header conditional-logpoint hidden"
+          ref={node => (this.conditionalLogpointHeader = node)}
+        >
+          <div className="text title">Conditional Logpoint: Line {line}</div>
+          <div
+            className="text remove-condition hidden"
+            ref={node => (this.removeConditionNode = node)}
+            onClick={this.removeCondition}
+          >
+            Remove the condition
+          </div>
+        </div>
+        <div className="conditional-row hidden" ref={node => (this.conditionalNode = node)}>
+          <div className="text label" ref={node => (this.conditionalLabelNode = node)}>
+            Conditional expression (e.g. x === true)
+          </div>
+          <div className="input-row">
+            <div
+              className="text label"
+              ref={node => {
+                this.conditionTinyLabelNode = node;
+              }}
+            >
+              Condition:
+            </div>
+            <textarea
+              defaultValue={this.getConditionValue()}
+              ref={input => this.createConditionEditor(input)}
+            />
+          </div>
+        </div>
+        <div className="text label log-message hidden" ref={node => (this.labelNode = node)}>
           Log message (e.g. &apos;x is x&apos;, x)
         </div>
         <div className="input-row">
-          <textarea defaultValue={defaultValue} ref={input => this.createEditor(input)} />
-          <div className="edit-actions hidden" ref={node => (this.editActionsNode = node)}>
+          <div
+            className="text label"
+            ref={node => {
+              this.logpointTinyLabelNode = node;
+            }}
+          >
+            Log:
+          </div>
+          <textarea defaultValue={defaultValue} ref={input => this.createLogpointEditor(input)} />
+          <div className="edit-actions hidden" ref={node => (this.compactEditActionsNode = node)}>
             <button className="text save" onClick={this.save}>
               Save
             </button>
@@ -218,6 +373,14 @@ export class Panel extends PureComponent {
               Cancel
             </button>
           </div>
+        </div>
+        <div className="edit-actions full hidden" ref={node => (this.fullEditActionsNode = node)}>
+          <button className="text save" onClick={this.save}>
+            Save
+          </button>
+          <button className="text cancel" onClick={this.cancel}>
+            Cancel
+          </button>
         </div>
       </div>,
       panel
