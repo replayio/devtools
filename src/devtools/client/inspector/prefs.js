cffa91800d1d911e3870bc7ace72acc91bf0ba84
Holger Benl <hbenl@evandor.de>
2020-11-04 15:56:08 +0100

    add feature flag for showing whitespace nodes

--- src/devtools/client/inspector/prefs.js
+++ src/devtools/client/inspector/prefs.js
@@ -10,6 +10,7 @@ pref("devtools.inspector.three-pane-enabled", false);
 pref("devtools.inspector.features.old-rulesview.enabled", false);
 pref("devtools.inspector.features.new-markupview.enabled", false);
 pref("devtools.inspector.features.old-markupview.enabled", true);
+pref("devtools.inspector.features.show-whitespace-nodes", true);
 
 export const prefs = new PrefsHelper("devtools.inspector", {
   threePaneEnabled: ["Bool", "three-pane-enabled"],
@@ -19,4 +20,5 @@ export const features = new PrefsHelper("devtools.inspector.features", {
   oldRulesView: ["Bool", "old-rulesview.enabled"],
   newMarkupView: ["Bool", "new-markupview.enabled"],
   oldMarkupView: ["Bool", "old-markupview.enabled"],
+  showWhitespaceNodes: ["Bool", "show-whitespace-nodes"],
 });
