6988a0b8639b71b8b3765e0255ac4bad23e04a49
Jaril Valenciano <jarilvalenciano@gmail.com>
2020-11-20 10:18:30 -0800

    Dock DevTools to the left

--- src/devtools/client/debugger/src/components/App.js
+++ src/devtools/client/debugger/src/components/App.js
@@ -235,7 +235,7 @@ class Debugger extends Component {
 
     return (
       <SplitBox
-        style={{ width: "100vw" }}
+        style={{ width: "100%" }}
         initialSize={prefs.endPanelSize}
         minSize={30}
         maxSize="70%"
@@ -247,7 +247,7 @@ class Debugger extends Component {
         }}
         startPanel={
           <SplitBox
-            style={{ width: "100vw" }}
+            style={{ width: "100%" }}
             initialSize={prefs.startPanelSize}
             minSize={30}
             maxSize="85%"
