2164097aeb18b6228fb133f216cc2ba326d9d326
Holger Benl <hbenl@evandor.de>
2020-11-25 12:20:59 +0100

    move the transformation of Rule/StyleFronts to json objects from the reducer to the action creator

--- src/devtools/client/inspector/rules/reducers/rules.js
+++ src/devtools/client/inspector/rules/reducers/rules.js
@@ -20,83 +20,6 @@ const INITIAL_RULES = {
   rules: [],
 };
 
-/**
- * Given a rule's TextProperty, returns the properties that are needed to render a
- * CSS declaration.
- *
- * @param  {TextProperty} declaration
- *         A TextProperty of a rule.
- * @param  {String} ruleId
- *         The rule id that is associated with the given CSS declaration.
- * @return {Object} containing the properties needed to render a CSS declaration.
- */
-function getDeclarationState(declaration, ruleId) {
-  return {
-    // Array of the computed properties for a CSS declaration.
-    computedProperties: declaration.computedProperties,
-    // An unique CSS declaration id.
-    id: declaration.id,
-    // Whether or not the declaration is valid. (Does it make sense for this value
-    // to be assigned to this property name?)
-    isDeclarationValid: declaration.isValid(),
-    // Whether or not the declaration is enabled.
-    isEnabled: declaration.enabled,
-    // Whether or not the declaration is invisible. In an inherited rule, only the
-    // inherited declarations are shown and the rest are considered invisible.
-    isInvisible: declaration.invisible,
-    // Whether or not the declaration's property name is known.
-    isKnownProperty: declaration.isKnownProperty,
-    // Whether or not the property name is valid.
-    isNameValid: declaration.isNameValid(),
-    // Whether or not the the declaration is overridden.
-    isOverridden: !!declaration.overridden,
-    // Whether or not the declaration is changed by the user.
-    isPropertyChanged: declaration.isPropertyChanged,
-    // The declaration's property name.
-    name: declaration.name,
-    // The declaration's priority (either "important" or an empty string).
-    priority: declaration.priority,
-    // The CSS rule id that is associated with this CSS declaration.
-    ruleId,
-    // The declaration's property value.
-    value: declaration.value,
-  };
-}
-
-/**
- * Given a Rule, returns the properties that are needed to render a CSS rule.
- *
- * @param  {Rule} rule
- *         A Rule object containing information about a CSS rule.
- * @return {Object} containing the properties needed to render a CSS rule.
- */
-function getRuleState(rule) {
-  return {
-    // Array of CSS declarations.
-    declarations: rule.declarations.map(declaration =>
-      getDeclarationState(declaration, rule.domRule.objectId())
-    ),
-    // An unique CSS rule id.
-    id: rule.domRule.objectId(),
-    // An object containing information about the CSS rule's inheritance.
-    inheritance: rule.inheritance,
-    // Whether or not the rule does not match the current selected element.
-    isUnmatched: rule.isUnmatched,
-    // Whether or not the rule is an user agent style.
-    isUserAgentStyle: rule.domRule.isSystem,
-    // An object containing information about the CSS keyframes rules.
-    // keyframesRule: rule.keyframesRule,
-    // The pseudo-element keyword used in the rule.
-    pseudoElement: rule.pseudoElement,
-    // An object containing information about the CSS rule's selector.
-    selector: rule.selector,
-    // An object containing information about the CSS rule's stylesheet source.
-    sourceLink: rule.sourceLink,
-    // The type of CSS rule.
-    type: rule.domRule.type,
-  };
-}
-
 const reducers = {
   [UPDATE_ADD_RULE_ENABLED](rules, { enabled }) {
     return {
@@ -114,9 +37,8 @@ const reducers = {
 
   [UPDATE_RULES](rules, { rules: newRules }) {
     return {
-      highlightedSelector: rules.highlightedSelector,
-      isAddRuleEnabled: rules.isAddRuleEnabled,
-      rules: newRules.map(rule => getRuleState(rule)),
+      ...rules,
+      rules: newRules,
     };
   },
 
