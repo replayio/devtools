2cf0d6d9237deebee883219da9601c73bc7a9d04
Jaril Valenciano <jarilvalenciano@gmail.com>
2020-12-02 15:22:03 -0800

    Rearrange the toolbar

--- src/ui/components/Toolbox.js
+++ src/ui/components/Toolbox.js
@@ -7,7 +7,6 @@ import "./Toolbox.css";
 export default function Toolbox() {
   return (
     <div id="toolbox">
-      <Toolbar />
       <div className="toolbox-top-panels">
         <div className="toolbox-panel" id="toolbox-content-debugger">
           <DebuggerApp />
--- src/ui/components/Views/DevView.js
+++ src/ui/components/Views/DevView.js
@@ -5,6 +5,7 @@ import Timeline from "../Timeline";
 import Viewer from "../Viewer";
 import Tooltip from "../Tooltip";
 import Toolbox from "../Toolbox";
+import Toolbar from "../Toolbar";
 import SplitBox from "devtools/client/shared/components/splitter/SplitBox";
 
 import { installObserver } from "../../../protocol/graphics";
@@ -22,24 +23,27 @@ function DevView({ updateTimelineDimensions }) {
   }, []);
 
   return (
-    <>
-      <SplitBox
-        style={{ width: "100%", overflow: "hidden" }}
-        splitterSize={1}
-        initialSize={prefs.toolboxHeight}
-        minSize="20%"
-        maxSize="80%"
-        vert={true}
-        onMove={handleMove}
-        startPanel={<Toolbox />}
-        endPanel={<Viewer />}
-        endPanelControl={false}
-      />
-      <div id="toolbox-timeline">
-        <Timeline />
-        <Tooltip />
+    <div className="dev-view-outer">
+      <Toolbar />
+      <div className="dev-view-inner">
+        <SplitBox
+          style={{ width: "100%", overflow: "hidden" }}
+          splitterSize={1}
+          initialSize={prefs.toolboxHeight}
+          minSize="20%"
+          maxSize="80%"
+          vert={true}
+          onMove={handleMove}
+          startPanel={<Toolbox />}
+          endPanel={<Viewer />}
+          endPanelControl={false}
+        />
+        <div id="toolbox-timeline">
+          <Timeline />
+          <Tooltip />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
