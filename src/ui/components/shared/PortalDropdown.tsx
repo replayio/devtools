461ae9371b07fab0cc4510b6d17a708244d07ced
Holger Benl <hbenl@evandor.de>
2020-12-02 17:20:34 +0100

    turn PortalDropdown into a function component

--- src/ui/components/shared/PortalDropdown.tsx
+++ src/ui/components/shared/PortalDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, PureComponent } from "react";
+import React, { CSSProperties, RefObject, useRef } from "react";
 import { createPortal } from "react-dom";
 import { DropdownProps } from "./Dropdown";
 import "./PortalDropdown.css";
@@ -7,71 +7,68 @@ interface PortalDropdownProps extends DropdownProps {
   distance?: number;
 }
 
-export default class PortalDropdown extends PureComponent<PortalDropdownProps> {
-  private buttonRef = React.createRef<HTMLButtonElement>();
+export default function PortalDropdown(props: PortalDropdownProps) {
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-  getContentPosition(): CSSProperties {
-    if (this.props.expanded && this.buttonRef.current) {
-      const { left, right, top, bottom } = this.buttonRef.current.getBoundingClientRect();
-      const { width, height } = document.body.getBoundingClientRect();
-      const distance = this.props.distance || 5;
+  const expand = () => {
+    props.setExpanded(true);
+  };
 
-      switch (this.props.position) {
-        case "top-right":
-          return { bottom: height - top + distance + "px", left: left + "px" };
-        case "bottom-right":
-          return { top: bottom + distance + "px", left: left + "px" };
-        case "top-left":
-          return { bottom: height - top + distance + "px", right: width - right + "px" };
-        case "bottom-left":
-          return { top: bottom + distance + "px", right: width - right + "px" };
-      }
+  const collapse = () => {
+    props.setExpanded(false);
+  };
+
+  const {
+    buttonContent,
+    children,
+    position = "bottom-left",
+    buttonStyle = "primary",
+    style,
+    expanded,
+  } = props;
+
+  const contentPosition = getContentPosition(props, buttonRef);
+
+  return (
+    <div className="portal-dropdown-wrapper">
+      <button className={`expand-dropdown ${buttonStyle}`} onClick={expand} ref={buttonRef}>
+        {buttonContent}
+      </button>
+      {expanded
+        ? createPortal(
+            <div className="portal-dropdown-container">
+              <div className="mask" onClick={collapse} />
+              <div className={`content ${position}`} style={{ ...style, ...contentPosition }}>
+                {children}
+              </div>
+            </div>,
+            document.body
+          )
+        : null}
+    </div>
+  );
+}
+
+function getContentPosition(
+  props: PortalDropdownProps,
+  buttonRef: RefObject<HTMLButtonElement>
+): CSSProperties {
+  if (props.expanded && buttonRef.current) {
+    const { left, right, top, bottom } = buttonRef.current.getBoundingClientRect();
+    const { width, height } = document.body.getBoundingClientRect();
+    const distance = props.distance || 5;
+
+    switch (props.position) {
+      case "top-right":
+        return { bottom: height - top + distance + "px", left: left + "px" };
+      case "bottom-right":
+        return { top: bottom + distance + "px", left: left + "px" };
+      case "top-left":
+        return { bottom: height - top + distance + "px", right: width - right + "px" };
+      case "bottom-left":
+        return { top: bottom + distance + "px", right: width - right + "px" };
     }
-
-    return {};
   }
 
-  expand = () => {
-    this.props.setExpanded(true);
-  };
-
-  collapse = () => {
-    this.props.setExpanded(false);
-  };
-
-  render() {
-    const {
-      buttonContent,
-      children,
-      position = "bottom-left",
-      buttonStyle = "primary",
-      style,
-      expanded,
-    } = this.props;
-
-    const contentPosition = this.getContentPosition();
-
-    return (
-      <div className="portal-dropdown-wrapper">
-        <button
-          className={`expand-dropdown ${buttonStyle}`}
-          onClick={this.expand}
-          ref={this.buttonRef}
-        >
-          {buttonContent}
-        </button>
-        {expanded
-          ? createPortal(
-              <div className="portal-dropdown-container">
-                <div className="mask" onClick={this.collapse} />
-                <div className={`content ${position}`} style={{ ...style, ...contentPosition }}>
-                  {children}
-                </div>
-              </div>,
-              document.body
-            )
-          : null}
-      </div>
-    );
-  }
+  return {};
 }
