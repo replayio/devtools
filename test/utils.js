c5abfde5be9f29822773b7f0a637df4e989a5f44
Brian Hackett <bhackett1024@gmail.com>
2020-12-14 17:58:23 +0000

    Fixes for running tests on linux

--- test/utils.js
+++ test/utils.js
@@ -2,19 +2,35 @@
 
 const fs = require("fs");
 
+const GeckoInstallDirectories = [
+  "/Applications/Replay.app", // macOS
+  `${process.env.HOME}/replay`, // linux
+];
+
 const GeckoSuffixes = [
   "Contents/MacOS/replay", // macOS
-  "bin/replay", // linux
+  "replay", // linux, using distribution.
+  "dist/bin/replay", // linux, testing with local build.
 ];
 
 // Get the executable to use when starting gecko.
 function findGeckoPath() {
-  const installDir = process.env.RECORD_REPLAY_PATH || "/Applications/Replay.app";
+  // Find directories where we should look for gecko.
+  let installDirs;
+  if (process.env.RECORD_REPLAY_PATH) {
+    // A path was explicitly specified via the environment.
+    installDirs = [process.env.RECORD_REPLAY_PATH];
+  } else {
+    // Search the default install directories.
+    installDirs = GeckoInstallDirectories;
+  }
 
-  for (const suffix of GeckoSuffixes) {
-    const path = `${installDir}/${suffix}`;
-    if (fs.existsSync(path)) {
-      return path;
+  for (const dir of installDirs) {
+    for (const suffix of GeckoSuffixes) {
+      const path = `${dir}/${suffix}`;
+      if (fs.existsSync(path)) {
+        return path;
+      }
     }
   }
   console.error("Can't find Gecko!");
