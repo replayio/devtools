729e85a9f1a7de6493a0830130f2476d3a6041d6
Brian Hackett <bhackett1024@gmail.com>
2020-12-25 15:42:11 -0700

    Add test for node object previews

---
+++ test/scripts/node_object_preview-01.js
@@ -0,0 +1,20 @@
+Test.describe(`basic node console behavior.`, async () => {
+  await Test.selectConsole();
+
+  await Test.waitForMessage("Array(20) [ 0, 1, 2, 3, 4, 5,");
+  await Test.waitForMessage("Uint8Array(20) [ 0, 1, 2, 3, 4, 5,");
+  await Test.waitForMessage("Set(22) [ {…}, {…}, 0, 1, 2, 3, 4, 5,");
+  await Test.waitForMessage("Map(21) { {…} → {…}, 0 → 1, 1 → 2, 2 → 3, 3 → 4, 4 → 5,");
+  await Test.waitForMessage("WeakSet(20) [ {…}, {…}, {…},");
+  await Test.waitForMessage("WeakMap(20) { {…} → {…}, {…} → {…},");
+  await Test.waitForMessage("Object { a: 0, a0: 0, a1: 1, a2: 2, a3: 3, a4: 4,");
+  await Test.waitForMessage("/abc/gi");
+  await Test.waitForMessage("Date");
+
+  await Test.waitForMessage('RangeError: "foo"');
+
+  msg = await Test.waitForMessage("function bar()");
+  Test.checkJumpIcon(msg);
+
+  await Test.waitForMessage('Array(6) [ undefined, true, 3, null, "z", 40n ]');
+});
