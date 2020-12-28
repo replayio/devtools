e5f2c7b54549810c10498557f5f86b9e5cf42f7a
karina <karina@replay.io>
2020-12-28 15:35:28 -0800

    removed classname from created date so it no longer turns blue or underlined on hover

--- src/ui/components/Dashboard/RecordingItem/RecordingListItem.js
+++ src/ui/components/Dashboard/RecordingItem/RecordingListItem.js
@@ -86,7 +86,7 @@ function ItemTitle({ data, editing, editingTitle, setEditingTitle }) {
           </div>
         ) : null}
       </div>
-      <div className="replay-created-at">Created {moment(data.date).fromNow()}</div>
+      <div>Created {moment(data.date).fromNow()}</div>
     </div>
   );
 }
