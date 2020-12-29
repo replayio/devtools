7f377fe8494a80e1d781ee3cdd13e114edfd665b
Jason Laster <jason.laster.11@gmail.com>
2020-12-29 08:29:08 -0800

    Deploy without first running the checks

--- .github/actions/deploy/main.js
+++ .github/actions/deploy/main.js
@@ -1,17 +1,36 @@
 const { spawnSync } = require("child_process");
 
 function spawnChecked(...args) {
+  console.log(`spawnSync`, args[0], args[1].join(" "));
   const rv = spawnSync.apply(this, args);
   if (rv.status != 0 || rv.error) {
     throw new Error("Spawned process failed");
   }
 }
 
+function uploadDir(src, dst) {
+  dst = dst || src;
+  spawnChecked("aws", ["s3", "cp", "--recursive", src, `s3://recordreplay-website/${dst}`], {
+    stdio: "inherit",
+  });
+}
+
+function upload(src, dst) {
+  dst = dst || src;
+  spawnChecked("aws", ["s3", "cp", src, `s3://recordreplay-website/${dst}`], { stdio: "inherit" });
+}
+
+function invalidateCloudFront() {
+  spawnChecked(
+    "aws",
+    ["cloudfront", "create-invalidation", "--distribution-id", "E3U30CHVUQVFAF", "--paths", "/*"],
+    { stdio: "inherit" }
+  );
+}
+
 spawnChecked("npm", ["install"]);
-console.log("Installed dependencies");
 
 spawnChecked("./node_modules/.bin/webpack", ["--mode=production"]);
-console.log("Built webpack");
 
 upload("index.html", "view");
 upload("dist/main.js");
@@ -21,30 +40,6 @@ upload("dist/parserWorker.js.map");
 upload("dist/searchWorker.js");
 upload("dist/searchWorker.js.map");
 
-upload("src/devtools/client/shared/vendor/whatwg-url.js");
+uploadDir("src/image/images", "images");
 
 invalidateCloudFront();
-
-function upload(src, dst) {
-  dst = dst || src;
-  spawnChecked(
-    "aws",
-    ["s3", "cp", src, `s3://recordreplay-website/${dst}`],
-    { stdio: "inherit" }
-  );
-}
-
-function invalidateCloudFront() {
-  spawnChecked(
-    "aws",
-    [
-      "cloudfront",
-      "create-invalidation",
-      "--distribution-id",
-      "E3U30CHVUQVFAF",
-      "--paths",
-      "/*",
-    ],
-    { stdio: "inherit" }
-  );
-}
