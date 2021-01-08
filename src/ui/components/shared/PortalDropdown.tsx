2331df29679e4810c60a13178b263d5f367c87c6
Jaril Valenciano <jarilvalenciano@gmail.com>
2021-01-08 11:42:16 -0500

    Have the dropdown adjust itself in case of vertical overflow

--- src/ui/components/shared/PortalDropdown.tsx
+++ src/ui/components/shared/PortalDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, RefObject, useRef } from "react";
+import React, { CSSProperties, RefObject, useRef, useState, useCallback } from "react";
 import { createPortal } from "react-dom";
 import { DropdownProps } from "./Dropdown";
 import "./PortalDropdown.css";
@@ -8,13 +8,27 @@ interface PortalDropdownProps extends DropdownProps {
   style?: CSSProperties;
 }
 
+type Direction = "top" | "bottom";
+type Position = "top-right" | "bottom-right" | "top-left" | "bottom-left";
+
+type ViewportOverflows = Direction[];
+
 export default function PortalDropdown(props: PortalDropdownProps) {
   const buttonRef = useRef<HTMLButtonElement>(null);
+  const [dropdownNode, setDropdownNode] = useState<HTMLDivElement | null>(null);
+  const dropdownRef = useCallback(
+    node => {
+      if (node !== null) {
+        console.log(setDropdownNode);
+        setDropdownNode(node);
+      }
+    },
+    [props.expanded]
+  );
 
   const expand = () => {
     props.setExpanded(true);
   };
-
   const collapse = () => {
     props.setExpanded(false);
   };
@@ -28,7 +42,7 @@ export default function PortalDropdown(props: PortalDropdownProps) {
     expanded,
   } = props;
 
-  const contentPosition = getContentPosition(props, buttonRef);
+  const contentPosition = getContentPosition(props, buttonRef, dropdownNode);
 
   return (
     <div className="portal-dropdown-wrapper">
@@ -39,7 +53,11 @@ export default function PortalDropdown(props: PortalDropdownProps) {
         ? createPortal(
             <div className="portal-dropdown-container">
               <div className="mask" onClick={collapse} />
-              <div className={`content ${position}`} style={{ ...style, ...contentPosition }}>
+              <div
+                className={`content ${position}`}
+                style={{ ...style, ...contentPosition }}
+                ref={dropdownRef}
+              >
                 {children}
               </div>
             </div>,
@@ -52,24 +70,62 @@ export default function PortalDropdown(props: PortalDropdownProps) {
 
 function getContentPosition(
   props: PortalDropdownProps,
-  buttonRef: RefObject<HTMLButtonElement>
+  buttonRef: RefObject<HTMLButtonElement>,
+  dropdownNode: HTMLDivElement | null
 ): CSSProperties {
   if (props.expanded && buttonRef.current) {
-    const { left, right, top, bottom } = buttonRef.current.getBoundingClientRect();
-    const { width, height } = document.body.getBoundingClientRect();
+    const buttonRect = buttonRef.current.getBoundingClientRect();
+    const docRect = document.body.getBoundingClientRect();
+    const dropdownRect = dropdownNode?.getBoundingClientRect();
+
+    const { left, right, top, bottom } = buttonRect;
+    const { width: docWidth, height: docHeight } = docRect;
     const distance = props.distance || 5;
 
-    switch (props.position || "bottom-left") {
+    let position = props.position || "bottom-left";
+    let viewportOverflows = getViewportOverflows(docRect, dropdownRect);
+
+    viewportOverflows.forEach(overflow => {
+      if (position.includes(overflow)) {
+        position = adjustPosition(position, overflow);
+      }
+    });
+
+    switch (position) {
       case "top-right":
-        return { bottom: height - top + distance + "px", left: left + "px" };
+        return { bottom: docHeight - top + distance + "px", left: left + "px" };
       case "bottom-right":
         return { top: bottom + distance + "px", left: left + "px" };
       case "top-left":
-        return { bottom: height - top + distance + "px", right: width - right + "px" };
+        return { bottom: docHeight - top + distance + "px", right: docWidth - right + "px" };
       case "bottom-left":
-        return { top: bottom + distance + "px", right: width - right + "px" };
+        return { top: bottom + distance + "px", right: docWidth - right + "px" };
     }
   }
 
   return {};
+}
+
+function getViewportOverflows(doc: ClientRect, dropdown?: ClientRect): ViewportOverflows {
+  const overflows: ViewportOverflows = [];
+
+  if (!dropdown) {
+    return [];
+  }
+
+  if (doc.bottom < dropdown.bottom) {
+    overflows.push("bottom");
+  }
+
+  if (doc.top > dropdown.top) {
+    overflows.push("top");
+  }
+
+  return overflows;
+}
+
+function adjustPosition(position: Position, overflow: Direction) {
+  const oppositeMap = { top: "bottom", bottom: "top" };
+
+  return position.replace(overflow, oppositeMap[overflow]) as Position;
 }
