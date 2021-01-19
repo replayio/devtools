d77a0ba3ba9552a7838320bf498287e8a63c7300
Brian Hackett <bhackett1024@gmail.com>
2021-01-19 14:14:24 -0700

    Test sharp module in N-API node test

--- test/scripts/node_napi.js
+++ test/scripts/node_napi.js
@@ -1,4 +1,5 @@
 Test.describe(`loading napi modules works.`, async () => {
   await Test.selectConsole();
   await Test.warpToMessage("true");
+  await Test.warpToMessage("iVBORw0KGgoAAAANSUhEUgAAAAQAAAAFCAYAAA");
 });
