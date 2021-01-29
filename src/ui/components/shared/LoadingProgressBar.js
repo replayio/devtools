3e2eea168d2062c4e9e447a5204351cfb9b017ea
Jaril Valenciano <jarilvalenciano@gmail.com>
2021-01-29 13:13:26 -0500

    Add a progress bar for editor and inspector

---
+++ src/ui/components/shared/LoadingProgressBar.js
@@ -0,0 +1,29 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./LoadingProgressBar.css";
+
+export default function LoadingProgressBar({ initialProgress = 0 }) {
+  const [displayedProgress, setDisplayedProgress] = useState(initialProgress);
+  const key = useRef(null);
+
+  useEffect(() => {
+    return () => {
+      clearTimeout(key.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    key.current = setTimeout(() => {
+      if (displayedProgress == 100) {
+        return clearTimeout(key.current);
+      }
+
+      const increment = Math.random();
+      const decayed = increment * ((100 - displayedProgress) / 10);
+      const newDisplayedProgress = displayedProgress + decayed;
+
+      setDisplayedProgress(newDisplayedProgress);
+    }, 200);
+  }, [displayedProgress]);
+
+  return <div className="loading-progress-bar" style={{ width: `${displayedProgress}%` }} />;
+}
