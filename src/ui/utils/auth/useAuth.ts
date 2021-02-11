dc4e8631775e911f8bee8ac8bde39e19f64cfc17
Ryan Duffy <ryan.duffy@lge.com>
2021-02-11 13:50:16 -0800

    fix using useClerk outside of SignedIn

--- src/ui/utils/auth/useAuth.ts
+++ src/ui/utils/auth/useAuth.ts
@@ -1,7 +1,18 @@
-import { useClerk } from "@clerk/clerk-react";
+import { LoadedClerkI, useClerk } from "@clerk/clerk-react";
 
 const useAuth = () => {
-  const clerk = useClerk();
+  let clerk: Pick<LoadedClerkI, "user" | "session">;
+  try {
+    // Clerk complains about using this before it can guarantee the client has
+    // loaded. This try/catch appears safe as long as it is used within the
+    // Clerk provider which re-renders when it has set up.
+    clerk = useClerk();
+  } catch (e) {
+    clerk = {
+      user: null,
+      session: null,
+    };
+  }
 
   return {
     user: clerk.user,
