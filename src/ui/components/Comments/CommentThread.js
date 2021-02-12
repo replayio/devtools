4b59a30b0edd35370819d450a7bcf1080d1ff071
Jaril Valenciano <jarilvalenciano@gmail.com>
2021-02-12 13:31:43 -0500

    Allow user to cancel adding a new comment

--- src/ui/components/Comments/CommentThread.js
+++ src/ui/components/Comments/CommentThread.js
@@ -10,6 +10,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import moment from "moment";
 
 import CommentEditor from "ui/components/Comments/CommentEditor";
+import "./CommentThread.css";
 
 function CommentThread({
   comment,
