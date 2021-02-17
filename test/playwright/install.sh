c73641a797eb85cc32fd1f19ce16678bff520f72
Brian Hackett <bhackett1024@gmail.com>
2021-02-17 11:46:18 -0700

    Update playwright install script, fixes #1852

--- test/playwright/install.sh
+++ test/playwright/install.sh
@@ -3,9 +3,6 @@
 mkdir browsers
 mkdir browsers/firefox-1225 # This might need changing if errors happen later when running.
 cd browsers/firefox-1225
-wget https://replay.io/downloads/macOS-replay-playwright.dmg
+wget https://replay.io/downloads/macOS-replay-playwright.tar.xz
 wget https://replay.io/downloads/macOS-recordreplay.so
-7z x macOS-replay-playwright.dmg # If 7z doesn't exist, try `brew install p7zip` first.
-mv Nightly firefox
-chmod +x firefox/Nightly.app/Contents/MacOS/firefox
-chmod +x firefox/Nightly.app/Contents/MacOS/plugin-container.app/Contents/MacOS/plugin-container
+tar xf macOS-replay-playwright.tar.xz
