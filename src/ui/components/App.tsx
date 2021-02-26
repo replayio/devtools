9d5b95f823517614ca9603d6168ef02a7b547ab5
Jaril <jarilvalenciano@gmail.com>
2021-02-26 04:52:27 -0500

    Add Cypress (#1904)

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 
