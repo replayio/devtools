852e3a6066d7afb523a4188721115efd0e88c06b
Jason Laster <jason.laster.11@gmail.com>
2021-02-27 20:03:04 -0800

    Show stalled in the hud

--- src/ui/components/Comments/VideoComments/Hud.tsx
+++ src/ui/components/Comments/VideoComments/Hud.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { selectors } from "ui/reducers";
 import { UIState } from "ui/state";
+const AccessibleImage = require("devtools/client/debugger/src/components/shared/AccessibleImage")
+  .default;
+
 import "./Hud.css";
 
-export function Hud({ hoverTime }: Props) {
-  if (!hoverTime) {
-    return null;
+export function Hud({ hoverTime, currentTime, stalled }: Props) {
+  if (stalled) {
+    return (
+      <div className="canvas-hud">
+        <AccessibleImage className="loader spin" />
+      </div>
+    );
   }
 
-  const time = new Date(hoverTime || 0);
+  const time = new Date(hoverTime || currentTime || 0);
   const seconds = time.getSeconds().toString().padStart(2, "0");
   const minutes = time.getMinutes();
 
@@ -17,8 +24,10 @@ export function Hud({ hoverTime }: Props) {
 }
 
 const connector = connect((state: UIState) => ({
+  currentTime: selectors.getCurrentTime(state),
   hoverTime: selectors.getHoverTime(state),
   zoomRegion: selectors.getZoomRegion(state),
+  stalled: selectors.isPlaybackStalled(state),
 }));
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
