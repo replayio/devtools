8bf57357791dbcb3d6a921306d6db3242e94b49c
Brian Hackett <bhackett1024@gmail.com>
2021-03-04 08:18:55 -0700

    Remove playwright content, fixes #1979

--- test/playwright/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir browsers
-mkdir browsers/firefox-1225 # This might need changing if errors happen later when running.
-cd browsers/firefox-1225
-wget https://replay.io/downloads/macOS-replay-playwright.tar.xz
-wget https://replay.io/downloads/macOS-recordreplay.so
-tar xf macOS-replay-playwright.tar.xz
