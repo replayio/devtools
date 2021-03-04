4f32fb6196d2cfefda92374db51353ed4bae66a5
Jaril Valenciano <jarilvalenciano@gmail.com>
2021-03-04 14:26:43 -0500

    Add motion toggle

--- src/ui/components/Header/Header.js
+++ src/ui/components/Header/Header.js
@@ -80,31 +80,10 @@ function HeaderTitle({ recordingId, editingTitle, setEditingTitle }) {
 
 function Header({ recordingId, sessionId }) {
   const [editingTitle, setEditingTitle] = useState(false);
-  // const { isAuthenticated } = useAuth0();
-
-  // const backIcon = <div className="img arrowhead-right" style={{ transform: "rotate(180deg)" }} />;
-  // const dashboardUrl = `${window.location.origin}/view`;
-
-  // const onNavigateBack = event => {
-  //   if (event.metaKey) {
-  //     return window.open(dashboardUrl);
-  //   }
-  //   window.location = dashboardUrl;
-  // };
 
   return (
     <div id="header">
       <div className="header-left">
-        {/* {isAuthenticated ? (
-          <IconWithTooltip
-            icon={backIcon}
-            content={"Back to Dashboard"}
-            handleClick={e => onNavigateBack(e)}
-          />
-        ) : (
-          <img className="logo" src="images/logo.svg" />
-        )} */}
-
         <HeaderTitle
           recordingId={recordingId}
           setEditingTitle={setEditingTitle}
--- src/ui/components/Header/ViewToggle.js
+++ src/ui/components/Header/ViewToggle.js
@@ -1,42 +1,57 @@
 import React from "react";
 import { connect } from "react-redux";
-import classnames from "classnames";
 import "./ViewToggle.css";
-import { clearSelectedLocation } from "../../../devtools/client/debugger/src/actions/sources";
-import { getContext } from "../../../devtools/client/debugger/src/reducers/pause";
+import { motion, AnimateSharedLayout } from "framer-motion";
 import { setViewMode } from "../../actions/app";
 import { getViewMode } from "../../reducers/app";
 
-function ViewToggle({ viewMode, setViewMode, clearSelectedLocation, cx }) {
+function Handle({ text, isOn }) {
   return (
-    <button className="view-toggle">
-      <div
-        onClick={() => setViewMode("non-dev")}
-        className={classnames("view-toggle-item view-toggle-non-dev", {
-          active: viewMode === "non-dev",
-        })}
-      >
-        VIEWER
-      </div>
-      <div
-        onClick={() => setViewMode("dev")}
-        className={classnames("view-toggle-item view-toggle-dev", {
-          active: viewMode === "dev",
-        })}
-      >
-        DEVTOOLS
-      </div>
-    </button>
+    <div className="option">
+      <div className={`text ${isOn ? "active" : null}`}>{text}</div>
+
+      {isOn && (
+        <motion.div
+          className="handle"
+          layoutId="handle"
+          animate={{ borderRadius: "8px" }}
+          transition={{
+            type: "spring",
+            stiffness: 600,
+            damping: 50,
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+function ViewToggle({ viewMode, setViewMode }) {
+  const onClick = () => {
+    if (viewMode == "dev") {
+      setViewMode("non-dev");
+    } else {
+      setViewMode("dev");
+    }
+  };
+
+  return (
+    <AnimateSharedLayout type="crossfade">
+      <button className="view-toggle" type="button" onClick={onClick}>
+        <div className="inner">
+          <Handle text="Viewer" isOn={viewMode == "non-dev"} />
+          <Handle text="DevTools" isOn={viewMode == "dev"} />
+        </div>
+      </button>
+    </AnimateSharedLayout>
   );
 }
 
 export default connect(
   state => ({
-    cx: getContext(state),
     viewMode: getViewMode(state),
   }),
   {
-    clearSelectedLocation,
     setViewMode,
   }
 )(ViewToggle);
