17d9e99acf57d28d2a1ea9b24372fecec1a1e42d
Ryan Duffy <ryan.duffy@lge.com>
2021-03-06 13:00:26 -0800

    Add emoji support to comments

--- src/ui/components/Comments/TranscriptComments/CommentEditor/DraftJSEditor.tsx
+++ src/ui/components/Comments/TranscriptComments/CommentEditor/DraftJSEditor.tsx
@@ -1,10 +1,61 @@
-import React, { useRef, useEffect } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { EditorState, KeyBindingUtil, getDefaultKeyBinding, Editor } from "draft-js";
+import PluginEditor from "@draft-js-plugins/editor";
+import { EmojiPlugin } from "@draft-js-plugins/emoji";
+import "draft-js/dist/Draft.css";
+import "@draft-js-plugins/emoji/lib/plugin.css";
+
+import "./DraftJSEditor.css";
+
+interface UseEditorConfig {
+  DraftJS: {
+    getDefaultKeyBinding: typeof getDefaultKeyBinding;
+    KeyBindingUtil: typeof KeyBindingUtil;
+  };
+  Editor: typeof PluginEditor;
+  emojiPlugin: EmojiPlugin;
+}
+
+export interface UseEditorResult extends Partial<UseEditorConfig> {
+  editorState?: EditorState;
+  setEditorState: React.Dispatch<React.SetStateAction<EditorState | undefined>>;
+}
+
+export function useEditor(initialContent = ""): UseEditorResult {
+  const [editorState, setEditorState] = useState<EditorState>();
+  const [config, setConfig] = useState<UseEditorConfig>();
+
+  useEffect(function importDraftJS() {
+    Promise.all([
+      import("draft-js"),
+      import("@draft-js-plugins/editor"),
+      import("@draft-js-plugins/emoji"),
+    ]).then(([DraftJS, EditorModule, { default: createEmojiPlugin, defaultTheme }]) => {
+      setEditorState(EditorModule.createEditorStateWithText(initialContent));
+      setConfig({
+        DraftJS,
+        Editor: EditorModule.default,
+        emojiPlugin: createEmojiPlugin({
+          theme: {
+            ...defaultTheme,
+            emojiSuggestions: `${defaultTheme.emojiSuggestions} emojiSuggestions`,
+          },
+        }),
+      });
+    });
+  }, []);
+
+  return {
+    ...config,
+    editorState,
+    setEditorState,
+  };
+}
 
 const moveSelectionToEnd = (editorState: any, DraftJS: any) => {
   const { EditorState, SelectionState } = DraftJS;
   const content = editorState.getCurrentContent();
   const blockMap = content.getBlockMap();
-
   const key = blockMap.last().getKey();
   const length = blockMap.last().getLength();
 
@@ -23,34 +74,50 @@ const moveSelectionToEnd = (editorState: any, DraftJS: any) => {
 
 interface DraftJSEditorProps {
   DraftJS: any;
-  editorState: any;
+  Editor: any;
+  editorState: EditorState;
+  emojiPlugin: any;
+  initialContent: string;
   placeholder: string;
-  setEditorState: (state: any) => void;
+  setEditorState: UseEditorResult["setEditorState"];
   handleSubmit: (inputValue: string) => void;
   handleCancel: () => void;
 }
 
 export default function DraftJSEditor({
   DraftJS,
+  Editor,
   editorState,
+  emojiPlugin,
+  handleCancel,
+  handleSubmit,
   placeholder,
   setEditorState,
-  handleSubmit,
-  handleCancel,
 }: DraftJSEditorProps) {
-  const editorNode = useRef<HTMLDivElement | null>(null);
-  const wrapperNode = useRef<HTMLDivElement | null>(null);
-  const { Editor, getDefaultKeyBinding, KeyBindingUtil } = DraftJS;
+  const editorNode = useRef<Editor>(null);
+  const wrapperNode = useRef<HTMLDivElement>(null);
+  const { getDefaultKeyBinding, KeyBindingUtil } = DraftJS;
 
+  const decorator = editorState.getDecorator();
   useEffect(() => {
+    // This guards against calling focus() with a stale editorNode reference
+    // that doesn't yet have the emoji decorators attached.
+    if (!decorator) return;
+
     // The order is important here — we focus the editor first before scrolling the
     // wrapper into view. Otherwise, the scrolling animation is interrupted by the focus.
     editorNode.current!.focus();
     wrapperNode.current!.scrollIntoView({ block: "center", behavior: "smooth" });
+    const { bottom } = wrapperNode.current!.getBoundingClientRect();
+    const { innerHeight } = window;
+
+    // Toggle a class to render the emoji popup above the editor when close to the bottom
+    wrapperNode.current!.classList.toggle("flipPopup", innerHeight - bottom < 300);
+
     // Move the cursor so that it's at the end of the selection instead of the beginning.
     // Which DraftJS doesn't make easy: https://github.com/brijeshb42/medium-draft/issues/71
-    setEditorState(moveSelectionToEnd(editorState, DraftJS));
-  }, []);
+    setEditorState(state => moveSelectionToEnd(state, DraftJS));
+  }, [decorator]);
 
   const keyBindingFn = (e: React.KeyboardEvent) => {
     if (e.keyCode == 13 && e.metaKey && KeyBindingUtil.hasCommandModifier(e)) {
@@ -75,6 +142,8 @@ export default function DraftJSEditor({
     return "not-handled";
   };
 
+  const { EmojiSuggestions } = emojiPlugin;
+
   return (
     <div className="draft-editor-container" ref={wrapperNode}>
       <Editor
@@ -83,9 +152,11 @@ export default function DraftJSEditor({
         handleKeyCommand={handleKeyCommand}
         keyBindingFn={keyBindingFn}
         placeholder={placeholder}
+        plugins={[emojiPlugin]}
         ref={editorNode}
         webDriverTestID="draftjs-editor"
       />
+      <EmojiSuggestions />
     </div>
   );
 }
