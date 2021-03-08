d847d39a98fbdfd31f286d616e1b02f578798878
Brian Hackett <bhackett1024@gmail.com>
2021-03-08 14:01:43 -0700

    Support using RECORD_REPLAY_PATH that isn't a gecko directory

--- test/utils.js
+++ test/utils.js
@@ -21,13 +21,10 @@ const GeckoSuffixes = [
 // Get the executable to use when starting gecko.
 function findGeckoPath() {
   // Find directories where we should look for gecko.
-  let installDirs;
+  const installDirs = [...GeckoInstallDirectories];
   if (process.env.RECORD_REPLAY_PATH) {
     // A path was explicitly specified via the environment.
-    installDirs = [process.env.RECORD_REPLAY_PATH];
-  } else {
-    // Search the default install directories.
-    installDirs = GeckoInstallDirectories;
+    installDirs.unshift(process.env.RECORD_REPLAY_PATH);
   }
 
   for (const dir of installDirs) {
