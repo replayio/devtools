94b935d133c283de4e09833bc00a5fcc80707a7e
Holger Benl <hbenl@evandor.de>
2021-03-10 11:45:05 +0100

    Ignore error messages in console_async_eval test (#2056)

--- test/scripts/console_async_eval.js
+++ test/scripts/console_async_eval.js
@@ -13,24 +13,30 @@ Test.describe(`Test global console evaluation in async frames.`, async () => {
   await Test.selectFrame(4);
   await Test.executeInConsole('"eval " + n');
 
-  await Test.checkAllMessages([
-    { type: "console-api", content: ["foo"] },
-    { type: "console-api", content: ["bar"] },
-    { type: "command", content: ['"eval " + n'] },
-    { type: "result", content: ['ReferenceError: "n is not defined"'] },
-    { type: "console-api", content: ["baz", "4"] },
-    { type: "logPoint", content: ["qux", "4"] },
-    { type: "command", content: ['"eval " + n'] },
-    { type: "result", content: ['"eval 4"'] },
-    { type: "console-api", content: ["baz", "3"] },
-    { type: "logPoint", content: ["qux", "3"] },
-    { type: "console-api", content: ["baz", "2"] },
-    { type: "logPoint", content: ["qux", "2"] },
-    { type: "command", content: ['"eval " + n'] },
-    { type: "result", content: ['"eval 2"'] },
-    { type: "console-api", content: ["baz", "1"] },
-    { type: "logPoint", content: ["qux", "1"] },
-    { type: "console-api", content: ["baz", "0"] },
-    { type: "logPoint", content: ["qux", "0"] },
-  ]);
+  await Test.checkAllMessages(
+    [
+      { type: "console-api", content: ["foo"] },
+      { type: "console-api", content: ["bar"] },
+      { type: "command", content: ['"eval " + n'] },
+      { type: "result", content: ['ReferenceError: "n is not defined"'] },
+      { type: "console-api", content: ["baz", "4"] },
+      { type: "logPoint", content: ["qux", "4"] },
+      { type: "command", content: ['"eval " + n'] },
+      { type: "result", content: ['"eval 4"'] },
+      { type: "console-api", content: ["baz", "3"] },
+      { type: "logPoint", content: ["qux", "3"] },
+      { type: "console-api", content: ["baz", "2"] },
+      { type: "logPoint", content: ["qux", "2"] },
+      { type: "command", content: ['"eval " + n'] },
+      { type: "result", content: ['"eval 2"'] },
+      { type: "console-api", content: ["baz", "1"] },
+      { type: "logPoint", content: ["qux", "1"] },
+      { type: "console-api", content: ["baz", "0"] },
+      { type: "logPoint", content: ["qux", "0"] },
+    ],
+    {
+      // ignore error messages, see issue #2056
+      ignoreErrors: true,
+    }
+  );
 });
