2c21a30bc2ec20c6bbf334877d5c04ebd0a3926e
Ryan Duffy <ryan.duffy@lge.com>
2021-04-06 13:59:37 -0700

    Add tailwindcss
    Signed-off-by: Ryan Duffy <ryan.duffy@lge.com>

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
