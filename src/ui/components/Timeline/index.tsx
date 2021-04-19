969ec36cd775b5b93139d55f74a16aa480400885
Holger Benl <hbenl@evandor.de>
2021-04-19 14:56:45 +0200

    Only show the playhead when the mouse is hovering over the timeline (#2256)

--- src/ui/components/Timeline/index.tsx
+++ src/ui/components/Timeline/index.tsx
@@ -123,6 +123,10 @@ class Timeline extends Component<PropsFromRedux> {
     }
   };
 
+  isHovering() {
+    return !!this.hoverInterval;
+  }
+
   renderCommands() {
     const {
       playback,
@@ -274,7 +278,7 @@ class Timeline extends Component<PropsFromRedux> {
               style={{ width: `${clamp(Math.min(hoverPercent, precachedPercent), 0, 100)}%` }}
             />
             <div className="progress-line" style={{ width: `${clamp(percent, 0, 100)}%` }} />
-            {percent >= 0 && percent <= 100 ? (
+            {this.isHovering() && percent >= 0 && percent <= 100 ? (
               <div className="progress-line-paused" style={{ left: `${percent}%` }} />
             ) : null}
             {viewMode == "dev" && selectedPanel == "console"
