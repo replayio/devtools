97069c9acd8f986afd04cdcf96c0fa0f2c10d7d6
Jaril <jarilvalenciano@gmail.com>
2021-04-20 21:46:02 -0400

    Wait for material icons font to be loaded (#2387)
    * Wait for material icons font to be loaded
    
    * Handle e2e test case

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import mixpanel from "mixpanel-browser";
 import * as Sentry from "@sentry/react";
 import { connect, ConnectedProps } from "react-redux";
@@ -13,19 +13,18 @@ import SharingModal from "./shared/SharingModal";
 import NewWorkspaceModal from "./shared/NewWorkspaceModal";
 import WorkspaceSettingsModal from "./shared/WorkspaceSettingsModal";
 import SettingsModal from "./shared/SettingsModal/index";
-import { isDeployPreview, skipTelemetry } from "ui/utils/environment";
+import { isDeployPreview, isTest, hasLoadingParam } from "ui/utils/environment";
 import { selectors } from "ui/reducers";
 import { actions } from "ui/actions";
-import { hasLoadingParam } from "ui/utils/environment";
 import ResizeObserverPolyfill from "resize-observer-polyfill";
 import LogRocket from "ui/utils/logrocket";
 import hooks from "ui/hooks";
-
-import "styles.css";
 import { setUserInBrowserPrefs } from "ui/utils/browser";
 import { UIState } from "ui/state";
 import { ModalType } from "ui/state/app";
 import useToken from "ui/utils/useToken";
+var FontFaceObserver = require("fontfaceobserver");
+import "styles.css";
 
 function AppModal({ modal }: { modal: ModalType }) {
   switch (modal) {
@@ -79,8 +78,23 @@ function setTelemetryContext(userId: string | undefined, userEmail: string | und
 function App({ theme, recordingId, modal, updateNarrowMode }: AppProps) {
   const auth = useAuth0();
   const { claims } = useToken();
+
   setTelemetryContext(claims?.hasura.userId, auth.user?.email);
   const { loading } = hooks.useMaybeClaimInvite();
+  const [fontLoading, setFontLoading] = useState(true);
+
+  useEffect(() => {
+    var font = new FontFaceObserver("Material Icons");
+
+    font.load().then(() => {
+      setFontLoading(false);
+    });
+
+    // FontFaceObserver doesn't work in e2e tests.
+    if (isTest()) {
+      setFontLoading(false);
+    }
+  }, []);
 
   useEffect(() => {
     document.body.parentElement!.className = theme || "";
@@ -98,7 +112,7 @@ function App({ theme, recordingId, modal, updateNarrowMode }: AppProps) {
     return <SkeletonLoader content={"Uploading resources"} />;
   }
 
-  if (loading) {
+  if (loading || fontLoading) {
     return <SkeletonLoader content={"Loading"} />;
   }
 
