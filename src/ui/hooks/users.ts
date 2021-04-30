4da16f23e944389a990ef76953ad07804eb73ea9
Dan Miller <dan@replay.io>
2021-04-30 11:50:46 -0700

    only skip getUser hook if there is no userId

--- src/ui/hooks/users.ts
+++ src/ui/hooks/users.ts
@@ -20,9 +20,6 @@ export function useGetUserId() {
 
 export function useGetUserInfo() {
   const userId = getUserId();
-  if (!userId) {
-    return;
-  }
   const { data, loading, error } = useQuery(
     gql`
       query GetUser($userId: uuid!) {
@@ -37,6 +34,7 @@ export function useGetUserInfo() {
       }
     `,
     {
+      skip: !userId,
       variables: { userId },
     }
   );
