05b5b18d4793928d0c27fe06e3deabd1917ff6b7
Jason Laster <jason.laster.11@gmail.com>
2021-05-04 18:28:47 -0700

    Fix commenting

--- src/ui/components/Comments/TranscriptComments/CommentEditor/draftjs.ts
+++ src/ui/components/Comments/TranscriptComments/CommentEditor/draftjs.ts
@@ -1,5 +1,5 @@
 import DraftJS from "draft-js";
-import Editor from "@draft-js-plugins/editor";
+import * as Editor from "@draft-js-plugins/editor";
 import createEmojiPlugin, { defaultTheme as defaultEmojiTheme } from "@draft-js-plugins/emoji";
 import createMentionPlugin, {
   defaultTheme as defaultMentionTheme,
