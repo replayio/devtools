78165d6295d4fc95a9b8cadefc0c5503854880d3
Dan Miller <dan@replay.io>
2021-05-05 10:05:57 -0700

    Convert Error component to TypeScript (#2509)

--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -4,7 +4,7 @@ import useAuth0 from "ui/utils/useAuth0";
 
 import DevTools from "./DevTools";
 const Account = require("./Account").default;
-const { AppErrors } = require("./shared/Error");
+import { AppErrors } from "./shared/Error";
 const LoginModal = require("./shared/LoginModal").default;
 const SkeletonLoader = require("ui/components/SkeletonLoader").default;
 import SharingModal from "./shared/SharingModal";
