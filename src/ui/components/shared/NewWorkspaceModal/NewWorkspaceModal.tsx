2ea48abe71d61192529e833af5e14ab2f6f8c516
Jon Bell <github@lot23.com>
2021-05-18 04:24:59 +0000

    Updating new workspace modal

--- src/ui/components/shared/NewWorkspaceModal/NewWorkspaceModal.tsx
+++ src/ui/components/shared/NewWorkspaceModal/NewWorkspaceModal.tsx
@@ -8,9 +8,6 @@ import { TextInput } from "../Forms";
 import MaterialIcon from "../MaterialIcon";
 import "./NewWorkspaceModal.css";
 
-const content1 = `Teams are another way for you to see other people's replays.`;
-const content2 = `You can do this by creating a team for your company, team or group and adding members. Replays deposited into a team are automatically visible to other members in that team.`;
-
 function NewWorkspaceModal({ hideModal }: PropsFromRedux) {
   const [inputValue, setInputValue] = useState("");
   const createNewWorkspace = hooks.useCreateNewWorkspace();
@@ -32,15 +29,11 @@ function NewWorkspaceModal({ hideModal }: PropsFromRedux) {
         <main>
           <h1>
             <MaterialIcon>add_circle</MaterialIcon>
-            <span>New Team</span>
-          </h1>
-          <div className="new-workspace-content">
-            <p>{content1}</p>
-            <p>{content2}</p>
-          </div>
+            <span>Create a new team</span>
+          </h1>         
           <form onSubmit={handleSave} className="flex flex-col space-y-4">
             <TextInput
-              placeholder="Your new team"
+              placeholder="Your new team name"
               value={inputValue}
               onChange={e => setInputValue(e.target.value)}
             />
