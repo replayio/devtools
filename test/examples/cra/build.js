6340821708c4d9e9ec5f06b3768fd58e9cea5b81
Holger Benl <hbenl@evandor.de>
2021-05-19 15:43:05 +0200

    Add test for ReactDevTools (#2364)

---
+++ test/examples/cra/build.js
@@ -0,0 +1,27 @@
+const os = require("os");
+const path = require("path");
+const fse = require("fs-extra");
+const { spawnSync } = require("child_process");
+
+const APP_NAME = "cra-e2e-test-app";
+
+const buildDir = fse.mkdtempSync(path.join(os.tmpdir(), "devtools-cra-e2e-test-"));
+const distDir = path.join(__dirname, "dist");
+
+spawnSync("npx", ["create-react-app", APP_NAME], {
+  cwd: buildDir,
+  stdio: "inherit",
+});
+fse.copySync(path.join(__dirname, "src/App.js"), path.join(buildDir, APP_NAME, "src/App.js"));
+
+spawnSync("npm", ["run", "build"], {
+  cwd: path.join(buildDir, APP_NAME),
+  env: {
+    ...process.env,
+    PUBLIC_URL: "http://localhost:8080/test/examples/cra/dist",
+  },
+  stdio: "inherit",
+});
+
+fse.emptyDirSync(distDir);
+fse.copySync(path.join(buildDir, APP_NAME, "build"), distDir);
---
+++ test/examples/cra/src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+
+// this example contains some delays for now to work around RecordReplay/gecko-dev#349
+function waitForTime(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function App() {
+  const [list, setList] = React.useState([]);
+
+  useEffect(() => {
+    async function update() {
+      setList([{ key: "1", text: "Foo" }]);
+      await waitForTime(500);
+      console.log("Initial list");
+
+      setList([
+        { key: "1", text: "Foo" },
+        { key: "2", text: "Bar" },
+      ]);
+      await waitForTime(100);
+      console.log("Added an entry");
+
+      setList([{ key: "2", text: "Bar" }]);
+      await waitForTime(100);
+      console.log("Removed an entry");
+
+      // eslint-disable-next-line no-undef
+      dump(`RecReplaySendAsyncMessage Example__Finished`);
+    }
+    update();
+  }, []);
+
+  return <List list={list} />;
+}
+
+function List({ list }) {
+  return (
+    <ul>
+      {list.map(data => (
+        <Item key={data.key} text={data.text} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ text }) {
+  return <li>{text}</li>;
+}
+
+export default App;
