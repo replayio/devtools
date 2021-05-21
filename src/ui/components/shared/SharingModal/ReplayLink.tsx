abe3f5726baf7ebfe12b7ba1d67fbd5069f72ff5
Jaril <jarilvalenciano@gmail.com>
2021-05-21 00:36:33 -0400

    Copy the replay link in the sharing modal on click (#2682)

--- src/ui/components/shared/SharingModal/ReplayLink.tsx
+++ src/ui/components/shared/SharingModal/ReplayLink.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./ReplayLink.css";
 import { RecordingId } from "@recordreplay/protocol";
 
 export default function ReplayLink({ recordingId }: { recordingId: RecordingId }) {
+  const [showCopied, setShowCopied] = useState(false);
+  const timeoutKey = useRef<NodeJS.Timeout | null>(null);
+  const url = `https://replay.io/view?id=${recordingId}`;
+
+  const onClick = () => {
+    navigator.clipboard.writeText(url);
+
+    if (timeoutKey.current) {
+      clearTimeout(timeoutKey.current);
+    }
+
+    setShowCopied(true);
+    timeoutKey.current = setTimeout(() => setShowCopied(false), 2000);
+  };
+
   return (
-    <div className="copy-link">
+    <div className="copy-link relative flex flex-col items-center">
       <input
         type="text"
-        value={`https://replay.io/view?id=${recordingId}`}
+        value={url}
         onKeyPress={e => e.preventDefault()}
         onChange={e => e.preventDefault()}
+        onClick={onClick}
       />
+      {showCopied ? (
+        <div className="absolute bottom-full p-2 bg-black bg-opacity-900 text-white shadow-2xl rounded-lg mb-2">
+          Copied
+        </div>
+      ) : null}
     </div>
   );
 }
