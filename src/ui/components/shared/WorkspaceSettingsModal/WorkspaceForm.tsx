7e10ac02d96a9a681787a8a492ac2106aeab5f6d
Jaril <jarilvalenciano@gmail.com>
2021-06-09 18:02:56 -0400

    Allow a team leader to invite members from the onboarding flow (#2806)

--- src/ui/components/shared/WorkspaceSettingsModal/WorkspaceForm.tsx
+++ src/ui/components/shared/WorkspaceSettingsModal/WorkspaceForm.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Autocomplete from "./Autocomplete";
 
-function validateEmail(email: string) {
+export function validateEmail(email: string) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
--- src/ui/components/shared/WorkspaceSettingsModal/WorkspaceSettingsModal.tsx
+++ src/ui/components/shared/WorkspaceSettingsModal/WorkspaceSettingsModal.tsx
@@ -5,12 +5,27 @@ const Modal = require("ui/components/shared/Modal").default;
 import hooks from "ui/hooks";
 import * as selectors from "ui/reducers/app";
 import { UIState } from "ui/state";
+import { WorkspaceUser } from "ui/types";
 import MaterialIcon from "../MaterialIcon";
 import WorkspaceForm from "./WorkspaceForm";
 import WorkspaceMember, { NonRegisteredWorkspaceMember } from "./WorkspaceMember";
 import "./WorkspaceSettingsModal.css";
 
 const content1 = `Manage members here so that everyone who belongs to this team can see each other's replays.`;
+
+export function WorkspaceMembers({ members }: { members: WorkspaceUser[] }) {
+  return (
+    <ul className="workspace-members">
+      {members.map(member =>
+        member.email ? (
+          <NonRegisteredWorkspaceMember member={member} key={`non-registered-${member.email}`} />
+        ) : (
+          <WorkspaceMember member={member} key={`registered-${member.userId}`} />
+        )
+      )}
+    </ul>
+  );
+}
 
 function WorkspaceSettingsModal({ workspaceId }: PropsFromRedux) {
   const { members, loading } = hooks.useGetWorkspaceMembers(workspaceId!);
@@ -29,20 +44,7 @@ function WorkspaceSettingsModal({ workspaceId }: PropsFromRedux) {
           <WorkspaceForm />
           <div className="workspace-members-container">
             <div className="subheader">MEMBERS</div>
-            <ul className="workspace-members">
-              {members &&
-                !loading &&
-                members.map(member =>
-                  member.email ? (
-                    <NonRegisteredWorkspaceMember
-                      member={member}
-                      key={`non-registered-${member.email}`}
-                    />
-                  ) : (
-                    <WorkspaceMember member={member} key={`registered-${member.userId}`} />
-                  )
-                )}
-            </ul>
+            {members && !loading ? <WorkspaceMembers members={members} /> : null}
           </div>
         </main>
       </Modal>
