ad88d1976bbe88f7c1673157499de07bf20d8be8
Jaril <jarilvalenciano@gmail.com>
2021-06-25 14:50:20 -0400

    Remove focus style on invitation button (#2962)

--- src/ui/components/shared/SettingsModal/ReplayInvitations.tsx
+++ src/ui/components/shared/SettingsModal/ReplayInvitations.tsx
@@ -67,7 +67,7 @@ export default function ReplayInvitations() {
           <button
             type="submit"
             value="Invite"
-            className="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md shadow-sm text-white bg-primaryAccent hover:bg-primaryAccentHover focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primaryAccent"
+            className="inline-flex items-center px-3 py-2 border border-transparent text-lg leading-4 font-medium rounded-md shadow-sm text-white bg-primaryAccent hover:bg-primaryAccentHover focus:outline-none focus:bg-primaryAccentHover"
           >
             Invite
           </button>
