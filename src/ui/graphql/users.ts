3a455d0a232e5566764f2824172ed3471b1fc116
Jaril <jarilvalenciano@gmail.com>
2021-07-08 17:12:50 -0400

    Fix formatting for graphql/users.ts

--- src/ui/graphql/users.ts
+++ src/ui/graphql/users.ts
@@ -1,15 +1,15 @@
 import { gql } from "@apollo/client";
 
 export const GET_USER_INFO = gql`
-query GetUser {
-  viewer {
-    user {
-      id
+  query GetUser {
+    viewer {
+      user {
+        id
+      }
+      email
+      internal
+      nags
+      acceptedTOSVersion
     }
-    email
-    internal
-    nags
-    acceptedTOSVersion
   }
-}
 `;
