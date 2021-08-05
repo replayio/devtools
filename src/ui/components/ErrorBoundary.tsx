15709927e765690ac3208909c059dd06a61b001c
Holger Benl <hbenl@evandor.de>
2021-08-05 17:53:33 +0200

    Ask the user to refresh the page when an unexpected error occurs (#3174)

---
+++ src/ui/components/ErrorBoundary.tsx
@@ -0,0 +1,41 @@
+import React, { Component, ReactNode } from "react";
+import { connect, ConnectedProps } from "react-redux";
+import { UIState } from "ui/state";
+import { getUnexpectedError } from "ui/reducers/app";
+import { setUnexpectedError } from "ui/actions/session";
+import BlankScreen from "./shared/BlankScreen";
+
+class ErrorBoundary extends Component<PropsFromRedux & { children: ReactNode }> {
+  componentDidCatch(error: any, errorInfo: any) {
+    const { setUnexpectedError } = this.props;
+    if (error.name === "ChunkLoadError") {
+      setUnexpectedError({
+        message: "Replay updated",
+        content: "Replay was updated since you opened it. Please refresh the page.",
+        action: "refresh",
+      });
+    } else {
+      setUnexpectedError({
+        message: "Unexpected error",
+        content: "An unexpected error occurred. Please refresh the page.",
+        action: "refresh",
+      });
+    }
+  }
+
+  render() {
+    const { unexpectedError } = this.props;
+    return unexpectedError ? <BlankScreen /> : this.props.children;
+  }
+}
+
+const connector = connect(
+  (state: UIState) => ({
+    unexpectedError: getUnexpectedError(state),
+  }),
+  {
+    setUnexpectedError,
+  }
+);
+type PropsFromRedux = ConnectedProps<typeof connector>;
+export default connector(ErrorBoundary);
