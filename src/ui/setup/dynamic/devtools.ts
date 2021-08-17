a80644a678b805643da69c57a92a4adaaa718aef
Holger Benl <hbenl@evandor.de>
2021-08-17 15:50:58 +0200

    Fix crashes in the Events panel

--- src/ui/setup/dynamic/devtools.ts
+++ src/ui/setup/dynamic/devtools.ts
@@ -74,6 +74,7 @@ const dispatch = url.searchParams.get("dispatch") || undefined;
   window.app.debugger = setupDebuggerHelper();
 
   const eventListenerBreakpoints = await asyncStore.eventListenerBreakpoints;
+  eventListenerBreakpoints.eventTypePoints ??= {};
   const initialDebuggerState = await dbgClient.loadInitialState();
   const initialConsoleState = getConsoleInitialState();
 
