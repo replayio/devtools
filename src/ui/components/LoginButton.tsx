fda96b51abc2889307d105bd65077bf1046eddc5
Jaril <jarilvalenciano@gmail.com>
2021-09-10 16:07:56 -0400

    Fix the sign in button size

--- src/ui/components/LoginButton.tsx
+++ src/ui/components/LoginButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import useAuth0 from "ui/utils/useAuth0";
 import Avatar from "ui/components/Avatar";
 import { handleIntercomLogout } from "ui/utils/intercom";
+import { PrimaryButton } from "./shared/Button";
 
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
@@ -16,17 +17,16 @@ const LoginButton = () => {
   }
 
   return (
-    <button
+    <PrimaryButton
       onClick={() =>
         loginWithRedirect({
           appState: { returnTo: window.location.pathname + window.location.search },
         })
       }
-      type="button"
-      className="inline-flex items-center px-4 py-1.5 text-xl rounded-lg bg-primaryAccent hover:bg-primaryAccentHover text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primaryAccent mr-0 sharebutton"
+      color="blue"
     >
       Sign In
-    </button>
+    </PrimaryButton>
   );
 };
 
