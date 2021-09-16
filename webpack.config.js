2357788858ce22c76c97a29b31bea20d721350ef
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-09-16 10:00:31 +0200

    Add missing Babel plugins to satisfy more target environments

--- webpack.config.js
+++ webpack.config.js
@@ -81,6 +81,8 @@ module.exports = {
               "@babel/plugin-transform-flow-strip-types",
               "@babel/plugin-transform-react-display-name",
               "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-logical-assignment-operators",
+              "@babel/plugin-proposal-nullish-coalescing-operator",
               "@babel/plugin-proposal-optional-chaining",
             ],
           },
