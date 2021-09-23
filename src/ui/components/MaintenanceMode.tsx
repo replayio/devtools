534eb71dc2782c34b466da3be5a867d4f8047a7b
Ryan Duffy <ryan@replay.io>
2021-09-23 12:50:24 -0700

    Add content to login page

--- src/ui/components/MaintenanceMode.tsx
+++ src/ui/components/MaintenanceMode.tsx
@@ -5,7 +5,7 @@ export default function MaintenanceModeScreen() {
   return (
     <div className="grid w-full h-full items-center justify-items-center">
       <div className="flex flex-col space-y-8 max-w-sm items-center">
-        <ReplayLogo className="w-32 h-32" />
+        <ReplayLogo size="lg" />
         <h1>Replay is down for maintenance, come back later!</h1>
       </div>
     </div>
