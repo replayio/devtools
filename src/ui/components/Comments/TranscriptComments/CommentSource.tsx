7e2642042487d76c5b9d2075bf5557143134b923
Jason Laster <jason.laster.11@gmail.com>
2021-09-23 21:31:54 -0700

    Select source location

--- src/ui/components/Comments/TranscriptComments/CommentSource.tsx
+++ src/ui/components/Comments/TranscriptComments/CommentSource.tsx
@@ -1,13 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { createLabels } from "ui/actions/comments";
+import { actions } from "ui/actions";
+import { selectors } from "ui/reducers";
+import { UIState } from "ui/state";
 
 type PropsFromParent = {
   comment: any;
 };
 type CommentSourceProps = PropsFromRedux & PropsFromParent;
 
-function CommentSource({ comment, createLabels }: CommentSourceProps) {
+function CommentSource({
+  comment,
+  createLabels,
+  context,
+  selectLocation,
+  setViewMode,
+}: CommentSourceProps) {
   const [labels, setLabels] = useState<{ primary: string | null; secondary: string | null }>({
     primary: comment.primaryLabel,
     secondary: comment.secondaryLabel,
@@ -22,8 +31,16 @@ function CommentSource({ comment, createLabels }: CommentSourceProps) {
     }
   }, []);
 
+  const onSelectSource = () => {
+    setViewMode("dev");
+    selectLocation(context, comment.sourceLocation);
+  };
+
   return (
-    <div className="space-y-5 p-3 rounded-xl rounded-b-none border-b border-gray-200">
+    <div
+      onClick={onSelectSource}
+      className="space-y-5 p-3 rounded-xl rounded-b-none border-b border-gray-200  hover:bg-toolbarBackground cursor-pointer"
+    >
       <div className="font-medium flex flex-col">
         <div className="font-semibold">{labels.primary}</div>
         <div
@@ -35,6 +52,15 @@ function CommentSource({ comment, createLabels }: CommentSourceProps) {
   );
 }
 
-const connector = connect(null, { createLabels });
+const connector = connect(
+  (state: UIState) => ({
+    context: selectors.getThreadContext(state),
+  }),
+  {
+    createLabels,
+    selectLocation: actions.selectLocation,
+    setViewMode: actions.setViewMode,
+  }
+);
 type PropsFromRedux = ConnectedProps<typeof connector>;
 export default connector(CommentSource);
