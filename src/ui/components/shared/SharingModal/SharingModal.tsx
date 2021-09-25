214b9989c57216acff9c4e2d50891c7ad273c4c8
Jason Laster <jason.laster.11@gmail.com>
2021-09-25 10:07:18 -0700

    Revert "Show a stripped-down share modal to non-owner/collaborators (#3728)"
    This reverts commit a13389366855e3412ca9eb3f22510fb74e78e238.

--- src/ui/components/shared/SharingModal/SharingModal.tsx
+++ src/ui/components/shared/SharingModal/SharingModal.tsx
@@ -10,7 +10,6 @@ import { PrimaryButton } from "../Button";
 import { actions } from "ui/actions";
 import { SharedWith } from "./SharedWith";
 import { CollaboratorDbData } from "./CollaboratorsList";
-import useAuth0 from "ui/utils/useAuth0";
 
 function SharingModalWrapper(props: PropsFromRedux) {
   const { recordingId } = props.modalOptions!;
@@ -42,13 +41,6 @@ function SharingModal({ recording, workspaces, collaborators, hideModal }: Shari
   const isPrivate = recording.private;
   const toggleIsPrivate = hooks.useToggleIsPrivate(recording.id, isPrivate);
 
-  const { isAuthenticated } = useAuth0();
-  const isOwner = hooks.useIsOwner(recording.id || "00000000-0000-0000-0000-000000000000");
-  const isCollaborator =
-    isAuthenticated &&
-    hooks.useIsCollaborator(recording.id || "00000000-0000-0000-0000-000000000000");
-  const showShare = (isOwner || isCollaborator) && false;
-
   const setPublic = () => {
     if (isPrivate) {
       toggleIsPrivate();
@@ -67,33 +59,31 @@ function SharingModal({ recording, workspaces, collaborators, hideModal }: Shari
         style={{ width: "600px" }}
       >
         <section className="space-y-4">
-          {showShare ? <SharedWith {...{ workspaces, recording, collaborators }} /> : null}
+          <SharedWith {...{ workspaces, recording, collaborators }} />
           <div className="space-y-2">
             <h2 className="text-xl">Link</h2>
             <ReplayLink recordingId={recording.id} />
-            {showShare ? (
-              <div className="flex flex-row items-center">
-                {isPrivate ? (
-                  <div className="w-full justify-between flex flex-row items-center">
-                    <div>
-                      <strong>Restricted</strong> Only people added can open this link
-                    </div>
-                    <PrimaryButton color="blue" onClick={setPublic}>
-                      Change to anyone
-                    </PrimaryButton>
+            <div className="flex flex-row items-center">
+              {isPrivate ? (
+                <div className="w-full justify-between flex flex-row items-center">
+                  <div>
+                    <strong>Restricted</strong> Only people added can open this link
                   </div>
-                ) : (
-                  <div className="w-full flex flex-row justify-between items-center">
-                    <div>
-                      <strong>Anyone</strong> on the internet with this link can view
-                    </div>
-                    <PrimaryButton color="blue" onClick={setPrivate}>
-                      Change to restricted
-                    </PrimaryButton>
+                  <PrimaryButton color="blue" onClick={setPublic}>
+                    Change to anyone
+                  </PrimaryButton>
+                </div>
+              ) : (
+                <div className="w-full flex flex-row justify-between items-center">
+                  <div>
+                    <strong>Anyone</strong> on the internet with this link can view
                   </div>
-                )}
-              </div>
-            ) : null}
+                  <PrimaryButton color="blue" onClick={setPrivate}>
+                    Change to restricted
+                  </PrimaryButton>
+                </div>
+              )}
+            </div>
           </div>
         </section>
       </div>
