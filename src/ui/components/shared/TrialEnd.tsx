c85d2a049bb425e6d81a7469e1047bce6d14660f
Jaril <jarilvalenciano@gmail.com>
2021-10-07 08:42:43 -0700

    Make the team trial badge consistent with its row

--- src/ui/components/shared/TrialEnd.tsx
+++ src/ui/components/shared/TrialEnd.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React from "react";
 import MaterialIcon from "./MaterialIcon";
 
@@ -11,9 +12,13 @@ function getDaysLeft(trialEndDate: string) {
 export function TrialEnd({
   trialEnds,
   color = "gray",
+  className,
+  onClick,
 }: {
   trialEnds: string;
   color?: "yellow" | "gray";
+  className?: string;
+  onClick?: () => void;
 }) {
   let style;
 
@@ -25,11 +30,15 @@ export function TrialEnd({
 
   const daysLeft = getDaysLeft(trialEnds);
   if (daysLeft >= 21) {
-    return null
+    return null;
   }
 
   return (
-    <div className="py-1 p-4 rounded-lg flex flex-row space-x-2 items-center" style={style}>
+    <div
+      className={classNames("py-1 p-4 rounded-lg flex flex-row space-x-2 items-center", className)}
+      style={style}
+      onClick={onClick}
+    >
       <MaterialIcon style={{ fontSize: "1.25rem" }}>timer</MaterialIcon>
       <span className="overflow-hidden whitespace-pre overflow-ellipsis">
         Trial expires in {daysLeft} days
