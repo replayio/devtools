70d08f65e30633d655a4dd53c54788d51f63b025
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-10-20 11:28:56 -0400

    use a gray background

--- src/devtools/client/debugger/src/components/Editor/Breakpoints/Panel/FirstEditNag.tsx
+++ src/devtools/client/debugger/src/components/Editor/Breakpoints/Panel/FirstEditNag.tsx
@@ -30,12 +30,12 @@ function FirstEditNag({ editing, nags }: { editing: boolean; nags: Nag[] }) {
     }
   }, [editing]);
 
-  if (nags?.includes(Nag.FIRST_BREAKPOINT_EDIT)) {
-    return null;
-  }
+  // if (nags?.includes(Nag.FIRST_BREAKPOINT_EDIT)) {
+  //   return null;
+  // }
 
   return (
-    <div className="p-2 py-1 text-white bg-black font-sans text-xs flex flex-row items-center space-x-1">
+    <div className="p-2 py-1 text-white bg-gray-400 font-sans text-xs flex flex-row items-center space-x-1">
       <img src="/images/sparkle.svg" className="w-3" />
       <span>{TEXT[step]}</span>
     </div>
