9cad29acddc02edcc904d597c7cda35c8aa490db
Jason Laster <jason.laster.11@gmail.com>
2021-10-26 10:30:16 -0700

    Revert "Only offer the original version of a file in quick open (#3788)"
    This reverts commit 61105b2cc08b37c7e89b59088eb473951c42f000.

--- src/protocol/thread/thread.ts
+++ src/protocol/thread/thread.ts
@@ -1139,11 +1139,6 @@ class _ThreadFront {
       this.updateLocation(location);
     }
   }
-
-  isPreferredSource(sourceId: SourceId) {
-    const chosen = this._chooseSourceId(this._getAlternateSourceIds(sourceId));
-    return sourceId === chosen.sourceId;
-  }
 }
 
 export const ThreadFront = new _ThreadFront();
