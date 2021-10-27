replayio/devtools

commit 7f20640326153cd723c7dffea093c529ff657751

Hide the nags when a breakpoint is disabled
Fixes #4105

This is a bigger change than you might expect because it involved
lifting some state up into a higher component, so that the
`analysisPoints` were available to more components without having to
load them multiple times.

https://user-images.githubusercontent.com/5903784/138999528-4a59f67e-0731-4b89-b570-fbe8bbbeda27.mp4

src/devtools/client/debugger/src/components/Editor/Breakpoints/Panel/Panel.js
@@ -12,11 +12,14 @@ import Widget from "./Widget";
 import "./Panel.css";
 import { connect } from "react-redux";
 import { actions } from "ui/actions";
+import { selectors } from "ui/reducers";
+import { getExecutionPoint } from "devtools/client/debugger/src/reducers/pause";
 import { inBreakpointPanel } from "devtools/client/debugger/src/utils/editor";
 import PanelSummary from "./PanelSummary";
 import FirstEditNag from "./FirstEditNag";
 import hooks from "ui/hooks";
 import { Nag } from "ui/hooks/users";
+import { prefs } from "ui/utils/prefs";
 
 function getPanelWidth({ editor }) {
   // The indent value is an adjustment for the distance from the gutter's left edge
@@ -26,17 +29,32 @@ function getPanelWidth({ editor }) {
   return editor.getScrollInfo().clientWidth - panelIndent;
 }
 
-function Panel({ breakpoint, editor, insertAt, setHoveredItem, clearHoveredItem }) {
+function Panel({
+  analysisPoints,
+  breakpoint,
+  editor,
+  executionPoint,
+  insertAt,
+  setHoveredItem,
+  clearHoveredItem,
+}) {
   const [editing, setEditing] = useState(false);
   const [showCondition, setShowCondition] = useState(!!breakpoint.options.condition);
   const [width, setWidth] = useState(getPanelWidth(editor));
   const [inputToFocus, setInputToFocus] = useState("logValue");
   const { nags } = hooks.useGetUserInfo();
   const updateUserNags = hooks.useUpdateUserNags();
+  const error = analysisPoints === "error";
+  const pausedOnHit =
+    !error &&
+    !!analysisPoints?.find(({ point, time }) => point == executionPoint && time == currentTime);
+  const isHot = !error && (analysisPoints?.length || 0) > prefs.maxHitsDisplayed;
+  const showNag =
+    analysisPoints && !error && !isHot && nags && !nags.includes(Nag.FIRST_BREAKPOINT_ADD);
 
   useEffect(() => {
     // Make sure to toggle off the first_breakpoint_add nag once the widget is opened.
-    if (nags && !nags.includes(Nag.FIRST_BREAKPOINT_ADD)) {
+    if (showNag) {
       const newNags = [...nags, Nag.FIRST_BREAKPOINT_ADD];
       updateUserNags({
         variables: { newNags },
@@ -81,7 +99,7 @@ function Panel({ breakpoint, editor, insertAt, setHoveredItem, clearHoveredItem 
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
       >
-        {<FirstEditNag editing={editing} />}
+        {showNag && <FirstEditNag editing={editing} />}
         <div className={classnames("breakpoint-panel", { editing })}>
           {editing ? (
             <PanelEditor
@@ -97,6 +115,8 @@ function Panel({ breakpoint, editor, insertAt, setHoveredItem, clearHoveredItem 
               breakpoint={breakpoint}
               toggleEditingOn={toggleEditingOn}
               setInputToFocus={setInputToFocus}
+              isHot={isHot}
+              pausedOnHit={pausedOnHit}
             />
           )}
           <BreakpointNavigation {...{ breakpoint, editing, showCondition, setShowCondition }} />
@@ -106,7 +126,17 @@ function Panel({ breakpoint, editor, insertAt, setHoveredItem, clearHoveredItem 
   );
 }
 
-export default connect(() => ({}), {
-  setHoveredItem: actions.setHoveredItem,
-  clearHoveredItem: actions.clearHoveredItem,
-})(Panel);
+export default connect(
+  (state, { breakpoint }) => ({
+    analysisPoints: selectors.getAnalysisPointsForLocation(
+      state,
+      breakpoint.location,
+      breakpoint.options.condition
+    ),
+    executionPoint: getExecutionPoint(state),
+  }),
+  {
+    setHoveredItem: actions.setHoveredItem,
+    clearHoveredItem: actions.clearHoveredItem,
+  }
+)(Panel);

src/devtools/client/debugger/src/components/Editor/Breakpoints/Panel/PanelSummary/index.tsx
@@ -9,7 +9,6 @@ import { UIState } from "ui/state";
 import { actions } from "ui/actions";
 import { selectors } from "ui/reducers";
 
-const { getExecutionPoint } = require("devtools/client/debugger/src/reducers/pause");
 const { prefs } = require("ui/utils/prefs");
 const { trackEvent } = require("ui/utils/telemetry");
 
@@ -20,24 +19,31 @@ import Condition from "./Condition";
 import useAuth0 from "ui/utils/useAuth0";
 import { useGetRecordingId } from "ui/hooks/recordings";
 import { useGetUserId } from "ui/hooks/users";
+import { PointDescription } from "@recordreplay/protocol";
 
 export type Input = "condition" | "logValue";
 
 type PanelSummaryProps = PropsFromRedux & {
+  analysisPoints: PointDescription[] | "error";
   breakpoint: any;
+  executionPoint: any;
+  isHot: boolean;
+  pausedOnHit: boolean;
   setInputToFocus: Dispatch<SetStateAction<Input>>;
   toggleEditingOn: () => void;
 };
 
 function PanelSummary({
-  breakpoint,
-  toggleEditingOn,
-  setInputToFocus,
-  createFrameComment,
-  createFloatingCodeComment,
-  executionPoint,
-  currentTime,
   analysisPoints,
+  breakpoint,
+  createFloatingCodeComment,
+  createFrameComment,
+  currentTime,
+  executionPoint,
+  isHot,
+  pausedOnHit,
+  setInputToFocus,
+  toggleEditingOn,
 }: PanelSummaryProps) {
   const { isTeamDeveloper } = hooks.useIsTeamDeveloper();
   const { user } = useAuth0();
@@ -46,11 +52,7 @@ function PanelSummary({
   const conditionValue = breakpoint.options.condition;
   const logValue = breakpoint.options.logValue;
 
-  const isHot = analysisPoints && analysisPoints.length > prefs.maxHitsDisplayed;
-  const isUnderMaxHitsEditable = !!(
-    analysisPoints && analysisPoints.length < prefs.maxHitsEditable
-  );
-  const isEditable = isUnderMaxHitsEditable && isTeamDeveloper;
+  const isEditable = !isHot && isTeamDeveloper;
 
   const handleClick = (event: React.MouseEvent, input: Input) => {
     if (!isEditable) {
@@ -61,10 +63,6 @@ function PanelSummary({
     toggleEditingOn();
     setInputToFocus(input);
   };
-
-  const pausedOnHit =
-    analysisPoints !== "error" &&
-    !!analysisPoints?.find(point => point.point == executionPoint && point.time == currentTime);
 
   const addComment = (e: React.MouseEvent) => {
     e.stopPropagation();
@@ -95,7 +93,7 @@ function PanelSummary({
     return (
       <div className="summary">
         <div className="options items-center flex-col flex-grow">
-          <Log value={logValue} hasCondition={!!conditionValue} {...{ isUnderMaxHitsEditable }} />
+          <Log value={logValue} hasCondition={!!conditionValue} isUnderMaxHitsEditable={!isHot} />
         </div>
         <CommentButton addComment={addComment} pausedOnHit={pausedOnHit} />
       </div>
@@ -127,13 +125,13 @@ function PanelSummary({
         {conditionValue ? (
           <Condition
             handleClick={handleClick}
-            isUnderMaxHitsEditable={isUnderMaxHitsEditable}
+            isUnderMaxHitsEditable={!isHot}
             value={conditionValue}
           />
         ) : null}
         <Log
           handleClick={handleClick}
-          isUnderMaxHitsEditable={isUnderMaxHitsEditable}
+          isUnderMaxHitsEditable={!isHot}
           hasCondition={!!conditionValue}
           value={logValue}
         />
@@ -150,13 +148,7 @@ function PanelSummary({
 
 const connector = connect(
   (state: UIState, { breakpoint }: { breakpoint: any }) => ({
-    executionPoint: getExecutionPoint(state),
     currentTime: selectors.getCurrentTime(state),
-    analysisPoints: selectors.getAnalysisPointsForLocation(
-      state,
-      breakpoint.location,
-      breakpoint.options.condition
-    ),
   }),
   {
     createFrameComment: actions.createFrameComment,

src/ui/state/app.ts
@@ -1,5 +1,4 @@
 import {
-  RecordingId,
   SessionId,
   PointDescription,
   Location,

commit 20e8f91f24dd988ff6769d75fa1370ae71b3f38c

Add currentTime to the breakpoint panel props
Hotfix for a bug introduced in #4181

src/devtools/client/debugger/src/components/Editor/Breakpoints/Panel/Panel.js
@@ -32,6 +32,7 @@ function getPanelWidth({ editor }) {
 function Panel({
   analysisPoints,
   breakpoint,
+  currentTime,
   editor,
   executionPoint,
   insertAt,
@@ -133,6 +134,7 @@ export default connect(
       breakpoint.location,
       breakpoint.options.condition
     ),
+    currentTime: selectors.getCurrentTime(state),
     executionPoint: getExecutionPoint(state),
   }),
   {