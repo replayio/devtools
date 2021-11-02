replayio/devtools

commit fe8e40b702c7b54e60cdf25074a47d615c8fb510

Rework breakpoint panel
There was a lot of unused CSS here, and also a lot of CSS that was sort
of circuitous. I found the best approach was to rip most of it out and
then rebuild. When you have click targets that extend outside of their
elements and are also close to each other the way that you mark up space
becomes *very* important. For instance, the difference between:

![CleanShot 2021-10-29 at 23 01 51](https://user-images.githubusercontent.com/5903784/139522357-d2cac10c-58c4-4399-9be3-f3490a29408a.png)

Where the whole background is a click target for A is *very* different
structurally from:

![CleanShot 2021-10-29 at 23 02 47](https://user-images.githubusercontent.com/5903784/139522377-ed3caf81-373a-47b5-8e32-12ef49378cfc.png)

Where the top half is a click target for B and the bottom half is a
click target for A. But once you get that structure right, some things
become easier. For instance, B is no longer a child of A's click target,
so you can remove some `preventDefault` style code, which is a sign that
you're moving with the grain rather than against it.

But getting that design right with a minimum of changes as you snap back
and forth between editor mode is *hard*. I still haven't eliminated all
movement, but I think this is as close to pixel-perfect as it is worth
pushing this particular iteration. I think the long term vision for this
component should actually be to remove CodeMirror from it. Don't get me
wrong, CodeMirror is awesome, but every time we render a conditional
break-point we are instantiating two full-blown code editors in JS, when
what we *really* want is just a text input tag with some syntax
highlighting. Even on a fast computer, you can *feel* the DOM rebuilding
itself as it transitions in and out of editor mode. It sort of feels
like trying to drive a nail in with a jackhammer. It's not that it won't
work, it just doesn't feel like the right tool for the job.

src/devtools/client/debugger/src/components/Editor/Breakpoints/Panel/PanelForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classnames from "classnames";
-const { PanelInput } = require("./PanelInput");
+import { PanelInput } from "./PanelInput";
 import { parser } from "devtools/client/debugger/src/utils/bootstrap";
 import { trackEvent } from "ui/utils/telemetry";
 
@@ -70,33 +70,29 @@ export default function PanelForm({
   };
 
   return (
-    <div className="flex-grow overflow-hidden">
-      <form className="flex flex-col space-y-1">
-        {showCondition ? (
-          <div className={classnames("form-row")}>
-            <div className="w-6 flex-shrink-0 mr-1">if</div>
-            <PanelInput
-              id="condition"
-              autofocus={inputToFocus == "condition"}
-              defaultValue={condition}
-              onChange={(cm: any) => onConditionChange(cm.getValue().trim())}
-              onEnter={handleSetBreakpoint}
-              onEscape={toggleEditingOff}
-            />
-          </div>
-        ) : null}
+    <form className="pl-2 flex-grow flex flex-col">
+      {showCondition ? (
         <div className={classnames("form-row")}>
-          {showCondition ? <div className="w-6 flex-shrink-0 mr-1">log</div> : null}
+          <div className="w-6 flex-shrink-0 mr-1">if</div>
           <PanelInput
-            id="logpoint"
-            autofocus={inputToFocus == "logValue"}
-            defaultValue={logValue}
-            onChange={(cm: any) => onLogValueChange(cm.getValue().trim())}
+            autofocus={inputToFocus == "condition"}
+            defaultValue={condition}
+            onChange={(cm: any) => onConditionChange(cm.getValue().trim())}
             onEnter={handleSetBreakpoint}
             onEscape={toggleEditingOff}
           />
         </div>
-      </form>
-    </div>
+      ) : null}
+      <div className={classnames("form-row")}>
+        {showCondition ? <div className="w-6 flex-shrink-0 mr-1">log</div> : null}
+        <PanelInput
+          autofocus={inputToFocus == "logValue"}
+          defaultValue={logValue}
+          onChange={(cm: any) => onLogValueChange(cm.getValue().trim())}
+          onEnter={handleSetBreakpoint}
+          onEscape={toggleEditingOff}
+        />
+      </div>
+    </form>
   );
 }

src/devtools/client/debugger/src/actions/types/ASTAction.js
@@ -1,5 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//

src/devtools/client/debugger/src/actions/types/BreakpointAction.js
@@ -1,5 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//

src/devtools/client/debugger/src/actions/types/PauseAction.js
@@ -1,5 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//

src/devtools/client/debugger/src/actions/types/PreviewAction.js
@@ -1,5 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//

src/devtools/client/debugger/src/actions/types/SourceAction.js
@@ -1,5 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//

src/devtools/client/debugger/src/actions/types/SourceActorAction.js
@@ -1,7 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//
-
-import {} from "../utils/middleware/promise";

src/devtools/client/debugger/src/actions/types/UIAction.js
@@ -1,5 +1,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//

commit 0ce2cce24c3ac259d2dc198bb10576d19bfcb21d

Fix breakpoint statement overflow
I must have introduced this in #4241. Thankfully it's a pretty easy fix.

https://user-images.githubusercontent.com/5903784/139914630-70ed6bdb-8158-4130-afb9-4b90d66d17ba.mp4

https://user-images.githubusercontent.com/5903784/139914828-5f5ae48a-4c48-4105-8bb2-f6e3801c43b2.mp4

src/devtools/client/debugger/src/components/Editor/Breakpoints/Panel/PanelForm.tsx
@@ -43,16 +43,16 @@ interface PanelFormProps {
 }
 
 export default function PanelForm({
-  logValue,
-  setLogValue,
-  setLogSyntaxError,
   condition,
+  handleSetBreakpoint,
+  inputToFocus,
+  logValue,
   setCondition,
   setConditionSyntaxError,
-  handleSetBreakpoint,
-  toggleEditingOff,
-  inputToFocus,
+  setLogSyntaxError,
+  setLogValue,
   showCondition,
+  toggleEditingOff,
 }: PanelFormProps) {
   const onLogValueChange = async (value: string) => {
     trackEvent("breakpoint.set_log");
@@ -70,7 +70,7 @@ export default function PanelForm({
   };
 
   return (
-    <form className="pl-2 flex-grow flex flex-col">
+    <form className="pl-2 flex-grow flex flex-col overflow-hidden">
       {showCondition ? (
         <div className={classnames("form-row")}>
           <div className="w-6 flex-shrink-0 mr-1">if</div>