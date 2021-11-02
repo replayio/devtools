13284bbd3a237d7a33b9dc793a68ae569b1f52e6
Brian Hackett <bhackett1024@gmail.com>
2021-11-02 16:32:27 -0600

    Log a warning to the console when the recording contains too many con… (#4213)
    * Log a warning to the console when the recording contains too many console messages
    
    * Update protocol package version

--- src/protocol/thread/thread.ts
+++ src/protocol/thread/thread.ts
@@ -777,7 +777,11 @@ class _ThreadFront {
   async findConsoleMessages(onConsoleMessage: (pause: Pause, message: Message) => void) {
     const sessionId = await this.waitForSession();
 
-    client.Console.findMessages({}, sessionId);
+    client.Console.findMessages({}, sessionId).then(({ overflow }) => {
+      if (overflow) {
+        console.warn("Too many console messages, not all will be shown");
+      }
+    });
     client.Console.addNewMessageListener(async ({ message }) => {
       await this.ensureAllSources();
       const pause = new Pause(sessionId);
