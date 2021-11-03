f1d18639df7062f86e52220f4dd84aa093196573
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-11-03 11:49:20 -0400

    allow tests in src/devtools

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   moduleNameMapper: {
     "ui/(.*)": "<rootDir>/src/ui/$1",
   },
-  // If you want to run all of the old tests then remove this ignore pattern
-  modulePathIgnorePatterns: ["src/devtools"],
+  testEnvironment: "jsdom",
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(test).[jt]s?(x)",
+    // If you want to run all of the old tests then remove this ignore pattern
+    // "**/?(*.)+(spec).[jt]s?(x)",
+  ],
 };
