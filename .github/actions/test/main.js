904973335bfa23a02a10168adefd6b89558ff17d
Ryan Duffy <ryan@replay.io>
2021-11-05 13:37:03 -0700

    asdf

--- .github/actions/test/main.js
+++ .github/actions/test/main.js
@@ -15,51 +15,51 @@ function checkForFile(path) {
   }
 }
 
-checkForFile("replay/replay.dmg");
-checkForFile("replay-node/macOS-replay-node");
-checkForFile("replay-driver/macOS-recordreplay.so");
+// checkForFile("replay/replay.dmg");
+// checkForFile("replay-node/macOS-replay-node");
+// checkForFile("replay-driver/macOS-recordreplay.so");
 
 console.log(new Date(), "Start");
 
 console.log(new Date(), "Unpackaged distribution");
 
-spawnChecked("hdiutil", ["attach", "replay/replay.dmg"]);
-spawnChecked("cp", ["-R", "/Volumes/Replay/Replay.app", "/Applications"]);
-try {
-  spawnChecked("hdiutil", ["detach", "/Volumes/Replay/"]);
-} catch (e) {}
+// spawnChecked("hdiutil", ["attach", "replay/replay.dmg"]);
+// spawnChecked("cp", ["-R", "/Volumes/Replay/Replay.app", "/Applications"]);
+// try {
+//   spawnChecked("hdiutil", ["detach", "/Volumes/Replay/"]);
+// } catch (e) {}
 
 console.log(new Date(), "Installed replay browser");
 
-spawnChecked("chmod", ["+x", "replay-node/macOS-replay-node"]);
+// spawnChecked("chmod", ["+x", "replay-node/macOS-replay-node"]);
 
 // Set environment variables needed to replay node recordings.
 process.env.RECORD_REPLAY_NODE = "replay-node/macOS-replay-node";
 process.env.RECORD_REPLAY_DRIVER = "replay-driver/macOS-recordreplay.so";
 
-const devServerProcess = spawn("./node_modules/bin/next", ["run", "dev"], {
+const devServerProcess = spawn("./node_modules/.bin/next", ["dev", "-p", "8080"], {
   detached: true,
-  stdio: "inherit", // ["inherit", "pipe", "inherit"],
+  stdio: ["inherit", "pipe", "inherit"],
 });
 
 (async function () {
-  //   console.log("Waiting for Webpack server start");
-  //   await Promise.race([
-  //     new Promise(r => {
-  //       devServerProcess.stdout.on("data", chunk => {
-  //         process.stdout.write(chunk);
-  //         // Once the dev server starts outputting stuff, we assume it has started
-  //         // its server and it is safe to curl.
-  //         r();
-  //       });
-  //     }),
-  //     new Promise(r => setTimeout(r, 10 * 1000)).then(() => {
-  //       throw new Error("Failed to start dev server");
-  //     }),
-  //   ]);
+  console.log("Waiting for Webpack server start");
+  await Promise.race([
+    new Promise(r => {
+      devServerProcess.stdout.on("data", chunk => {
+        process.stdout.write(chunk);
+        // Once the dev server starts outputting stuff, we assume it has started
+        // its server and it is safe to curl.
+        r();
+      });
+    }),
+    new Promise(r => setTimeout(r, 10 * 1000)).then(() => {
+      throw new Error("Failed to start dev server");
+    }),
+  ]);
 
   console.log("Waiting for Webpack build");
-  await new Promise(r => setTimeout(r, 20000));
+  // await new Promise(r => setTimeout(r, 20000));
 
   // Wait for the initial Webpack build to complete before
   // trying to run the tests so the tests don't run
