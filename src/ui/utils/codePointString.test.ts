17a2f204c509dc9a217e4360f5934bbd735075a3
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-11-18 13:42:52 -0500

    codePointString utils

---
+++ src/ui/utils/codePointString.test.ts
@@ -0,0 +1,39 @@
+import { getCodePoints, getCodePointsLength, sliceCodePoints } from "./codePointString";
+
+const unicodeStr = "Boo 👻";
+
+describe("getCodePoints", () => {
+  it("returns an array of code points", () => {
+    const codePoints = getCodePoints(unicodeStr);
+    expect(codePoints).toMatchInlineSnapshot(`
+      Array [
+        "B",
+        "o",
+        "o",
+        " ",
+        "👻",
+      ]
+    `);
+    expect(codePoints).not.toEqual(unicodeStr.split(""));
+  });
+});
+
+describe("getCodePointsLength", () => {
+  it("returns the length in code points", () => {
+    const length = getCodePointsLength(unicodeStr);
+    expect(length).toBe(5);
+
+    // as opposed to code units...
+    expect(unicodeStr.length).toBe(6);
+  });
+});
+
+describe("sliceCodePoints", () => {
+  it("returns the correct substring", () => {
+    const substring = sliceCodePoints(unicodeStr, 4, 5);
+    expect(substring).toMatchInlineSnapshot(`"👻"`);
+
+    // as opposed to code units...
+    expect(substring).not.toEqual(unicodeStr.slice(4, 5));
+  });
+});
