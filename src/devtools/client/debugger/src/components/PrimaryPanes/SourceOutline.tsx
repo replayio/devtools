e5eec45216169fc03c918d9a0a13acc871f2c72e
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-11-19 19:24:30 -0500

    amost there

--- src/devtools/client/debugger/src/components/PrimaryPanes/SourceOutline.tsx
+++ src/devtools/client/debugger/src/components/PrimaryPanes/SourceOutline.tsx
@@ -1,128 +1,196 @@
-import React, { useMemo, useCallback, useState, HTMLProps } from "react";
+import React, { useMemo, useCallback, useState, HTMLProps, useRef, useEffect } from "react";
 import classnames from "classnames";
 
 import { fuzzySearch } from "../../utils/function";
 import uniq from "lodash/uniq";
 import { FixedSizeList as List } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
+import { Source, SourceLocation } from "graphql";
+import OutlineFilter from "./OutlineFilter";
+import { Redacted } from "ui/components/Redacted";
+import PreviewFunction from "../shared/PreviewFunction";
+import { groupBy, keyBy, reduce, sortBy } from "lodash";
+import { findClosestEnclosedSymbol } from "../../utils/ast";
 import { Location } from "@recordreplay/protocol";
-import { Class } from "@babel/types";
-import { SourceLocation } from "graphql";
-import { Source } from "protocol/thread/thread";
-
-function formatData(symbols: any, filter: string) {
-  if (!symbols || !symbols.functions) {
-    return { classes: [], namedFunctions: [], functions: [] };
-  }
-
-  const functions = symbols.functions.filter(func => func.name != "anonymous");
-
-  let classes = uniq(functions.map(func => func.klass));
-
-  let namedFunctions = functions.filter(
-    func => fuzzySearch(func.name, filter) && !func.klass && !classes.includes(func.name)
-  );
-
-  return { classes, namedFunctions };
-}
-
-type SymbolLocation = {
-  start: SourceLocation;
-  end: SourceLocation;
-};
 
 type ClassSymbol = {
   name: string;
-  parent: null | ClassSymbol;
+  location: {
+    start: SourceLocation;
+    end: SourceLocation;
+  };
 };
 
 type FunctionSymbol = {
   name: string | null;
-  identifier: string | null;
-  index: number;
   klass: string | null;
-  location: SymbolLocation;
+  location: {
+    start: SourceLocation;
+    end: SourceLocation;
+  };
   parameterNames: string[];
 };
 
+type OutlineSymbol = ClassSymbol | FunctionSymbol;
+
+function isFunctionSymbol(symbol: OutlineSymbol): symbol is FunctionSymbol {
+  return "parameterNames" in symbol;
+}
+
+function getOutlineSymbols(
+  { classes, functions }: { classes: ClassSymbol[]; functions: FunctionSymbol[] },
+  filter: string
+) {
+  const classNames = new Set(classes.map(s => s.name));
+  const functionsByName = keyBy(functions, "name");
+  const filteredFunctions = functions.filter(
+    ({ name }) =>
+      !!name && name !== "anonymous" && !classNames.has(name) && fuzzySearch(name, filter)
+  );
+  const functionsByClass = groupBy(filteredFunctions, func => func.klass || "");
+
+  return classes.reduce((funcs: Array<ClassSymbol | FunctionSymbol>, classSymbol) => {
+    const classFuncs = functionsByClass[classSymbol.name];
+    if (classFuncs?.length > 0) {
+      funcs.push(functionsByName[classSymbol.name] || classSymbol, ...classFuncs);
+    }
+    return funcs;
+  }, functionsByClass[""] || []);
+}
+
+const SourceOutlineFunction = React.memo(function OutlineFunction({
+  isFocused,
+  func,
+  onSelect,
+}: {
+  isFocused: boolean;
+  func: FunctionSymbol;
+  onSelect?: (func: FunctionSymbol) => void;
+}) {
+  return (
+    <li
+      className={classnames("outline-list__element cursor-pointer", {
+        "px-2": !!func.klass,
+        focused: isFocused,
+      })}
+      onClick={onSelect ? () => onSelect(func) : undefined}
+    >
+      <span className="outline-list__element-icon">λ</span>
+      <Redacted className="inline-block">
+        <PreviewFunction func={func} />
+      </Redacted>
+    </li>
+  );
+});
+
+const SourceOutlineClass = React.memo(function OutlineClassFunctions({
+  klass,
+  isFocused,
+  onSelect,
+}: {
+  klass: OutlineSymbol;
+  isFocused: boolean;
+  onSelect: (klass: ClassSymbol | FunctionSymbol) => void;
+}) {
+  return (
+    <li className="outline-list__class" key={klass.name}>
+      <h2
+        className={classnames("cursor-pointer", { focused: isFocused })}
+        onClick={() => onSelect(klass)}
+      >
+        {"paramNames" in klass ? (
+          <SourceOutlineFunction func={klass} isFocused={false} />
+        ) : (
+          <div>
+            <span className="keyword">class</span> {klass.name}
+          </div>
+        )}
+      </h2>
+    </li>
+  );
+});
+
 type PropTypes = HTMLProps<HTMLDivElement> & {
-  selectLocation: {
-    id: string;
-    url: string;
-    relativeUrl: string;
-    isPrettyPrinted: boolean;
-    content: {
-      state: string;
-      value: {
-        type: "text";
-        value: string;
-      };
-    };
-  };
+  cursorPosition: Location;
+  onChangeCursorPosition: (selectedLocation: Location) => void;
+  sourceId: string;
   symbols: {
     functions: FunctionSymbol[];
     classes: ClassSymbol[];
-    hasJsx: boolean;
-    hasTypes: boolean;
-    loading: boolean;
+    loading: false;
   };
 };
 
 export function SourceOutline({
   className,
-  selectLocation,
-  selectedSource,
+  cursorPosition,
+  onChangeCursorPosition,
+  sourceId,
   symbols,
   ...props
 }: PropTypes) {
   const [filter, setFilter] = useState("");
 
-  const namedFunctions = useMemo(() => {
-    const { namedFunctions } = formatData(symbols, filter);
-    return namedFunctions;
-  }, [filter, symbols]);
+  const items = useMemo(() => getOutlineSymbols(symbols, filter), [filter, symbols]);
 
-  const Function = useCallback(
+  const focusedSymbol = useMemo(
+    () => findClosestEnclosedSymbol(symbols, cursorPosition),
+    [cursorPosition, symbols]
+  );
+
+  const handleSelectSymbol = useCallback(
+    (symbol: ClassSymbol | FunctionSymbol) =>
+      onChangeCursorPosition({
+        sourceId,
+        line: symbol.location.start.line,
+        column: symbol.location.start.column,
+      }),
+    []
+  );
+
+  const OutlineItem = useCallback(
     ({ index, style }) => {
-      const func = namedFunctions[index];
-      const { name, location, parameterNames } = func;
-      console.info(func);
-
+      const symbol = items[index];
+      const isFocused = focusedSymbol === symbol;
       return (
-        <li
-          className={classnames("outline-list__element cursor-pointer px-2 py-1", {
-            focused: false,
-          })}
-          style={style}
-        >
-          <span className="outline-list__element-icon">λ</span>
-          {name}
-          {/* <Redacted className="inline-block">
-            <PreviewFunction func={{ name, parameterNames }} />
-          </Redacted> */}
-        </li>
+        <div style={style}>
+          {isFunctionSymbol(symbol) ? (
+            <SourceOutlineFunction
+              isFocused={isFocused}
+              func={symbol}
+              onSelect={handleSelectSymbol}
+            />
+          ) : (
+            <SourceOutlineClass
+              isFocused={isFocused}
+              klass={symbol}
+              onSelect={handleSelectSymbol}
+            />
+          )}
+        </div>
       );
     },
-    [namedFunctions]
+    [handleSelectSymbol, items]
   );
-  console.info(namedFunctions);
 
   return (
-    <div {...props} className={classnames("h-full", className)}>
-      {/* <OutlineFilter filter={filter} updateFilter={setFilter} /> */}
-      <AutoSizer>
-        {({ height, width }) => (
-          <List
-            innerElementType="ol"
-            height={height}
-            width={width}
-            itemCount={namedFunctions.length}
-            itemSize={22}
-          >
-            {Function}
-          </List>
-        )}
-      </AutoSizer>
+    <div {...props} className={classnames("h-full flex flex-col", className)}>
+      <OutlineFilter filter={filter} updateFilter={setFilter} />
+      <div className="flex-grow my-1 mx-3">
+        <AutoSizer>
+          {({ height, width }) => (
+            <List
+              innerElementType="ol"
+              height={height}
+              width={width}
+              itemCount={items.length}
+              itemSize={24}
+            >
+              {OutlineItem}
+            </List>
+          )}
+        </AutoSizer>
+      </div>
     </div>
   );
 }
