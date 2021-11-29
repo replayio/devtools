6066b66b22320dfff668e23ef59423adcee9f04f
Jason Laster <jason.laster.11@gmail.com>
2021-11-29 08:08:44 -0800

    Outline: refactor outline view (#4673)
    * Outline: refactor outline view
    * Fix classes
    
    Co-authored-by: Jaril <jarilvalenciano@gmail.com>

---
+++ src/devtools/client/debugger/src/components/Outline/ClassFunctionsList.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { OutlineClassFunctions } from "./OutlineClassFunctions";
+import { OutlineFunction } from "./OutlineFunction";
+import { filterOutlineItem } from "./filterOutlineItem";
+import { getFunctionKey } from "./Outline";
+
+export function ClassFunctionsList({
+  symbols,
+  functions,
+  outlineList,
+  klass,
+  focusedItem,
+  onSelect,
+  filter,
+}) {
+  if (!symbols || symbols.loading || klass == null || functions.length == 0) {
+    return null;
+  }
+
+  const classFunc = symbols.functions.find(func => func.name === klass);
+  const classInfo = symbols.functions.find(c => c.klass === klass);
+  let classFunctions = functions.filter(
+    func => filterOutlineItem(func.name, filter) && !!func.klass
+  );
+
+  const item = classFunc || classInfo;
+  const isFocused = focusedItem === item;
+
+  return (
+    <OutlineClassFunctions
+      classFunc={classFunc}
+      classInfo={classInfo}
+      isFocused={isFocused}
+      outlineList={outlineList}
+      onSelect={onSelect}
+    >
+      {classFunctions.map(func => (
+        <OutlineFunction
+          key={getFunctionKey(func)}
+          isFocused={focusedItem === func}
+          func={func}
+          onSelect={onSelect}
+          outlineList={outlineList}
+        />
+      ))}
+    </OutlineClassFunctions>
+  );
+}
