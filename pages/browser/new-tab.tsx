f22c2a735d38d9b9912b931a3de174476e06b928
Ryan Duffy <ryan@replay.io>
2021-11-30 17:35:18 -0800

    fix width of motd on new-tab

--- pages/browser/new-tab.tsx
+++ pages/browser/new-tab.tsx
@@ -6,10 +6,10 @@ export default function NewTab() {
 
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <div className="text-center space-y-8">
+      <div className="text-center space-y-8" style={{ width: 420 }}>
         {motd ? <h2 className="text-2xl text-gray-600">{motd}</h2> : null}
         <img src="/images/logo.svg" className="inline-block h-24 w-24" />
-        <h1 style={{ width: 420 }} className="text-2xl text-gray-600">
+        <h1 className="text-2xl text-gray-600">
           Please navigate to the page you want to record, then press the blue record button.
         </h1>
       </div>
