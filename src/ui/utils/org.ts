c9d1c7dbc87c6a5d9270e9100b1cb6190dd18e4a
Ryan Duffy <ryan@replay.io>
2021-12-01 18:51:39 -0800

    Fix moving from my library (#4728)

--- src/ui/utils/org.ts
+++ src/ui/utils/org.ts
@@ -18,3 +18,14 @@ export function getOrganizationSettings(workspaces: Workspace[]) {
   const org = workspaces.find(w => w.isOrganization);
   return org?.settings || getDefaultOrganizationSettings();
 }
+
+export function isPublicDisabled(workspaces: Workspace[], selectedWorkspaceId: string) {
+  const workspace = workspaces.find(w => w.id === selectedWorkspaceId);
+  const publicDisabledMyLibrary = workspaces.some(
+    w => w.settings.features.recording.public === false
+  );
+  return (
+    (selectedWorkspaceId === "My Library" && publicDisabledMyLibrary) ||
+    workspace?.settings.features.recording.public === false
+  );
+}
