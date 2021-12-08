950aa03c4bfb97645c7faefa4e3bc31cc3913865
Ryan Duffy <ryan@replay.io>
2021-12-08 12:16:53 -0800

    Add CSP policy for using the local backend with prod

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,8 +14,10 @@ const csp = (props: any) => {
   return [
     `default-src 'self'`,
     `connect-src 'self' https://api.replay.io wss://dispatch.replay.io https://webreplay.us.auth0.com https://api-js.mixpanel.com https://*.sentry.io https://*.intercom.io wss://*.intercom.io https://*.launchdarkly.com https://*.logrocket.io https://*.lr-ingest.io https://*.logrocket.com https://*.lr-in.com https://api.stripe.com ${
-      // Required to talk to local backend in development
-      isDev ? "http://localhost:* http://*.replay.local ws://localhost:* " : ""
+      // Required to talk to local backend in development. Enabling
+      // localhost:8000 for prod to support the ?dispatch parameter when running
+      // the local backend
+      isDev ? "http://localhost:* http://*.replay.local ws://localhost:*" : "ws://localhost:8000"
     }`,
     `frame-src https://js.stripe.com https://hooks.stripe.com`,
     `script-src 'self' https://widget.intercom.io https://js.intercomcdn.com/ https://cdn.logrocket.io https://cdn.lr-ingest.io https://cdn.lr-in.com https://js.stripe.com ${hash} ${
