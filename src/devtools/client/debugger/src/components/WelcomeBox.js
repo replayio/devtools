3dbf6171df3f4618c4cdf1e634ead3e6e8dbce79
Jason Laster <jason.laster.11@gmail.com>
2021-12-13 23:07:51 -0800

    Add a new launcher

--- src/devtools/client/debugger/src/components/WelcomeBox.js
+++ src/devtools/client/debugger/src/components/WelcomeBox.js
@@ -8,6 +8,32 @@ import actions from "../actions";
 import { formatKeyShortcut } from "../utils/text";
 import { useGetUserSettings } from "ui/hooks/settings";
 import { setSelectedPrimaryPanel } from "ui/actions/app";
+import BubbleBackground from "ui/components/shared/Onboarding/BubbleBackground";
+import MaterialIcon from "ui/components/shared/MaterialIcon";
+
+function LaunchButton({ action }) {
+  return (
+    <div
+      className="mx-4 w-32 h-32 py-2 flex flex-col justify-around items-center blur rounded-md"
+      style={{ background: "rgba(255,255,255,0.6)" }}
+      role="button"
+      tabIndex="0"
+      onClick={action.onClick}
+    >
+      <div
+        className="w-16 h-16 flex items-center justify-center bg-primaryAccent color-white"
+        style={{ borderRadius: "80px" }}
+      >
+        <MaterialIcon className="text-white -translate-x-1/2 transform" iconSize="4xl">
+          {action.icon}
+        </MaterialIcon>
+      </div>
+      <span style={{ color: "#3487FF" }} className="text-md font-semibold">
+        {action.label}
+      </span>
+    </div>
+  );
+}
 
 function WelcomeBox({
   setActiveSearch,
@@ -20,50 +46,25 @@ function WelcomeBox({
     return null;
   }
 
-  return (
-    <div className="welcomebox">
-      <div className="alignlabel">
-        <div className="shortcutFunction">
-          <div
-            className="welcomebox__searchSources"
-            role="button"
-            tabIndex="0"
-            onClick={() => openQuickOpen()}
-          >
-            <span className="shortcutLabel">{"%S Go to file".substring(2)}</span>
-            <span className="shortcutKey">{formatKeyShortcut("CmdOrCtrl+P")}</span>
-          </div>
+  const actions = [
+    { label: "Open file", icon: "description", onClick: () => openQuickOpen() },
+    { label: "Search functions", icon: "alternate_email", onClick: () => openQuickOpen("@", true) },
+    {
+      label: "Search text",
+      icon: "search",
+      onClick: () => setSelectedPrimaryPanel("search"),
+    },
+  ];
 
-          {userSettings.enableGlobalSearch && (
-            <div
-              className="welcomebox__searchSources"
-              role="button"
-              tabIndex="0"
-              onClick={() => openQuickOpen("@", true)}
-            >
-              <span className="shortcutLabel">{"%S Search functions".substring(2)}</span>
-              <span className="shortcutKey">{formatKeyShortcut("CmdOrCtrl+O")}</span>
-            </div>
-          )}
-          <div
-            className="welcomebox__searchProject"
-            role="button"
-            tabIndex="0"
-            onClick={() => setSelectedPrimaryPanel("search")}
-          >
-            <span className="shortcutLabel">{"%S Find in files".substring(2)}</span>
-            <span className="shortcutKey">{formatKeyShortcut("CmdOrCtrl+Shift+F")}</span>
-          </div>
-          <div
-            className="welcomebox__allShortcuts"
-            role="button"
-            tabIndex="0"
-            onClick={() => toggleShortcutsModal()}
-          >
-            <span className="shortcutLabel">{"Show all shortcuts"}</span>
-            <span className="shortcutKey">{formatKeyShortcut("CmdOrCtrl+/")}</span>
-          </div>
-        </div>
+  return (
+    <div className="welcomebox relative flex flex-col items-center h-full w-full ">
+      <div className="launcher justify-center z-10 flex mt-14 px-8 w-full ">
+        {actions.map(action => (
+          <LaunchButton action={action} />
+        ))}
+      </div>
+      <div className="absolute z-0">
+        <img src="/images/bubble.svg" className=" " style={{ transform: "scale(2.4)" }} />
       </div>
     </div>
   );
