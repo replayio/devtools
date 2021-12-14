1a206b1a4a38bb04bfe85d986bb3029ed1c8b66a
Dragan Okanovic <dragan.okan@gmail.com>
2021-12-14 15:54:01 +0100

    Migrated Rule component to modern React/TS

---
+++ src/devtools/client/inspector/rules/components/Rule.tsx
@@ -0,0 +1,96 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React, { FC, useRef } from "react";
+import { Declarations } from "devtools/client/inspector/rules/components/Declarations";
+import { RuleState } from "../state/rules";
+import Selector from "devtools/client/inspector/rules/components/Selector";
+import { SourceLink } from "devtools/client/inspector/rules/components/SourceLink";
+
+type RuleProps = {
+  // onToggleDeclaration: Function;
+  // onToggleSelectorHighlighter: Function;
+  // showDeclarationNameEditor: Function;
+  // showDeclarationValueEditor: Function;
+  // showNewDeclarationEditor: Function;
+  showSelectorEditor: Function;
+  query: string;
+  rule: RuleState;
+};
+
+export const Rule: FC<RuleProps> = ({
+  // onToggleDeclaration,
+  // onToggleSelectorHighlighter,
+  // showDeclarationNameEditor,
+  // showDeclarationValueEditor,
+  // showNewDeclarationEditor,
+  showSelectorEditor,
+  query,
+  rule: { id, declarations, sourceLink, selector, type, isUserAgentStyle, isUnmatched },
+}) => {
+  const closeBraceSpan = useRef<HTMLDivElement | null>(null);
+
+  return (
+    <div
+      className={
+        "ruleview-rule devtools-monospace" +
+        (isUnmatched ? " unmatched" : "") +
+        (isUserAgentStyle ? " uneditable" : "")
+      }
+      data-rule-id={id}
+    >
+      <SourceLink
+        id={id}
+        isUserAgentStyle={isUserAgentStyle ?? false}
+        sourceLink={sourceLink}
+        type={type}
+      />
+      <div className="ruleview-code">
+        <div>
+          <Selector
+            id={id}
+            isUserAgentStyle={isUserAgentStyle}
+            selector={selector}
+            showSelectorEditor={showSelectorEditor}
+            type={type}
+            query={query}
+          />
+          {
+            // type !== CSSRule.KEYFRAME_RULE
+            //   ? SelectorHighlighter({
+            //       onToggleSelectorHighlighter,
+            //       selector,
+            //     })
+            //   : null,
+          }
+          <span className="ruleview-ruleopen">{" {"}</span>
+        </div>
+        <Declarations declarations={declarations} query={query} />
+        {
+          // dom.li(
+          //   {
+          //     className: "ruleview-property ruleview-newproperty",
+          //     style: {
+          //       display: this.state.isNewDeclarationEditorVisible ? "block" : "none",
+          //     },
+          //   },
+          //   dom.span({
+          //     className: "ruleview-propertyname",
+          //     ref: this.newDeclarationSpan,
+          //   })
+          // )
+        }
+        <div
+          className="ruleview-ruleclose"
+          ref={closeBraceSpan}
+          tabIndex={!isUserAgentStyle ? 0 : -1}
+        >
+          {"}"}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Rule;
