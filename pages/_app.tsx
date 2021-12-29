5612ee0d9109bc6f8303876168aca09ae3925074
Jaril <jarilvalenciano@gmail.com>
2021-12-29 15:30:27 -0500

    First pass on the new accordion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -70,6 +70,7 @@ import "devtools/client/debugger/src/components/ShortcutsModal.css";
 import "devtools/client/debugger/src/utils/editor/source-editor.css";
 import "devtools/client/inspector/components/InspectorTabPanel.css";
 import "devtools/client/shared/components/Accordion.css";
+import "devtools/client/shared/components/NewAccordion/Accordion.css";
 import "devtools/client/shared/components/menu/MenuList.css";
 import "devtools/client/shared/components/SidebarToggle.css";
 import "devtools/client/shared/components/splitter/SplitBox.css";
