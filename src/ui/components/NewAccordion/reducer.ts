03fe8c85092f8eac25c3fd81c9c6a3beead83a0f
Jaril <jarilvalenciano@gmail.com>
2022-01-05 17:33:38 -0500

    Better resizing handle

--- src/ui/components/NewAccordion/reducer.ts
+++ src/ui/components/NewAccordion/reducer.ts
@@ -17,7 +17,7 @@ type ExpandSectionAction = { type: "expand_section"; index: number };
 type CollapseSectionAction = { type: "collapse_section"; index: number };
 type StartResizingAction = { type: "start_resizing"; index: number; initialY: number };
 type EndResizingAction = { type: "end_resizing" };
-type ResizeAction = { type: "resize" };
+type ResizeAction = { type: "resize"; currentY: number };
 type AccordionAction =
   | ExpandSectionAction
   | CollapseSectionAction
@@ -49,8 +49,8 @@ export function startResizing(index: number, initialY: number): StartResizingAct
 export function endResizing(): EndResizingAction {
   return { type: "end_resizing" };
 }
-export function resize(): ResizeAction {
-  return { type: "resize" };
+export function resize(currentY: number): ResizeAction {
+  return { type: "resize", currentY };
 }
 
 // Selectors
@@ -74,14 +74,17 @@ export const getPosition = (state: AccordionState, index: number) => {
 };
 export const getIsIndexResizable = (state: AccordionState, index: number) => {
   const { sections } = state;
-  const hasTargetIndex = getNextTargetIndex(sections, index);
-  const isExpanded = state.sections[index].expanded;
-  const isLastIndex = index === state.sections.length - 1;
 
-  return !!(hasTargetIndex && isExpanded && !isLastIndex);
+  const hasTargetIndex = getNextTargetIndex(sections, index, index);
+  const isFirstIndex = index === 0;
+
+  return !!(!isFirstIndex && hasTargetIndex > -1);
 };
 export const getIsResizing = (state: AccordionState) => {
   return !!state.resizingParams;
+};
+export const getResizingParams = (state: AccordionState) => {
+  return state.resizingParams;
 };
 
 // Reducer
@@ -128,7 +131,27 @@ export function reducer(state: AccordionState, action: AccordionAction) {
       return { ...state, resizingParams: null };
     }
     case "resize": {
-      return { ...state };
+      const { sections, resizingParams } = state;
+
+      if (!resizingParams) return { ...state };
+
+      const { currentY } = action;
+      const { initialY, index } = resizingParams;
+
+      const delta = currentY - initialY;
+      let newSections = [...sections];
+
+      console.log({ delta });
+      if (delta < 0) {
+        // moving up:
+        // - section height keeps expanding by annexing its immediate preceding section(s)
+        // bottom stays where itis
+      } else {
+        // moving down:
+        // - section height keeps expanding by annexing the
+      }
+
+      return { ...state, sections: newSections };
     }
     default: {
       throw new Error("unknown Accordion action");
@@ -205,3 +228,66 @@ const ensmallenSection = (sections: Section[], index: number) => {
 
   return sections;
 };
+
+// const resizeHandler = () => {
+//   const { index, initialY, originalSections } = resizingParams;
+//   const originalHeight = originalSections[index];
+
+//   const newSections = [...originalSections];
+
+//   const originalDelta = e.screenY - initialY;
+//   let remainingDelta = originalDelta;
+
+//   // if delta is positive, we're squeezing the proceeding sections.
+//   // need to make sure we're allowed to in the first place, and as we
+//   // make the selected section smaller, that we embiggen the preceding
+//   // sections accordingly.
+//   if (remainingDelta > 0) {
+//     let nextIndex = getLastExpandedIndex(collapsed);
+
+//     // Walk back from the last expanded index until you reach the current index.
+//     while (nextIndex && nextIndex > index) {
+//       const originalCrease = originalCreases[nextIndex];
+//       const receivableDelta = Math.min(originalCrease - MIN_HEIGHT, remainingDelta);
+
+//       if (receivableDelta) {
+//         newCreases[nextIndex] = originalCreases[nextIndex] - receivableDelta;
+//         remainingDelta -= receivableDelta;
+//       }
+
+//       nextIndex = getLastExpandedIndex(collapsed, nextIndex);
+//     }
+
+//     newCreases[index] = originalHeight + originalDelta - remainingDelta;
+//   } else if (remainingDelta < 0) {
+//     // if delta is negative, we're squeezing the preceding sections.
+//     // need to make sure we're allowed to in the first place. The selected
+//     // section will get larger, and we need to embiggen the proceeding sections
+//     // accordingly.
+//     let nextIndex = getFirstExpandedIndex(collapsed);
+
+//     // Walk forward from the first expanded index until you reach the current index.
+//     while (nextIndex >= 0 && nextIndex <= index) {
+//       const originalCrease = originalCreases[nextIndex];
+//       const receivableDelta = Math.max(MIN_HEIGHT - originalCrease, remainingDelta);
+
+//       if (receivableDelta) {
+//         newCreases[nextIndex] = originalCreases[nextIndex] + receivableDelta;
+//         remainingDelta -= receivableDelta;
+//       }
+
+//       nextIndex = getFirstExpandedIndex(collapsed, nextIndex + 1);
+//     }
+
+//     const lastExpandedIndex = getLastExpandedIndex(collapsed);
+//     // Because there's extra space after the current index, we need to
+//     // redistribute it to the last expanded index.
+//     if (lastExpandedIndex && lastExpandedIndex !== index) {
+//       newCreases[lastExpandedIndex] =
+//         originalCreases[lastExpandedIndex] - (originalDelta - remainingDelta);
+//     }
+//   }
+
+//   setCreases(newCreases);
+
+// }
