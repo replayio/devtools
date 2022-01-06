ea5d90ca8eba7d62360588ed42a34730fb77b6ac
Ryan Duffy <ryan@replay.io>
2022-01-06 13:01:16 -0800

    Add basic social cards for public replays

--- pages/recording/[id].tsx
+++ pages/recording/[id].tsx
@@ -2,14 +2,24 @@ import React, { useEffect, useState } from "react";
 import { connect, ConnectedProps, useStore } from "react-redux";
 import { isTest } from "ui/utils/environment";
 import { getAccessibleRecording } from "ui/actions/session";
-import { Recording as RecordingInfo } from "ui/types";
+import { Recording, Recording as RecordingInfo } from "ui/types";
 import { useGetRecordingId } from "ui/hooks/recordings";
 import LoadingScreen from "ui/components/shared/LoadingScreen";
 import Upload from "./upload";
 import DevTools from "ui/components/DevTools";
 import setup from "ui/setup/dynamic/devtools";
+import Head from "next/head";
 
-function Recording({ getAccessibleRecording }: PropsFromRedux) {
+interface MetadataProps {
+  metadata: {
+    title?: string;
+    url?: string;
+    duration?: number;
+    image?: string;
+  };
+}
+
+function RecordingPage({ getAccessibleRecording, metadata }: PropsFromRedux & MetadataProps) {
   const store = useStore();
   const recordingId = useGetRecordingId();
   const [recording, setRecording] = useState<RecordingInfo | null>();
@@ -24,8 +34,26 @@ function Recording({ getAccessibleRecording }: PropsFromRedux) {
     getRecording();
   }, [recordingId, store]);
 
+  const head = metadata ? (
+    <Head>
+      <meta property="og:title" content={metadata.title} />
+      <meta
+        property="og:description"
+        content={`${Math.round((metadata.duration || 0) / 1000)} second replay`}
+      />
+      <meta property="og:image" content={metadata.image} />
+      <meta name="twitter:card" content={metadata.image ? "summary_large_image" : "summary"} />
+      <meta name="twitter:site" content="@replayio" />
+    </Head>
+  ) : null;
+
   if (!recording || typeof window === "undefined") {
-    return <LoadingScreen />;
+    return (
+      <>
+        {head}
+        <LoadingScreen />
+      </>
+    );
   }
 
   // Add a check to make sure the recording has an associated user ID.
@@ -34,11 +62,73 @@ function Recording({ getAccessibleRecording }: PropsFromRedux) {
   if (!uploadComplete && recording.isInitialized === false && !isTest() && recording.userId) {
     return <Upload onUpload={() => setUploadComplete(true)} />;
   } else {
-    return <DevTools uploadComplete={uploadComplete} />;
+    return (
+      <>
+        {head}
+        <DevTools uploadComplete={uploadComplete} />
+      </>
+    );
   }
+}
+
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const resp = await fetch(process.env.NEXT_PUBLIC_API_URL!, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query GetRecording($recordingId: UUID!) {
+          recording(uuid: $recordingId) {
+            uuid
+            url
+            title
+            duration
+            private
+            isInitialized
+            thumbnail
+            comments {
+              id
+            }
+            workspace {
+              name
+            }
+          }
+        }
+      `,
+      variables: {
+        recordingId: params.id,
+      },
+    }),
+  });
+
+  const json: { data: { recording: Recording & { thumbnail?: string } }; error: any } =
+    await resp.json();
+
+  if (json.error || !json.data.recording) {
+    return {
+      props: {},
+    };
+  }
+
+  return {
+    props: {
+      metadata: {
+        title: json.data.recording.title,
+        url: json.data.recording.url,
+        duration: json.data.recording.duration,
+        image: json.data.recording.thumbnail,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return { paths: [], fallback: true };
 }
 
 const connector = connect(null, { getAccessibleRecording });
 type PropsFromRedux = ConnectedProps<typeof connector>;
 
-export default connector(Recording);
+export default connector(RecordingPage);
