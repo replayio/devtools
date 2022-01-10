00634fe9cc521c8946fb94a0f609d8a105517ef8
Jon Bell <github@lot23.com>
2022-01-10 01:33:18 +0000

    Halfway done with a new loading screen

--- src/ui/components/shared/Onboarding/BubbleBackground.tsx
+++ src/ui/components/shared/Onboarding/BubbleBackground.tsx
@@ -3,18 +3,16 @@ import React from "react";
 export default function BubbleBackground() {
   return (
     <div className="w-full h-full absolute">
-      <div className="absolute">
+      <div className="absolute top-bubble">
         <img
           src="/images/bubble.svg"
-          className="transform rotate-90 -translate-x-1/2 -translate-y-1/2 "
-          style={{ width: "75vw" }}
+          className="transform rotate-90 -translate-x-1/2 -translate-y-1/2"
         />
       </div>
-      <div className="absolute bottom-0 right-0">
+      <div className="absolute bottom-bubble">
         <img
           src="/images/bubble.svg"
-          className="transform -rotate-90 translate-x-1/2 translate-y-1/2"
-          style={{ width: "75vw" }}
+          className="transform -rotate-300 translate-x-1/2 translate-y-1/2"
         />
       </div>
     </div>
