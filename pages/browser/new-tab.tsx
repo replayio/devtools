57ba717c4c116bd2fecbb7cf4a25948d23bbc418
Jon Bell <github@lot23.com>
2022-01-14 05:50:26 +1300

    New loading screen (#4986)
    * New loading scareen
    
    * Review changes
    
    Co-authored-by: Jaril <jarilvalenciano@gmail.com>

--- pages/browser/new-tab.tsx
+++ pages/browser/new-tab.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { LoadingScreenTemplate, StaticLoadingScreen } from "ui/components/shared/LoadingScreen";
+import { LoadingScreenTemplate } from "ui/components/shared/LoadingScreen";
 import { useGetUserInfo } from "ui/hooks/users";
 
 export default function NewTab() {
@@ -7,9 +7,9 @@ export default function NewTab() {
 
   return (
     <LoadingScreenTemplate showTips>
-      <div className="text-center space-y-8" style={{ width: 420 }}>
+      <div className="text-center space-y-8">
         {motd ? <h2 className="text-2xl text-gray-600">{motd}</h2> : null}
-        <h1 className="text-2xl text-gray-600">
+        <h1 className="text-lg text-gray-600">
           Please navigate to the page you want to record, then press the blue record button.
         </h1>
       </div>
--- src/ui/components/shared/Onboarding/BubbleBackground.tsx
+++ src/ui/components/shared/Onboarding/BubbleBackground.tsx
@@ -2,19 +2,17 @@ import React from "react";
 
 export default function BubbleBackground() {
   return (
-    <div className="w-full h-full absolute">
-      <div className="absolute">
+    <div className="w-full h-full absolute pointer-events-none">
+      <div className="absolute top-bubble">
         <img
           src="/images/bubble.svg"
-          className="transform rotate-90 -translate-x-1/2 -translate-y-1/2 "
-          style={{ width: "75vw" }}
+          className="transform rotate-90 -translate-x-1/2 -translate-y-1/2"
         />
       </div>
-      <div className="absolute bottom-0 right-0">
+      <div className="absolute bottom-bubble">
         <img
           src="/images/bubble.svg"
-          className="transform -rotate-90 translate-x-1/2 translate-y-1/2"
-          style={{ width: "75vw" }}
+          className="transform -rotate-300 translate-x-1/2 translate-y-1/2"
         />
       </div>
     </div>
