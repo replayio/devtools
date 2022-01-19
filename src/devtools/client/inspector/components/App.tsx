1b0bf192ab7d8442a6ceee1677d0e74e4c257ff8
Dragan Okanovic <dragan.okan@gmail.com>
2022-01-20 00:18:19 +0100

    Inspector to be fully FC

--- src/devtools/client/inspector/components/App.tsx
+++ src/devtools/client/inspector/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ReactNode } from "react";
+import React, { FC, ReactNode, useMemo, useRef, useEffect } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import classnames from "classnames";
 import { UIState } from "ui/state";
@@ -22,137 +22,131 @@ const INSPECTOR_TAB_TITLES: Record<InspectorActiveTab, string> = {
   eventlistenersview: "Event Listeners",
 } as const;
 
-class InspectorApp extends Component<PropsFromRedux> {
-  private sidebarContainerRef = React.createRef<HTMLDivElement>();
-  private splitBoxRef = React.createRef<SplitBox>();
+const availableTabs: readonly InspectorActiveTab[] = [
+  "ruleview",
+  "computedview",
+  "layoutview",
+  "eventlistenersview",
+];
 
-  private onSplitboxResize = (width: number) => {
+const InspectorApp: FC<PropsFromRedux> = ({ initializedPanels, activeTab, setActiveTab }) => {
+  const sidebarContainerRef = useRef<HTMLDivElement>(null);
+  const splitBoxRef = useRef<SplitBox>(null);
+
+  const onSplitboxResize = (width: number) => {
     prefs.splitSidebarSize = width;
   };
 
-  renderSidebar() {
-    const { initializedPanels, activeTab, setActiveTab } = this.props;
+  const inspector = gToolbox.getPanel("inspector");
 
-    const inspector = gToolbox.getPanel("inspector");
+  console.log("Inspector render");
 
-    const availableTabs: readonly InspectorActiveTab[] = [
-      "ruleview",
-      "computedview",
-      "layoutview",
-      "eventlistenersview",
-    ];
+  useEffect(() => {
+    console.log("inspector change");
+  }, [inspector]);
 
-    let activePanel: ReactNode | null = null;
-    if (inspector && initializedPanels.includes("inspector")) {
-      switch (activeTab) {
-        case "ruleview": {
-          activePanel = <RulesApp {...inspector.rules.getRulesProps()} />;
-          break;
-        }
+  const inspectorInited = inspector && initializedPanels.includes("inspector");
 
-        case "computedview": {
-          activePanel = <ComputedApp />;
-          break;
-        }
+  const markupView: JSX.Element | undefined = useMemo(() => {
+    if (!inspectorInited) {
+      return undefined;
+    }
+    return <MarkupApp inspector={inspector} />;
+  }, [inspector, inspectorInited]);
 
-        case "layoutview": {
-          const layoutProps = {
-            ...inspector.getCommonComponentProps(),
-            ...inspector.boxModel.getComponentProps(),
-            showBoxModelProperties: true,
-          };
-          activePanel = <LayoutApp {...layoutProps} />;
-          break;
-        }
-
-        case "eventlistenersview": {
-          activePanel = <EventListenersApp />;
-          break;
-        }
+  const activePanel: ReactNode = useMemo(() => {
+    if (!inspectorInited) {
+      return null;
+    }
+    switch (activeTab) {
+      case "ruleview": {
+        return <RulesApp {...inspector.rules.getRulesProps()} />;
       }
+      case "computedview": {
+        return <ComputedApp />;
+      }
+      case "layoutview": {
+        const layoutProps = {
+          ...inspector.getCommonComponentProps(),
+          ...inspector.boxModel.getComponentProps(),
+          showBoxModelProperties: true,
+        };
+        return <LayoutApp {...layoutProps} />;
+      }
+      case "eventlistenersview": {
+        return <EventListenersApp />;
+      }
+      default:
+        return null;
     }
+  }, [inspector, inspectorInited, activeTab]);
 
-    return (
-      <div className="devtools-sidebar-tabs">
-        <div className="tabs">
-          <nav className="tabs-navigation">
-            <ul className="tabs-menu" role="tablist">
-              {availableTabs.map(panelId => {
-                const isPanelSelected = activeTab === panelId;
-
-                return (
-                  <li
-                    key={panelId}
-                    className={classnames("tabs-menu-item", { "is-active": isPanelSelected })}
-                    role="presentation"
-                  >
-                    <span className="devtools-tab-line"></span>
-                    <a
-                      id={`${panelId}-tab`}
-                      tabIndex={isPanelSelected ? 0 : -1}
-                      title={INSPECTOR_TAB_TITLES[panelId]}
-                      role="tab"
-                      onClick={() => setActiveTab(panelId)}
-                    >
-                      {INSPECTOR_TAB_TITLES[panelId]}
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </nav>
-          <div className="panels">{activePanel}</div>
-        </div>
-      </div>
-    );
-  }
-
-  render() {
-    const { initializedPanels } = this.props;
-
-    const inspector = gToolbox.getPanel("inspector");
-    let markupView, rulesView;
-    if (inspector && initializedPanels.includes("inspector")) {
-      markupView = <MarkupApp inspector={inspector} />;
-    }
-
-    return (
-      <div
-        className="inspector-responsive-container theme-body inspector"
-        data-localization-bundle="devtools/client/locales/inspector.properties"
-      >
-        <div id="inspector-splitter-box" ref={this.sidebarContainerRef}>
-          <SplitBox
-            ref={this.splitBoxRef}
-            className="inspector-sidebar-splitter"
-            initialSize={`${prefs.splitSidebarSize}px`}
-            minSize="20%"
-            maxSize="80%"
-            onMove={this.onSplitboxResize}
-            splitterSize={1}
-            endPanelControl={true}
-            startPanel={markupView}
-            endPanel={
-              <div className="devtools-inspector-tab-panel">
-                <div id="inspector-sidebar-container">
-                  <div id="inspector-sidebar" hidden={false}>
-                    {this.renderSidebar()}
+  return (
+    <div
+      className="inspector-responsive-container theme-body inspector"
+      data-localization-bundle="devtools/client/locales/inspector.properties"
+    >
+      <div id="inspector-splitter-box" ref={sidebarContainerRef}>
+        <SplitBox
+          ref={splitBoxRef}
+          className="inspector-sidebar-splitter"
+          initialSize={`${prefs.splitSidebarSize}px`}
+          minSize="20%"
+          maxSize="80%"
+          onMove={onSplitboxResize}
+          splitterSize={1}
+          endPanelControl={true}
+          startPanel={markupView}
+          endPanel={
+            <div className="devtools-inspector-tab-panel">
+              <div id="inspector-sidebar-container">
+                <div id="inspector-sidebar" hidden={false}>
+                  <div className="devtools-sidebar-tabs">
+                    <div className="tabs">
+                      <nav className="tabs-navigation">
+                        <ul className="tabs-menu" role="tablist">
+                          {availableTabs.map(panelId => {
+                            const isPanelSelected = activeTab === panelId;
+                            return (
+                              <li
+                                key={panelId}
+                                className={classnames("tabs-menu-item", {
+                                  "is-active": isPanelSelected,
+                                })}
+                                role="presentation"
+                              >
+                                <span className="devtools-tab-line"></span>
+                                <a
+                                  id={`${panelId}-tab`}
+                                  tabIndex={isPanelSelected ? 0 : -1}
+                                  title={INSPECTOR_TAB_TITLES[panelId]}
+                                  role="tab"
+                                  onClick={() => setActiveTab(panelId)}
+                                >
+                                  {INSPECTOR_TAB_TITLES[panelId]}
+                                </a>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </nav>
+                      <div className="panels">{activePanel}</div>
+                    </div>
                   </div>
                 </div>
               </div>
-            }
-          />
-        </div>
+            </div>
+          }
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 const connector = connect(
   (state: UIState) => ({
     initializedPanels: selectors.getInitializedPanels(state),
     activeTab: state.inspector.activeTab,
-    markupRootNode: state.markup.tree[state.markup.rootNode!],
   }),
   InspectorActions
 );
