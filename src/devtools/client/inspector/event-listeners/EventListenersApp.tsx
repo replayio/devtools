8050d5ed6a90bbbc87869ba0ad7e97ab9b905ac9
Dragan Okanovic <dragan.okan@gmail.com>
2022-01-20 11:01:25 +0100

    Fetch listeners of the current selection

--- src/devtools/client/inspector/event-listeners/EventListenersApp.tsx
+++ src/devtools/client/inspector/event-listeners/EventListenersApp.tsx
@@ -1,5 +1,5 @@
 import { NodeFront, WiredEventListener } from "protocol/thread/node";
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useMemo, useState } from "react";
 
 type EventListenersAppProps = {};
 
@@ -20,6 +20,13 @@ export const EventListenersApp: FC<EventListenersAppProps> = () => {
       setListeners(listeners ?? []);
     };
 
+    // try getting listeners of the current selection
+    const nodeFront = gToolbox.selection.nodeFront;
+    if (!!nodeFront) {
+      handler(nodeFront);
+    }
+
+    // in either case, subscribe to changes to the selection
     window.gToolbox.selection.on("new-node-front", handler);
     window.gToolbox.selection.on("detached-front", handler);
 
@@ -29,13 +36,51 @@ export const EventListenersApp: FC<EventListenersAppProps> = () => {
     };
   }, []);
 
+  const groupedListeners: Record<string, WiredEventListener[]> = useMemo(() => {
+    const groups: Record<string, WiredEventListener[]> = {};
+    for (const listener of listeners) {
+      if (groups[listener.type] === undefined) {
+        groups[listener.type] = [];
+      }
+      groups[listener.type].push(listener);
+    }
+    return groups;
+  }, [listeners]);
+
+  /*
+
+  event type
+    node (remove) location in files
+      useCapture: bool
+      passive: bool
+      once: bool
+      handler _ summary
+        [[Scopes]]
+        [[Prototype]]
+        [[FunctionLocation]]
+        caller (...) (click to eval)
+        argument (...) (click to eval)
+        prototype
+          [[Prototype]]
+          constructor
+        name
+        length
+  */
+
+  console.log(groupedListeners);
+
   return (
     <div>
       {listeners.length === 0 ? (
         <div className="devtools-sidepanel-no-result">No event listeners</div>
       ) : (
-        listeners.map(listener => (
-          <div key={listener.handler.id()}>{listener.handler.functionName()}</div>
+        Object.entries(groupedListeners).map(([eventType, listeners]) => (
+          <div key={eventType}>
+            {eventType}
+            {listeners.map(listener => (
+              <div key={listener.handler.id()}>{43}</div>
+            ))}
+          </div>
         ))
       )}
     </div>
