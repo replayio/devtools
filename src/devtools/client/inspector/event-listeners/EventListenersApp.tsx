115c94df0b2a4308e07121c5d7dfd3db59b8c2c4
Dragan Okanovic <dragan.okan@gmail.com>
2022-01-20 03:05:59 +0100

    Getting the listeners

--- src/devtools/client/inspector/event-listeners/EventListenersApp.tsx
+++ src/devtools/client/inspector/event-listeners/EventListenersApp.tsx
@@ -1,7 +1,38 @@
-import React, { FC } from "react";
+import { NodeFront, WiredEventListener } from "protocol/thread/node";
+import React, { FC, useEffect, useState } from "react";
 
 type EventListenersAppProps = {};
 
 export const EventListenersApp: FC<EventListenersAppProps> = () => {
-  return <div></div>;
+  const [listeners, setListeners] = useState<WiredEventListener[]>([]);
+
+  useEffect(() => {
+    if (!gToolbox) {
+      return;
+    }
+
+    const handler = async (node: NodeFront | null) => {
+      if (!node) {
+        return;
+      }
+      const listeners = await node.getEventListeners();
+      setListeners(listeners ?? []);
+    };
+
+    window.gToolbox.selection.on("new-node-front", handler);
+    window.gToolbox.selection.on("detached-front", handler);
+
+    return () => {
+      window.gToolbox.selection.off("new-node-front", handler);
+      window.gToolbox.selection.off("detached-front", handler);
+    };
+  }, []);
+
+  return (
+    <div>
+      {listeners.map(listener => (
+        <div key={listener.handler.id()}>{listener.handler.functionName()}</div>
+      ))}
+    </div>
+  );
 };
