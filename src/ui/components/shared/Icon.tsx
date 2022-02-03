240dee00ea3d63a4d5a8ad5b104d1d36a3109c05
Jon Bell <github@lot23.com>
2022-02-03 23:54:12 +0000

    Removing height and width variables

--- src/ui/components/shared/Icon.tsx
+++ src/ui/components/shared/Icon.tsx
@@ -1,4 +1,6 @@
 import React from "react";
-export default function Icon({ filename, className = "bg-gray-800", height = 20, width = 20 }) {
-  return <div className={`icon ${filename} ${className}`} style={{ height, width }} />;
+export default function Icon({ filename, className = "bg-gray-800" }) {
+  return (
+    <div className={`icon ${filename} ${className}`} style={{ height: "20px", width: "20px" }} />
+  );
 }
