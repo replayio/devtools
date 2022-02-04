4053f346c675c611d5d4bd43c43f8b35dbc49022
Jon Bell <github@lot23.com>
2022-02-04 02:16:17 +0000

    Fixing TS error

--- src/ui/components/shared/Icon.tsx
+++ src/ui/components/shared/Icon.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-export default function Icon({ filename, className = "bg-gray-800" }) {
+export default function Icon({
+  filename,
+  className = "bg-gray-800",
+}: {
+  filename: string;
+  className: string;
+}) {
   return (
     <div className={`icon ${filename} ${className}`} style={{ height: "20px", width: "20px" }} />
   );
