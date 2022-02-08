d63d86b3af350064bd5a2272b3ee198171b7b4d5
Dragan Okanovic <1355455+abstractalgo@users.noreply.github.com>
2022-02-08 17:03:58 +0100

    Bump Tailwind UI to v3 (#5253)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  purge: {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
       short: { raw: "(max-height: 615px)" },
@@ -31,13 +28,6 @@ module.exports = {
       cursor: {
         "ew-resize": "ew-resize",
       },
-    },
-  },
-  variants: {
-    width: ["hover", "group-hover"],
-    extend: {
-      borderWidth: ["last"],
-      textColor: ["disabled"],
     },
   },
   plugins: [require("@tailwindcss/forms")],
