fe5194de2744d8e3f90b99798a753e69f45a9028
Jon Bell <github@lot23.com>
2022-02-10 19:45:07 +0000

    Improved new tab screen

--- pages/browser/new-tab.tsx
+++ pages/browser/new-tab.tsx
@@ -6,12 +6,12 @@ export default function NewTab() {
   const { motd } = useGetUserInfo();
 
   return (
-    <LoadingScreenTemplate showTips>
+    <LoadingScreenTemplate>
       <div className="space-y-8 text-center">
         {motd ? <h2 className="text-2xl text-gray-600">{motd}</h2> : null}
-        <h1 className="text-lg text-gray-600">
+        <div className="text-md text-gray-600">
           Please navigate to the page you want to record, then press the blue record button.
-        </h1>
+        </div>
       </div>
     </LoadingScreenTemplate>
   );
