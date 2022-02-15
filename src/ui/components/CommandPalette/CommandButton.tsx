5941802a8cfc5f2826cde440813c93337322d365
Dragan Okanovic <1355455+abstractalgo@users.noreply.github.com>
2022-02-16 00:00:58 +0100

    Command Button cleanup (#5322)

--- src/ui/components/CommandPalette/CommandButton.tsx
+++ src/ui/components/CommandPalette/CommandButton.tsx
@@ -1,19 +1,23 @@
 import { formatKeyShortcut } from "devtools/client/debugger/src/utils/text";
-import React, { useEffect, useRef } from "react";
-import { connect, ConnectedProps } from "react-redux";
+import React, { FC, useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import cx from "classnames";
 import { actions } from "ui/actions";
 import { Command } from "./CommandPalette";
 
-type CommandButtonProps = PropsFromRedux & { command: Command; active: boolean };
+type CommandButtonProps = {
+  command: Command;
+  active: boolean;
+};
 
-function CommandButton({ command, executeCommand, active }: CommandButtonProps) {
+const CommandButton: FC<CommandButtonProps> = ({ command, active }) => {
+  const dispatch = useDispatch();
+
   const buttonNode = useRef<HTMLButtonElement | null>(null);
   const { label, shortcut, key } = command;
   const onClick = () => {
-    executeCommand(key);
+    dispatch(actions.executeCommand(key));
   };
-
-  const bgColors = active ? "bg-primaryAccent text-white" : "hover:bg-gray-200";
 
   useEffect(() => {
     if (active && buttonNode.current) {
@@ -25,21 +29,18 @@ function CommandButton({ command, executeCommand, active }: CommandButtonProps) 
     <button
       key={label}
       onClick={onClick}
-      className={`${bgColors} flex justify-between px-6 py-2 transition`}
+      className={cx(
+        "flex justify-between px-6 py-2 transition",
+        active ? "bg-primaryAccent text-white" : "hover:bg-gray-200"
+      )}
       ref={buttonNode}
     >
       <div>{label}</div>
       {shortcut ? (
-        <div className={`${active ? "text-white" : "text-gray-400"}`}>
-          {formatKeyShortcut(shortcut)}
-        </div>
+        <div className={active ? "text-white" : "text-gray-400"}>{formatKeyShortcut(shortcut)}</div>
       ) : null}
     </button>
   );
-}
+};
 
-const connector = connect(null, {
-  executeCommand: actions.executeCommand,
-});
-type PropsFromRedux = ConnectedProps<typeof connector>;
-export default connector(CommandButton);
+export default CommandButton;
