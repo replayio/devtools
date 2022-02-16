be76120b3a5888929d9e26c95ff57056e11e9181
Dragan Okanovic <dragan.okan@gmail.com>
2022-02-16 02:47:54 +0100

    Bump packages

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,15 +3,17 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-postcss",
     "storybook-addon-root-attribute/register",
     "storybook-css-modules-preset",
-  ],
-  options: {
-    postcssLoaderOptions: {
-      implementation: require("postcss"),
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
     },
-  },
+  ],
   core: {
     builder: "webpack5",
   },
