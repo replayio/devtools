58f37273d46d093da7abe22bd9d2887007a77dc1
Dragan Okanovic <dragan.okan@gmail.com>
2022-02-18 15:37:12 +0100

    Comments

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const { RetryChunkLoadPlugin } = require("webpack-retry-chunk-load-plugin");
 
 module.exports = {
+  // bumping Next from 12.0.9 to 12.1.0 required this as a temp WAR
+  // (see https://github.com/vercel/next.js/pull/34500)
   experimental: {},
+
   productionBrowserSourceMaps: true,
 
   async redirects() {
