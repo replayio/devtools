29002dee6f0ba3535991ee6869958a70384e7d6f
Jason Laster <jason.laster.11@gmail.com>
2022-02-24 17:07:47 -0800

    Revert "Add eslint recommend"
    This reverts commit 04ee3dce31a2c5324071b17f38b4fb4c366a8c6c.

--- src/ui/setup/dynamic/devtools.ts
+++ src/ui/setup/dynamic/devtools.ts
@@ -1,6 +1,13 @@
 import { bindActionCreators, Store } from "redux";
 
-import { sessionError, uploadedData } from "@recordreplay/protocol";
+import {
+  sessionError,
+  uploadedData,
+  CommandMethods,
+  CommandParams,
+  CommandResult,
+  CommandHasPauseId,
+} from "@recordreplay/protocol";
 import { initSocket, addEventListener } from "protocol/socket";
 import { ThreadFront } from "protocol/thread";
 import { setupGraphics } from "protocol/graphics";
