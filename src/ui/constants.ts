73fd6f31833f991374f08a526fca13651ab06e3a
Jaril <jarilvalenciano@gmail.com>
2022-03-04 07:14:59 -0800

    Disable event breakpoints when there's more than 200 hits

--- src/ui/constants.ts
+++ src/ui/constants.ts
@@ -1,1 +1,6 @@
 export const timelineMarkerWidth = 11;
+
+// This maps to the backend's hard cap on the number of analysis points
+// it's willing to analyze on one controller.
+// https://github.com/RecordReplay/backend/blob/cbb48ebfd2de92d4463fcfb5a9691ccfd40da028/src/control/handlers/analysis.ts#L313
+export const maxAnalysisPoints = 200;
