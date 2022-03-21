178dbb55685f26197606f6107473e88cfa7f8d86
Cecelia Martinez <40367173+ceceliacreates@users.noreply.github.com>
2022-03-21 17:22:58 -0400

    Issue 5202 toggle animation delay (#5815)
    * sets pointer-events to auto for this component so group-hover class will work for dropdown error. Overrides common.css behavior for this file only.
    
    * moves toggle delay logic to Redux to account for all types of view changes.
    
    * updated types for layout actions so they are consistently named.
    
    * updates formatting for linter.
    
    * removes CSS change from other branch
    
    * updates toggleMode to viewToggleMode, removes timeout key, removes unused imports, removes toggleMode type.
    
    * linting fix

--- src/ui/components/Header/ViewToggle.tsx
+++ src/ui/components/Header/ViewToggle.tsx
@@ -1,14 +1,12 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import { connect, ConnectedProps } from "react-redux";
 import classnames from "classnames";
 import hooks from "ui/hooks";
 import { isTest } from "ui/utils/environment";
 import { UIState } from "ui/state";
-import { getViewMode } from "ui/reducers/layout";
+import { getViewToggleMode } from "ui/reducers/layout";
 import { setViewMode } from "ui/actions/layout";
 import { ViewMode } from "ui/state/layout";
-
-const TOGGLE_DELAY = 300;
 
 const MODES = [
   {
@@ -21,35 +19,13 @@ const MODES = [
   },
 ] as const;
 
-function ViewToggle({ viewMode, setViewMode }: PropsFromRedux) {
+function ViewToggle({ toggleMode, setViewMode }: PropsFromRedux) {
   const recordingId = hooks.useGetRecordingId();
   const { recording, loading } = hooks.useGetRecording(recordingId);
   const { userId } = hooks.useGetUserId();
   const isAuthor = userId && userId == recording?.userId;
-  const [localViewMode, setLocalViewMode] = useState(viewMode);
-  const toggleTimeoutKey = useRef<NodeJS.Timeout | null>(null);
-
-  useEffect(() => {
-    // It's possible for the view to be toggled by something else apart from this component.
-    // When that happens, we need to update the localViewMode so that it displays
-    // the right state.
-    if (viewMode !== localViewMode) {
-      setLocalViewMode(viewMode);
-    }
-  }, [viewMode]);
 
   const handleToggle = async (mode: ViewMode) => {
-    setLocalViewMode(mode);
-
-    // Delay updating the viewMode in redux so that the toggle can fully animate
-    // before re-rendering all of devtools in the new viewMode.
-    clearTimeout(toggleTimeoutKey.current!);
-    const delayPromise = new Promise<void>(resolve => {
-      toggleTimeoutKey.current = setTimeout(() => resolve(), TOGGLE_DELAY);
-    });
-
-    await delayPromise;
-
     setViewMode(mode);
   };
 
@@ -64,12 +40,12 @@ function ViewToggle({ viewMode, setViewMode }: PropsFromRedux) {
       <div
         className="handle"
         style={{
-          left: `${(MODES.findIndex(({ mode }) => mode === localViewMode) / MODES.length) * 100}%`,
+          left: `${(MODES.findIndex(({ mode }) => mode === toggleMode) / MODES.length) * 100}%`,
         }}
       ></div>
       {MODES.map(({ mode, label }) => (
         <div key={mode} className="option" onClick={() => handleToggle(mode)}>
-          <div className={classnames("text", { active: localViewMode === mode })}>{label}</div>
+          <div className={classnames("text", { active: toggleMode === mode })}>{label}</div>
         </div>
       ))}
     </div>
@@ -78,7 +54,7 @@ function ViewToggle({ viewMode, setViewMode }: PropsFromRedux) {
 
 const connector = connect(
   (state: UIState) => ({
-    viewMode: getViewMode(state),
+    toggleMode: getViewToggleMode(state),
   }),
   {
     setViewMode,
