c1ac2bb3837c3fb8721b00388794f42ea46c7f9b
Jaril <jarilvalenciano@gmail.com>
2022-03-22 15:44:03 -0700

    Extract EditorWithAutocomplete for reuse (#5922)
    * Extract EditorWithAutocomplete
    
    * Move the ControlledCodeMirror and EditorWithAutocomplete implementation into a shared folder

--- src/devtools/client/webconsole/components/Input/JSTerm.tsx
+++ src/devtools/client/webconsole/components/Input/JSTerm.tsx
@@ -2,35 +2,14 @@ import React, { useRef, useState } from "react";
 import { Editor } from "codemirror";
 import { useDispatch, useSelector } from "react-redux";
 import { useGetRecording, useGetRecordingId } from "ui/hooks/recordings";
-import { getCursorIndex, getRemainingCompletedTextAfterCursor } from "../../utils/autocomplete";
-import AutocompleteMatches from "./AutocompleteMatches";
-import CodeMirror from "./CodeMirror";
 import { evaluateExpression, paywallExpression } from "../../actions/input";
 import EagerEvalFooter from "./EagerEvalFooter";
-import useAutocomplete from "./useAutocomplete";
 import useEvaluationHistory from "./useEvaluationHistory";
 import { getIsInLoadedRegion } from "ui/reducers/timeline";
-import { isTest } from "ui/utils/environment";
-
-enum Keys {
-  BACKSPACE = "Backspace",
-  ENTER = "Enter",
-  ESCAPE = "Escape",
-  TAB = "Tab",
-  ARROW_DOWN = "ArrowDown",
-  ARROW_UP = "ArrowUp",
-  ARROW_RIGHT = "ArrowRight",
-  ARROW_LEFT = "ArrowLeft",
-}
-
-const DISMISS_KEYS = [
-  Keys.BACKSPACE,
-  Keys.ENTER,
-  Keys.TAB,
-  Keys.ESCAPE,
-  Keys.ARROW_RIGHT,
-  Keys.ARROW_LEFT,
-];
+import {
+  EditorWithAutocomplete,
+  Keys,
+} from "ui/components/shared/CodeEditor/EditorWithAutocomplete";
 
 const getJsTermApi = (
   editor: Editor,
@@ -67,22 +46,13 @@ export default function JSTerm() {
   const isInLoadedRegion = useSelector(getIsInLoadedRegion);
 
   const [value, setValue] = useState("");
-  const inputNode = useRef<HTMLDivElement | null>(null);
+  const [autocompletePreview, setAutocompletePreview] = useState<string | null>(null);
   const executeRef = useRef(() => {});
   const _execute = () => executeRef.current();
 
   const { moveHistoryCursor, setHistoryIndex } = useEvaluationHistory(setValue);
-  const {
-    autocompleteIndex,
-    matches,
-    shouldShowAutocomplete,
-    applySelectedMatch,
-    moveAutocompleteCursor,
-    resetAutocompleteIndex,
-    setHideAutocomplete,
-  } = useAutocomplete(value);
 
-  const onRegularKeyPress = (e: KeyboardEvent) => {
+  const onKeyPress = (e: KeyboardEvent) => {
     if (e.key === Keys.ENTER) {
       e.preventDefault();
       _execute();
@@ -92,60 +62,7 @@ export default function JSTerm() {
       moveHistoryCursor(-1);
     }
   };
-  const onAutocompleteKeyPress = (e: KeyboardEvent) => {
-    if (e.key === Keys.ENTER || e.key === Keys.TAB || e.key === Keys.ARROW_RIGHT) {
-      e.preventDefault();
-      autocomplete();
-    } else if (e.key === Keys.ARROW_DOWN) {
-      e.preventDefault();
-      moveAutocompleteCursor(-1);
-    } else if (e.key === Keys.ARROW_UP) {
-      e.preventDefault();
-      moveAutocompleteCursor(1);
-    }
-
-    if ((DISMISS_KEYS as string[]).includes(e.key)) {
-      setHideAutocomplete(true);
-    }
-  };
-  // for use in e2e tests
-  const _showAutocomplete = isTest()
-    ? (show: boolean) => {
-        setHideAutocomplete(!show);
-        if (show) {
-          resetAutocompleteIndex();
-        }
-      }
-    : undefined;
-  const onKeyPress = (e: KeyboardEvent) => {
-    if (shouldShowAutocomplete) {
-      onAutocompleteKeyPress(e);
-    } else {
-      onRegularKeyPress(e);
-    }
-
-    if (
-      ![
-        Keys.ARROW_DOWN,
-        Keys.ARROW_UP,
-        Keys.ESCAPE,
-        Keys.ENTER,
-        Keys.TAB,
-        Keys.ARROW_RIGHT,
-      ].includes(e.key as Keys)
-    ) {
-      setHideAutocomplete(false);
-      resetAutocompleteIndex();
-    }
-  };
-  const onSelection = (obj?: any) => {
-    const cursorMoved = obj?.origin && ["*mouse", "+move"].includes(obj.origin);
-
-    if (cursorMoved) {
-      setHideAutocomplete(true);
-    }
-  };
-  const autocomplete = () => setValue(applySelectedMatch());
+  const onPreviewAvailable = (previewValue: string | null) => setAutocompletePreview(previewValue);
   executeRef.current = () => {
     if (!value) {
       return;
@@ -166,51 +83,27 @@ export default function JSTerm() {
     <div>
       <div className="relative">
         <div
-          className="jsterm-input-container devtools-input relative"
+          className="jsterm-input-container"
           key="jsterm-container"
           aria-live="off"
           tabIndex={-1}
-          ref={inputNode}
         >
           {isInLoadedRegion ? (
-            <>
-              <CodeMirror
-                onKeyPress={onKeyPress}
-                value={value}
-                onSelection={onSelection}
-                setValue={setValue}
-                onEditorMount={(editor: Editor) =>
-                  (window.jsterm = getJsTermApi(editor, _execute, _showAutocomplete))
-                }
-              />
-              {shouldShowAutocomplete ? (
-                <div
-                  className="absolute ml-8 opacity-50"
-                  style={{ left: `${value.length}ch`, top: `5px` }}
-                >
-                  {getRemainingCompletedTextAfterCursor(value, matches[autocompleteIndex])}
-                </div>
-              ) : null}
-            </>
+            <EditorWithAutocomplete
+              onEditorMount={(editor: Editor, showAutocomplete?: (show: boolean) => void) =>
+                (window.jsterm = getJsTermApi(editor, _execute, showAutocomplete))
+              }
+              onPreviewAvailable={onPreviewAvailable}
+              value={value}
+              setValue={setValue}
+              onRegularKeyPress={onKeyPress}
+            />
           ) : (
-            <div className="load-container flex items-center h-full italic text-gray-400">
-              Loading…
-            </div>
+            <div className="flex items-center h-full italic text-gray-400">Loading…</div>
           )}
         </div>
-        {shouldShowAutocomplete ? (
-          <AutocompleteMatches
-            leftOffset={getCursorIndex(value)}
-            matches={matches}
-            selectedIndex={autocompleteIndex}
-            onMatchClick={autocomplete}
-          />
-        ) : null}
       </div>
-      <EagerEvalFooter
-        expression={value}
-        completedExpression={shouldShowAutocomplete ? applySelectedMatch() : null}
-      />
+      <EagerEvalFooter expression={value} completedExpression={autocompletePreview} />
     </div>
   );
 }
