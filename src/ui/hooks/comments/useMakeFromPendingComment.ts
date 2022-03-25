5fa77e6cfb5f4fbca8bb1c8d2b73f2db2cf0c9f2
Dragan Okanovic <dragan.okan@gmail.com>
2022-03-25 17:25:33 +0100

    Finalize useMakeFromPendingComment

--- src/ui/hooks/comments/useMakeFromPendingComment.ts
+++ src/ui/hooks/comments/useMakeFromPendingComment.ts
@@ -1,19 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { createLabels } from "ui/actions/comments";
 import { PENDING_COMMENT_ID } from "ui/reducers/comments";
 import { UIState } from "ui/state";
 import { Comment, ROOT_COMMENT_ID } from "ui/state/comments";
 import { useGetRecordingId } from "../recordings";
 import { useGetUserInfo } from "../users";
 
-type MakeCommentFn = (parentId: Comment["id"] | null, content: string) => Comment;
+type MakeCommentAsyncFn = (parentId: Comment["id"] | null, content: string) => Promise<Comment>;
 
-export const useMakeFromPendingComment = (): MakeCommentFn => {
+export const useMakeFromPendingComment = (): MakeCommentAsyncFn => {
   const recordingId = useGetRecordingId();
   const user = useGetUserInfo();
   const allPendingCommentData = useSelector((state: UIState) => state.comments.pendingCommentData);
 
-  const makeComment: MakeCommentFn = (parentId, content) => {
+  const makeComment: MakeCommentAsyncFn = async (parentId, content) => {
     const pendingCommentData = allPendingCommentData[parentId ?? ROOT_COMMENT_ID];
+
     return {
       id: PENDING_COMMENT_ID,
       parentId,
