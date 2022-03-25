ff803d0dab3e973c0b642b3c6459370963f6c25b
Dragan Okanovic <dragan.okan@gmail.com>
2022-03-25 15:55:11 +0100

    Helper hook useMakeFromPendingComment

--- src/ui/hooks/comments/index.ts
+++ src/ui/hooks/comments/index.ts
@@ -2,10 +2,12 @@ import { useGetComments } from "./useGetComments";
 import { useAddComment } from "./useAddComment";
 import { useUpdateComment } from "./useUpdateComment";
 import { useDeleteComment } from "./useDeleteComment";
+import { useMakeFromPendingComment } from "./useMakeFromPendingComment";
 
 export const commentsHooks = {
   useGetComments,
   useAddComment,
   useUpdateComment,
   useDeleteComment,
+  useMakeFromPendingComment,
 };
---
+++ src/ui/hooks/comments/useMakeFromPendingComment.ts
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import { PENDING_COMMENT_ID } from "ui/reducers/comments";
+import { UIState } from "ui/state";
+import { Comment, ROOT_COMMENT_ID } from "ui/state/comments";
+import { useGetRecordingId } from "../recordings";
+import { useGetUserInfo } from "../users";
+
+type MakeCommentFn = (parentId: Comment["id"] | null, content: string) => Comment;
+
+export const useMakeFromPendingComment = (): MakeCommentFn => {
+  const recordingId = useGetRecordingId();
+  const user = useGetUserInfo();
+  const allPendingCommentData = useSelector((state: UIState) => state.comments.pendingCommentData);
+
+  const makeComment: MakeCommentFn = (parentId, content) => {
+    const pendingCommentData = allPendingCommentData[parentId ?? ROOT_COMMENT_ID];
+    return {
+      id: PENDING_COMMENT_ID,
+      parentId,
+      createdAt: new Date().toISOString(),
+      recordingId,
+      replies: [],
+      ...pendingCommentData,
+      updatedAt: new Date().toISOString(),
+      content,
+      user,
+    };
+  };
+
+  return makeComment;
+};
