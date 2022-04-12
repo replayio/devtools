b10351bb2232485384a17cc18811a799f4af21e3
Jaril <jarilvalenciano@gmail.com>
2022-04-12 13:08:23 -0700

    Fix hovering in the gutter

--- src/devtools/client/debugger/src/components/Editor/StaticTooltip.tsx
+++ src/devtools/client/debugger/src/components/Editor/StaticTooltip.tsx
@@ -9,7 +9,7 @@ type StaticTooltipProps = {
 
 export default function StaticTooltip({ targetNode, children }: StaticTooltipProps) {
   return ReactDOM.createPortal(
-    <div className="absolute z-50 flex flex-row space-x-px transform -right-1 translate-x-full bottom-4 mb-0.5">
+    <div className="absolute z-50 flex flex-row space-x-px transform -right-1 translate-x-full bottom-4 mb-0.5 pointer-events-none">
       {children}
     </div>,
     targetNode
