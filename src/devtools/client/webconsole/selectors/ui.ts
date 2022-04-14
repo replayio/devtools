44dd33b08032c6554192748defccb6a9c543dbf2
Mark Erikson <mark@isquaredsoftware.com>
2022-04-14 13:50:37 -0400

    Migrate selectors/ui content to TS

--- src/devtools/client/webconsole/selectors/ui.ts
+++ src/devtools/client/webconsole/selectors/ui.ts
@@ -2,12 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-"use strict";
+import type { UIState } from "ui/state";
 
-function getAllUi(state) {
+export function getAllUi(state: UIState) {
   return state.consoleUI;
 }
-
-module.exports = {
-  getAllUi,
-};
