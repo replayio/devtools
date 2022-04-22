ef3f61d9b1f7d8f2ed93caef49162a1b6bdb5251
Holger Benl <hbenl@evandor.de>
2022-04-22 18:15:37 +0200

    Fix some e2e tests

--- test/manifest.js
+++ test/manifest.js
@@ -49,8 +49,6 @@ module.exports = [
     example: "doc_navigate.html",
     script: "breakpoints-07.js",
     targets: ["gecko", "chromium"],
-    // Disabled because the test contains a navigation that playwright doesn't handle well
-    disabled: true,
   },
   {
     example: "node/control_flow.js",
@@ -142,12 +140,12 @@ module.exports = [
     targets: ["gecko", "chromium"],
   },
   {
-    example: "cra/dist/index.html",
-    script: "sourcemap_stacktrace.js",
-    targets: ["gecko", "chromium"],
     // Disabled because we can't record the example in CI
     // https://github.com/RecordReplay/gecko-dev/issues/726
     disabled: true,
+    example: "cra/dist/index.html",
+    script: "sourcemap_stacktrace.js",
+    targets: ["gecko", "chromium"],
   },
   {
     example: "node/basic.js",
@@ -227,8 +225,6 @@ module.exports = [
     // Not supported on chromium, needs source maps.
     // https://github.com/RecordReplay/chromium/issues/5
     targets: ["gecko"],
-    // Disabled because of https://github.com/RecordReplay/backend/issues/4483
-    disabled: true,
   },
   {
     example: "node/objects.js",
@@ -318,12 +314,12 @@ module.exports = [
     targets: ["gecko", "chromium"],
   },
   {
-    example: "cra/dist/index.html",
-    script: "react_devtools.js",
-    targets: ["gecko", "chromium"],
     // Disabled because we can't record the example in CI
     // https://github.com/RecordReplay/gecko-dev/issues/726
     disabled: true,
+    example: "cra/dist/index.html",
+    script: "react_devtools.js",
+    targets: ["gecko", "chromium"],
   },
   {
     example: "doc_control_flow.html",
@@ -341,6 +337,9 @@ module.exports = [
     targets: ["node"],
   },
   {
+    // Disabled because we can't record the example
+    // https://github.com/RecordReplay/node/issues/69
+    disabled: true,
     example: "node/napi.js",
     script: "node_napi.js",
     targets: ["node"],
