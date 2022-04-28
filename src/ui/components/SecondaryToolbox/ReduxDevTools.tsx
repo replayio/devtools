d434409fcc4f518e6c0ee08f517c7805d9052da3
Mark Erikson <mark@isquaredsoftware.com>
2022-04-28 11:02:25 -0400

    Render Redux DevTools UI component (but needs CSS import hack)
    The @redux-devtools/ui `<Editor>` component imports CSS directly,
    so Next is erroring.  I've temporarily hand-edited my code locally,
    but this is just a hack workaround that needs to be fixed.
    
    On the bright side... rendering this component shows the whole
    Redux DevTools UI immediately! So that's great.

--- src/ui/components/SecondaryToolbox/ReduxDevTools.tsx
+++ src/ui/components/SecondaryToolbox/ReduxDevTools.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Root } from "@redux-devtools/app";
 
 interface ReduxDevToolsProps {}
 
 export const ReduxDevToolsPanel = (props: ReduxDevToolsProps) => {
-  return <div>Redux DevTools go here!</div>;
+  return <Root />;
 };
