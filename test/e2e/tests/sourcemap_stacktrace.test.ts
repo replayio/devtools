360b068930e51ac435aba42dfde5f70bfd1c23e4
Holger Benl <hbenl@evandor.de>
2022-05-04 14:45:45 +0200

    Enable the sourcemap_stacktrace e2e test

--- test/e2e/tests/sourcemap_stacktrace.test.ts
+++ test/e2e/tests/sourcemap_stacktrace.test.ts
@@ -1,8 +1,6 @@
 import { runClassicTest } from "../runTest";
 
-// Disabled because we can't record the example in CI
-// https://github.com/RecordReplay/gecko-dev/issues/726
-it.skip("Test that stacktraces are sourcemapped.", async () => {
+it("Test that stacktraces are sourcemapped.", async () => {
   await runClassicTest({
     example: "cra/dist/index.html",
     script: "sourcemap_stacktrace.js",
