1b242dfc4ca53fe060a577820563d25e860137fb
Holger Benl <hbenl@evandor.de>
2022-05-12 13:15:07 +0200

    Update and reenable the react_devtools e2e test

--- test/e2e/tests/react_devtools.test.ts
+++ test/e2e/tests/react_devtools.test.ts
@@ -1,8 +1,6 @@
 import { runClassicTest } from "../runTest";
 
-// Disabled because we can't record the example in CI
-// https://github.com/RecordReplay/gecko-dev/issues/726
-it.skip("Test React DevTools.", async () => {
+it("Test React DevTools.", async () => {
   await runClassicTest({
     example: "cra/dist/index.html",
     script: "react_devtools.js",
