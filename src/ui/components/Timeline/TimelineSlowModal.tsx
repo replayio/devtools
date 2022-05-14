ab4e80e2a30d0193e731347cf86a6fcf0b95213b
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-05-13 20:08:24 -0400

    Modal warning

--- src/ui/components/Timeline/TimelineSlowModal.tsx
+++ src/ui/components/Timeline/TimelineSlowModal.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { hideModal } from "ui/actions/app";
 
-import { PrimaryButton } from "../shared/Button";
 import Icon from "../shared/Icon";
 import Modal from "../shared/NewModal";
 
@@ -10,17 +9,32 @@ export default function TimelineSlowModal() {
   const dispatch = useDispatch();
   const onMaskClick = () => dispatch(hideModal());
 
-  const reload = () => {};
-
   return (
     <Modal onMaskClick={onMaskClick}>
-      <div className="flex w-80 flex-col items-center gap-4 rounded-xl bg-modalBgcolor p-4">
+      <div className="modal-drop-shadow flex w-80 flex-col items-center gap-4 rounded-xl bg-modalBgcolor p-4">
         <Icon filename="replay-logo" className="h-16 w-16 bg-secondaryAccent" size="custom" />
-        <div className="text-xl font-bold">Recording is loading slowly</div>
-        <div className="text-sm">Sometimes reloading can fix it.</div>
-        <PrimaryButton color="pink" onClick={reload}>
-          Reload
-        </PrimaryButton>
+        <div className="text-xl font-bold">Loading</div>
+        <div className="text-sm">
+          Loading is going slower than expected. Try using{" "}
+          <a
+            href="https://docs.replay.io/docs/viewer-26591deb256c473a946d0f64abb67859#bf19baaa57004b0d9282cc0a02b281f5"
+            rel="noreferrer noopener"
+            style={{ textDecoration: "underline" }}
+            target="_blank"
+          >
+            Focus Mode
+          </a>{" "}
+          to narrow down further, and please let us know on our{" "}
+          <a
+            href="https://replay.io/discord"
+            rel="noreferrer noopener"
+            style={{ textDecoration: "underline" }}
+            target="_blank"
+          >
+            Discord
+          </a>{" "}
+          if you think something is wrong.
+        </div>
       </div>
     </Modal>
   );
