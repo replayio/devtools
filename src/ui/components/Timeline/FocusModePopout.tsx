1a0148400449e84149567ee5b455dbbb6de26e1e
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-05-19 07:55:58 -0400

    Enter confirms and saves focus mode edit

--- src/ui/components/Timeline/FocusModePopout.tsx
+++ src/ui/components/Timeline/FocusModePopout.tsx
@@ -40,11 +40,18 @@ export default function FocusModePopout() {
     hideModal();
   };
 
-  // Dismiss modal if the "Escape" key is pressed.
+  // Keyboard shortcuts handler.
   useEffect(() => {
     const onDocumentKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        discardPendingChanges(true);
+      switch (event.key) {
+        case "Enter": {
+          savePendingChanges();
+          break;
+        }
+        case "Escape": {
+          discardPendingChanges(true);
+          break;
+        }
       }
     };
     document.addEventListener("keydown", onDocumentKeyDown);
