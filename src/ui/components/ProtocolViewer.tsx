69a4a51ffcc824e791080b13290cd9c0e0bd5565
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-05-19 19:16:25 -0400

    Added filter and clear support to Protocol Viewer

--- src/ui/components/ProtocolViewer.tsx
+++ src/ui/components/ProtocolViewer.tsx
@@ -1,9 +1,9 @@
-import classNames from "classnames";
-import { padStart } from "lodash";
+import { filter, padStart } from "lodash";
 import dynamic from "next/dynamic";
 import { CommandResponse } from "protocol/socket";
-import { MutableRefObject, useLayoutEffect, useMemo, useRef, useState } from "react";
+import React, { MutableRefObject, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import Icon from "ui/components/shared/Icon";
 import { getTheme } from "ui/reducers/app";
 import {
   getFullRequestDetails,
@@ -42,6 +42,8 @@ const chunkedRequests = (requests: RequestSummary[]): RequestSummaryChunk[] => {
   return requests.reduce((accumulated: RequestSummaryChunk[], request: RequestSummary) => {
     const current = accumulated[accumulated.length - 1];
 
+    // TODO [bvaughn] Equality check is ID based not method.
+    // TODO [bvaughn] Store start and stop time on Mpa in iD.
     if (
       current == null ||
       current.method !== fullMethod(request) ||
@@ -91,13 +93,13 @@ const ProtocolRequestDetail = ({
 }) => {
   return (
     <>
-      <h3 className={styles.Header}>Request</h3>
+      <h3 className={styles.SubHeader}>Request</h3>
       <div className={styles.Panel}>
         <JSONViewer src={request} />
       </div>
       {response && (
         <>
-          <h3 className={styles.Header}>Response</h3>
+          <h3 className={styles.SubHeader}>Response</h3>
           <div className={styles.Panel}>
             <JSONViewer src={response} />
           </div>
@@ -105,7 +107,7 @@ const ProtocolRequestDetail = ({
       )}
       {error && (
         <>
-          <h3 className={styles.Header}>Error</h3>
+          <h3 className={styles.SubHeader}>Error</h3>
           <div className={styles.Panel}>
             <JSONViewer src={error} />
           </div>
@@ -116,16 +118,63 @@ const ProtocolRequestDetail = ({
 };
 
 const ProtocolViewer = () => {
+  const [clearBeforeIndex, setClearBeforeIndex] = useState(0);
+  const [filterText, setFilterText] = useState("");
+
   const requests = useSelector(getProtocolRequests);
+
   const chunks = useMemo(() => chunkedRequests(requests), [requests]);
+  const filteredChunks = useMemo(
+    () =>
+      chunks.slice(clearBeforeIndex).filter(chunk => {
+        return chunk.method.includes(filterText);
+      }),
+    [chunks, clearBeforeIndex, filterText]
+  );
+
   const [selectedChunk, setSelectedChunk] = useState<RequestSummaryChunk | null>(null);
   const selectedRequestDetails = useSelector(getFullRequestDetails(selectedChunk?.ids ?? []));
+
+  const onFilterTextInputChange = (event: React.ChangeEvent) => {
+    setFilterText(event.currentTarget.value);
+  };
+
+  const onClearButtonClick = () => {
+    setClearBeforeIndex(chunks.length);
+  };
+
+  // TODO [bvaughn] Chunk and show collapsed count (1)
+  // TODO [bvaughn] Could use color for duration (and tooltip on hover)
+  // TODO [bvaughn] Maybe hide the domain (e.g. "Console") and show on hover
+  // TODO [bvaughn] Track down performance rendering problem
 
   return (
     <div className={styles.Container}>
       <h3 className={styles.Header}>Protocol Info</h3>
+
+      <div className={styles.HeaderControls}>
+        <input
+          className={styles.FilterInput}
+          placeholder="Filter"
+          value={filterText}
+          onChange={onFilterTextInputChange}
+        />
+
+        <button
+          className={styles.ClearButton}
+          disabled={filteredChunks.length === 0}
+          title="Clear protocol log"
+          onClick={onClearButtonClick}
+        >
+          <Icon
+            filename="trash"
+            className={filteredChunks.length === 0 ? styles.ClearIconDisabled : styles.ClearIcon}
+          />
+        </button>
+      </div>
+
       <div className={styles.Panel}>
-        {chunks.map(chunk => (
+        {filteredChunks.map(chunk => (
           <ProtocolChunk
             key={`${chunk.method}:${chunk.startedAt}`}
             chunk={chunk}
