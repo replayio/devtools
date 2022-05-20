82a00fe58eb4421f4fef071cb8365b82adf2bd0e
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-05-20 05:14:34 -0700

    Protocol Viewer 2.0 🎉 (#6861)
    * Added filter text and clear support to Protocol Viewer
    * Add duration tracking (and show as colored chips)
    * Improve performance (replace arrays with inline .find with maps, memoize/cache, limit loading of request/error results to 10 by default)

--- src/ui/components/ProtocolViewer.tsx
+++ src/ui/components/ProtocolViewer.tsx
@@ -1,71 +1,90 @@
-import classNames from "classnames";
 import { padStart } from "lodash";
 import dynamic from "next/dynamic";
 import { CommandResponse } from "protocol/socket";
-import { MutableRefObject, useLayoutEffect, useMemo, useRef, useState } from "react";
+import React, {
+  MutableRefObject,
+  ReactElement,
+  ReactNode,
+  useDeferredValue,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { useSelector } from "react-redux";
+import Icon from "ui/components/shared/Icon";
+import MaterialIcon from "ui/components/shared/MaterialIcon";
 import { getTheme } from "ui/reducers/app";
 import {
-  getFullRequestDetails,
-  getProtocolRequests,
+  getProtocolErrorMap,
+  getProtocolRequestMap,
+  getProtocolResponseMap,
   Recorded,
   RequestSummary,
 } from "ui/reducers/protocolMessages";
 
 import styles from "./ProtocolViewer.module.css";
+import { PrimarySmButton } from "./shared/Button";
 
 const ReactJson = dynamic(() => import("react-json-view"), {
   ssr: false,
 });
+
+const MAX_DETAILS_TO_RENDER = 10;
+const REQUEST_DURATION_MEDIUM_THRESHOLD_MS = 250;
+const REQUEST_DURATION_SLOW_THRESHOLD_MS = 1000;
 
 const msAsMinutes = (ms: number) => {
   const seconds = Math.round(ms / 1000.0);
   return `${Math.floor(seconds / 60)}:${padStart(String(seconds % 60), 2, "0")}`;
 };
 
-const fullMethod = (request: RequestSummary): string => {
-  return `${request.class}.${request.method}`;
-};
-
 type RequestSummaryChunk = {
+  class: string;
+  errored: boolean;
   ids: number[];
   count: number;
   method: string;
   pending: boolean;
-  errored: boolean;
   startedAt: number;
 };
 
-// Group requests by common properties, if they are the same method, the same
-// status, and next to each other, then we push them into a `chunk` together
-const chunkedRequests = (requests: RequestSummary[]): RequestSummaryChunk[] => {
-  return requests.reduce((accumulated: RequestSummaryChunk[], request: RequestSummary) => {
-    const current = accumulated[accumulated.length - 1];
+// Collapses consecutive requests with the same method name and shows the count.
+const flattenRequests = (
+  requestMap: { [key: number]: RequestSummary },
+  responseMap: { [key: number]: CommandResponse & Recorded }
+): RequestSummaryChunk[] => {
+  const flattened: RequestSummaryChunk[] = [];
+  let current: RequestSummaryChunk | null = null;
 
-    if (
-      current == null ||
-      current.method !== fullMethod(request) ||
-      current.pending !== request.pending ||
-      current.errored !== request.errored
-    ) {
-      accumulated.push({
+  for (let id in requestMap) {
+    const request = requestMap[id];
+    const response = responseMap[id];
+
+    if (current == null || current.class !== request.class || current.method !== request.method) {
+      current = {
+        class: request.class,
         count: 1,
-        ids: [request.id],
         errored: request.errored,
-        method: fullMethod(request),
+        ids: [request.id],
+        method: request.method,
         pending: request.pending,
         startedAt: request.recordedAt,
-      });
+      };
+
+      flattened.push(current);
     } else {
       current.count++;
+      current.errored ||= request.errored;
+      current.pending ||= request.pending;
       current.ids.push(request.id);
     }
+  }
 
-    return accumulated;
-  }, []);
+  return flattened;
 };
 
-const JSONViewer = ({ src }: { src: object }) => {
+function JSONViewer({ src }: { src: object }) {
   const theme = useSelector(getTheme);
 
   return (
@@ -78,86 +97,214 @@ const JSONViewer = ({ src }: { src: object }) => {
       displayObjectSize={false}
     />
   );
-};
+}
 
-const ProtocolRequestDetail = ({
-  request,
-  response,
-  error,
+function ProtocolRequestDetailPanel({
+  autoExpand,
+  children,
+  header,
+  subHeader,
 }: {
-  request: RequestSummary;
-  response: (CommandResponse & Recorded) | undefined;
-  error: (CommandResponse & Recorded) | undefined;
-}) => {
+  autoExpand: boolean;
+  children: ReactNode;
+  header: ReactNode;
+  subHeader: ReactNode;
+}) {
+  const [isExpanded, setIsExpanded] = useState(autoExpand);
+
   return (
     <>
-      <h3 className={styles.Header}>Request</h3>
-      <div className={styles.Panel}>
-        <JSONViewer src={request} />
-      </div>
-      {response && (
-        <>
-          <h3 className={styles.Header}>Response</h3>
-          <div className={styles.Panel}>
-            <JSONViewer src={response} />
-          </div>
-        </>
-      )}
-      {error && (
-        <>
-          <h3 className={styles.Header}>Error</h3>
-          <div className={styles.Panel}>
-            <JSONViewer src={error} />
-          </div>
-        </>
-      )}
+      <h3 className={styles.AccordionHeader} onClick={() => setIsExpanded(!isExpanded)}>
+        <MaterialIcon iconSize="2xl">{isExpanded ? "arrow_drop_down" : "arrow_right"}</MaterialIcon>
+        <span className={styles.AccordionHeaderPrimaryText}>{header}</span>{" "}
+        <small className={styles.AccordionHeaderSecondaryText}>{subHeader}</small>
+      </h3>
+      {isExpanded && children}
     </>
   );
-};
+}
 
-const ProtocolViewer = () => {
-  const requests = useSelector(getProtocolRequests);
-  const chunks = useMemo(() => chunkedRequests(requests), [requests]);
+function ProtocolRequestDetail({
+  error,
+  index,
+  request,
+  response,
+}: {
+  error: (CommandResponse & Recorded) | undefined;
+  index: number;
+  request: RequestSummary;
+  response: (CommandResponse & Recorded) | undefined;
+}) {
+  return (
+    <>
+      <ProtocolRequestDetailPanel
+        autoExpand={index === 0}
+        header={
+          <>
+            Request <small>#{index + 1}</small>
+          </>
+        }
+        subHeader={msAsMinutes(request.recordedAt)}
+      >
+        <JSONViewer src={request} />
+        {response && (
+          <>
+            <h3 className={styles.AccordionSubHeader}>Response</h3>
+            <JSONViewer src={response} />
+          </>
+        )}
+        {error && (
+          <>
+            <h3 className={styles.AccordionSubHeader}>Error</h3>
+            <JSONViewer src={error} />
+          </>
+        )}
+      </ProtocolRequestDetailPanel>
+    </>
+  );
+}
+
+export default function ProtocolViewer() {
+  const [clearBeforeIndex, setClearBeforeIndex] = useState(0);
+  const [filterText, setFilterText] = useState("");
+  const deferredFilterText = useDeferredValue(filterText);
+
+  const errorMap = useSelector(getProtocolErrorMap);
+  const requestMap = useSelector(getProtocolRequestMap);
+  const responseMap = useSelector(getProtocolResponseMap);
+
   const [selectedChunk, setSelectedChunk] = useState<RequestSummaryChunk | null>(null);
-  const selectedRequestDetails = useSelector(getFullRequestDetails(selectedChunk?.ids ?? []));
+
+  const chunks = useMemo(() => flattenRequests(requestMap, responseMap), [requestMap, responseMap]);
+  const filteredChunks = useMemo(
+    () =>
+      chunks.slice(clearBeforeIndex).filter(chunk => {
+        const fullString = `${chunk.class}.${chunk.method}`.toLowerCase();
+        return fullString.includes(deferredFilterText.toLowerCase());
+      }),
+    [chunks, clearBeforeIndex, deferredFilterText]
+  );
+  const doesFilteredChunksContainSelectedChunk = useMemo(
+    () => selectedChunk !== null && filteredChunks.includes(selectedChunk),
+    [filteredChunks, selectedChunk]
+  );
+
+  const onFilterTextInputChange = (event: React.ChangeEvent) => {
+    setFilterText((event.currentTarget as HTMLInputElement).value);
+  };
+
+  const onClearButtonClick = () => {
+    setClearBeforeIndex(chunks.length);
+  };
 
   return (
     <div className={styles.Container}>
       <h3 className={styles.Header}>Protocol Info</h3>
+
+      <div className={styles.HeaderControls}>
+        <input
+          className={styles.FilterInput}
+          placeholder="Filter"
+          value={filterText}
+          onChange={onFilterTextInputChange}
+        />
+
+        <button
+          className={styles.ClearButton}
+          disabled={filteredChunks.length === 0}
+          title="Clear protocol log"
+          onClick={onClearButtonClick}
+        >
+          <Icon
+            filename="trash"
+            className={filteredChunks.length === 0 ? styles.ClearIconDisabled : styles.ClearIcon}
+          />
+        </button>
+      </div>
+
       <div className={styles.Panel}>
-        {chunks.map(chunk => (
+        {filteredChunks.map(chunk => (
           <ProtocolChunk
-            key={`${chunk.method}:${chunk.startedAt}`}
+            key={chunk.ids[0]}
             chunk={chunk}
+            responseMap={responseMap}
+            requestMap={requestMap}
             selectedChunk={selectedChunk}
             setSelectedChunk={setSelectedChunk}
           />
         ))}
       </div>
-      {selectedRequestDetails.length > 0 && (
-        <div className={styles.Details}>
-          {selectedRequestDetails.map(({ request, response, error }) => {
-            return (
-              <ProtocolRequestDetail
-                key={request!.id}
-                request={request!}
-                response={response}
-                error={error}
-              />
-            );
-          })}
+      {selectedChunk !== null && doesFilteredChunksContainSelectedChunk && (
+        <SelectedRequestDetails
+          key={selectedChunk.ids[0]}
+          errorMap={errorMap}
+          requestMap={requestMap}
+          responseMap={responseMap}
+          selectedChunk={selectedChunk}
+        />
+      )}
+    </div>
+  );
+}
+
+function SelectedRequestDetails({
+  errorMap,
+  requestMap,
+  responseMap,
+  selectedChunk,
+}: {
+  errorMap: { [id: number]: CommandResponse & Recorded };
+  requestMap: { [id: number]: RequestSummary };
+  responseMap: { [id: number]: CommandResponse & Recorded };
+  selectedChunk: RequestSummaryChunk | null;
+}) {
+  const [loadAll, setLoadAll] = useState(false);
+
+  if (selectedChunk === null) {
+    return null;
+  }
+
+  const ids = loadAll ? selectedChunk.ids : selectedChunk.ids.slice(0, MAX_DETAILS_TO_RENDER);
+  const hiddenCount = loadAll ? 0 : Math.max(0, selectedChunk.ids.length - MAX_DETAILS_TO_RENDER);
+
+  return (
+    <div className={styles.Details}>
+      {ids.map((id, index) => {
+        const error = errorMap[id];
+        const request = requestMap[id];
+        const response = responseMap[id];
+
+        return (
+          <ProtocolRequestDetail
+            key={request!.id}
+            index={index}
+            request={request!}
+            response={response}
+            error={error}
+          />
+        );
+      })}
+      {hiddenCount > 0 && (
+        <div className={styles.LoadRemainingDetails}>
+          <PrimarySmButton color="blue" onClick={() => setLoadAll(true)}>
+            Load additional {hiddenCount} requests...
+          </PrimarySmButton>
         </div>
       )}
     </div>
   );
-};
+}
 
 function ProtocolChunk({
   chunk,
+  responseMap,
+  requestMap,
   selectedChunk,
   setSelectedChunk,
 }: {
   chunk: RequestSummaryChunk;
+  responseMap: { [key: number]: CommandResponse & Recorded };
+  requestMap: { [key: number]: RequestSummary };
   selectedChunk: RequestSummaryChunk | null;
   setSelectedChunk: React.Dispatch<React.SetStateAction<RequestSummaryChunk | null>>;
 }) {
@@ -181,21 +328,46 @@ function ProtocolChunk({
   let className = styles.Chunk;
   if (isSelected) {
     className = styles.ChunkSelected;
-  } else if (chunk.pending) {
-    className = styles.ChunkPending;
   } else if (chunk.errored) {
     className = styles.ChunkErrored;
+  } else if (chunk.pending) {
+    className = styles.ChunkPending;
   }
 
+  const durations = chunk.ids.reduce((total, id) => {
+    const request = requestMap[id];
+    const response = responseMap[id];
+    if (request && response) {
+      total += response.recordedAt - request.recordedAt;
+    }
+    return total;
+  }, 0);
+  const averageDuration = Math.round(durations / chunk.ids.length);
+
+  const selectChunk = () => setSelectedChunk(chunk);
+
+  let durationName = styles.ChunkDurationFast;
+  if (averageDuration > REQUEST_DURATION_SLOW_THRESHOLD_MS) {
+    durationName = styles.ChunkDurationSlow;
+  } else if (averageDuration > REQUEST_DURATION_MEDIUM_THRESHOLD_MS) {
+    durationName = styles.ChunkDurationMedium;
+  }
+
+  const durationTitle =
+    chunk.ids.length > 1 ? `${averageDuration}ms average` : `${Math.round(durations)}ms`;
+
   return (
-    <div ref={ref} className={className} onClick={() => setSelectedChunk(chunk)}>
-      <span>
-        {chunk.method}
-        {chunk.count > 1 ? `(${chunk.count})` : null}
+    <div ref={ref} className={className} onClick={selectChunk}>
+      <span className={styles.ChunkCount}>
+        {chunk.count > 1 ? <span className={styles.ChunkCountBadge}>{chunk.count}</span> : null}
       </span>
-      <span>{msAsMinutes(chunk.startedAt)}</span>
+      <span className={styles.ChunkMethod} title={`${chunk.class}.${chunk.method}`}>
+        {chunk.method}
+      </span>
+      <span className={styles.ChunkStartTime}>{msAsMinutes(chunk.startedAt)}</span>
+      <span className={styles.ChunkDuration}>
+        {durations > 0 && <div className={durationName} title={durationTitle} />}
+      </span>
     </div>
   );
 }
-
-export default ProtocolViewer;
