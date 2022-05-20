e077bc84e5ccc39e234a37a0442ac9d8a71f29d0
Holger Benl <hbenl@evandor.de>
2022-05-20 11:30:16 +0200

    Remove duplicate assert function

--- src/ui/utils/assert.tsx
+++
@@ -1,9 +0,0 @@
-import * as Sentry from "@sentry/browser";
-
-export function assert(v: any, why = ""): asserts v {
-  if (!v) {
-    const error = new Error(`Assertion Failed: ${why}`);
-    Sentry.captureException(error);
-    throw error;
-  }
-}
