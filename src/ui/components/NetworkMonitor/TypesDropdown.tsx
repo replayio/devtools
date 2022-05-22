60bf51586bf34b176197e71b677b5bca486cc16f
yeison <yeison@tesorio.com>
2022-05-22 09:24:48 -0500

    network types dropdown changes

--- src/ui/components/NetworkMonitor/TypesDropdown.tsx
+++ src/ui/components/NetworkMonitor/TypesDropdown.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-
-import PortalDropdown from "ui/components/shared/PortalDropdown";
+import classNames from "classnames";
+import React from "react";
 import { Dropdown, DropdownItem, DropdownItemContent } from "ui/components/Library/LibraryDropdown";
 import MaterialIcon from "ui/components/shared/MaterialIcon";
+
 import { CanonicalRequestType, RequestTypeOptions } from "./utils";
 
 export default function TypesDropdown({
@@ -12,51 +12,26 @@ export default function TypesDropdown({
   types: Set<CanonicalRequestType>;
   toggleType: (type: CanonicalRequestType) => void;
 }) {
-  const [expanded, setExpanded] = useState(false);
-
-  const button = (
-    <MaterialIcon className="mr-2" iconSize="lg" outlined={true}>
-      filter_alt
-    </MaterialIcon>
-  );
-
-  const TypeItem = ({
-    icon,
-    label,
-    type,
-  }: {
-    icon: string;
-    type: CanonicalRequestType;
-    label: string;
-  }) => {
-    return (
-      <DropdownItem onClick={() => toggleType(type)}>
-        <DropdownItemContent selected={types.has(type)} icon={icon}>
-          {label}
-        </DropdownItemContent>
-      </DropdownItem>
-    );
-  };
-
   return (
-    <PortalDropdown
-      buttonContent={button}
-      setExpanded={setExpanded}
-      expanded={expanded}
-      buttonStyle={`${types.size > 0 ? "text-primaryAccent" : "text-gray-400"}`}
-      distance={0}
-      position="bottom-right"
+    <Dropdown
+      placement="bottom-start"
+      triggerClassname={classNames({
+        "text-primaryAccent hover:text-primaryAccentHover focus:text-primaryAccentHover": types.size > 0,
+        "text-gray-400 hover:text-primaryAccentHover focus:text-primaryAccentHover": types.size === 0,
+      })}
+      trigger={
+        <MaterialIcon className="mr-2" iconSize="lg" outlined={true}>
+          filter_alt
+        </MaterialIcon>
+      }
     >
-      <Dropdown>
-        {RequestTypeOptions.map(canonicalType => (
-          <TypeItem
-            key={canonicalType.type}
-            label={canonicalType.label}
-            icon={canonicalType.icon}
-            type={canonicalType.type}
-          />
-        ))}
-      </Dropdown>
-    </PortalDropdown>
+      {RequestTypeOptions.map(({ type, label, icon }) => (
+        <DropdownItem key={type} onClick={() => toggleType(type)}>
+          <DropdownItemContent selected={types.has(type)} icon={icon}>
+            {label}
+          </DropdownItemContent>
+        </DropdownItem>
+      ))}
+    </Dropdown>
   );
 }
