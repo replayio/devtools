951238ff254ecd66b03712ae0345cbab693c044f
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-05-24 08:44:13 -0400

    Intentionally breka things

--- src/ui/components/Redacted.tsx
+++ src/ui/components/Redacted.tsx
@@ -3,10 +3,11 @@ import classnames from "classnames";
 import { prefs } from "ui/utils/prefs";
 
 export function Redacted({ className, ...rest }: React.HTMLProps<HTMLDivElement>) {
-  const showRedactions = prefs.showRedactions;
-  return (
-    <div {...rest} data-private={true} className={classnames(className, { showRedactions })} />
-  );
+  return null;
+  // const showRedactions = prefs.showRedactions;
+  // return (
+  //   <div {...rest} data-private={true} className={classnames(className, { showRedactions })} />
+  // );
 }
 
 export function RedactedSpan({ className, ...rest }: React.HTMLProps<HTMLSpanElement>) {
