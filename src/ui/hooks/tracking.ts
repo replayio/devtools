588f4298c5038471f3de24dfb911838c68447a07
Jaril <jarilvalenciano@gmail.com>
2022-05-25 10:49:07 -0400

    Wait until the upload is finished before tracking kpi-loading-idle-time

--- src/ui/hooks/tracking.ts
+++ src/ui/hooks/tracking.ts
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getUploading } from "ui/reducers/app";
 import { Recording } from "ui/types";
 import { trackTiming } from "ui/utils/telemetry";
 
 type LoadingContext = "warm" | "cold";
 
 export function useTrackLoadingIdleTime(context: LoadingContext, recording?: Recording) {
+  const uploading = useSelector(getUploading);
   const [started, setStarted] = useState(false);
 
   useEffect(() => {
-    if (recording && !started) {
+    if (recording && !uploading && !started) {
       setStarted(true);
       trackTiming("kpi-loading-idle-time");
     }
-  }, [recording, started, setStarted]);
+  }, [recording, started, setStarted, uploading]);
 
   const trackLoadingIdleTime = (sessionId: string) => {
     trackTiming("kpi-loading-idle-time", { sessionId, context });
