4289753ceb9f261063b52010710f362c317299da
Jaril <jarilvalenciano@gmail.com>
2022-05-26 08:31:34 -0400

    Keep a record of whether the upload progress was shown during loading idle time

--- src/ui/hooks/tracking.ts
+++ src/ui/hooks/tracking.ts
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { getUploading } from "ui/reducers/app";
 import { Recording } from "ui/types";
 import { trackTiming } from "ui/utils/telemetry";
 
-type LoadingContext = "warm" | "cold";
-
-export function useTrackLoadingIdleTime(context: LoadingContext, recording?: Recording) {
+export function useTrackLoadingIdleTime(uploadComplete: boolean, recording?: Recording) {
+  const uploading = useSelector(getUploading);
+  const context = uploadComplete ? "warm" : "cold";
   const [started, setStarted] = useState(false);
+  const [uploadProgressShown, setUploadProgressShown] = useState(false);
 
   useEffect(() => {
     if (recording && !started) {
@@ -13,9 +16,14 @@ export function useTrackLoadingIdleTime(context: LoadingContext, recording?: Rec
       trackTiming("kpi-loading-idle-time");
     }
   }, [recording, started, setStarted]);
+  useEffect(() => {
+    if (uploading) {
+      setUploadProgressShown(true);
+    }
+  }, [uploading]);
 
   const trackLoadingIdleTime = (sessionId: string) => {
-    trackTiming("kpi-loading-idle-time", { sessionId, context });
+    trackTiming("kpi-loading-idle-time", { sessionId, context, uploadProgressShown });
   };
 
   return { trackLoadingIdleTime };
