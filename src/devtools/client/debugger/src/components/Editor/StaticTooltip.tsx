992d63cdb84717935cc8fa56b7ec965b57648a01
ryota-murakami <dojce1048@gmail.com>
2022-06-05 05:06:14 +0900

    fix: "Large Text" setting results in offset location for line hover button #7038

--- src/devtools/client/debugger/src/components/Editor/StaticTooltip.tsx
+++ src/devtools/client/debugger/src/components/Editor/StaticTooltip.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useFeature } from "ui/hooks/settings";
+import classNames from "classnames";
 
 type StaticTooltipProps = {
   targetNode: HTMLElement;
@@ -8,8 +10,15 @@ type StaticTooltipProps = {
 };
 
 export default function StaticTooltip({ targetNode, children }: StaticTooltipProps) {
+  const { value: enableLargeText } = useFeature("enableLargeText");
+
   return ReactDOM.createPortal(
-    <div className="absolute z-50 flex flex-row space-x-px transform -right-1 translate-x-full bottom-4 mb-0.5 pointer-events-none">
+    <div
+      className={classNames(
+        "absolute z-50 flex flex-row space-x-px transform -right-1 translate-x-full bottom-4 mb-0.5 pointer-events-none",
+        enableLargeText && "bottom-6"
+      )}
+    >
       {children}
     </div>,
     targetNode
