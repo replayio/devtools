6e7939e402add4c7debc6e4c1230cc040cf17ac8
Travis Arnold <travis@replay.io>
2022-06-06 12:35:32 -0700

    remove PrintStatementPanel

--- packages/components/PrintStatementPanel/index.ts
+++
@@ -1,1 +0,0 @@
-export { PrintStatementPanel } from "./PrintStatementPanel";
--- packages/components/index.ts
+++ packages/components/index.ts
@@ -2,7 +2,6 @@ export * from "./AddCommentButton";
 export * from "./Column";
 export * from "./Icon";
 export * from "./PrefixBadgePicker";
-export * from "./PrintStatementPanel";
 export * from "./Row";
 
 export * from "./types";
