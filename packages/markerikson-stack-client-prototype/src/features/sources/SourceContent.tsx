845ba623f7db1fb44e5b141023dad8af3b8add93
Mark Erikson <mark@isquaredsoftware.com>
2022-06-13 15:47:28 -0400

    Add basic CodeMirror setup and JS highlighting

--- packages/markerikson-stack-client-prototype/src/features/sources/SourceContent.tsx
+++ packages/markerikson-stack-client-prototype/src/features/sources/SourceContent.tsx
@@ -1,4 +1,7 @@
 import { skipToken } from "@reduxjs/toolkit/dist/query";
+import CodeMirror from "@uiw/react-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
 import { useGetSourceTextQuery, useGetSourceHitCountsQuery } from "../../app/api";
 import { useAppSelector } from "../../app/hooks";
 
@@ -12,5 +15,12 @@ export const SourceContent = () => {
     return null;
   }
 
-  return <pre style={{ maxHeight: 600, overflowY: "auto", minWidth: 800 }}>{sourceText}</pre>;
+  return (
+    <CodeMirror
+      value={sourceText}
+      editable={false}
+      style={{ maxHeight: 600, overflowY: "auto", minWidth: 800, maxWidth: 1000 }}
+      extensions={[javascript({ jsx: true })]}
+    />
+  );
 };
