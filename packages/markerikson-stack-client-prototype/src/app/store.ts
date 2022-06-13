b2642b9d2466f2718ba578f6350ae7535949c3b8
Mark Erikson <mark@isquaredsoftware.com>
2022-06-13 12:49:14 -0400

    Rip out Redux counter code

--- packages/markerikson-stack-client-prototype/src/app/store.ts
+++ packages/markerikson-stack-client-prototype/src/app/store.ts
@@ -1,24 +1,22 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit'
-
-import counterReducer from '../features/counter/counterSlice'
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 
 export function makeStore() {
   return configureStore({
-    reducer: { counter: counterReducer },
-  })
+    reducer: () => ({}),
+  });
 }
 
-const store = makeStore()
+const store = makeStore();
 
-export type AppState = ReturnType<typeof store.getState>
+export type AppState = ReturnType<typeof store.getState>;
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   AppState,
   unknown,
   Action<string>
->
+>;
 
-export default store
+export default store;
