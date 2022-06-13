17abad0196ac01b815fcb2ba09a0f3ef4bf77bf8
Holger Benl <hbenl@evandor.de>
2022-06-13 14:20:42 +0200

    Extend the react_devtools e2e test (FE-128)

--- public/test/scripts/react_devtools.js
+++ public/test/scripts/react_devtools.js
@@ -2,6 +2,14 @@ const getComponents = () => document.querySelectorAll("[class^=Tree] [class^=Wra
 const getInspectedItems = () =>
   document.querySelectorAll("[class^=InspectedElement] [class^=Name]");
 const getInspectedItem = name => [...getInspectedItems()].find(item => item.textContent === name);
+const getItemValue = item => item.parentElement.querySelector("[class^=Value]").textContent;
+const waitForInspectedItem = (name, value) => Test.waitUntil(
+  () => {
+    const item = getInspectedItem(name);
+    return item && getItemValue(item) === value;
+  },
+  { waitingFor: `An inspected item with name ${name} and value ${value}` }
+);
 
 Test.describe(`Test React DevTools.`, async () => {
   await Test.selectConsole();
@@ -19,6 +27,22 @@ Test.describe(`Test React DevTools.`, async () => {
   await Test.waitUntil(() => getComponents().length === 4, {
     waitingFor: "There to be 4 components in ReactDevTools",
   });
+
+  // enable the react devtools node picker
+  document.querySelector("[data-react-devtools-portal-root] [class^=ToggleOff]").click();
+
+  const result = await Test.app.threadFront.evaluate({ text: "document.querySelector('li:first-child').getBoundingClientRect()" });
+  const bounds = await result.returned.getJSON();
+  const x = (bounds.left + bounds.right) / 2;
+  const y = (bounds.top + bounds.bottom) / 2;
+  await Test.waitUntil(
+    () => {
+      Test.dispatchMouseEventInGraphics("mousemove", x, y);
+      return document.querySelector("[class^=InactiveSelectedElement]");
+    },
+    { waitingFor: "A React node to be selected" },
+  )
+  await waitForInspectedItem("text", "\"Foo\"");
 
   await Test.selectConsole();
   await Test.warpToMessage("Removed an entry");
@@ -40,9 +64,14 @@ Test.describe(`Test React DevTools.`, async () => {
   });
 
   getInspectedItem("1State").parentElement.firstChild.click();
-  await Test.waitUntil(() => getInspectedItem("0"), { waitingFor: "0 to be among the inspected items" });
+  await waitForInspectedItem("0", "{key: \"2\", text: \"Bar\"}");
   getInspectedItem("0").parentElement.firstChild.click();
-  await Test.waitUntil(() => getInspectedItem("key") && getInspectedItem("text"), {
-    waitingFor: "key and text to be among the inspected items",
-  });
+  await waitForInspectedItem("key", "\"2\"");
+  await waitForInspectedItem("text", "\"Bar\"");
+
+  await Test.app.actions.seekToTime(0);
+  await Test.waitUntil(
+    () => document.querySelector(".secondary-toolbox-content").textContent === "Mounting your React application...Try picking a different point on the timeline.",
+    { waitingFor: 'ReactDevTools to say "Try picking a different point"' },
+  );
 });
