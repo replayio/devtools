86b1e15286f8e69fb41c6c9bfe86f58e84527eca
Mark Erikson <mark@isquaredsoftware.com>
2022-06-14 15:22:59 -0400

    Simplify `useAppDispatch` definition to remove an extra fn call

--- src/ui/setup/hooks.ts
+++ src/ui/setup/hooks.ts
@@ -3,5 +3,5 @@ import type { AppDispatch } from "./store";
 import type { UIState } from "ui/state";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<UIState> = useSelector;
