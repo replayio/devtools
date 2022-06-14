be613cf6e40932abd766e0d18bb4dc2be2f5d61f
Ryan Duffy <ryan@replay.io>
2022-06-14 10:35:10 -0700

    move public api key to action

--- test/e2e/config.ts
+++ test/e2e/config.ts
@@ -10,14 +10,6 @@ export default {
   driverPath: process.env.RECORD_REPLAY_DRIVER,
   headless: !!process.env.RECORD_REPLAY_PLAYWRIGHT_HEADLESS,
   nodePath: process.env.RECORD_REPLAY_NODE,
-  // This API key allows access to the whole team that holds all these test
-  // recordings. If someone wanted to, they could go in and delete the workspace
-  // or recordings in it or anything. While thats not great, it's also not
-  // the end of the world. If someone does that, we can always change
-  // this code to only run in CI in the main repo and have this as a secret.
-  // It's a lot easier to hardcode it for now though.
-  // trunk-ignore(gitleaks/generic-api-key)
-  replayApiKey: "rwk_7XPbO5fhz0bkhANYXtN2dkm74wNQCchXf2OxVgAerTQ",
   updateFixtures: !!process.env.SHOULD_UPDATE_FIXTURES,
   useExampleFile: !process.env.SHOULD_RECORD_EXAMPLES,
 };
