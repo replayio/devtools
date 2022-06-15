2be4aa26478fc94fc96b32c69add7466cab78caf
Mark Erikson <mark@isquaredsoftware.com>
2022-06-15 14:46:37 -0400

    Proof of Concept prototype: use RTK Query for fetching Replay Protocol data to show Pause stack frames (#7158)

---
+++ packages/markerikson-stack-client-prototype/src/features/sources/SourcesList.tsx
@@ -0,0 +1,29 @@
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { useGetSourcesQuery } from "../../app/api";
+
+import { sourceEntrySelected } from "./sourcesSlice";
+
+export const SourcesList = () => {
+  const dispatch = useAppDispatch();
+  const selectedSourceId = useAppSelector(state => state.sources.selectedSourceId);
+  const { data } = useGetSourcesQuery();
+
+  return (
+    <ul>
+      {data?.src.map(entry => {
+        const isSelected = selectedSourceId === entry.sourceId;
+        let entryText: React.ReactNode = new URL(entry.url!).pathname;
+        if (isSelected) {
+          entryText = <span style={{ fontWeight: "bold" }}>{entryText}</span>;
+        }
+
+        const onLineClicked = () => dispatch(sourceEntrySelected(entry.sourceId));
+        return (
+          <li key={entry.sourceId} onClick={onLineClicked}>
+            {entryText}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
