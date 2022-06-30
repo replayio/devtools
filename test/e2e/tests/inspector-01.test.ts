5743150a4b7421b017597d9ae8fc39946fad02f5
Mark Erikson <mark@isquaredsoftware.com>
2022-06-30 12:54:45 -0400

    Update one test to run with Chromium in coverage mode

--- test/e2e/tests/inspector-01.test.ts
+++ test/e2e/tests/inspector-01.test.ts
@@ -4,7 +4,7 @@ import { runClassicTest } from "../runTest";
 it("Test basic inspector functionality: the inspector is able to show contents when paused according to the child's current position.", async () => {
   // DOM inspection tests are not currently supported on chromium.
   // https://github.com/RecordReplay/chromium/issues/11
-  if (config.browserName !== "firefox") {
+  if (config.browserName !== "firefox" && !config.shouldSaveCoverageData) {
     return;
   }
   await runClassicTest({
