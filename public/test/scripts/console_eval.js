38e43e6e8fb44153a0645312fa99ff8a3e1fb869
Jason Laster <jason.laster.11@gmail.com>
2022-06-30 19:15:31 -0700

    Fix global evaluations in the terminal

--- public/test/scripts/console_eval.js
+++ public/test/scripts/console_eval.js
@@ -1,5 +1,11 @@
 Test.describe(`Test global console evaluation.`, async () => {
   await Test.selectConsole();
+  
+  // we are seeking to an arbitrary point in time 
+  // so that the backend is happy. Yes this is a hack.
+  await Test.app.actions.seekToTime(1570)
+  await Test.executeInConsole("333");
+
   await Test.warpToMessage("ExampleFinished");
 
   await Test.executeInConsole("number");
