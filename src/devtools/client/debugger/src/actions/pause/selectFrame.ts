cff6f67f37fb94fa28d36382d35fd15e34adbb57
Mark Erikson <mark@isquaredsoftware.com>
2022-07-05 17:33:32 -0400

    Migrate pause/selectFrame to TS

--- src/devtools/client/debugger/src/actions/pause/selectFrame.ts
+++ src/devtools/client/debugger/src/actions/pause/selectFrame.ts
@@ -2,21 +2,31 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
 
-//
+import type { UIThunkAction } from "ui/actions";
+import type { Context } from "devtools/client/debugger/src/reducers/pause";
 
 import { selectLocation } from "../sources";
 import { fetchScopes } from "./fetchScopes";
 import { setFramePositions } from "./setFramePositions";
+import { SourceLocation } from "../../reducers/types";
+
+interface TempFrame {
+  asyncIndex: number;
+  protocolId: string;
+  state: string;
+  location: SourceLocation;
+}
 
 /**
  * @memberof actions/pause
  * @static
  */
-export function selectFrame(cx, frame) {
+export function selectFrame(cx: Context, frame: TempFrame): UIThunkAction {
   return async (dispatch, getState, { client }) => {
     // Frames that aren't on-stack do not support evalling and may not
     // have live inspectable scopes, so we do not allow selecting them.
     if (frame.state !== "on-stack") {
+      // @ts-expect-error sourcelocation mismatch
       return dispatch(selectLocation(cx, frame.location));
     }
 
@@ -28,6 +38,7 @@ export function selectFrame(cx, frame) {
 
     client.fetchAncestorFramePositions(frame.asyncIndex, frame.protocolId);
 
+    // @ts-expect-error sourcelocation mismatch
     dispatch(selectLocation(cx, frame.location));
     dispatch(setFramePositions());
 
