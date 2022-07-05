a2c16d8c4aa956b617898dcbee4735d85741f324
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-07-05 09:47:47 -0400

    Convert hit counts to CSS vars, per theme

--- packages/bvaughn-architecture-demo/components/sources/Source.tsx
+++ packages/bvaughn-architecture-demo/components/sources/Source.tsx
@@ -3,7 +3,7 @@ import { PointsContext } from "@bvaughn/src/contexts/PointsContext";
 import { getSourceContents, getSourceHitCounts } from "@bvaughn/src/suspense/SourcesCache";
 import { suspendInParallel } from "@bvaughn/src/utils/suspense";
 import { newSource as ProtocolSource, SourceId as ProtocolSourceId } from "@replayio/protocol";
-import { ChangeEvent, Fragment, useContext, useState } from "react";
+import { Fragment, useContext, useMemo } from "react";
 import { ReplayClientContext } from "shared/client/ReplayClientContext";
 
 import PointPanel from "./PointPanel";
@@ -22,18 +22,38 @@ export default function Source({
 
   const { addPoint, deletePoint, points } = useContext(PointsContext);
 
-  const [sourceHitCounts, sourceContents] = suspendInParallel(
+  const [hitCounts, sourceContents] = suspendInParallel(
     () => getSourceHitCounts(client, sourceId),
     () => getSourceContents(client, sourceId)
   );
 
+  // Min/max hit counts are used to determine heat map color.
+  const { minHitCount, maxHitCount } = useMemo(() => {
+    let minHitCount = Infinity;
+    let maxHitCount = 0;
+    if (hitCounts) {
+      hitCounts.forEach(hitCount => {
+        const hits = hitCount.columnHits?.[0].hits || 0;
+        if (hits > 0) {
+          if (minHitCount > hits) {
+            minHitCount = hits;
+          }
+          if (maxHitCount < hits) {
+            maxHitCount = hits;
+          }
+        }
+      });
+    }
+    return { minHitCount, maxHitCount };
+  }, [hitCounts]);
+
   const onAddPointButtonClick = (lineNumber: number) => {
-    const lineHasHits = sourceHitCounts.has(lineNumber);
+    const lineHasHits = hitCounts.has(lineNumber);
     if (!lineHasHits) {
       return;
     }
 
-    const hitCountsForLine = sourceHitCounts.get(lineNumber)!;
+    const hitCountsForLine = hitCounts.get(lineNumber)!;
     const closestColumnNumber = hitCountsForLine.columnHits[0]!.location.column;
     const fileName = source?.url?.split("/")?.pop();
 
@@ -55,13 +75,27 @@ export default function Source({
       <div className={styles.SourceContents}>
         {sourceContents.contents.split("\n").map((line, index) => {
           const lineNumber = index + 1;
-          const lineHasHits = sourceHitCounts.has(lineNumber);
-          const lineHits = lineHasHits
-            ? sourceHitCounts.get(lineNumber)?.columnHits[0].hits || 0
-            : 0;
+          const lineHasHits = hitCounts.has(lineNumber);
+          const hitCount = hitCounts?.get(lineNumber)?.columnHits?.[0].hits || 0;
 
-          const hitCountBadge =
-            lineHits === 0 ? 1 : lineHits < 10 ? 1 : lineHits < 20 ? 2 : lineHits < 50 ? 3 : 4;
+          // We use a gradient to indicate the "heat" (the number of hits).
+          // This absolute hit count values are relative, per file.
+          // Cubed root prevents high hit counts from lumping all other values together.
+          const NUM_GRADIENT_COLORS = 3;
+          let hitCountClassName = styles.LineHitCount0;
+          let hitCountIndex = NUM_GRADIENT_COLORS - 1;
+          if (hitCount > 0) {
+            if (minHitCount !== maxHitCount) {
+              hitCountIndex = Math.min(
+                NUM_GRADIENT_COLORS - 1,
+                Math.round(
+                  ((hitCount - minHitCount) / (maxHitCount - minHitCount)) * NUM_GRADIENT_COLORS
+                )
+              );
+            }
+            hitCountClassName = styles[`LineHitCount${hitCountIndex + 1}`];
+          }
+
           const linePoint = points.find(
             point => point.location.sourceId === sourceId && point.location.line === lineNumber
           );
@@ -105,9 +139,7 @@ export default function Source({
               >
                 <div className={styles.LineNumber}>{lineNumber}</div>
                 {hoverButton}
-                <div
-                  className={`${styles[`LineHitCount-${hitCountBadge}`]} ${styles.LineHitCount}`}
-                />
+                <div className={hitCountClassName} />
                 {lineSegments}
               </div>
               {linePoint && <PointPanel className={styles.PointPanel} point={linePoint} />}
