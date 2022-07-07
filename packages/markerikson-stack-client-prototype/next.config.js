3d64d7e0cf7b7255101387b73ef107c233bc2a8a
Mark Erikson <mark@isquaredsoftware.com>
2022-07-07 14:24:56 -0400

    Configure my prototype to reference Brian's prototype

--- packages/markerikson-stack-client-prototype/next.config.js
+++ packages/markerikson-stack-client-prototype/next.config.js
@@ -1,13 +1,37 @@
 const transpileModules = require("next-transpile-modules");
+const { patchWebpackConfig } = require("next-global-css");
 
 const withTM = transpileModules(["protocol"]);
 
+/**
+ * @type {Pick<
+ *   import('next').NextConfig,
+ *   | 'webpack'
+ * >}
+ */
 const baseNextConfig = {
   reactStrictMode: true,
 
   // This setting allows the Next app to import code from e.g. "packages/protocol"
   experimental: {
     externalDir: true,
+  },
+
+  /**
+   * @type {(
+   *   config: import('webpack').Configuration,
+   *   context: {
+   *     dev: boolean
+   *     webpack: import('webpack')
+   *   }
+   * ) => import('webpack').Configuration}
+   */
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Allow CSS imported from `node_modules`, to work around an error
+    // from importing `<Expandable>` from Brian's protottype
+    patchWebpackConfig(config, { isServer });
+
+    return config;
   },
 };
 
