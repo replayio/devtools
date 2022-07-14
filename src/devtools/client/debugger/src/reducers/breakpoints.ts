9cf13d3506670607436c57a02353667f6b70961d
Josh Morrow <josh@jcmorrow.com>
2022-07-13 17:12:50 -0700

    Fix analysis edge case
    This is a super weird one.

--- src/devtools/client/debugger/src/reducers/breakpoints.ts
+++ src/devtools/client/debugger/src/reducers/breakpoints.ts
@@ -17,7 +17,6 @@ import {
 } from "@replayio/protocol";
 import uniqBy from "lodash/uniqBy";
 import type { Context } from "devtools/client/debugger/src/reducers/pause";
-import string from "devtools/packages/devtools-reps/reps/string";
 import { AnalysisError, MAX_POINTS_FOR_FULL_ANALYSIS } from "protocol/thread/analysis";
 import { compareNumericStrings } from "protocol/utils";
 import type { UIState } from "ui/state";
@@ -25,12 +24,11 @@ import { filterToFocusRegion, isFocusRegionSubset } from "ui/utils/timeline";
 
 import { getBreakpointsList } from "../selectors/breakpoints";
 import assert from "../utils/assert";
-import { shallowEqual } from "../utils/resource/compare";
 import { getLocationKey, isMatchingLocation, isLogpoint } from "../utils/breakpoint";
 
 import { getSelectedSource } from "./sources";
 import type { Breakpoint, SourceLocation } from "./types";
-import { FocusRegion, UnsafeFocusRegion } from "ui/state/timeline";
+import { FocusRegion } from "ui/state/timeline";
 export type { Breakpoint } from "./types";
 
 type LocationWithoutColumn = Omit<Location, "column">;
@@ -541,13 +539,19 @@ export const getAnalysisPointsForLocation = createSelector(
 
     const pointsPerEntry = matchingEntries.map(entry => {
       const { points = [], results = [] } = entry;
-      if (condition && entry.status === AnalysisStatus.Completed) {
-        // Currently the backend does not filter returned points by condition, only analysis results.
-        // If there _is_ a condition, _and_ we have results back, we should filter the total points
-        // based on the analysis results.
-        const resultPointsSet = new Set<string>(results.map(result => result.key));
-        const filteredConditionPoints = points.filter(point => resultPointsSet.has(point.point));
-        return filteredConditionPoints;
+      if (condition) {
+        // Currently the backend does not filter returned points by condition,
+        // only analysis results.  If there _is_ a condition, _and_ we have
+        // results back, we should filter the total points based on the analysis
+        // results.
+        if (entry.status === AnalysisStatus.Completed) {
+          const resultPointsSet = new Set<string>(results.map(result => result.key));
+          const filteredConditionPoints = points.filter(point => resultPointsSet.has(point.point));
+          return filteredConditionPoints;
+        }
+        // If there is a condition, but this analysis has not completed, we
+        // cannot trust the points it got back, since they will not be filtered.
+        return [];
       }
 
       return points;
