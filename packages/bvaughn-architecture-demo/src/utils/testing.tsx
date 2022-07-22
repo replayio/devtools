e4fde471f0575438f24855141f4064a442d40cec
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-07-22 10:59:52 -0400

    Remove unused sourceIds from SessionContext

--- packages/bvaughn-architecture-demo/src/utils/testing.tsx
+++ packages/bvaughn-architecture-demo/src/utils/testing.tsx
@@ -39,7 +39,6 @@ export async function render(
     endPoint: "1000",
     recordingId: "fakeRecordingId",
     sessionId: "fakeSessionId",
-    sourceIds: [],
     ...options?.sessionContext,
   };
 
