643ae0627aec0bf6d0e27934b60e32ac79828f1a
Jaril <jarilvalenciano@gmail.com>
2022-08-02 15:00:05 -0700

    Make sure to check authentication even if user is going straight through the /team route

--- pages/team/[[...param]].tsx
+++ pages/team/[[...param]].tsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { setLoadingFinished } from "ui/actions/app";
 import Library from "ui/components/Library";
+import Login from "ui/components/shared/Login/Login";
 import { useAppDispatch } from "ui/setup/hooks";
+import useAuth0 from "ui/utils/useAuth0";
 
 export default function TeamIndex() {
   const dispatch = useAppDispatch();
+  const { isAuthenticated } = useAuth0();
 
   useEffect(() => {
     dispatch(setLoadingFinished(true));
   }, [dispatch]);
+
+  if (!isAuthenticated) {
+    return <Login returnToPath={window.location.pathname + window.location.search} />;
+  }
 
   return <Library />;
 }
