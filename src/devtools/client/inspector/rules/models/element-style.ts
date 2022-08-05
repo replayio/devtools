a0e8bd00e869672e57c904a92e463fc99fba5b4b
Mark Erikson <mark@isquaredsoftware.com>
2022-08-05 17:01:59 -0400

    Remove dead code in `element-style` and `models/rule`

--- src/devtools/client/inspector/rules/models/element-style.ts
+++ src/devtools/client/inspector/rules/models/element-style.ts
@@ -79,18 +79,6 @@ export default class ElementStyle {
     if (!("userProperties" in this.store)) {
       this.store.userProperties = new UserProperties();
     }
-
-    // if (!("disabled" in this.store)) {
-    //   this.store.disabled = new WeakMap();
-    // }
-
-    // this.onRefresh = this.onRefresh.bind(this);
-
-    // if (this.ruleView.isNewRulesView) {
-    //   this.pageStyle.on("stylesheet-updated", this.onRefresh);
-    //   this.ruleView.inspector.styleChangeTracker.on("style-changed", this.onRefresh);
-    //   this.ruleView.selection.on("pseudoclass", this.onRefresh);
-    // }
   }
 
   get unusedCssEnabled() {
@@ -109,29 +97,9 @@ export default class ElementStyle {
     this.pseudoElements = [];
 
     for (const rule of this.rules || []) {
-      // if (rule.editor) {
-      //   rule.editor.destroy();
-      // }
-
       rule.destroy();
     }
-
-    // if (this.ruleView.isNewRulesView) {
-    //   this.pageStyle.off("stylesheet-updated", this.onRefresh);
-    //   this.ruleView.inspector.styleChangeTracker.off("style-changed", this.onRefresh);
-    //   this.ruleView.selection.off("pseudoclass", this.onRefresh);
-    // }
   }
-
-  /**
-   * Called by the Rule object when it has been changed through the
-   * setProperty* methods.
-   */
-  // _changed() {
-  //   if (this.onChanged) {
-  //     this.onChanged();
-  //   }
-  // }
 
   /**
    * Refresh the list of rules to be displayed for the active element.
@@ -194,10 +162,6 @@ export default class ElementStyle {
 
     // Mark overridden computed styles.
     this.onRuleUpdated();
-
-    // if (this.ruleView.isNewRulesView) {
-    //   this.subscribeRulesToLocationChange();
-    // }
   }
 
   /**
@@ -210,27 +174,6 @@ export default class ElementStyle {
   getRule(id: string | null) {
     return id ? this.rules?.find(rule => rule.domRule.objectId() === id) : undefined;
   }
-
-  /**
-   * Get the font families in use by the element.
-   *
-   * Returns a promise that will be resolved to a list of CSS family
-   * names. The list might have duplicates.
-   */
-  // getUsedFontFamilies() {
-  //   return new Promise((resolve, reject) => {
-  //     this.ruleView.styleWindow.requestIdleCallback(async () => {
-  //       try {
-  //         const fonts = await this.pageStyle.getUsedFontFaces(this.element, {
-  //           includePreviews: false,
-  //         });
-  //         resolve(fonts.map(font => font.CSSFamilyName));
-  //       } catch (e) {
-  //         reject(e);
-  //       }
-  //     });
-  //   });
-  // }
 
   /**
    * Add a rule if it's one we care about. Filters out duplicates and
@@ -471,280 +414,6 @@ export default class ElementStyle {
   }
 
   /**
-   * Adds a new declaration to the rule.
-   *
-   * @param  {String} ruleId
-   *         The id of the Rule to be modified.
-   * @param  {String} value
-   *         The new declaration value.
-   */
-  // addNewDeclaration(ruleId, value) {
-  //   const rule = this.getRule(ruleId);
-  //   if (!rule) {
-  //     return;
-  //   }
-
-  //   const declarationsToAdd = parseNamedDeclarations(this.cssProperties.isKnown, value, true);
-  //   if (!declarationsToAdd.length) {
-  //     return;
-  //   }
-
-  //   this._addMultipleDeclarations(rule, declarationsToAdd);
-  // }
-
-  /**
-   * Adds a new rule. The rules view is updated from a "stylesheet-updated" event
-   * emitted the PageStyleActor as a result of the rule being inserted into the
-   * the stylesheet.
-   */
-  // async addNewRule() {
-  //   await this.pageStyle.addNewRule(this.element, this.element.pseudoClassLocks);
-  // }
-
-  /**
-   * Given the id of the rule and the new declaration name, modifies the existing
-   * declaration name to the new given value.
-   *
-   * @param  {String} ruleId
-   *         The Rule id of the given CSS declaration.
-   * @param  {String} declarationId
-   *         The TextProperty id for the CSS declaration.
-   * @param  {String} name
-   *         The new declaration name.
-   */
-  // async modifyDeclarationName(ruleId, declarationId, name) {
-  //   const rule = this.getRule(ruleId);
-  //   if (!rule) {
-  //     return;
-  //   }
-
-  //   const declaration = rule.getDeclaration(declarationId);
-  //   if (!declaration || declaration.name === name) {
-  //     return;
-  //   }
-
-  //   // Adding multiple rules inside of name field overwrites the current
-  //   // property with the first, then adds any more onto the property list.
-  //   const declarations = parseDeclarations(this.cssProperties.isKnown, name);
-  //   if (!declarations.length) {
-  //     return;
-  //   }
-
-  //   await declaration.setName(declarations[0].name);
-
-  //   if (!declaration.enabled) {
-  //     await declaration.setEnabled(true);
-  //   }
-  // }
-
-  /**
-   * Helper function to addNewDeclaration() and modifyDeclarationValue() for
-   * adding multiple declarations to a rule.
-   *
-   * @param  {Rule} rule
-   *         The Rule object to write new declarations to.
-   * @param  {Array<Object>} declarationsToAdd
-   *         An array of object containg the parsed declaration data to be added.
-   * @param  {TextProperty|null} siblingDeclaration
-   *         Optional declaration next to which the new declaration will be added.
-   */
-  // _addMultipleDeclarations(rule, declarationsToAdd, siblingDeclaration = null) {
-  //   for (const { commentOffsets, name, value, priority } of declarationsToAdd) {
-  //     const isCommented = Boolean(commentOffsets);
-  //     const enabled = !isCommented;
-  //     siblingDeclaration = rule.createProperty(name, value, priority, enabled, siblingDeclaration);
-  //   }
-  // }
-
-  /**
-   * Parse a value string and break it into pieces, starting with the
-   * first value, and into an array of additional declarations (if any).
-   *
-   * Example: Calling with "red; width: 100px" would return
-   * { firstValue: "red", propertiesToAdd: [{ name: "width", value: "100px" }] }
-   *
-   * @param  {String} value
-   *         The string to parse.
-   * @return {Object} An object with the following properties:
-   *         firstValue: A string containing a simple value, like
-   *                     "red" or "100px!important"
-   *         declarationsToAdd: An array with additional declarations, following the
-   *                            parseDeclarations format of { name, value, priority }
-   */
-  // _getValueAndExtraProperties(value: string) {
-  //   // The inplace editor will prevent manual typing of multiple declarations,
-  //   // but we need to deal with the case during a paste event.
-  //   // Adding multiple declarations inside of value editor sets value with the
-  //   // first, then adds any more onto the declaration list (below this declarations).
-  //   let firstValue = value;
-  //   let declarationsToAdd = [];
-
-  //   const declarations = parseDeclarations(this.cssProperties.isKnown, value);
-
-  //   // Check to see if the input string can be parsed as multiple declarations
-  //   if (declarations.length) {
-  //     // Get the first property value (if any), and any remaining
-  //     // declarations (if any)
-  //     if (!declarations[0].name && declarations[0].value) {
-  //       firstValue = declarations[0].value;
-  //       declarationsToAdd = declarations.slice(1);
-  //     } else if (declarations[0].name && declarations[0].value) {
-  //       // In some cases, the value could be a property:value pair
-  //       // itself.  Join them as one value string and append
-  //       // potentially following declarations
-  //       firstValue = declarations[0].name + ": " + declarations[0].value;
-  //       declarationsToAdd = declarations.slice(1);
-  //     }
-  //   }
-
-  //   return {
-  //     declarationsToAdd,
-  //     firstValue,
-  //   };
-  // }
-
-  /**
-   * Given the id of the rule and the new declaration value, modifies the existing
-   * declaration value to the new given value.
-   *
-   * @param  {String} ruleId
-   *         The Rule id of the given CSS declaration.
-   * @param  {String} declarationId
-   *         The TextProperty id for the CSS declaration.
-   * @param  {String} value
-   *         The new declaration value.
-   */
-  // async modifyDeclarationValue(ruleId, declarationId, value) {
-  //   const rule = this.getRule(ruleId);
-  //   if (!rule) {
-  //     return;
-  //   }
-
-  //   const declaration = rule.getDeclaration(declarationId);
-  //   if (!declaration) {
-  //     return;
-  //   }
-
-  //   const { declarationsToAdd, firstValue } = this._getValueAndExtraProperties(value);
-  //   const parsedValue = parseSingleValue(this.cssProperties.isKnown, firstValue);
-
-  //   if (
-  //     !declarationsToAdd.length &&
-  //     declaration.value === parsedValue.value &&
-  //     declaration.priority === parsedValue.priority
-  //   ) {
-  //     return;
-  //   }
-
-  //   // First, set this declaration value (common case, only modified a property)
-  //   await declaration.setValue(parsedValue.value, parsedValue.priority);
-
-  //   if (!declaration.enabled) {
-  //     await declaration.setEnabled(true);
-  //   }
-
-  //   this._addMultipleDeclarations(rule, declarationsToAdd, declaration);
-  // }
-
-  /**
-   * Modifies the existing rule's selector to the new given value.
-   *
-   * @param  {String} ruleId
-   *         The id of the Rule to be modified.
-   * @param  {String} selector
-   *         The new selector value.
-   */
-  // async modifySelector(ruleId, selector) {
-  //   try {
-  //     const rule = this.getRule(ruleId);
-  //     if (!rule) {
-  //       return;
-  //     }
-
-  //     const response = await rule.domRule.modifySelector(this.element, selector);
-  //     const { ruleProps, isMatching } = response;
-
-  //     if (!ruleProps) {
-  //       // Notify for changes, even when nothing changes, just to allow tests
-  //       // being able to track end of this request.
-  //       this.ruleView.emit("ruleview-invalid-selector");
-  //       return;
-  //     }
-
-  //     const newRule = new Rule(this, {
-  //       ...ruleProps,
-  //       isUnmatched: !isMatching,
-  //     });
-
-  //     // Recompute the list of applied styles because editing a
-  //     // selector might cause this rule's position to change.
-  //     const appliedStyles = await this.pageStyle.getApplied(this.element, {
-  //       inherited: true,
-  //       matchedSelectors: true,
-  //       filter: this.showUserAgentStyles ? "ua" : undefined,
-  //     });
-  //     const newIndex = appliedStyles.findIndex(r => r.rule == ruleProps.rule);
-  //     const oldIndex = this.rules.indexOf(rule);
-
-  //     // Remove the old rule and insert the new rule according to where it appears
-  //     // in the list of applied styles.
-  //     this.rules.splice(oldIndex, 1);
-  //     // If the selector no longer matches, then we leave the rule in
-  //     // the same relative position.
-  //     this.rules.splice(newIndex === -1 ? oldIndex : newIndex, 0, newRule);
-
-  //     // Recompute, mark and update the UI for any properties that are
-  //     // overridden or contain inactive CSS according to the new list of rules.
-  //     this.onRuleUpdated();
-
-  //     // In order to keep the new rule in place of the old in the rules view, we need
-  //     // to remove the rule again if the rule was inserted to its new index according
-  //     // to the list of applied styles.
-  //     // Note: you might think we would replicate the list-modification logic above,
-  //     // but that is complicated due to the way the UI installs pseudo-element rules
-  //     // and the like.
-  //     if (newIndex !== -1) {
-  //       this.rules.splice(newIndex, 1);
-  //       this.rules.splice(oldIndex, 0, newRule);
-  //     }
-  //     this._changed();
-  //   } catch (e) {
-  //     console.error(e);
-  //   }
-  // }
-
-  /**
-   * Subscribes all the rules to location changes.
-   */
-  // subscribeRulesToLocationChange() {
-  //   for (const rule of this.rules) {
-  //     rule.subscribeToLocationChange();
-  //   }
-  // }
-
-  /**
-   * Toggles the enabled state of the given CSS declaration.
-   *
-   * @param  {String} ruleId
-   *         The Rule id of the given CSS declaration.
-   * @param  {String} declarationId
-   *         The TextProperty id for the CSS declaration.
-   */
-  // toggleDeclaration(ruleId, declarationId) {
-  //   const rule = this.getRule(ruleId);
-  //   if (!rule) {
-  //     return;
-  //   }
-
-  //   const declaration = rule.getDeclaration(declarationId);
-  //   if (!declaration) {
-  //     return;
-  //   }
-
-  //   declaration.setEnabled(!declaration.enabled);
-  // }
-
-  /**
    * Mark a given TextProperty as overridden or not depending on the
    * state of its computed properties. Clears the _overriddenDirty state
    * on all computed properties.
@@ -788,22 +457,4 @@ export default class ElementStyle {
     const variables = this.variablesMap.get(pseudo);
     return variables ? variables.get(name) : null;
   }
-
-  /**
-   * Handler for "stylesheet-updated", "style-changed" and "pseudoclass" events.
-   * Refreshes the list of rules on the page.
-   */
-  // async onRefresh() {
-  //   // Repopulate the element style once the current modifications are done.
-  //   const promises = [];
-  //   for (const rule of this.rules) {
-  //     if (rule._applyingModifications) {
-  //       promises.push(rule._applyingModifications);
-  //     }
-  //   }
-
-  //   await Promise.all(promises);
-  //   await this.populate();
-  //   this._changed();
-  // }
 }
