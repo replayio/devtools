56878fe9c047cad868aed379358d601d7ae95437
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-08-09 12:00:53 -0400

    Remove unused context

--- packages/bvaughn-architecture-demo/components/inspector/values/ObjectStackContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-type ObjectIdMap = { [objectId: number]: boolean };
-
-const ObjectStackContext = createContext<ObjectIdMap>({});
-
-export default ObjectStackContext;
