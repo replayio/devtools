e630488a6426ab3bd9c2e7a857aa37b113946f76
Jason Laster <jason.laster.11@gmail.com>
2022-08-14 13:08:33 -0700

    remove old tests

--- test/e2e/tests/breakpoints-07.test.ts
+++
@@ -1,8 +0,0 @@
-import { runClassicTest } from "../runTest";
-
-it("Test that breakpoints work across navigations.", async () => {
-  await runClassicTest({
-    example: "doc_navigate.html",
-    script: "breakpoints-07.js",
-  });
-});
--- test/e2e/tests/stepping-06.test.ts
+++
@@ -1,8 +0,0 @@
-import { runClassicTest } from "../runTest";
-
-it("Test stepping in async frames and async call stacks.", async () => {
-  await runClassicTest({
-    example: "doc_async.html",
-    script: "stepping-06.js",
-  });
-});
