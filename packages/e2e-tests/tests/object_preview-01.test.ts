a9d1ef01a45511b21c98f12f91ce58721c883247
Jason Laster <jason.laster.11@gmail.com>
2022-08-14 12:56:52 -0700

    object tests

--- packages/e2e-tests/tests/object_preview-01.test.ts
+++ packages/e2e-tests/tests/object_preview-01.test.ts
@@ -2,44 +2,51 @@ import { test } from "@playwright/test";
 import {
   openExample,
   clickDevTools,
-  rewindToLine,
-  stepOverToLine,
-  stepOutToLine,
-  stepInToLine,
-  addEventListenerLogpoints,
-  addBreakpoint,
-  resumeToLine,
+  executeInConsole,
+  checkMessageObjectContents,
+  checkJumpIcon,
   selectConsole,
   warpToMessage,
+  waitForConsoleMessage,
 } from "../helpers";
 
 // Test the objects produced by console.log() calls and by evaluating various
 test(`expressions in the console after time warping.`, async ({ page }) => {
+  let msg;
+  await openExample(page, "doc_rr_objects.html");
+  await clickDevTools(page);
   await selectConsole(page);
 
-  await waitForMessage(page, "Array(20) [ 0, 1, 2, 3, 4, 5,");
-  await waitForMessage(page, "Uint8Array(20) [ 0, 1, 2, 3, 4, 5,");
-  await waitForMessage(page, "Set(22) [ {…}, {…}, 0, 1, 2, 3, 4, 5,");
-  await waitForMessage(page, "Map(21) { {…} → {…}, 0 → 1, 1 → 2, 2 → 3, 3 → 4, 4 → 5,");
-  await waitForMessage(page, "WeakSet(20) [ {…}, {…}, {…},");
-  await waitForMessage(page, "WeakMap(20) { {…} → {…}, {…} → {…},");
-  await waitForMessage(page, "Object { a: 0, a0: 0, a1: 1, a2: 2, a3: 3, a4: 4,");
-  await waitForMessage(page, "/abc/gi");
-  await waitForMessage(page, "Date");
+  await waitForConsoleMessage(page, "Array(20) [ 0, 1, 2, 3, 4, 5,");
+  await waitForConsoleMessage(page, "Uint8Array(20) [ 0, 1, 2, 3, 4, 5,");
+  await waitForConsoleMessage(page, "Set(22) [ {…}, {…}, 0, 1, 2, 3, 4, 5,");
 
-  await waitForMessage(page, 'RangeError: "foo"');
-  await waitForMessage(page, '<div id="foo" class="bar" style="visibility: visible" blahblah="">');
+  await waitForConsoleMessage(page, "Map(21) { {…} → {…}, 0 → 1, 1 → 2, 2 → 3, 3 → 4, 4 → 5,");
+  await waitForConsoleMessage(page, "WeakSet(20) [ {…}, {…}, {…},");
+  await waitForConsoleMessage(page, "WeakMap(20) { {…} → {…}, {…} → {…},");
+  await waitForConsoleMessage(page, "Object { a: 0, a0: 0, a1: 1, a2: 2, a3: 3, a4: 4,");
+  await waitForConsoleMessage(page, "/abc/gi");
+  await waitForConsoleMessage(page, "Date");
 
-  msg = await waitForMessage(page, "function bar()");
-  checkJumpIcon(page, msg);
+  await waitForConsoleMessage(page, `RangeError: "foo"`);
+  await waitForConsoleMessage(
+    page,
+    '<div id="foo" class="bar" style="visibility: visible" blahblah="">'
+  );
 
-  await waitForMessage(page, 'Array(6) [ undefined, true, 3, null, "z", 40n ]');
-  await waitForMessage(page, "Proxy {  }");
-  await waitForMessage(page, "Symbol()");
-  await waitForMessage(page, "Symbol(symbol)");
-  await waitForMessage(page, `Object { "Symbol()": 42, "Symbol(symbol)": "Symbol()" }`);
+  await waitForConsoleMessage(page, "function bar()");
+  await checkJumpIcon(page, "function bar()");
+  // await new Promise(r => setTimeout(r, 100_000));
 
-  msg = await waitForMessage(page, "Object { _foo: C }");
+  await waitForConsoleMessage(page, 'Array(6) [ undefined, true, 3, null, "z", 40n ]');
+  await waitForConsoleMessage(page, "Proxy {  }");
+  await waitForConsoleMessage(page, "Symbol()");
+  await waitForConsoleMessage(page, "Symbol(symbol)");
+  await waitForConsoleMessage(page, `Object { "Symbol()": 42, "Symbol(symbol)": "Symbol()" }`);
+
+  msg = await waitForConsoleMessage(page, "Object { _foo: C }");
+
+  // TODO: add support for expanding objects in the console
   await checkMessageObjectContents(
     page,
     msg,
@@ -50,7 +57,7 @@ test(`expressions in the console after time warping.`, async ({ page }) => {
   await warpToMessage(page, "Done");
 
   await executeInConsole(page, "Error('helo')");
-  await waitForMessage(page, 'Error: "helo"');
+  await waitForConsoleMessage(page, 'Error: "helo"');
 
   await executeInConsole(
     page,
@@ -62,29 +69,29 @@ test(`expressions in the console after time warping.`, async ({ page }) => {
       `
   );
   // FIXME the first line in this stack isn't right.
-  await waitForMessage(page, 'Error: "there"');
+  await waitForConsoleMessage(page, 'Error: "there"');
 
   executeInConsole(page, "Array(1, 2, 3)");
-  msg = await waitForMessage(page, "Array(3) [ 1, 2, 3 ]");
+  msg = await waitForConsoleMessage(page, "Array(3) [ 1, 2, 3 ]");
   await checkMessageObjectContents(page, msg, ["0: 1", "1: 2", "2: 3", "length: 3"]);
 
   await executeInConsole(page, "new Uint8Array([1, 2, 3, 4])");
-  msg = await waitForMessage(page, "Uint8Array(4) [ 1, 2, 3, 4 ]");
+  msg = await waitForConsoleMessage(page, "Uint8Array(4) [ 1, 2, 3, 4 ]");
   await checkMessageObjectContents(page, msg, ["0: 1", "1: 2", "2: 3", "3: 4", "length: 4"]);
 
   await executeInConsole(page, `RegExp("abd", "g")`);
-  msg = await waitForMessage(page, "/abd/g");
+  msg = await waitForConsoleMessage(page, "/abd/g");
 
   // RegExp object properties are not currently available in chromium.
 
   await checkMessageObjectContents(page, msg, ["global: true", `source: "abd"`]);
 
   await executeInConsole(page, "new Set([1, 2, 3])");
-  msg = await waitForMessage(page, "Set(3) [ 1, 2, 3 ]");
+  msg = await waitForConsoleMessage(page, "Set(3) [ 1, 2, 3 ]");
   await checkMessageObjectContents(page, msg, ["0: 1", "1: 2", "2: 3", "size: 3"], ["<entries>"]);
 
   await executeInConsole(page, "new Map([[1, {a:1}], [2, {b:2}]])");
-  msg = await waitForMessage(page, "Map { 1 → {…}, 2 → {…} }");
+  msg = await waitForConsoleMessage(page, "Map { 1 → {…}, 2 → {…} }");
   await checkMessageObjectContents(
     page,
     msg,
@@ -93,7 +100,7 @@ test(`expressions in the console after time warping.`, async ({ page }) => {
   );
 
   await executeInConsole(page, "new WeakSet([{a:1}, {b:2}])");
-  msg = await waitForMessage(page, "WeakSet(2) [ {…}, {…} ]");
+  msg = await waitForConsoleMessage(page, "WeakSet(2) [ {…}, {…} ]");
   await checkMessageObjectContents(
     page,
     msg,
@@ -102,7 +109,7 @@ test(`expressions in the console after time warping.`, async ({ page }) => {
   );
 
   await executeInConsole(page, "new WeakMap([[{a:1},{b:1}], [{a:2},{b:2}]])");
-  msg = await waitForMessage(page, "WeakMap { {…} → {…}, {…} → {…} }");
+  msg = await waitForConsoleMessage(page, "WeakMap { {…} → {…}, {…} → {…} }");
   await checkMessageObjectContents(
     page,
     msg,
@@ -111,21 +118,21 @@ test(`expressions in the console after time warping.`, async ({ page }) => {
   );
 
   await executeInConsole(page, "new Promise(() => {})");
-  msg = await waitForMessage(page, "Promise {  }");
+  msg = await waitForConsoleMessage(page, "Promise {  }");
 
   // Promise contents aren't currently available in chromium.
 
   await checkMessageObjectContents(page, msg, ['"pending"'], []);
 
   await executeInConsole(page, "Promise.resolve({ a: 1 })");
-  msg = await waitForMessage(page, "Promise {  }");
+  msg = await waitForConsoleMessage(page, "Promise {  }");
 
   await checkMessageObjectContents(page, msg, ['"fulfilled"', "a: 1"], ["<value>"]);
 
   await executeInConsole(page, "Promise.reject({ a: 1 })");
-  msg = await waitForMessage(page, "Promise {  }");
+  msg = await waitForConsoleMessage(page, "Promise {  }");
 
   await executeInConsole(page, "baz");
-  msg = await waitForMessage(page, "function baz()");
+  msg = await waitForConsoleMessage(page, "function baz()");
   checkJumpIcon(page, msg);
 });
