383d57952861b366f39e62961f7a332bfa6c9fa1
Jason Laster <jason.laster.11@gmail.com>
2022-08-14 13:08:33 -0700

    remove old tests

--- test/e2e/tests/breakpoints-04.test.ts
+++
@@ -1,8 +0,0 @@
-import { runClassicTest } from "../runTest";
-
-it("Test hitting breakpoints when using tricky control flow constructs: catch, finally, generators, and async/await.", async () => {
-  await runClassicTest({
-    example: "doc_control_flow.html",
-    script: "breakpoints-04.js",
-  });
-});
--- test/e2e/tests/breakpoints-05.test.ts
+++
@@ -1,8 +0,0 @@
-import { runClassicTest } from "../runTest";
-
-it("Test interaction of breakpoints with debugger statements.", async () => {
-  await runClassicTest({
-    example: "doc_debugger_statements.html",
-    script: "breakpoints-05.js",
-  });
-});
