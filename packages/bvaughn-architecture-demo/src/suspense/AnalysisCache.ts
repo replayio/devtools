e89e4ffe0ecb61b432688acd66219d831a698233
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-08-17 15:27:21 -0400

    Removed local analysis for break points

--- packages/bvaughn-architecture-demo/src/suspense/AnalysisCache.ts
+++ packages/bvaughn-architecture-demo/src/suspense/AnalysisCache.ts
@@ -46,8 +46,6 @@ const locationAndTimeToValueMap: Map<string, Record<AnalysisResults>> = new Map(
 // TODO (FE-469) Filter in-memory if the range gets smaller (and we haven't overflowed)
 // Currently we re-run the analysis which seems wasteful.
 
-// TODO (FE-469) Add focus region awareness to analysis.
-
 export function runAnalysis(
   client: ReplayClientInterface,
   focusRange: PointRange | null,
@@ -86,106 +84,48 @@ async function runLocalOrRemoteAnalysis(
   wakeable: Wakeable<AnalysisResults>
 ) {
   try {
-    const analysisResults = await runLocalAnalysis(code);
+    const results = await client.runAnalysis<RemoteAnalysisResult>({
+      effectful: false,
+      locations: [{ location }],
+      mapper: createMapperForAnalysis(code),
+      range: focusRange || undefined,
+    });
+
+    if (results.length === 0) {
+      throw new Error("No results returned from analysis");
+    }
+
+    const resultsMap = new Map();
+    results.forEach(result => {
+      const objects = result.data.objects;
+      if (objects) {
+        // Pre-cache pause data so we don't need to refetch when rendering in inspecting values in the console.
+        preCacheObjects(result.pauseId, objects);
+      }
+
+      resultsMap.set(result.point, {
+        executionPoint: result.point,
+        isRemote: true,
+        pauseId: result.pauseId,
+        time: result.time,
+        values: result.values,
+      });
+    });
+
+    const analysisResults = (timeStampedPoint: TimeStampedPoint) => {
+      return resultsMap.get(timeStampedPoint.point) || null;
+    };
 
     record.status = STATUS_RESOLVED;
     record.value = analysisResults;
 
     wakeable.resolve(analysisResults);
   } catch (error) {
-    try {
-      const analysisResults = await runRemoteAnalysis(client, focusRange, location, code);
+    record.status = STATUS_REJECTED;
+    record.value = error;
 
-      record.status = STATUS_RESOLVED;
-      record.value = analysisResults;
-
-      wakeable.resolve(analysisResults);
-    } catch (error) {
-      record.status = STATUS_REJECTED;
-      record.value = error;
-
-      wakeable.reject(error);
-    }
+    wakeable.reject(error);
   }
-}
-
-export async function runLocalAnalysis(code: string): Promise<AnalysisResults> {
-  return new Promise((resolve, reject) => {
-    if (code === "location") {
-      reject();
-    }
-
-    const escapedCode = code.replace(/"/g, '\\"');
-    // Most globals (like window and document) aren't defined in a Worker, but the location global is.
-    const workerCode = `
-        const values = ((location) => eval("[${escapedCode}]"))(null);
-        postMessage(values);
-    `;
-    const blob = new Blob([workerCode], { type: "text/javascript" });
-    const objectURL = URL.createObjectURL(blob);
-    const worker = new Worker(objectURL);
-    worker.addEventListener("message", event => {
-      const values = event.data;
-      const analysisResults: AnalysisResults = (timeStampedPoint: TimeStampedPoint) => ({
-        executionPoint: timeStampedPoint.point,
-        isRemote: false,
-        pauseId: null,
-        time: timeStampedPoint.time,
-        values,
-      });
-
-      resolve(analysisResults);
-      clearTimeout(timeoutID);
-    });
-    worker.addEventListener("error", event => {
-      reject(event.message);
-      clearTimeout(timeoutID);
-    });
-    worker.postMessage("start");
-    const timeoutID = setTimeout(() => {
-      worker.terminate();
-      reject("Timed out");
-    }, 500);
-  });
-}
-
-export async function runRemoteAnalysis(
-  client: ReplayClientInterface,
-  focusRange: PointRange | null,
-  location: Location,
-  code: string
-): Promise<AnalysisResults> {
-  const results = await client.runAnalysis<RemoteAnalysisResult>({
-    effectful: false,
-    locations: [{ location }],
-    mapper: createMapperForAnalysis(code),
-    range: focusRange || undefined,
-  });
-
-  if (results.length === 0) {
-    throw new Error("No results returned from analysis");
-  }
-
-  const resultsMap = new Map();
-  results.forEach(result => {
-    const objects = result.data.objects;
-    if (objects) {
-      // This will avoid us having to turn around and request them again when rendering the logs.
-      preCacheObjects(result.pauseId, objects);
-    }
-
-    resultsMap.set(result.point, {
-      executionPoint: result.point,
-      isRemote: true,
-      pauseId: result.pauseId,
-      time: result.time,
-      values: result.values,
-    });
-  });
-
-  return (timeStampedPoint: TimeStampedPoint) => {
-    return resultsMap.get(timeStampedPoint.point) || null;
-  };
 }
 
 function createMapperForAnalysis(code: string): string {
