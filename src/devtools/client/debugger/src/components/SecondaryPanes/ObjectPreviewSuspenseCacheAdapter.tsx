3010056a2ea7eeac216d422f1af8c348cb36cc39
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-08-16 20:58:29 -0400

    ObjectPreviewSuspenseCacheAdapter handles circular references when cloning

--- src/devtools/client/debugger/src/components/SecondaryPanes/ObjectPreviewSuspenseCacheAdapter.tsx
+++ src/devtools/client/debugger/src/components/SecondaryPanes/ObjectPreviewSuspenseCacheAdapter.tsx
@@ -2,8 +2,21 @@ import { preCacheObjects } from "@bvaughn/src/suspense/ObjectPreviews";
 import { trackExecutionPointPauseIds } from "@bvaughn/src/suspense/PauseCache";
 import { ExecutionPoint, PauseData, PauseId } from "@replayio/protocol";
 import { addPauseDataListener, removePauseDataListener } from "protocol/thread/pause";
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useFeature } from "ui/hooks/settings";
+
+function createReplacer() {
+  const cache = new Set();
+  return function replacer(_: string, value: any): any {
+    if (typeof value === "object" && value !== null) {
+      if (cache.has(value)) {
+        return undefined;
+      }
+      cache.add(value);
+    }
+    return value;
+  };
+}
 
 // Connects legacy PauseData to the new Object Inspector Suspense cache.
 // This avoids requiring the new Object Inspector to load redundant data,
@@ -11,7 +24,9 @@ import { useFeature } from "ui/hooks/settings";
 export default function ObjectPreviewSuspenseCacheAdapter() {
   const enableNewComponentArchitecture = useFeature("enableNewComponentArchitecture");
 
-  useEffect(() => {
+  // It's important to not miss pre-cached data because there's no way to access it after the fact.
+  // So use a layout effect for subscription rather than a passive effect.
+  useLayoutEffect(() => {
     if (!enableNewComponentArchitecture) {
       return;
     }
@@ -21,7 +36,9 @@ export default function ObjectPreviewSuspenseCacheAdapter() {
       if (objects) {
         // Be sure to clone object data before pre-caching it.
         // Otherwise ValueFronts might deeply mutate it and change its structure.
-        preCacheObjects(pauseId, JSON.parse(JSON.stringify(objects)));
+        // Because object data may contain circular references, we need to also filter out empty entries.
+        const cloned = JSON.parse(JSON.stringify(objects, createReplacer())).filter(Boolean);
+        preCacheObjects(pauseId, cloned);
       }
 
       trackExecutionPointPauseIds(executionPoint, pauseId);
