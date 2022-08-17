8791ddf88950a93b6590c660d2675cf942522e3f
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-08-17 09:26:54 -0400

    Removed circular reference handling now that ValueFronts are not involved (again)

--- src/devtools/client/debugger/src/components/SecondaryPanes/ObjectPreviewSuspenseCacheAdapter.tsx
+++ src/devtools/client/debugger/src/components/SecondaryPanes/ObjectPreviewSuspenseCacheAdapter.tsx
@@ -5,22 +5,8 @@ import { addPauseDataListener, removePauseDataListener } from "protocol/thread/p
 import { useLayoutEffect } from "react";
 import { useFeature } from "ui/hooks/settings";
 
-function createReplacer() {
-  const cache = new Set();
-  return function replacer(_: string, value: any): any {
-    if (typeof value === "object" && value !== null) {
-      if (cache.has(value)) {
-        return undefined;
-      }
-      cache.add(value);
-    }
-    return value;
-  };
-}
-
 // Connects legacy PauseData to the new Object Inspector Suspense cache.
-// This avoids requiring the new Object Inspector to load redundant data,
-// and also prevents potential runtime errors because of how the protocolValueToClientValue() utility works.
+// This avoids requiring the new Object Inspector to load redundant data.
 export default function ObjectPreviewSuspenseCacheAdapter() {
   const enableNewComponentArchitecture = useFeature("enableNewComponentArchitecture");
 
@@ -36,8 +22,7 @@ export default function ObjectPreviewSuspenseCacheAdapter() {
       if (objects) {
         // Be sure to clone object data before pre-caching it.
         // Otherwise ValueFronts might deeply mutate it and change its structure.
-        // Because object data may contain circular references, we need to also filter out empty entries.
-        const cloned = JSON.parse(JSON.stringify(objects, createReplacer())).filter(Boolean);
+        const cloned = JSON.parse(JSON.stringify(objects)).filter(Boolean);
         preCacheObjects(pauseId, cloned);
       }
 
