c5d50db352f3b84105e4f740f79944d1d8d8ff4a
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-08-23 09:20:35 -0400

    Special case typed arrays in protocol server-to-client util

--- packages/bvaughn-architecture-demo/src/utils/protocol.ts
+++ packages/bvaughn-architecture-demo/src/utils/protocol.ts
@@ -131,6 +131,17 @@ export function protocolValueToClientValue(
       let type: ValueType = "object";
       switch (className) {
         case "Array":
+        case "BigInt64Array":
+        case "BigUint64Array":
+        case "Float32Array":
+        case "Float64Array":
+        case "Int8Array":
+        case "Int16Array":
+        case "Int32Array":
+        case "Uint8Array":
+        case "Uint8ClampedArray":
+        case "Uint16Array":
+        case "Uint32Array":
           type = "array";
           break;
         case "Date":
