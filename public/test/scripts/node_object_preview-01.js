a7e3429a2842af99e1d1fc52cd24ff5ba4b22e41
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-08-23 10:19:19 -0400

    Update node_object_preview-01 test

--- public/test/scripts/node_object_preview-01.js
+++ public/test/scripts/node_object_preview-01.js
@@ -1,13 +1,13 @@
 Test.describe(`showing console objects in node.`, async () => {
   await Test.selectConsole();
 
-  await Test.waitForMessage("Array(20) [ 0, 1, 2, 3, 4, 5,");
-  await Test.waitForMessage("Uint8Array(20) [ 0, 1, 2, 3, 4, 5,");
-  await Test.waitForMessage("Set(22) [ {…}, {…}, 0, 1, 2, 3, 4, 5,");
-  await Test.waitForMessage("Map(21) { {…} → {…}, 0 → 1, 1 → 2, 2 → 3, 3 → 4, 4 → 5,");
-  await Test.waitForMessage("WeakSet(20) [ {…}, {…}, {…},");
-  await Test.waitForMessage("WeakMap(20) { {…} → {…}, {…} → {…},");
-  await Test.waitForMessage("Object { a: 0, a0: 0, a1: 1, a2: 2, a3: 3, a4: 4,");
+  await Test.waitForMessage("Array(20) [0, 1, 2, 3, 4,");
+  await Test.waitForMessage("Uint8Array(20) [0, 1, 2, 3, 4,");
+  await Test.waitForMessage("Set(22) [{…}, {…}, 0, 1, 2, 3, 4,");
+  await Test.waitForMessage("Map(21) {{…} → {…}, 0 → 1, 1 → 2, 2 → 3, 3 → 4,");
+  await Test.waitForMessage("WeakSet(20) [{…}, {…}, {…},");
+  await Test.waitForMessage("WeakMap(20) {{…} → {…}, {…} → {…},");
+  await Test.waitForMessage("Object {a: 0, a0: 0, a1: 1, a2: 2, a3: 3,");
   await Test.waitForMessage("/abc/gi");
   await Test.waitForMessage("Date");
 
