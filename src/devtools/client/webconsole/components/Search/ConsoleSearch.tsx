7e10bcb1a1b79cd64d5ebb74ed2fdf0633aafa1f
Mark Erikson <mark@isquaredsoftware.com>
2022-08-24 16:20:45 -0400

    Migrate SearchInput content to TS

--- src/devtools/client/webconsole/components/Search/ConsoleSearch.tsx
+++ src/devtools/client/webconsole/components/Search/ConsoleSearch.tsx
@@ -73,7 +73,7 @@ export default function ConsoleSearch({}: Props) {
         onKeyDown={onKeyDown}
         placeholder="Find string in logs"
         query={query}
-        summaryMsg={summaryMsg}
+        summaryMsg={summaryMsg || ""}
       />
     </div>
   );
