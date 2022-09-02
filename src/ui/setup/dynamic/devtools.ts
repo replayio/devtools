953b14f39c2beb2bf595fc7cf61dfa04ba0771bc
Jon Bell <github@lot23.com>
2022-09-03 11:31:26 +1200

    Better error message

--- src/ui/setup/dynamic/devtools.ts
+++ src/ui/setup/dynamic/devtools.ts
@@ -116,7 +116,7 @@ const SessionErrorMessages: Record<number, Partial<UnexpectedError>> = {
   },
   [SessionError.OldBuild]: {
     content:
-      "This error has been fixed in an updated version of Replay. Please try upgrading Replay and trying a new recording.",
+      "This recording is no longer available because we have updated Replay. We apologise for the inconvenience.",
   },
   [SessionError.LongRecording]: {
     content: "You’ve hit an error that happens with long recordings. Can you try a shorter one?",
