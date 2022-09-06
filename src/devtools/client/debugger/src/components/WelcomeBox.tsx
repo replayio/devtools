e49e3fa3d5e8428573933cd90bbf25946c85bea5
Jon Bell <github@lot23.com>
2022-09-07 08:47:54 +1200

    Draft commit

--- src/devtools/client/debugger/src/components/WelcomeBox.tsx
+++ src/devtools/client/debugger/src/components/WelcomeBox.tsx
@@ -7,10 +7,31 @@ import CommandPalette from "ui/components/CommandPalette";
 
 export default function WelcomeBox() {
   return (
-    <div className="flex h-full w-full flex-col items-center overflow-hidden">
-      <div className="relative flex h-full w-full justify-center px-8 pt-14">
-        <div className="relative flex h-full w-full flex-col items-center">
-          <CommandPalette />
+    <div className="flex flex-col items-center w-full h-full overflow-hidden">
+      <div className="relative flex justify-center w-full h-full px-8 pt-14">
+        <div className="relative flex flex-col items-center w-full h-full text-lg">
+          
+
+        
+          <div className="flex flex-row space-x-4 text-sm">
+            
+            <div>
+              <div className="pb-3 text-right">Command palette</div>
+              <div className="pb-3 text-right">Go to file</div>
+              <div className="pb-3 text-right">Find in file</div>
+            </div>
+
+            <div>
+              <div className="flex flex-row pb-1.5"><img src="/images/command.svg" /> <img src="/images/k.svg" /></div>
+              <div className="flex flex-row pb-1.5"><img src="/images/command.svg" /> <img src="/images/p.svg" /></div>
+              <div className="flex flex-row pb-1.5"><img src="/images/command.svg" /> <img src="/images/shift.svg" /> <img src="/images/f.svg" /></div>
+            </div>
+
+          </div>
+          
+
+
+
         </div>
       </div>
     </div>
