ffcdf7fe39d746bea89bbf9cd21d474fd20b9536
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-09-13 15:43:38 -0400

    Optimized PointsCache to avoid making unnecessary protocol requests
    Switched cache from using getPointNearTime to getPointsBoundingTime
    
    Also added unit tests

--- packages/bvaughn-architecture-demo/src/suspense/PointsCache.ts
+++ packages/bvaughn-architecture-demo/src/suspense/PointsCache.ts
@@ -1,5 +1,6 @@
 import {
   ExecutionPoint,
+  getPointsBoundingTimeResult as PointsBoundingTime,
   Location,
   TimeStampedPoint,
   TimeStampedPointRange,
@@ -12,89 +13,84 @@ import { isExecutionPointsLessThan } from "../utils/time";
 
 import { Record, STATUS_PENDING, STATUS_REJECTED, STATUS_RESOLVED, Wakeable } from "./types";
 
+const cachedPointsForTime: Map<number, ExecutionPoint> = new Map();
 const locationToHitPointsMap: Map<string, Record<HitPointsAndStatusTuple>> = new Map();
 const sortedExecutionPoints: TimeStampedPoint[] = [];
-const timeToExecutionPointMap: Map<number, Record<ExecutionPoint>> = new Map();
+const sortedPointsBoundingTimes: PointsBoundingTime[] = [];
+const timeToInFlightRequestMap: Map<number, Wakeable<ExecutionPoint>> = new Map();
+const timeToErrorMap: Map<number, any> = new Map();
 
-export function getClosestPointForTime(
-  client: ReplayClientInterface,
-  time: number
-): ExecutionPoint {
-  let record = timeToExecutionPointMap.get(time);
-  if (record == null) {
-    const wakeable = createWakeable<ExecutionPoint>();
-
-    record = {
-      status: STATUS_PENDING,
-      value: wakeable,
-    };
-
-    timeToExecutionPointMap.set(time, record);
-
-    fetchClosestPointForTime(client, time, record, wakeable);
-  }
-
-  if (record.status === STATUS_RESOLVED) {
-    return record.value;
-  } else {
-    throw record.value;
-  }
-}
-
-export function getHitPointsForLocation(
-  client: ReplayClientInterface,
-  location: Location,
-  condition: string | null,
-  focusRange: TimeStampedPointRange | null
-): HitPointsAndStatusTuple {
-  // TODO We could add an optimization here to avoid re-fetching if we ever fetched all points (no focus range)
-  // without any overflow, and then later fetch for a focus range. Right now we re-fetch in this case.
-
-  const locationKey = `${location.sourceId}:${location.line}:${location.column}:${condition}`;
-  const key = focusRange
-    ? `${locationKey}:${focusRange.begin.point}:${focusRange.end.point}`
-    : locationKey;
-  let record = locationToHitPointsMap.get(key);
-  if (record == null) {
-    const wakeable = createWakeable<HitPointsAndStatusTuple>();
-
-    record = {
-      status: STATUS_PENDING,
-      value: wakeable,
-    };
-
-    locationToHitPointsMap.set(key, record);
-
-    fetchHitPointsForLocation(client, focusRange, location, condition, record, wakeable);
-  }
-
-  if (record.status === STATUS_RESOLVED) {
-    return record.value;
-  } else {
-    throw record.value;
-  }
-}
-
-async function fetchClosestPointForTime(
+async function fetchPointsBoundingTime(
   client: ReplayClientInterface,
   time: number,
-  record: Record<ExecutionPoint>,
-  wakeable: Wakeable<ExecutionPoint>
+  wakeable: Wakeable<ExecutionPoint>,
+  rethrowError: boolean
 ) {
   try {
-    const { point } = await client.getPointNearTime(time);
+    const pointsBoundingTime = await client.getPointsBoundingTime(time);
+    const point = getClosestPointInPointsBoundingTime(time, pointsBoundingTime);
 
-    record.status = STATUS_RESOLVED;
-    record.value = point;
-
+    // Pre-cache time-to-point match and insert into sorted ExecutionPoints array.
     preCacheExecutionPointForTime({ point, time });
+
+    // Also insert bounding points into sorted array; ignore duplicates!
+    if (sortedPointsBoundingTimes.length === 0) {
+      sortedPointsBoundingTimes.push(pointsBoundingTime);
+    } else {
+      const beforePoint = pointsBoundingTime.before.point;
+      const afterPoint = pointsBoundingTime.after.point;
+
+      let indexBegin = 0;
+      let indexEnd = sortedPointsBoundingTimes.length - 1;
+      let indexMiddle = 0;
+
+      while (indexBegin <= indexEnd) {
+        indexMiddle = Math.floor((indexBegin + indexEnd) / 2);
+
+        const currentPointsBoundingTime = sortedPointsBoundingTimes[indexMiddle];
+
+        if (
+          currentPointsBoundingTime.before.point === beforePoint &&
+          currentPointsBoundingTime.after.point === afterPoint
+        ) {
+          // Session.getPointsBoundingTime should never return overlapping ranges that are not exact matches.
+          // And we can skip adding add duplicate ranges.
+          // These shouldn't be common, but if two times within the same range are requested in parallel, we might see this.
+          indexMiddle = -1;
+          break;
+        } else if (currentPointsBoundingTime.after.point < beforePoint) {
+          indexBegin = indexMiddle + 1;
+        } else {
+          indexEnd = indexMiddle - 1;
+        }
+      }
+
+      if (indexMiddle >= 0) {
+        const closestPointsBoundingTime = sortedPointsBoundingTimes[indexMiddle];
+        if (pointsBoundingTime.before.point < closestPointsBoundingTime.after.point) {
+          sortedPointsBoundingTimes.splice(indexMiddle, 0, pointsBoundingTime);
+        } else {
+          sortedPointsBoundingTimes.splice(indexMiddle + 1, 0, pointsBoundingTime);
+        }
+      }
+    }
 
     wakeable.resolve(point);
   } catch (error) {
-    record.status = STATUS_REJECTED;
-    record.value = error;
+    if (rethrowError) {
+      throw error;
+    } else {
+      // TODO
+      // Note that we might be caching unloaded region errors by doing this.
+      // That's better than flooding the backend with repeated requests,
+      // but we also probably don't want to permanently cache this type of error.
+      // Maybe we should somehow make loaded regions part of the cache key?
+      timeToErrorMap.set(time, error);
+    }
 
     wakeable.reject(error);
+  } finally {
+    timeToInFlightRequestMap.delete(time);
   }
 }
 
@@ -125,44 +121,27 @@ async function fetchHitPointsForLocation(
   }
 }
 
-// Note that this method does not work with Suspense.
-// Use getClosestPointForTime() instead.
-export async function imperativelyGetClosestPointForTime(
-  client: ReplayClientInterface,
-  time: number
-): Promise<ExecutionPoint> {
-  // If we already have a match for this time, use it.
-  const record = timeToExecutionPointMap.get(time);
-  if (record?.status === STATUS_RESOLVED) {
-    return record.value;
-  }
+function findCachePointsForTime(time: number): ExecutionPoint | null {
+  let indexBegin = 0;
+  let indexEnd = sortedPointsBoundingTimes.length - 1;
 
-  // Next try asking the backend for a match and cache it.
-  // Note that the backend may throw an error if this time isn't within a loaded region.
-  try {
-    const { point } = await client.getPointNearTime(time);
+  while (indexBegin <= indexEnd) {
+    let indexMiddle = Math.floor((indexBegin + indexEnd) / 2);
+    const pointsBoundingTime = sortedPointsBoundingTimes[indexMiddle];
 
-    preCacheExecutionPointForTime({ point, time });
-
-    return point;
-  } catch (error) {
-    if (!isUnloadedRegionError(error)) {
-      throw error;
+    if (pointsBoundingTime.after.time < time) {
+      indexBegin = indexMiddle + 1;
+    } else if (pointsBoundingTime.before.time > time) {
+      indexEnd = indexMiddle - 1;
+    } else {
+      return getClosestPointInPointsBoundingTime(time, pointsBoundingTime);
     }
   }
 
-  // If we can't query the backend, fallback to the in-memory cache.
-  const index = getNearestCachedIndex(time);
-  if (index >= 0) {
-    return sortedExecutionPoints[index].point;
-  }
-
-  // If our in-memory cache is empty it probably means we haven't yet loaded any regions,
-  // in which case the best we can do is fall back to point "0".
-  return "0";
+  return null;
 }
 
-function getNearestCachedIndex(time: number): number {
+function findNearestCachedSortedExecutionPointIndex(time: number): number {
   if (sortedExecutionPoints.length === 0) {
     return -1;
   }
@@ -196,13 +175,138 @@ function getNearestCachedIndex(time: number): number {
   }
 }
 
+export function getClosestPointForTime(
+  client: ReplayClientInterface,
+  time: number
+): ExecutionPoint {
+  // If we have already fetched a point range that contains this time, we don't need to re-ask.
+  const cachedPoint = cachedPointsForTime.get(time);
+  if (cachedPoint != null) {
+    return cachedPoint;
+  } else {
+    const executionPoint = findCachePointsForTime(time);
+    if (executionPoint !== null) {
+      cachedPointsForTime.set(time, executionPoint);
+      return executionPoint;
+    }
+  }
+
+  // If this request has failed before, don't get caught in a loop of re-asking.
+  const error = timeToErrorMap.get(time);
+  if (error != null) {
+    throw error;
+  }
+
+  // Otherwise let's fetch the closest points for this time.
+  let wakeable = timeToInFlightRequestMap.get(time);
+  if (wakeable == null) {
+    wakeable = createWakeable<ExecutionPoint>();
+
+    timeToInFlightRequestMap.set(time, wakeable);
+
+    // Fire and forget for the purposes of Suspense.
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchPointsBoundingTime(client, time, wakeable, false);
+  }
+
+  throw wakeable;
+}
+
+function getClosestPointInPointsBoundingTime(
+  time: number,
+  pointsBoundingTime: PointsBoundingTime
+): ExecutionPoint {
+  return Math.abs(pointsBoundingTime.before.time - time) <
+    Math.abs(pointsBoundingTime.after.time - time)
+    ? pointsBoundingTime.before.point
+    : pointsBoundingTime.after.point;
+}
+
+export function getHitPointsForLocation(
+  client: ReplayClientInterface,
+  location: Location,
+  condition: string | null,
+  focusRange: TimeStampedPointRange | null
+): HitPointsAndStatusTuple {
+  // TODO We could add an optimization here to avoid re-fetching if we ever fetched all points (no focus range)
+  // without any overflow, and then later fetch for a focus range. Right now we re-fetch in this case.
+
+  const locationKey = `${location.sourceId}:${location.line}:${location.column}:${condition}`;
+  const key = focusRange
+    ? `${locationKey}:${focusRange.begin.point}:${focusRange.end.point}`
+    : locationKey;
+  let record = locationToHitPointsMap.get(key);
+  if (record == null) {
+    const wakeable = createWakeable<HitPointsAndStatusTuple>();
+
+    record = {
+      status: STATUS_PENDING,
+      value: wakeable,
+    };
+
+    locationToHitPointsMap.set(key, record);
+
+    // Fire and forget for the purposes of Suspense.
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchHitPointsForLocation(client, focusRange, location, condition, record, wakeable);
+  }
+
+  if (record.status === STATUS_RESOLVED) {
+    return record.value;
+  } else {
+    throw record.value;
+  }
+}
+
+// Note that this method does not work with Suspense.
+// Use getClosestPointForTime() instead.
+export async function imperativelyGetClosestPointForTime(
+  client: ReplayClientInterface,
+  time: number
+): Promise<ExecutionPoint> {
+  // If we have already fetched a point range that contains this time, we don't need to re-ask.
+  const cachedPoint = cachedPointsForTime.get(time);
+  if (cachedPoint != null) {
+    return cachedPoint;
+  } else {
+    const executionPoint = findCachePointsForTime(time);
+    if (executionPoint !== null) {
+      cachedPointsForTime.set(time, executionPoint);
+      return executionPoint;
+    }
+  }
+
+  // Next try asking the backend for a match and cache it.
+  // Note that the backend may throw an error if this time isn't within a loaded region.
+  try {
+    await fetchPointsBoundingTime(client, time, createWakeable<ExecutionPoint>(), true);
+    return cachedPointsForTime.get(time)!;
+  } catch (error) {
+    if (!isUnloadedRegionError(error)) {
+      throw error;
+    }
+  }
+
+  // If we can't query the backend, fallback to the in-memory cache.
+  const index = findNearestCachedSortedExecutionPointIndex(time);
+  if (index >= 0) {
+    return sortedExecutionPoints[index].point;
+  }
+
+  // If our in-memory cache is empty it probably means we haven't yet loaded any regions,
+  // in which case the best we can do is fall back to point "0".
+  return "0";
+}
+
 export function preCacheExecutionPointForTime(timeStampedPoint: TimeStampedPoint): void {
   const { point, time } = timeStampedPoint;
+
+  cachedPointsForTime.set(time, point);
 
   if (sortedExecutionPoints.length === 0) {
     sortedExecutionPoints.push(timeStampedPoint);
   } else {
-    const index = getNearestCachedIndex(time);
+    const index = findNearestCachedSortedExecutionPointIndex(time);
     const nearest = sortedExecutionPoints[index];
 
     if (isExecutionPointsLessThan(point, nearest.point)) {
@@ -210,10 +314,5 @@ export function preCacheExecutionPointForTime(timeStampedPoint: TimeStampedPoint
     } else {
       sortedExecutionPoints.splice(index + 1, 0, timeStampedPoint);
     }
-  }
-
-  // Also pre-cache for Suspense.
-  if (!timeToExecutionPointMap.has(time)) {
-    timeToExecutionPointMap.set(time, { status: STATUS_RESOLVED, value: point });
   }
 }
