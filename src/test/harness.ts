0bbcf3e6f773baa223098bb84f124a0b55d33757
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-09-26 18:40:05 -0400

    Revert "Don't suppress e2e test failures (FE-772)"
    This reverts commit 22241aa8f511840fa7cbd511ba62292ab34bd80e.

--- src/test/harness.ts
+++ src/test/harness.ts
@@ -1016,11 +1016,16 @@ const commands = mapValues(testCommands, (command, name) => {
     // @ts-expect-error spreading args is FINE TS!
     let result = command(...args);
     if (isThenable(result)) {
-      return result.then(async result => {
-        const duration = new Date().getTime() - startTime;
-        console.log(`Finished ${name} in ${duration}ms`);
-        return result;
-      });
+      return result.then(
+        async result => {
+          const duration = new Date().getTime() - startTime;
+          console.log(`Finished ${name} in ${duration}ms`);
+          return result;
+        },
+        error => {
+          console.error(`Error for ${name}`, error);
+        }
+      );
     }
     const duration = new Date().getTime() - startTime;
     console.log(`Finished ${name} in ${duration}ms`);
