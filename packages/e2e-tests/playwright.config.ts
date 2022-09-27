0f3bc4d5570328aa697f04b8570ff15ce0c77cec
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-09-27 13:42:56 -0400

    Fix a few selectors

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -2,23 +2,13 @@ import { PlaywrightTestConfig, devices } from "@playwright/test";
 import { devices as replayDevices } from "@replayio/playwright";
 
 const config: PlaywrightTestConfig = {
-  projects: [
-    // {
-    //   name: "replay-firefox",
-    //   use: { ...(replayDevices["Replay Firefox"] as any) },
-    // },
-    // {
-    //   name: "replay-chromium",
-    //   use: { ...(replayDevices["Replay Chromium"] as any) },
-    // },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
+  use: {
+    browserName: "chromium",
+    viewport: {
+      width: 1024,
+      height: 600,
     },
-    // {
-    //   name: "chromium",
-    //   use: { ...devices["Desktop Chromium"] },
-    // },
-  ],
+  },
 };
+
 export default config;
