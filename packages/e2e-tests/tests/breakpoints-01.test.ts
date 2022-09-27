199be66641514bc3324fe828dd46d555b54642ef
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-09-27 16:43:37 -0400

    Got breakpoints-01.test passing (#7817)

--- packages/e2e-tests/tests/breakpoints-01.test.ts
+++ packages/e2e-tests/tests/breakpoints-01.test.ts
@@ -14,22 +14,24 @@ test(`Test basic breakpoint functionality.`, async ({ screen }) => {
 
   await addBreakpoint(screen, "doc_rr_basic.html", 21);
 
-  await rewindToLine(screen, 21);
+  await rewindToLine(screen, {
+    lineNumber: 21,
+  });
   await checkEvaluateInTopFrame(screen, "number", "10");
-  await rewindToLine(screen, 21);
+  await rewindToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "9");
-  await rewindToLine(screen, 21);
+  await rewindToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "8");
-  await rewindToLine(screen, 21);
+  await rewindToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "7");
-  await rewindToLine(screen, 21);
+  await rewindToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "6");
-  await resumeToLine(screen, 21);
+  await resumeToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "7");
-  await resumeToLine(screen, 21);
+  await resumeToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "8");
-  await resumeToLine(screen, 21);
+  await resumeToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "9");
-  await resumeToLine(screen, 21);
+  await resumeToLine(screen, { lineNumber: 21 });
   await checkEvaluateInTopFrame(screen, "number", "10");
 });
