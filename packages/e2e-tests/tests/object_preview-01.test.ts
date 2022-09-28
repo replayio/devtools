1353757829b03f12b8786529ca38b2b6a7599b79
Travis Arnold <travis@replay.io>
2022-09-28 11:41:17 -0700

    update remaining executeInConsole tests

--- packages/e2e-tests/tests/object_preview-01.test.ts
+++ packages/e2e-tests/tests/object_preview-01.test.ts
@@ -53,7 +53,7 @@ test(`expressions in the console after time warping.`, async ({ screen }) => {
   // // TODO: add support for expanding objects in the console
   // await checkMessageObjectContents(
   //   screen,
-  //   msg,
+  //   message,
   //   ['foo: C { baz: "baz" }', 'bar: "bar"', 'baz: "baz"'],
   //   ["foo", "bar"]
   // );
@@ -63,81 +63,73 @@ test(`expressions in the console after time warping.`, async ({ screen }) => {
   await executeInConsole(screen, "Error('helo')");
   await waitForConsoleMessage(screen, "Error: helo");
 
-  // await executeInConsole(
-  //   screen,
-  //   `
-  //       function f() {
-  //         throw Error("there");
-  //       }
-  //       f();
-  //     `
-  // );
-  // // FIXME the first line in this stack isn't right.
-  // await waitForConsoleMessage(screen, 'Error: "there"');
+  await executeInConsole(screen, 'function f() { throw Error("there"); }()');
+  // FIXME the first line in this stack isn't right.
+  await waitForConsoleMessage(screen, "Error: there");
 
-  // executeInConsole(screen, "Array(1, 2, 3)");
-  // msg = await waitForConsoleMessage(screen, "Array(3) [ 1, 2, 3 ]");
-  // await checkMessageObjectContents(screen, msg, ["0: 1", "1: 2", "2: 3", "length: 3"]);
+  await executeInConsole(screen, "Array(1, 2, 3)");
+  message = await waitForConsoleMessage(screen, "(3) [1, 2, 3]");
+  // await checkMessageObjectContents(screen, message, ["0: 1", "1: 2", "2: 3", "length: 3"]);
 
-  // await executeInConsole(screen, "new Uint8Array([1, 2, 3, 4])");
-  // msg = await waitForConsoleMessage(screen, "Uint8Array(4) [ 1, 2, 3, 4 ]");
-  // await checkMessageObjectContents(screen, msg, ["0: 1", "1: 2", "2: 3", "3: 4", "length: 4"]);
+  await executeInConsole(screen, "new Uint8Array([1, 2, 3, 4])");
+  message = await waitForConsoleMessage(screen, "Uint8Array(4) [1, 2, 3, 4]");
+  // await checkMessageObjectContents(screen, message, ["0: 1", "1: 2", "2: 3", "3: 4", "length: 4"]);
 
-  // await executeInConsole(screen, `RegExp("abd", "g")`);
-  // msg = await waitForConsoleMessage(screen, "/abd/g");
+  await executeInConsole(screen, `RegExp("abd", "g")`);
+  message = await waitForConsoleMessage(screen, "/abd/g");
 
-  // // RegExp object properties are not currently available in chromium.
+  // RegExp object properties are not currently available in chromium.
 
-  // await checkMessageObjectContents(screen, msg, ["global: true", `source: "abd"`]);
+  // await checkMessageObjectContents(screen, message, ["global: true", `source: "abd"`]);
 
-  // await executeInConsole(screen, "new Set([1, 2, 3])");
-  // msg = await waitForConsoleMessage(screen, "Set(3) [ 1, 2, 3 ]");
-  // await checkMessageObjectContents(screen, msg, ["0: 1", "1: 2", "2: 3", "size: 3"], ["<entries>"]);
+  await executeInConsole(screen, "new Set([1, 2, 3])");
+  message = await waitForConsoleMessage(screen, "Set(3) [1, 2, 3]");
+  // await checkMessageObjectContents(screen, message, ["0: 1", "1: 2", "2: 3", "size: 3"], ["<entries>"]);
 
-  // await executeInConsole(screen, "new Map([[1, {a:1}], [2, {b:2}]])");
-  // msg = await waitForConsoleMessage(screen, "Map { 1 → {…}, 2 → {…} }");
+  await executeInConsole(screen, "new Map([[1, {a:1}], [2, {b:2}]])");
+  message = await waitForConsoleMessage(screen, "Map(2) {1 → {…}, 2 → {…}}");
   // await checkMessageObjectContents(
   //   screen,
-  //   msg,
+  //   message,
   //   ["0: 1 → Object { a: 1 }", "1: 2 → Object { b: 2 }", "size: 2"],
   //   ["<entries>"]
   // );
 
-  // await executeInConsole(screen, "new WeakSet([{a:1}, {b:2}])");
-  // msg = await waitForConsoleMessage(screen, "WeakSet(2) [ {…}, {…} ]");
+  await executeInConsole(screen, "new WeakSet([{a:1}, {b:2}])");
+  message = await waitForConsoleMessage(screen, "WeakSet(2) [{…}, {…}]");
   // await checkMessageObjectContents(
   //   screen,
-  //   msg,
+  //   message,
   //   ["Object { a: 1 }", "Object { b: 2 }"],
   //   ["<entries>"]
   // );
 
-  // await executeInConsole(screen, "new WeakMap([[{a:1},{b:1}], [{a:2},{b:2}]])");
-  // msg = await waitForConsoleMessage(screen, "WeakMap { {…} → {…}, {…} → {…} }");
+  await executeInConsole(screen, "new WeakMap([[{a:1},{b:1}], [{a:2},{b:2}]])");
+  message = await waitForConsoleMessage(screen, "WeakMap(2) {{…} → {…}, {…} → {…}}");
   // await checkMessageObjectContents(
   //   screen,
-  //   msg,
+  //   message,
   //   ["Object { a: 1 } → Object { b: 1 }", "Object { a: 2 } → Object { b: 2 }"],
   //   ["<entries>"]
   // );
 
-  // await executeInConsole(screen, "new Promise(() => {})");
-  // msg = await waitForConsoleMessage(screen, "Promise {  }");
+  await executeInConsole(screen, "new Promise(() => {})");
+  message = await waitForConsoleMessage(screen, "Promise{}");
 
-  // // Promise contents aren't currently available in chromium.
+  // Promise contents aren't currently available in chromium.
 
-  // await checkMessageObjectContents(screen, msg, ['"pending"'], []);
+  // await checkMessageObjectContents(screen, message, ['"pending"'], []);
 
-  // await executeInConsole(screen, "Promise.resolve({ a: 1 })");
-  // msg = await waitForConsoleMessage(screen, "Promise {  }");
+  await executeInConsole(screen, "Promise.resolve({ a: 1 })");
+  message = await waitForConsoleMessage(screen, "Promise{}");
 
-  // await checkMessageObjectContents(screen, msg, ['"fulfilled"', "a: 1"], ["<value>"]);
+  // await checkMessageObjectContents(screen, message, ['"fulfilled"', "a: 1"], ["<value>"]);
 
-  // await executeInConsole(screen, "Promise.reject({ a: 1 })");
-  // msg = await waitForConsoleMessage(screen, "Promise {  }");
+  await executeInConsole(screen, "Promise.reject({ a: 1 })");
+  message = await waitForConsoleMessage(screen, "Promise{}");
 
-  // await executeInConsole(screen, "baz");
-  // msg = await waitForConsoleMessage(screen, "function baz()");
-  // const text = await msg.textContent();
+  await executeInConsole(screen, "baz");
+  message = await waitForConsoleMessage(screen, "ƒbaz()");
+  // const text = await message.textContent();
   // checkJumpIcon(screen, text!);
 });
