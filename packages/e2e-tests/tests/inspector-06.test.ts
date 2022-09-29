a6170cc33484facb9680dac08b1ee8c05cb4df8b
Holger Benl <hbenl@evandor.de>
2022-09-29 17:25:30 +0200

    Migrate remaining inspector tests

---
+++ packages/e2e-tests/tests/inspector-06.test.ts
@@ -0,0 +1,65 @@
+import test from "@playwright/test";
+
+import { startTest, openDevToolsTab } from "../helpers";
+import { openConsolePanel, warpToMessage } from "../helpers/console-panel";
+import {
+  activateInspectorTool,
+  checkAppliedRules,
+  expandLonghands,
+  selectElementsRowWithText,
+} from "../helpers/elements-panel";
+
+test("Test that styles for elements can be viewed.", async ({ page }) => {
+  await startTest(page, "doc_inspector_shorthand.html");
+  await openDevToolsTab(page);
+  await openConsolePanel(page);
+  await warpToMessage(page, "ExampleFinished");
+  await activateInspectorTool(page);
+
+  await selectElementsRowWithText(page, '<div id="first" class="parent">');
+  await checkAppliedRules(page, [
+    {
+      selector: ".parent",
+      source: "shorthand_styles.css:1",
+      properties: [
+        { text: "margin-left: 10px;", overridden: false },
+        { text: "font: italic small-caps bold 24px courier;", overridden: false },
+      ],
+    },
+  ]);
+
+  await expandLonghands(page);
+  await checkAppliedRules(page, [
+    {
+      selector: ".parent",
+      source: "shorthand_styles.css:1",
+      properties: [
+        { text: "margin-left: 10px;", overridden: false },
+        {
+          text: "font: italic small-caps bold 24px courier;",
+          overridden: false,
+          longhandProps: [
+            { text: "font-style: italic;", overridden: false },
+            { text: "font-variant-caps: small-caps;", overridden: false },
+            { text: "font-weight: bold;", overridden: false },
+            { text: "font-stretch: normal;", overridden: false },
+            { text: "font-size: 24px;", overridden: false },
+            { text: "line-height: normal;", overridden: false },
+            { text: "font-family: courier;", overridden: false },
+            { text: "font-size-adjust: none;", overridden: false },
+            { text: "font-kerning: auto;", overridden: false },
+            { text: "font-optical-sizing: auto;", overridden: false },
+            { text: "font-variant-alternates: normal;", overridden: false },
+            { text: "font-variant-east-asian: normal;", overridden: false },
+            { text: "font-variant-ligatures: normal;", overridden: false },
+            { text: "font-variant-numeric: normal;", overridden: false },
+            { text: "font-variant-position: normal;", overridden: false },
+            { text: "font-language-override: normal;", overridden: false },
+            { text: "font-feature-settings: normal;", overridden: false },
+            { text: "font-variation-settings: normal;", overridden: false },
+          ],
+        },
+      ],
+    },
+  ]);
+});
