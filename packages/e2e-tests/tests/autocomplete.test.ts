a5cd73e11ef9c235c190be23aaa5245090ae00fe
Travis Arnold <travis@replay.io>
2022-09-29 17:41:16 -0700

    port remaining tests

--- packages/e2e-tests/tests/autocomplete.test.ts
+++ packages/e2e-tests/tests/autocomplete.test.ts
@@ -11,19 +11,63 @@ test(`Test basic breakpoint functionality.`, async ({ page }) => {
   await openConsolePanel(page);
   await warpToMessage(page, "Done");
 
+  const objectProperties = Object.getOwnPropertyNames(Object.prototype);
   const textArea = await focusConsoleTextArea(page);
 
+  // show all properties (including getters) of an object in the current scope
   textArea.fill("r.");
 
   await page.evaluate(() => {
     window.jsterm.showAutocomplete!(true);
   });
 
-  const objectProperties = Object.getOwnPropertyNames(Object.prototype);
-
   await checkAutocompleteMatches(page, objectProperties.concat(["_foo", "foo"]));
 
-  textArea.fill("foo.ba");
+  clearTextArea(page, textArea);
+
+  // show all properties (including getters) of a nested object
+  textArea.fill("r.foo.");
+
+  await checkAutocompleteMatches(page, ["bar", "baz", "constructor"]);
+
+  clearTextArea(page, textArea);
+
+  // show matching properties (including getters) of a nested object
+  textArea.fill("r.foo.ba");
 
   await checkAutocompleteMatches(page, ["bar", "baz"]);
+
+  clearTextArea(page, textArea);
+
+  // show all properties (including getters) of a nested object
+  // using bracket notation without a quotation mark
+  textArea.fill("r.foo[");
+
+  await checkAutocompleteMatches(page, ["bar", "baz", "constructor"]);
+
+  // clear the autocomplete matches before the next test
+  // because it expects the same matches as the previous one
+  clearTextArea(page, textArea);
+
+  await page.evaluate(() => window.jsterm.showAutocomplete!(true));
+
+  await checkAutocompleteMatches(page, []);
+
+  // show matching properties (including getters) of a nested object
+  // using bracket notation with a double quotation mark
+  textArea.fill('r.foo["ba');
+
+  await page.evaluate(() => window.jsterm.showAutocomplete!(true));
+
+  await checkAutocompleteMatches(page, ["bar", "baz"]);
+
+  clearTextArea(page, textArea);
+
+  // show all properties (including getters) of a nested object
+  // using bracket notation with a single quotation mark
+  textArea.fill("r.foo[");
+
+  await page.evaluate(() => window.jsterm.showAutocomplete!(true));
+
+  await checkAutocompleteMatches(page, ["bar", "baz", "constructor"]);
 });
