821bcec593e3610a1d649613a2abd96e579888b8
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-09-30 18:52:51 -0400

    Reworte console_warp-01.test

--- packages/e2e-tests/tests/console_warp-01.test.ts
+++ packages/e2e-tests/tests/console_warp-01.test.ts
@@ -1,36 +1,54 @@
-Test.describe(`Test basic console time warping functionality.`, async () => {
-  await Test.selectConsole();
-  await Test.warpToMessage("Number 5");
+import test from "@playwright/test";
 
-  await Test.checkEvaluateInTopFrame("number", 5);
+import { getRecordingTarget, openDevToolsTab, startTest } from "../helpers";
+import {
+  executeAndVerifyTerminalExpression,
+  openConsolePanel,
+  warpToMessage,
+} from "../helpers/console-panel";
+import {
+  resumeToLine,
+  reverseStepOverToLine,
+  rewindToLine,
+} from "../helpers/pause-information-panel";
+import { addBreakpoint } from "../helpers/source-panel";
 
-  const target = await Test.getRecordingTarget();
+const url = "doc_rr_error.html";
+
+test(`should support warping to console messages.`, async ({ page }) => {
+  await startTest(page, url);
+  await openDevToolsTab(page);
+  await openConsolePanel(page);
+
+  await warpToMessage(page, "Number 5", 19);
+  await executeAndVerifyTerminalExpression(page, "number", 5);
+
+  const target = await getRecordingTarget(page);
   if (target == "gecko") {
     // Initially we are paused inside the 'new Error()' call on line 19. The
     // first reverse step takes us to the start of that line.
-    await Test.reverseStepOverToLine(19);
+    await reverseStepOverToLine(page, 19);
   }
 
-  await Test.reverseStepOverToLine(18);
-  await Test.addBreakpoint("doc_rr_error.html", 12);
-  await Test.rewindToLine(12);
-  await Test.checkEvaluateInTopFrame("number", 4);
-  await Test.resumeToLine(12);
-  await Test.checkEvaluateInTopFrame("number", 5);
+  await reverseStepOverToLine(page, 18);
+  await addBreakpoint(page, { lineNumber: 12, url });
+  await rewindToLine(page, { lineNumber: 12, url });
+  await executeAndVerifyTerminalExpression(page, "number", 4);
+  await resumeToLine(page, { lineNumber: 12, url });
+  await executeAndVerifyTerminalExpression(page, "number", 5);
 
   // This error message has different text on gecko vs. chromium.
-  const errorText = (target == "gecko")
-    ? "window.foo is undefined"
-    : "Cannot set property 'bar' of undefined";
-  await Test.warpToMessage(errorText);
-  await Test.reverseStepOverToLine(7);
+  const errorText =
+    target == "gecko" ? "window.foo is undefined" : "Cannot set property 'bar' of undefined";
+  await warpToMessage(page, errorText);
+  await reverseStepOverToLine(page, 8);
 
-  await Test.warpToMessage("superclass");
+  await warpToMessage(page, "superclass", 40);
 
   if (target == "gecko") {
     // As above, we need an additional reverse step over in gecko.
-    await Test.reverseStepOverToLine(40);
+    await reverseStepOverToLine(page, 40);
   }
 
-  await Test.reverseStepOverToLine(39);
+  await reverseStepOverToLine(page, 39);
 });
