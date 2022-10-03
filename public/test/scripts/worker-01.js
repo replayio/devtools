bba2fc815e26eeff604d01d89a6b9f0588b13afb
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-10-03 10:48:34 -0400

    Deleted a batch of tests that have already been migrated

--- public/test/scripts/worker-01.js
+++
@@ -1,5 +0,0 @@
-// Test that workers function when recording/replaying. For now workers can't
-Test.describe(`be inspected, so we're making sure that nothing crashes.`, async () => {
-  await Test.addBreakpoint("doc_rr_worker.html", 15);
-  await Test.rewindToLine(15);
-});
