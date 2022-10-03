800bb3424e6d363597a00e4e4766718f9976d8aa
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-10-03 10:18:30 -0400

    Revert "Fix CI e2e tests"
    This reverts commit f06a3f972496c216744e17eb79b463670171120f.

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -1,12 +1,11 @@
 import { PlaywrightTestConfig, devices } from "@playwright/test";
-import { devices as replayDevices } from "@replayio/playwright";
 
 const { DEBUG, SLOW_MO } = process.env;
 
 const config: PlaywrightTestConfig = {
   use: {
     headless: !DEBUG,
-    // browserName: "chromium",
+    browserName: "chromium",
     launchOptions: {
       slowMo: SLOW_MO ? parseInt(SLOW_MO, 10) : 0,
     },
@@ -15,24 +14,6 @@ const config: PlaywrightTestConfig = {
       height: 1024,
     },
   },
-  projects: [
-    {
-      name: "replay-firefox",
-      use: { ...replayDevices["Replay Firefox"] as any },
-    },
-    {
-      name: "replay-chromium",
-      use: { ...replayDevices["Replay Chromium"] as any },
-    },
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chromium"] },
-    },
-  ],
 };
 
 if (DEBUG) {
