9495fc7fc0bf3caaa6a88e6cb744f9a7b052b778
Mark Erikson <mark@isquaredsoftware.com>
2022-10-03 13:11:58 -0400

    Remove dead node test files

--- public/test/scripts/node_napi.js
+++
@@ -1,7 +0,0 @@
-Test.describe(`loading napi modules works.`, async () => {
-  await Test.selectConsole();
-  await Test.warpToMessage("true");
-  await Test.warpToMessage("iVBORw0KGgoAAAANSUhEUgAAAAQAAAAFCAYAAA");
-  await Test.warpToMessage("Unmasked [4,4,4,12]");
-  await Test.warpToMessage("Remasked [1,2,3,4]");
-});
