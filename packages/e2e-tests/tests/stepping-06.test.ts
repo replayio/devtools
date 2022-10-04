9d0781d88bd00c5437cb33f7e67ab0b7d65dfaad
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-10-03 22:16:48 -0400

    Combined stepping-06 tests (#7895)

--- packages/e2e-tests/tests/stepping-06.test.ts
+++ packages/e2e-tests/tests/stepping-06.test.ts
@@ -1,6 +1,6 @@
 import test from "@playwright/test";
 
-import { openDevToolsTab, startTest } from "../helpers";
+import { getRecordingTarget, openDevToolsTab, startTest } from "../helpers";
 import { executeAndVerifyTerminalExpression, warpToMessage } from "../helpers/console-panel";
 import {
   reverseStepOverToLine,
@@ -14,33 +14,29 @@ import {
 
 const url = "doc_async.html";
 
+// Because stepping works differently between gecko and chromium,
+// frame timeline percentages are different in this test.
 test(`Test stepping in async frames and async call stacks.`, async ({ page }) => {
+  const target = await getRecordingTarget(page);
+
   await startTest(page, url);
   await openDevToolsTab(page);
 
   await warpToMessage(page, "baz 2");
   await verifyFramesCount(page, 5);
-
   await waitForScopeValue(page, "n", "2");
-
-  await waitForFrameTimeline(page, "25%");
-
+  await waitForFrameTimeline(page, target == "gecko" ? "25%" : "0%");
   await selectFrame(page, 1);
   await waitForScopeValue(page, "n", "3");
-  await waitForFrameTimeline(page, "87%");
-
+  await waitForFrameTimeline(page, target == "gecko" ? "87%" : "80%");
   await selectFrame(page, 2);
   await waitForScopeValue(page, "n", "4");
-  await waitForFrameTimeline(page, "87%");
-
+  await waitForFrameTimeline(page, target == "gecko" ? "87%" : "80%");
   await selectFrame(page, 3);
-  await waitForFrameTimeline(page, "71%");
-
+  await waitForFrameTimeline(page, target == "gecko" ? "71%" : "75%");
   await selectFrame(page, 4);
-  await waitForFrameTimeline(page, "100%");
-
+  await waitForFrameTimeline(page, target == "gecko" ? "100%" : "0%");
   await selectFrame(page, 0);
-
   await stepOverToLine(page, 20);
   await stepOverToLine(page, 21);
   await stepOverToLine(page, 22);
