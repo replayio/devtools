86f48815be5b9cc71c27952555d85a7de1166a30
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-10-04 16:39:07 -0400

    Rewrote Breakpoints component from class to function

--- src/devtools/client/debugger/src/components/Editor/Breakpoints/Breakpoints.tsx
+++ src/devtools/client/debugger/src/components/Editor/Breakpoints/Breakpoints.tsx
@@ -1,57 +1,41 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
+import type { Context } from "devtools/client/debugger/src/reducers/pause";
+import { getFirstVisibleBreakpoints } from "devtools/client/debugger/src/selectors";
+import SourceEditor from "devtools/client/debugger/src/utils/editor/source-editor";
+import { getSelectedSource } from "ui/reducers/sources";
+import { useAppSelector } from "ui/setup/hooks";
 
-//
-import React, { Component } from "react";
-import { connect, ConnectedProps } from "react-redux";
-import type { UIState } from "ui/state";
-import type { Context } from "../../../reducers/pause";
 import Breakpoint from "./Breakpoint";
 
-import { getFirstVisibleBreakpoints } from "devtools/client/debugger/src/selectors";
-import { getSelectedSource } from "ui/reducers/sources";
-import { getLocationKey } from "devtools/client/debugger/src/utils/breakpoint";
+export default function Breakpoints({ cx, editor }: { cx: Context; editor: SourceEditor }) {
+  const breakpoints = useAppSelector(getFirstVisibleBreakpoints);
+  const selectedSource = useAppSelector(getSelectedSource);
 
-const connector = connect((state: UIState) => ({
-  // Retrieves only the first breakpoint per line so that the
-  // breakpoint marker represents only the first breakpoint
-  breakpoints: getFirstVisibleBreakpoints(state),
-  selectedSource: getSelectedSource(state),
-}));
-
-type $FixTypeLater = any;
-
-type PropsFromRedux = ConnectedProps<typeof connector>;
-type BreakpointsProps = PropsFromRedux & {
-  editor: $FixTypeLater;
-  cx: Context;
-};
-
-class Breakpoints extends Component<BreakpointsProps> {
-  render() {
-    const { cx, breakpoints, selectedSource, editor } = this.props;
-
-    if (!selectedSource || !breakpoints) {
-      return null;
-    }
-
-    return (
-      <div>
-        {breakpoints.map(bp => {
-          return (
-            <Breakpoint
-              cx={cx}
-              key={getLocationKey(bp.location)}
-              breakpoint={bp}
-              selectedSource={selectedSource}
-              editor={editor}
-            />
-          );
-        })}
-      </div>
-    );
+  if (!selectedSource || !breakpoints) {
+    return null;
   }
-}
 
-export default connector(Breakpoints);
+  // We only need to render one gutter breakpoint per line.
+  const renderedLines = new Set();
+
+  return (
+    <div>
+      {breakpoints.map(breakpoint => {
+        if (renderedLines.has(breakpoint.location.line)) {
+          return null;
+        }
+
+        renderedLines.add(breakpoint.location.line);
+
+        return (
+          <Breakpoint
+            cx={cx}
+            key={breakpoint.id}
+            breakpoint={breakpoint}
+            selectedSource={selectedSource}
+            editor={editor}
+          />
+        );
+      })}
+    </div>
+  );
+}
