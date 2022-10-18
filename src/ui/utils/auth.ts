a98e286bb6abbab28eb37813a5a0413c41d9a058
Ryan Duffy <ryan@replay.io>
2022-10-18 12:02:40 -0700

    Expose auth config environment variables to client (#7963)

--- src/ui/utils/auth.ts
+++ src/ui/utils/auth.ts
@@ -1,7 +1,7 @@
 export function getAuthHost() {
-  return process.env.RECORD_REPLAY_AUTH_HOST || "webreplay.us.auth0.com";
+  return process.env.NEXT_PUBLIC_AUTH_HOST || "webreplay.us.auth0.com";
 }
 
 export function getAuthClientId() {
-  return process.env.RECORD_REPLAY_AUTH_CLIENT_ID || "4FvFnJJW4XlnUyrXQF8zOLw6vNAH1MAo";
+  return process.env.NEXT_PUBLIC_AUTH_CLIENT_ID || "4FvFnJJW4XlnUyrXQF8zOLw6vNAH1MAo";
 }
