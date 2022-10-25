040ce7b9ab8289ba7abaf0af7b853b26b0ba874b
Holger Benl <hbenl@evandor.de>
2022-10-25 13:35:19 +0200

    Don't assume Message.data.frames is sorted (FE-867)

--- packages/bvaughn-architecture-demo/src/utils/messages.ts
+++ packages/bvaughn-architecture-demo/src/utils/messages.ts
@@ -14,14 +14,14 @@ export function isInNodeModules(message: ProtocolMessage): boolean {
   if (messageToNodeModuleCache.has(message)) {
     return messageToNodeModuleCache.get(message)!;
   } else {
-    let lastFrame = null;
-    if (message.data.frames) {
-      lastFrame = message.data.frames[message.data.frames.length - 1];
+    let topFrame = null;
+    if (message.stack?.length && message.data.frames?.length) {
+      topFrame = message.data.frames.find(frame => frame.frameId === message.stack![0]);
     }
 
     let returnValue = false;
-    if (lastFrame) {
-      const sourceId = lastFrame.location?.[0].sourceId;
+    if (topFrame) {
+      const sourceId = topFrame.location?.[0].sourceId;
       const source = sourceId ? getSourceIfAlreadyLoaded(sourceId) : null;
       if (source) {
         returnValue = !!(source.url?.includes("node_modules") || source.url?.includes("unpkg.com"));
