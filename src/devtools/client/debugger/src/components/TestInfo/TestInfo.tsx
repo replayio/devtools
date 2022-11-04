8365652cefa25542c36bfaa0388d5f2963c4a246
Jaril <jarilvalenciano@gmail.com>
2022-11-03 19:25:40 -0700

    Polish test info UI (#8064)
    * WIP
    
    * add test results to header
    
    * Adding polish to testsuites (#8065)
    
    * First iteration of style changes
    
    * Fixing some layout issues, feeling groovy
    
    * Adding custom icons
    
    * Removing MaterialIcons
    
    * Removing result line for now
    
    * A few tweaks for dark theme
    
    * Dark theme improvements
    
    * More fine-tuning
    
    * Darker red
    
    * Fixing some other visual issues
    
    * Merging with main (#8073)
    
    * New Source viewer gets search modifier support (#8060)
    
    * Add search modifier support for new Source viewer
    * Update screenshots
    * Fix prettier.config.js
    
    * Enable tracking telemetry events in new codebase (#8063)
    
    * Syntax parsing cache util function (urlToLanguage) strips query params from fileName (#8061)
    
    * Upgrade @replayio/protocol to v0.32.3 (#8066)
    
    * Update test Proxy library to support event callbacks (#8067)
    
    Replay APIs sometimes use the pattern of accepting callback parameters and returning a Promise that can be awaited to signal completion. (The promise itself typically has no meaningful resolution/value.)
    
    This commit updates the client Proxy library (used only for frontend Playwright tests) to support this pattern via manual override.
    
    * Add streaming source cache API (#8023)
    
    * Integrate with streaming Source API
    * Update fixture data and snapshots
    
    * Remove get source contents cache (#8068)
    
    * Removed getSourceContents Suspense and ReplayClient methods
    
    All new requests should be made through the new streaming API. Keeping both APIs in the client seemed likely to result in duplicate fetches/caches.
    
    * Update fixture data
    
    * Fix throttle logic bug (#8069)
    
    * Escape newline chars before parsing (#8071)
    
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>
    Co-authored-by: Mark Erikson <mark@isquaredsoftware.com>
    
    * lint
    
    Co-authored-by: Ryan Duffy <ryan@replay.io>
    Co-authored-by: Jon Bell <github@lot23.com>
    Co-authored-by: Brian Vaughn <brian.david.vaughn@gmail.com>
    Co-authored-by: Mark Erikson <mark@isquaredsoftware.com>

--- src/devtools/client/debugger/src/components/TestInfo/TestInfo.tsx
+++ src/devtools/client/debugger/src/components/TestInfo/TestInfo.tsx
@@ -1,6 +1,7 @@
 import { SourceLocation } from "graphql";
 import { useState } from "react";
 
+import Icon from "ui/components/shared/Icon";
 import MaterialIcon from "ui/components/shared/MaterialIcon";
 import { useFetchCypressSpec } from "ui/hooks/useFetchCypressSpec";
 import { getRecordingDuration } from "ui/reducers/timeline";
@@ -25,27 +26,18 @@ export default function TestInfo({
 
   return (
     <div className="flex flex-col space-y-1 px-4 py-2">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-1">
-          {result ? <Status result={result} /> : null}
-          <div className="font-bold">{spec}</div>
-        </div>
-        {recordingDuration ? <div>{getFormattedTime(recordingDuration)}</div> : null}
-      </div>
-      <div className="flex flex-col space-y-1 py-2">
-        {testCases.map((t, i) => (
-          <TestCase test={t} key={i} location={cypressResults?.[i]?.location} />
-        ))}
-      </div>
+      {testCases.map((t, i) => (
+        <TestCase test={t} key={i} location={cypressResults?.[i]?.location} />
+      ))}
     </div>
   );
 }
 
 function TestCase({ test, location }: { test: TestItem; location?: SourceLocation }) {
-  const [expandError, setExpandError] = useState(false);
   const [expandSteps, setExpandSteps] = useState(false);
   const dispatch = useAppDispatch();
   const cx = useAppSelector(getThreadContext);
+  const expandable = test.steps || test.error;
 
   const onClick = () => {
     if (location) {
@@ -53,28 +45,33 @@ function TestCase({ test, location }: { test: TestItem; location?: SourceLocatio
     }
   };
   const toggleExpand = () => {
+    if (!expandable) {
+      return;
+    }
     setExpandSteps(!expandSteps);
   };
-  const expandable = test.steps || test.error;
 
   return (
-    <div className="flex flex-col gap-3">
-      <div className="group flex flex-row items-center justify-between gap-1 pl-4 transition hover:bg-chrome">
+    <div className="flex flex-col">
+      <div className="group flex flex-row items-center justify-between gap-1 rounded-lg p-1 transition hover:cursor-pointer">
         <button
           onClick={toggleExpand}
           disabled={!expandable}
-          className="flex flex-grow flex-row items-center gap-1 overflow-hidden"
+          className="flex flex-grow flex-row gap-1 overflow-hidden"
         >
           <Status result={test.result} />
           {test.steps ? (
             <MaterialIcon>{expandSteps ? "expand_more" : "chevron_right"}</MaterialIcon>
           ) : null}
-          <div
-            className={`overflow-hidden overflow-ellipsis whitespace-pre ${
-              expandable ? "group-hover:underline" : ""
-            }`}
-          >
-            {test.title}
+
+          <div className="flex flex-col items-start text-bodyColor">
+            <div className="overflow-hidden overflow-ellipsis whitespace-pre">{test.title}</div>
+
+            {test.error ? (
+              <div className="mt-1 overflow-hidden rounded-lg bg-testsuitesErrorBgcolor py-1 px-2 text-left">
+                {test.error.message}
+              </div>
+            ) : null}
           </div>
         </button>
         {location ? (
@@ -83,60 +80,53 @@ function TestCase({ test, location }: { test: TestItem; location?: SourceLocatio
           </button>
         ) : null}
       </div>
-      {expandSteps ? (
-        <>
-          {test.error ? (
-            <div className="ml-7 space-y-1">
-              <div className="font-bold">Error</div>
-              <div className="space-y-1 bg-chrome p-2">
-                <div className={`overflow-hidden font-mono ${!expandError ? "truncate" : ""}`}>
-                  {test.error.message}
-                </div>
-                <button className="hover:underline" onClick={() => setExpandError(!expandError)}>
-                  Show {expandError ? "less" : "more"}
-                </button>
-              </div>
-            </div>
-          ) : null}
-          {test.steps ? <TestSteps steps={test.steps} startTime={test.relativeStartTime} /> : null}
-        </>
-      ) : null}
+      {expandSteps ? <TestSteps test={test} startTime={test.relativeStartTime} /> : null}
     </div>
   );
 }
 
-function TestSteps({ steps, startTime }: { steps: TestStep[]; startTime: number }) {
+function TestSteps({ test, startTime }: { test: TestItem; startTime: number }) {
+  const { steps, error } = test;
+
   return (
-    <div className="flex flex-col space-y-1 pl-8">
-      <div className="font-bold">Steps</div>
-      <div className="flex flex-col gap-1 bg-chrome p-2">
-        {steps?.map((s, i) => (
-          <div key={i} className="flex items-center justify-between overflow-hidden">
-            <div className="flex items-center space-x-2 overflow-hidden whitespace-pre">
-              <MaterialIcon outlined className={s.error ? "text-red-500" : "text-green-500"}>
-                {s.error ? "close" : "done"}
-              </MaterialIcon>
-              <div className="overflow-hidden overflow-ellipsis">{s.name}</div>
-              <div className="overflow-hidden whitespace-pre opacity-50">
-                {s.args?.length ? `${s.args.toString()}` : ""}
-              </div>
+    <div className="flex flex-col rounded-lg py-2 pl-11">
+      {steps?.map((s, i) => (
+        <div
+          key={i}
+          className="flex items-center justify-between overflow-hidden border-b border-themeBase-90 bg-testsuitesStepsBgcolor px-3 py-2"
+        >
+          <div className="flex items-center space-x-2 overflow-hidden whitespace-pre">
+            <div className="overflow-hidden overflow-ellipsis font-medium text-bodyColor">
+              {s.name}
             </div>
-            {/* <div>{getFormattedTime(s.relativeStartTime)}</div> */}
+            <div className="overflow-hidden whitespace-pre opacity-70">
+              {s.args?.length ? `${s.args.toString()}` : ""}
+            </div>
           </div>
-        ))}
-      </div>
+          {/* <div>{getFormattedTime(s.relativeStartTime)}</div> */}
+        </div>
+      ))}
+      {test.error ? (
+        <div className="border-l-2 border-red-500 bg-testsuitesErrorBgcolor text-testsuitesErrorColor">
+          <div className="flex flex-row items-center space-x-1 p-2">
+            <Icon filename="warning" size="small" className="bg-testsuitesErrorColor" />
+            <div className="font-bold">Error</div>
+          </div>
+          <div className="wrap space-y-1 overflow-hidden bg-testsuitesErrorBgcolor p-2 font-mono">
+            {test.error.message}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
 
 function Status({ result }: { result: TestResult }) {
   return (
-    <MaterialIcon
-      iconSize="2xl"
-      outlined
-      className={result === "passed" ? "text-green-500" : "text-red-500"}
-    >
-      {result === "passed" ? "check_circle" : "highlight_off"}
-    </MaterialIcon>
+    <Icon
+      filename={result === "passed" ? "testsuites-success" : "testsuites-fail"}
+      size="small"
+      className={result === "passed" ? "bg-[#219653]" : "bg-[#EB5757]"}
+    />
   );
 }
