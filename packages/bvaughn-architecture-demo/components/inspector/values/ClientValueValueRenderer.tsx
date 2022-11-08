05800f95e6b754f8b33149e2590f03f79594e08d
Mark Erikson <mark@isquaredsoftware.com>
2022-11-08 11:25:22 -0500

    Update string truncation behavior based on feedback
    - Truncate the end of the string
    - Use `"[...]"` as the truncation indicator
    - Show a "More/Less" button at the end of the string

--- packages/bvaughn-architecture-demo/components/inspector/values/ClientValueValueRenderer.tsx
+++ packages/bvaughn-architecture-demo/components/inspector/values/ClientValueValueRenderer.tsx
@@ -20,24 +20,11 @@ const previewTypeToClassname: Partial<Record<ValueType, string>> = {
 
 // Arbitrary cutoffs for now
 const MAX_NESTED_STRING_LENGTH = 100;
-const MAX_EXPANDED_STRING_LENGTH = 250;
+const MAX_EXPANDED_STRING_LENGTH = 200;
+const TRUNCATION_INDICATOR = "[...]";
 
 // Copied from utils/string and made global
 export const NEW_LINE_REGEX = /\r\n?|\n|\u2028|\u2029/g;
-
-// Modified from https://stackoverflow.com/a/5723274/62937
-function innerTruncate(fullStr: string, strLen: number, separator = " ...*truncated*... ") {
-  if (fullStr.length <= strLen) {
-    return fullStr;
-  }
-
-  const sepLen = separator.length;
-  const charsToShow = strLen - sepLen;
-  const frontChars = Math.ceil(charsToShow / 2);
-  const backChars = Math.floor(charsToShow / 2);
-
-  return `${fullStr.slice(0, frontChars)}${separator}${fullStr.slice(fullStr.length - backChars)}`;
-}
 
 // Default renderer for primitive values (like numbers, strings, etc).
 export default function ClientValueValueRenderer({
@@ -61,14 +48,16 @@ export default function ClientValueValueRenderer({
         // preview text on a single line
         preview = preview.replace(NEW_LINE_REGEX, "\\n");
 
-        preview = innerTruncate(preview, MAX_NESTED_STRING_LENGTH);
+        if (preview.length > MAX_NESTED_STRING_LENGTH) {
+          preview = preview.slice(0, MAX_NESTED_STRING_LENGTH) + TRUNCATION_INDICATOR;
+        }
       } else {
         // We want to show the string as-is including actual newlines.
         // However, we'll still potentially truncate it to save vertical space.
         // We need to determine if it _can_ be truncated, and if we're _displaying_ it truncated.
         needsExpandStringToggle = preview.length > MAX_EXPANDED_STRING_LENGTH;
         if (needsExpandStringToggle && !isStringExpanded) {
-          preview = innerTruncate(preview, MAX_EXPANDED_STRING_LENGTH);
+          preview = preview.slice(0, MAX_EXPANDED_STRING_LENGTH) + TRUNCATION_INDICATOR;
         }
       }
 
@@ -88,13 +77,16 @@ export default function ClientValueValueRenderer({
   let contents: React.ReactNode = finalFormattedValue;
 
   if (needsExpandStringToggle) {
-    // Abuse Expandable a bit - we always want to show the string, it's just a question
-    // of whether we're showing the truncated version or the full version,
-    // so put the text as the `header` prop.
     contents = (
-      <Expandable header={finalFormattedValue} onChange={setIsStringExpanded}>
-        {null}
-      </Expandable>
+      <>
+        {finalFormattedValue}
+        <button
+          className="text-themeBodyColor inline rounded-md bg-chrome px-2 py-1"
+          onClick={() => setIsStringExpanded(!isStringExpanded)}
+        >
+          {isStringExpanded ? "Less" : "More"}
+        </button>
+      </>
     );
   }
 
