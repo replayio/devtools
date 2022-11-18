f0a249f9564ad0ddc8f27b317de993f14c16f529
Jaril <jarilvalenciano@gmail.com>
2022-11-18 12:31:55 -0800

    Bring back the left past/current border, and color the progress red if it's on an error-y step

--- src/devtools/client/debugger/src/components/TestInfo/ProgressBar.tsx
+++ src/devtools/client/debugger/src/components/TestInfo/ProgressBar.tsx
@@ -2,13 +2,13 @@ import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 
 import styles from "./ProgressBar.module.css";
 
-export function ProgressBar({ progress }: { progress: number }) {
+export function ProgressBar({ progress, error }: { progress: number; error: boolean }) {
   return (
     <CircularProgressbar
       className={styles.CircularProgressbar}
       strokeWidth={20}
       styles={buildStyles({
-        pathColor: `var(--focus-mode-loaded-indexed-color)`,
+        pathColor: error ? "rgb(239 68 68)" : `var(--focus-mode-loaded-indexed-color)`,
         trailColor: `var(--chrome)`,
       })}
       value={progress}
