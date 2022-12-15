354949082daed8c1e0f2722deea6d990aaf419df
Ryan Duffy <ryan@replay.io>
2022-12-14 16:02:52 -0800

    Add cypress test suite test

---
+++ packages/e2e-tests/helpers/testsuites.ts
@@ -0,0 +1,25 @@
+import { Locator, Page } from "@playwright/test";
+
+export async function getTestRows(page: Page) {
+  return page.locator('[data-test-id="TestSuite-TestCaseRow"]');
+}
+
+export async function getCypressLogo(page: Page) {
+  return page.locator('[data-test-name="ToolbarButton-CypressPanel"]');
+}
+
+export async function getTestRowChevron(row: Locator) {
+  return row.locator(":scope", { hasText: "chevron_right" });
+}
+
+export async function getTestRowError(row: Locator) {
+  return row.locator('[data-test-id="TestSuite-TestCaseRow-Error"]');
+}
+
+export async function getTestCaseSections(row: Locator) {
+  return row.locator('[data-test-id="TestSuites-TestCase-SectionHeader"]');
+}
+
+export async function getTestCaseSteps(row: Locator) {
+  return row.locator('[data-test-id="TestSuites-TestCase-TestStepRow"]');
+}
