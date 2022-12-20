7c4dfa8017424c44d0073cd4bd05cb9b15b1417e
Brian Vaughn <brian.david.vaughn@gmail.com>
2022-12-20 16:24:12 -0500

    Reduce inspector-02 test flakiness (#8396)
    In FE-1075, Logan pointed out that the inspector-02 test sometimes gets stuck waiting for the Elements panel to load because the test loads at the end of the recording, and document may not still be available at that time.
    
    This commit changes the test to seek to an earlier point in time before waiting for the Elements panel to load.

--- packages/e2e-tests/tests/inspector-02.test.ts
+++ packages/e2e-tests/tests/inspector-02.test.ts
@@ -1,6 +1,7 @@
 import test, { expect } from "@playwright/test";
 
 import { openDevToolsTab, startTest } from "../helpers";
+import { warpToMessage } from "../helpers/console-panel";
 import {
   activateInspectorTool,
   getElementsPanelSelection,
@@ -14,7 +15,13 @@ const url = "doc_inspector_basic.html";
 test(`inspector-02: element picker and iframe behavior`, async ({ page }) => {
   await startTest(page, url);
   await openDevToolsTab(page);
+
+  // DOM/document may not be available at the end of the recording;
+  // to avoid getting stuck waiting on it to load, seek to a known safe point.
+  await warpToMessage(page, "ExampleFinished");
+
   await openElementsPanel(page);
+
   await waitForElementsToLoad(page);
 
   await activateInspectorTool(page);
