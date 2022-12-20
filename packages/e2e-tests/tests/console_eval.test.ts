a200dc90081a2a71a046b3fc695552978b3c8360
Logan Smyth <loganfsmyth@gmail.com>
2022-12-20 12:05:13 -0800

    Run console expression eval at ExampleFinished. (#8393)

--- packages/e2e-tests/tests/console_eval.test.ts
+++ packages/e2e-tests/tests/console_eval.test.ts
@@ -6,21 +6,16 @@ import {
   openConsolePanel,
   warpToMessage,
 } from "../helpers/console-panel";
-import { seekToPreviousScreenshot } from "../helpers/timeline";
 
 const url = "doc_rr_basic.html";
 
-test("console_eval: support global console evaluations", async ({ page }) => {
+test("console_eval: support console evaluations", async ({ page }) => {
   await startTest(page, url);
   await openDevToolsTab(page);
   await openConsolePanel(page);
 
-  // in e2e tests replay always starts at the very end of the recording but at that point
-  // console evaluations always fail, so we seek to the point of the last screenshot instead
-  await seekToPreviousScreenshot(page);
-  await executeAndVerifyTerminalExpression(page, "333", 333);
-
   await warpToMessage(page, "ExampleFinished", 7);
+  await executeAndVerifyTerminalExpression(page, "333", 333);
   await executeAndVerifyTerminalExpression(page, "number", 10);
   await executeAndVerifyTerminalExpression(page, "window.updateNumber", "ƒupdateNumber()");
 });
