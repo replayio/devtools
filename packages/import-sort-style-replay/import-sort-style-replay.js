8c1bbc90f1058251fcf1d07199661cde159f456d
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-01-03 13:09:53 -0500

    Update import sort check

--- packages/import-sort-style-replay/import-sort-style-replay.js
+++ packages/import-sort-style-replay/import-sort-style-replay.js
@@ -22,7 +22,8 @@ function sortImports(styleApi) {
   const isReactEcosystemModule = imported =>
     Boolean(imported.moduleName.match(/^(react|react-dom|prop-types|redux|\@reduxjs|next)/));
 
-  const isNewPrototypeModule = imported => Boolean(imported.moduleName.match(/^(bvaughn|shared)/));
+  const isNewPrototypeModule = imported =>
+    Boolean(imported.moduleName.match(/^(replay-next|shared)/));
   const isProtocolModule = imported => Boolean(imported.moduleName.match(/^protocol/));
   const isOtherInternalPackageModule = imported =>
     Boolean(imported.moduleName.match(/^(accordion|design|third-party)/));
