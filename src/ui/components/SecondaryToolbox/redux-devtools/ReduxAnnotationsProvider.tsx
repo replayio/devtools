9c9324cb521f4d6c3cbd8ab2b3b02c7d9898f6b8
Mark Erikson <mark@isquaredsoftware.com>
2023-01-11 13:50:28 -0500

    Parse hardcoded Redux annotations

--- src/ui/components/SecondaryToolbox/redux-devtools/ReduxAnnotationsProvider.tsx
+++ src/ui/components/SecondaryToolbox/redux-devtools/ReduxAnnotationsProvider.tsx
@@ -1,3 +1,4 @@
+import { Annotation } from "@replayio/protocol";
 import React, { useEffect, useRef, useState } from "react";
 
 import { ThreadFront } from "protocol/thread";
@@ -11,6 +12,44 @@ interface RAPProps {
   children?: React.ReactNode;
 }
 
+const fetchedAnnotations: Annotation[] = [
+  {
+    point: "5841333965870490996444469078786217",
+    time: 3912,
+    kind: "redux-devtools-data",
+    contents:
+      '{"event":"action","payload":{"actionType":"counter/increment","shouldIgnoreAction":false,"connectionType":"redux","instanceId":1}}',
+  },
+  {
+    point: "8112963841484235038054569073442992",
+    time: 5300,
+    kind: "redux-devtools-data",
+    contents:
+      '{"event":"action","payload":{"actionType":"counter/increment","shouldIgnoreAction":false,"connectionType":"redux","instanceId":1}}',
+  },
+  {
+    point: "9735556609780795890548039468712121",
+    time: 6467,
+    kind: "redux-devtools-data",
+    contents:
+      '{"event":"action","payload":{"actionType":"counter/increment","shouldIgnoreAction":false,"connectionType":"redux","instanceId":1}}',
+  },
+  {
+    point: "12331705039059159000342538859053263",
+    time: 7864,
+    kind: "redux-devtools-data",
+    contents:
+      '{"event":"action","payload":{"actionType":"counter/decrement","shouldIgnoreAction":false,"connectionType":"redux","instanceId":1}}',
+  },
+  {
+    point: "14603334914677673831138701986496738",
+    time: 9252,
+    kind: "redux-devtools-data",
+    contents:
+      '{"event":"action","payload":{"actionType":"counter/incrementByAmount","shouldIgnoreAction":false,"connectionType":"redux","instanceId":1}}',
+  },
+];
+
 export const ReduxAnnotationsProvider = ({ children }: RAPProps) => {
   const [annotations, setAnnotations] = useState<ReduxActionAnnotation[]>([]);
   const dispatch = useAppDispatch();
@@ -18,7 +57,11 @@ export const ReduxAnnotationsProvider = ({ children }: RAPProps) => {
 
   useEffect(() => {
     // TODO Re-enable Redux DevTools annotations handling
-    /*
+
+    if (ThreadFront.recordingId !== "1ff386de-f3b4-4ff1-a5a3-8c137387b620") {
+      return;
+    }
+
     // React will double-run effects in dev. Avoid trying to subscribe twice,
     // as `socket.ts` throws errors if you call `getAnnotations()` more than once.
     if (isStrictEffectsSecondRenderRef.current) {
@@ -27,16 +70,13 @@ export const ReduxAnnotationsProvider = ({ children }: RAPProps) => {
 
     isStrictEffectsSecondRenderRef.current = true;
 
-    ThreadFront.getAnnotations(annotations => {
-      if (annotations.length) {
-        // Pre-process Redux annotations by parsing the string messages,
-        // then add them to the state array and pass down via context.
-        setAnnotations(prevAnnotations =>
-          prevAnnotations.concat(processReduxAnnotations(annotations))
-        );
-      }
-    }, "redux-devtools-bridge");
-    */
+    if (fetchedAnnotations.length) {
+      // Pre-process Redux annotations by parsing the string messages,
+      // then add them to the state array and pass down via context.
+      setAnnotations(prevAnnotations =>
+        prevAnnotations.concat(processReduxAnnotations(fetchedAnnotations))
+      );
+    }
   }, [dispatch]);
 
   return (
--- src/ui/components/SecondaryToolbox/redux-devtools/redux-annotations.ts
+++ src/ui/components/SecondaryToolbox/redux-devtools/redux-annotations.ts
@@ -3,96 +3,29 @@ import type { Action } from "@reduxjs/toolkit";
 import { Annotation, TimeStampedPoint } from "@replayio/protocol";
 import { createContext } from "react";
 
-import type {
-  PageScriptToContentScriptMessageForwardedToMonitors,
-  SplitMessage,
-  UpdateStateRequest,
-} from "./api";
+interface ReduxActionFields {
+  actionType: string;
+  shouldIgnoreAction: boolean;
+  connectionType: "redux" | "generic";
+  instanceId: number;
+}
 
-export interface ReduxActionAnnotation<State = unknown> extends TimeStampedPoint {
-  /** The Redux DevTools update action, with data parsed from a JSON string and wrapped in an action.
-   * Contents may vary. */
-  action: UpdateStateAction<State, Action>;
+interface ReduxAnnotationContents {
+  event: string;
+  payload: ReduxActionFields;
+}
+
+export interface ReduxActionAnnotation extends TimeStampedPoint, ReduxAnnotationContents {
+  kind: string;
 }
 
 export const ReduxAnnotationsContext = createContext<ReduxActionAnnotation[]>([]);
-
-export interface UpdateStateAction<S, A extends Action<unknown>> {
-  readonly type: typeof UPDATE_STATE;
-  request: UpdateStateRequest<S, A>;
-  readonly id: string | number;
-}
-
-const chunks: {
-  [instanceId: string]: PageScriptToContentScriptMessageForwardedToMonitors<
-    unknown,
-    Action<unknown>
-  >;
-} = {};
-
-// Copied from the guts of the RDT extension logic
-export const createDevtoolsAction = <S, A extends Action<unknown>>(
-  tabId: number,
-  request: UpdateStateRequest<S, A> | SplitMessage
-) => {
-  const action: UpdateStateAction<S, A> = {
-    type: "devTools/UPDATE_STATE",
-    request,
-    id: tabId,
-  } as UpdateStateAction<S, A>;
-  const instanceId = `${tabId}/${request.instanceId}`;
-  if ("split" in request) {
-    if (request.split === "start") {
-      chunks[instanceId] = request as any;
-      return;
-    }
-    if (request.split === "chunk") {
-      (chunks[instanceId] as any)[request.chunk[0]] =
-        ((chunks[instanceId] as any)[request.chunk[0]] || "") + request.chunk[1];
-      return;
-    }
-    action.request = chunks[instanceId] as any;
-    delete chunks[instanceId];
-  }
-  if (request.instanceId) {
-    action.request.instanceId = instanceId;
-  }
-
-  // Crank up the max actions the RDT logic will process before compacting old ones
-  // This is usually 50 by default, but hey, let's be generous.  (We'll see if this
-  // ends up causing any memory issues or anything)
-  // @ts-ignore
-  request.maxAge = 500;
-
-  // @ts-ignore
-  if (request.libConfig) {
-    // Try to mark RDT features as "disabled", since these aren't relevant in Replay
-    // @ts-ignore
-    Object.assign(request.libConfig, {
-      lock: false,
-      export: false,
-      import: false,
-      persist: false,
-      pause: false,
-      reorder: false,
-      jump: false,
-      skip: false,
-      dispatch: false,
-      sync: false,
-      test: false,
-    });
-  }
-
-  return action;
-};
 
 export const processReduxAnnotations = (annotations: Annotation[]): ReduxActionAnnotation[] => {
   return annotations.map(annotation => {
     const { contents, kind, point, time } = annotation;
     // Parse the stringified data from the Redux DevTools on the backend
-    const message = JSON.parse(contents);
-    // Wrap that data in an action we can use to update the DevTools store in the panel
-    const action = createDevtoolsAction(1, message)!;
-    return { kind, point, time, action };
+    const fields = JSON.parse(contents) as ReduxAnnotationContents;
+    return { kind, time, point, ...fields };
   });
 };
