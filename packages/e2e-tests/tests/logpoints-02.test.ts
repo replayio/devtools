5b6b570bfc14fb47863518dc19db5acfdc87cfe2
Holger Benl <hbenl@evandor.de>
2023-01-25 15:23:38 +0100

    Use the logpoint's condition in the logpoint panel (FE-1165)

--- packages/e2e-tests/tests/logpoints-02.test.ts
+++ packages/e2e-tests/tests/logpoints-02.test.ts
@@ -2,7 +2,11 @@ import test, { expect } from "@playwright/test";
 
 import { openDevToolsTab, startTest } from "../helpers";
 import { findConsoleMessage } from "../helpers/console-panel";
-import { addLogpoint } from "../helpers/source-panel";
+import {
+  addLogpoint,
+  seekToPreviousLogPointHit,
+  verifyLogpointStep,
+} from "../helpers/source-panel";
 
 const url = "doc_rr_basic.html";
 
@@ -33,4 +37,9 @@ test(`logpoints-02: conditional log-points`, async ({ page }) => {
   await expect(logPointMessages).toHaveCount(7); // 5 logs in the loop + beginning and end
   await expect(logPointMessages.first()).toHaveText(/Beginning/);
   await expect(logPointMessages.last()).toHaveText(/Ending/);
+
+  await seekToPreviousLogPointHit(page, 20);
+  await verifyLogpointStep(page, "5/5", { lineNumber: 20 });
+  await seekToPreviousLogPointHit(page, 20);
+  await verifyLogpointStep(page, "4/5", { lineNumber: 20 });
 });
