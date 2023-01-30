5af5b155537ff9a5830450d0c6d34b2a0fe1999b
Ryan Duffy <ryan@replay.io>
2023-01-30 09:32:11 -0800

    Add missing steps warning to test panel (#8621)

--- src/devtools/client/debugger/src/components/TestInfo/TestInfo.tsx
+++ src/devtools/client/debugger/src/components/TestInfo/TestInfo.tsx
@@ -5,6 +5,7 @@ import React, { createContext, useContext, useEffect, useMemo, useState } from "
 import ErrorBoundary from "replay-next/components/ErrorBoundary";
 import PropertiesRenderer from "replay-next/components/inspector/PropertiesRenderer";
 import useLocalStorage from "replay-next/src/hooks/useLocalStorage";
+import { useTestInfo } from "ui/hooks/useTestInfo";
 import { getSelectedTest, setSelectedTest } from "ui/reducers/reporter";
 import { setPlaybackFocusRegion } from "ui/reducers/timeline";
 import { useAppDispatch, useAppSelector } from "ui/setup/hooks";
@@ -31,6 +32,10 @@ export default function TestInfo({ testCases }: { testCases: TestItem[] }) {
   const [consoleProps, setConsoleProps] = useState<ProtocolObject>();
   const [loading, setLoading] = useState<boolean>(true);
   const [pauseId, setPauseId] = useState<string | null>(null);
+  const info = useTestInfo();
+
+  const missingSteps =
+    !info.loading && info.supportsSteps && !testCases.some(t => t.steps && t.steps.length > 0);
 
   useEffect(() => {
     dispatch(setPlaybackFocusRegion(!!selectedTest));
@@ -54,6 +59,55 @@ export default function TestInfo({ testCases }: { testCases: TestItem[] }) {
       <TestInfoContextMenuContextRoot>
         <div className="flex flex-grow flex-col overflow-hidden">
           <div className="relative flex flex-grow flex-col space-y-1 overflow-auto border-t border-splitter px-2 pt-3">
+            {missingSteps ? (
+              <aside className="m-1 space-y-4 rounded-lg border border-amber-200 bg-amber-100 px-4 py-3">
+                <div>
+                  <strong>Hey there!</strong>
+                </div>
+                <div>
+                  You seem to be missing test steps for this replay. You'll still be able to use the
+                  core Replay tools but adding test steps will give you a much better debugging
+                  experience.
+                </div>
+                {info.runner === "cypress" ? (
+                  <div>
+                    This usually means you need to include the{" "}
+                    <code>@replayio/cypress/support</code> module in your project's support file.
+                    More information is available in our{" "}
+                    <a
+                      className="underline"
+                      href="https://docs.replay.io/recording-browser-tests-(beta)/cypress-instructions"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      docs
+                    </a>
+                    .
+                  </div>
+                ) : null}
+                <div>
+                  If you're stuck, reach out to us over{" "}
+                  <a
+                    className="underline"
+                    href="mailto:support@replay.io"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    email
+                  </a>{" "}
+                  or{" "}
+                  <a
+                    href="https://replay.io/discord"
+                    className="underline"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    discord
+                  </a>{" "}
+                  and we can help you get set up.
+                </div>
+              </aside>
+            ) : null}
             {selectedTest == null ? (
               <TestCaseTree testCases={testCases} />
             ) : (
