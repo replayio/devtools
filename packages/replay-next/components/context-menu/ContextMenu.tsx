6307b1d4f8a25d6c6aaca4d20500ac51c9d35322
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-01-30 13:46:33 -0500

    Prevent preview popup from being triggered while context menus are open (#8635)
    Added an invisible background blocker when context menus are open that prevents other mouse events from being triggered.
    
    Also renamed the data-test-sourceid attribute to data-test-source-id while I was in here, because I noticed the inconsistency and it was bugging me.

--- packages/replay-next/components/context-menu/ContextMenu.tsx
+++ packages/replay-next/components/context-menu/ContextMenu.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useRef } from "react";
+import { MouseEvent, ReactNode, useRef } from "react";
 import { createPortal } from "react-dom";
 
 import useModalDismissSignal from "replay-next/src/hooks/useModalDismissSignal";
@@ -24,19 +24,28 @@ export default function ContextMenu({
 
   useModalDismissSignal(ref, hide, true);
 
+  const onClick = () => {
+    hide();
+  };
+
+  const onMouseMove = (event: MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return createPortal(
-    <div
-      className={styles.ContextMenu}
-      data-test-id={dataTestId}
-      data-test-name={dataTestName}
-      onClick={() => hide()}
-      ref={ref}
-      style={{
-        left: pageX,
-        top: pageY,
-      }}
-    >
-      {children}
+    <div className={styles.Backdrop} onClick={onClick} onMouseMove={onMouseMove}>
+      <div
+        className={styles.ContextMenu}
+        data-test-id={dataTestId}
+        data-test-name={dataTestName}
+        ref={ref}
+        style={{
+          left: pageX,
+          top: pageY,
+        }}
+      >
+        {children}
+      </div>
     </div>,
     document.body
   );
