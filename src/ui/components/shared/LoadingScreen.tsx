9e53081b7be6831c57ee91153382296ee60fdb6e
Jon Bell <github@lot23.com>
2023-02-03 17:07:46 +1300

    Add Overboard hoverboard to loading screen (#8681)
    * Animated hoverboard on loading screen
    * showTips should be true
    * Make hoverboard load dynamically
    * Update the hoverboard every 5 seconds
    
    ---------
    
    Co-authored-by: Jason Laster <jason.laster.11@gmail.com>

--- src/ui/components/shared/LoadingScreen.tsx
+++ src/ui/components/shared/LoadingScreen.tsx
@@ -1,25 +1,47 @@
-import React, { ReactNode, useEffect, useMemo, useState } from "react";
+import { Colorway } from "@replayio/overboard";
+import dynamic from "next/dynamic";
+import React, { ReactNode, useEffect, useState } from "react";
 import { ConnectedProps, connect } from "react-redux";
 
 import { getAwaitingSourcemaps, getLoadingFinished, getUploading } from "ui/reducers/app";
 import { UIState } from "ui/state";
 
 import { LoadingTips } from "./LoadingTips";
-import ReplayLogo from "./ReplayLogo";
 import { BubbleViewportWrapper } from "./Viewport";
+
+const colorOptions: Array<"blue" | "green" | "red"> = ["blue", "green", "red"];
+
+const Hoverboard = dynamic(() => import("./Hoverboard"), {
+  ssr: false,
+  loading: () => <div />,
+});
 
 export function LoadingScreenTemplate({
   children,
-  showTips,
+  showTips = true,
 }: {
   children?: ReactNode;
   showTips?: boolean;
 }) {
+  const [hoverboardColor, setHoverboardColor] = useState(colorOptions[2]);
+
+  const changeHoverboardColor = () => {
+    const randomIndex = Math.floor(Math.random() * colorOptions.length);
+    setHoverboardColor(colorOptions[randomIndex]);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(changeHoverboardColor, 5_000);
+    return () => clearInterval(interval);
+  }, [hoverboardColor]);
+
   return (
     <BubbleViewportWrapper>
       <div className="relative flex w-96 flex-col items-center space-y-1 rounded-lg bg-loadingBoxes p-8 py-12 shadow-sm">
         <div className="flex flex-col items-center space-y-8">
-          <ReplayLogo size="md" />
+          <div className="w-32" onClick={changeHoverboardColor}>
+            <Hoverboard color={hoverboardColor} />
+          </div>
           {children}
         </div>
       </div>
