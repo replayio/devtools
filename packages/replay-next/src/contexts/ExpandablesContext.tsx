eef95069e8391935163d5cf84a253a5b9eb61beb
Holger Benl <hbenl@evandor.de>
2023-02-08 10:22:56 +0100

    Rename function to avoid confusion

--- packages/replay-next/src/contexts/ExpandablesContext.tsx
+++ packages/replay-next/src/contexts/ExpandablesContext.tsx
@@ -2,23 +2,26 @@ import { PropsWithChildren, createContext, useCallback, useMemo } from "react";
 
 export type InspectorContextType = {
   isExpanded(key: string): boolean | undefined;
-  setIsExpanded(key: string, expanded: boolean): void;
+  persistIsExpanded(key: string, expanded: boolean): void;
 };
 
 export const ExpandablesContext = createContext<InspectorContextType>({
   isExpanded: () => undefined,
-  setIsExpanded: () => {},
+  persistIsExpanded: () => {},
 });
 
 export function ExpandablesContextRoot({ children }: PropsWithChildren) {
   const expandedKeys = useMemo(() => new Map<string, boolean>(), []);
   const isExpanded = useCallback((key: string) => expandedKeys.get(key), [expandedKeys]);
-  const setIsExpanded = useCallback(
+  const persistIsExpanded = useCallback(
     (key: string, expanded: boolean) => expandedKeys.set(key, expanded),
     [expandedKeys]
   );
 
-  const context = useMemo(() => ({ isExpanded, setIsExpanded }), [isExpanded, setIsExpanded]);
+  const context = useMemo(
+    () => ({ isExpanded, persistIsExpanded }),
+    [isExpanded, persistIsExpanded]
+  );
 
   return <ExpandablesContext.Provider value={context}>{children}</ExpandablesContext.Provider>;
 }
