f93441eb045d911d11ebdc57b84a0366855cf737
Jon Bell <github@lot23.com>
2023-02-09 11:41:42 +1300

    Don’t show icon unless mouse or key (#8707)

--- src/ui/components/Events/Event.tsx
+++ src/ui/components/Events/Event.tsx
@@ -253,19 +253,21 @@ export default function Event({ currentTime, executionPoint, event, onSeek }: Ev
           <Label>{label}</Label>
         </div>
         <div className="flex space-x-2 opacity-0 group-hover:opacity-100">
-          <div
-            onClick={onClickJumpToCode}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-            className={`${
-              isHovered ? "h-6 px-2 shadow-sm" : "h-6 w-6"
-            } flex items-center justify-center rounded-full bg-primaryAccent transition-width duration-100 ease-out`}
-          >
-            <div className="flex items-center space-x-1">
-              {isHovered && <span className="truncate text-white ">Jump to code</span>}
-              <Icon type={timeLabel} className="w-3.5 text-white" />              
+          {(event.kind === "mousedown" || event.kind === "keypress") ? (
+            <div
+              onClick={onClickJumpToCode}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              className={`${
+                isHovered ? "h-6 px-2 shadow-sm" : "h-6 w-6"
+              } flex items-center justify-center rounded-full bg-primaryAccent transition-width duration-100 ease-out`}
+            >
+              <div className="flex items-center space-x-1">
+                {isHovered && <span className="truncate text-white ">Jump to code</span>}
+                <Icon type={timeLabel} className="w-3.5 text-white" />
+              </div>
             </div>
-          </div>
+          ) : null}
         </div>
       </div>
       {contextMenu}
