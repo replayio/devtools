a86154b4bbb482e7439344895205dc40db2b59fd
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-02-13 12:54:23 -0500

    Re-add temporary "high-priority" store name to POINTS_DATABASE IDB

--- packages/replay-next/src/contexts/points/constants.ts
+++ packages/replay-next/src/contexts/points/constants.ts
@@ -5,5 +5,11 @@ import { IDBOptions } from "../../hooks/useIndexedDB";
 export const POINTS_DATABASE: IDBOptions = {
   databaseName: "Points",
   databaseVersion: 2,
-  storeNames: ["points", "point-behaviors"],
+  storeNames: [
+    "points",
+    "point-behaviors",
+    // TODO [FE-1138] Remove this legacy table after a few weeks with the new Points storage format
+    // See useLocalPoints() for more
+    "high-priority",
+  ],
 };
