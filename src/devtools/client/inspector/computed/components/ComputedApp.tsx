2a8685c5594d4eef4aff421951f1a2c8eec780d5
Krishna <krishnamadhwani2@gmail.com>
2023-03-01 04:45:27 +0530

    added scrolbar feature in the the Elements panel

--- src/devtools/client/inspector/computed/components/ComputedApp.tsx
+++ src/devtools/client/inspector/computed/components/ComputedApp.tsx
@@ -7,7 +7,9 @@ export default function ComputedApp() {
   return (
     <div id="sidebar-panel-computedview" className="theme-sidebar inspector-tabpanel">
       <ComputedToolbar />
+      <div style={{overflow:"scroll",height:"180px"}}>
       <ComputedProperties />
+      </div>
     </div>
   );
 }
