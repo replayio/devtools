9c262802b96071ecd58d36515f001ef0e9101818
Tejas Kumar <tejas@tejas.qa>
2023-03-04 16:26:42 +0100

    Add OpenAI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,7 @@ const csp = (props: any) => {
   const authHost = getAuthHost();
   return [
     `default-src 'self'`,
-    `connect-src 'self' https://api.replay.io wss://api.replay.io wss://dispatch.replay.io ws://*.replay.prod http://*.replay.prod https://telemetry.replay.io https://${authHost} https://api-js.mixpanel.com https://*.sentry.io https://*.launchdarkly.com https://*.logrocket.io https://*.lr-ingest.io https://*.logrocket.com https://*.lr-in.com https://api.stripe.com https://vitals.vercel-insights.com ${
+    `connect-src 'self' https://api.openai.com https://api.replay.io wss://api.replay.io wss://dispatch.replay.io ws://*.replay.prod http://*.replay.prod https://telemetry.replay.io https://${authHost} https://api-js.mixpanel.com https://*.sentry.io https://*.launchdarkly.com https://*.logrocket.io https://*.lr-ingest.io https://*.logrocket.com https://*.lr-in.com https://api.stripe.com https://vitals.vercel-insights.com ${
       // Required to talk to local backend in development. Enabling
       // localhost:8000 for prod to support the ?dispatch parameter when running
       // the local backend
---
+++ pages/api/ai.ts
@@ -0,0 +1,39 @@
+import { NextApiHandler } from "next";
+
+const handler: NextApiHandler = async (req, res) => {
+    const { code, lineNumber } = req.body;
+
+    const responseFromChatGpt = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "system",
+                    content:
+                        "You are an experienced web developer who is familiar with finding bugs and explaining code",
+                },
+                {
+                    role: "user",
+                    content: `Given this file:
+       
+${code.join('\n')}
+
+Explain the following line to a new developer:
+
+${code[lineNumber - 1]}
+
+Specifically explain how we got here and what bugs might be lurking in this code. If you can do bullet points just for the potential bugs, do bullet points.`,
+                },
+            ],
+        }),
+    }).then(r => r.json());
+
+    res.end(responseFromChatGpt.choices[0].message.content)
+}
+
+export default handler
