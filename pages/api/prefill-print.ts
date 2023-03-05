de7a654f09bcb4f8a92de9ac72d1cb3f55cebdf2
Jason Laster <jason.laster.11@gmail.com>
2023-03-05 12:45:39 -0800

    new prompts

--- pages/api/prefill-print.ts
+++ pages/api/prefill-print.ts
@@ -24,9 +24,55 @@ Here are some suggestions for prefixes:
 
 Could you also include a description of what the print statement will log and why you chose the variables/expressions you used?`;
 
+const SYSTEM_PROMPT_2 = () => `
+You are an experienced web developer whose job is to help me understand the JavaScript code that I am looking at.
+
+Can you list 5 questions you have about the code and suggest 5 print statements that can help us understand the code? 
+
+Here is an example print statement:
+\`\`\`
+console.log("Rendering post", post?.id, user, shouldShowComments);
+\`\`\`
+
+Here are some suggestions for the values to include:
+- It's useful to include 3 values per print statement
+- It's helpful to include recently defined values
+- It's helpful for expressions to use optionally chaining e.g. foo?.bar
+
+Here are some suggestions for the prefix to include:
+- The prefix should describe what the code is doing e.g. "Rendering post", "Fetching user"
+- Text should be concise. Less than 30 characters. Two words or less.
+- Text should not include brackets [], parentheses (), or other special characters
+- Text should not be capitalized
+- Text should not include terms like logging, debug, or info
+
+Here is an example output
+\`\`\`
+Questions:
+<list of questions>
+
+Print statements:
+<list of print statements>
+
+Descriptions:
+<list of descriptions>
+\`\`\`
+`;
+
 const USER_PROMPT = ({ code, lineNumber }: { code: string; lineNumber: number }) => `
 
 Please return a print statement that is on line ${lineNumber}
+
+~~~
+${code
+  .split("\n")
+  .slice(Math.max(0, lineNumber - 10), lineNumber - 1)
+  .join("\n")}
+~~~`;
+
+const USER_PROMPT_2 = ({ code, lineNumber }: { code: string; lineNumber: number }) => `
+
+Please share 5 questions you have about the code and 5 print statements for line ${lineNumber} with descriptions for why you chose them.
 
 ~~~
 ${code
@@ -88,14 +134,74 @@ async function addPrintStatements({
     }).then(r => r.json());
 
     const content = responseFromChatGpt.choices[0].message.content;
-    const expression = content.match(/console.log\((.*)\)/)?.[1];
+    const match = content.match(/console.log\((.*)\)/);
+    const expression = match?.[1];
     if (!expression) {
       console.log("could not find expression", content);
     }
 
-    const description = content.split("\n").slice(5).join("\n");
+    // The description is the text after the expression
+    const description = content
+      .slice(content.indexOf(expression) + expression.length + 2)
+      .replace(/\`\`\`/, "")
+      .trim();
 
     return { expression, content, description };
+  } catch (e) {
+    return { expression: "", content: "", description: "" };
+  }
+}
+
+async function suggestPrintStatements({
+  code,
+  lineNumber,
+  errors,
+}: {
+  code: string;
+  lineNumber: number;
+  errors: { expression: string; error: string }[];
+}): Promise<{ expression: string; content: string; description: string }> {
+  try {
+    const responseFromChatGpt = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: SYSTEM_PROMPT_2(),
+          },
+          {
+            role: "user",
+            content: USER_PROMPT_2({ code, lineNumber }),
+          },
+          ...errors.map(error => ({
+            role: "user",
+            content: USER_ERROR_PROMPT({ error, lineNumber }),
+          })),
+        ],
+      }),
+    }).then(r => r.json());
+
+    const content = responseFromChatGpt.choices[0].message.content;
+    // const match = content.match(/console.log\((.*)\)/);
+    // const expression = match?.[1];
+    // if (!expression) {
+    //   console.log("could not find expression", content);
+    // }
+
+    // // The description is the text after the expression
+    // const description = content
+    //   .slice(content.indexOf(expression) + expression.length + 2)
+    //   .replace(/\`\`\`/, "")
+    //   .trim();
+
+    console.log("**suggestions**", content, "\n\n");
+    // return { expression, content, description };
   } catch (e) {
     return { expression: "", content: "", description: "" };
   }
@@ -158,7 +264,14 @@ const handler: NextApiHandler = async (req, res) => {
   const { code, lineNumber, token, recordingId, closestHitPoint } = req.body;
 
   let attempts = 0;
-  const errors = [];
+  const errors: { error: string; expression: string }[] = [];
+  console.log(`--starting (${lineNumber})--`);
+
+  await suggestPrintStatements({
+    code,
+    lineNumber,
+    errors,
+  });
 
   try {
     while (attempts++ < 5) {
@@ -169,8 +282,13 @@ const handler: NextApiHandler = async (req, res) => {
       });
 
       const { expression, content, description } = statement;
-      console.log(`content`, content);
-      console.log(`description`, description);
+
+      console.log(`**expression**`, expression);
+      if (description) {
+        console.log(`**description**`, description);
+      } else {
+        console.log(`**content**`, content);
+      }
 
       const result = await evalExpression({
         expression,
@@ -179,7 +297,8 @@ const handler: NextApiHandler = async (req, res) => {
         closestHitPoint,
       });
 
-      console.log(`result`, result);
+      console.log(`**result**`, result);
+      console.log(`--finished (${lineNumber})--`);
 
       if (result.succeeded) {
         return res.status(200).json({ status: true, expression, description });
