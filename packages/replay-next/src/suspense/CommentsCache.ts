6640c7f400fe7e3076f3e10d7090689599935ab0
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-03-06 15:14:01 -0500

    Updated CommentCache to use "suspense" createCache() API

--- packages/replay-next/src/suspense/CommentsCache.ts
+++ packages/replay-next/src/suspense/CommentsCache.ts
@@ -1,25 +1,24 @@
 import { RecordingId } from "@replayio/protocol";
+import { createCache } from "suspense";
 
-import { createGenericCache } from "replay-next/src/suspense/createGenericCache";
 import { getComments as getCommentsGraphQL } from "shared/graphql/Comments";
 import { GraphQLClientInterface } from "shared/graphql/GraphQLClient";
 import { Comment } from "shared/graphql/types";
 
 export const {
-  getValueSuspense: getCommentListSuspense,
-  getValueAsync: getCommentListAsync,
+  cache: setLatestIDBValue,
   getValueIfCached: getCommentListIfCached,
-  addValue: setLatestIDBValue,
-} = createGenericCache<
-  [graphQLClient: GraphQLClientInterface, accessToken: string | null],
-  [recordingId: RecordingId],
+  read: getCommentListSuspense,
+  readAsync: getCommentListAsync,
+} = createCache<
+  [recordingId: RecordingId, graphQLClient: GraphQLClientInterface, accessToken: string | null],
   Comment[]
->(
-  "CommentsCache: getCommentsGraphQL",
-  async (
+>({
+  debugLabel: "CommentsCache",
+  load: async (
     recordingId: RecordingId,
     graphQLClient: GraphQLClientInterface,
     accessToken: string | null
   ) => await getCommentsGraphQL(graphQLClient, recordingId, accessToken),
-  (recordingId: RecordingId) => recordingId
-);
+  getKey: (recordingId: RecordingId) => recordingId,
+});
