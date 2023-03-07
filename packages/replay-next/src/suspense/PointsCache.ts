62c1d38d9511e70df1588157f74d1134f58110d6
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-03-06 17:31:56 -0800

    Updated PointsCache to use "suspense" createCache() API

--- packages/replay-next/src/suspense/PointsCache.ts
+++ packages/replay-next/src/suspense/PointsCache.ts
@@ -1,26 +1,24 @@
 import { RecordingId } from "@replayio/protocol";
+import { createCache } from "suspense";
 
 import { Point } from "shared/client/types";
 import { GraphQLClientInterface } from "shared/graphql/GraphQLClient";
 import { getPoints as getPointsGraphQL } from "shared/graphql/Points";
 
-import { createGenericCache } from "./createGenericCache";
-
 export const {
-  getValueSuspense: getPointsSuspense,
-  getValueAsync: getPointsAsync,
+  cache: setLatestIDBValue,
   getValueIfCached: getPointsIfCached,
-  addValue: setLatestIDBValue,
-} = createGenericCache<
-  [graphQLClient: GraphQLClientInterface, accessToken: string | null],
-  [recordingId: RecordingId],
+  read: getPointsSuspense,
+  readAsync: getPointsAsync,
+} = createCache<
+  [recordingId: RecordingId, graphQLClient: GraphQLClientInterface, accessToken: string | null],
   Point[]
->(
-  "PointsCache: getPointsGraphQL",
-  async (
+>({
+  debugLabel: "PointsCache: getPointsGraphQL",
+  load: async (
     recordingId: RecordingId,
     graphQLClient: GraphQLClientInterface,
     accessToken: string | null
   ) => await getPointsGraphQL(graphQLClient, recordingId, accessToken),
-  (recordingId: RecordingId) => recordingId
-);
+  getKey: (recordingId: RecordingId) => recordingId,
+});
