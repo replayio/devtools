48a449ae80acfb5d9fa7214151ad4b6b08045d27
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-03-14 18:17:27 -0400

    Console context menu: Add disable and scroll-to-pause (#8939)
    Also fix minor CSS/style regression (from #8874)

--- packages/replay-next/components/console/CurrentTimeIndicator.tsx
+++ packages/replay-next/components/console/CurrentTimeIndicator.tsx
@@ -1,4 +1,4 @@
-import { useContext, useLayoutEffect, useRef } from "react";
+import { useContext, useLayoutEffect } from "react";
 
 import { TimelineContext } from "replay-next/src/contexts/TimelineContext";
 
@@ -7,18 +7,16 @@ import styles from "./CurrentTimeIndicator.module.css";
 export default function CurrentTimeIndicator() {
   const { executionPoint } = useContext(TimelineContext);
 
-  const ref = useRef<HTMLDivElement>(null);
-
-  useLayoutEffect(() => {
-    const div = ref.current!;
-    div.scrollIntoView({ behavior: "smooth", block: "nearest" });
-  }, [executionPoint]);
+  useLayoutEffect(scrollCurrentTimeIndicatorIntoView, [executionPoint]);
 
   return (
-    <div
-      ref={ref}
-      className={styles.CurrentTimeIndicator}
-      data-test-id="Console-CurrentTimeIndicator"
-    />
+    <div className={styles.CurrentTimeIndicator} data-test-id="Console-CurrentTimeIndicator" />
   );
+}
+
+export function scrollCurrentTimeIndicatorIntoView() {
+  const indicator = document.querySelector(`[data-test-id="Console-CurrentTimeIndicator"]`);
+  if (indicator) {
+    indicator.scrollIntoView({ behavior: "smooth", block: "nearest" });
+  }
 }
