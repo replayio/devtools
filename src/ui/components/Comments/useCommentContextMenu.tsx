c5a72f789c5c79a8b3100c50ac0a40660c7b7639
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-03-20 18:51:57 -0400

    Don't show edit/delete menu for comments you don't own (#8969)

--- src/ui/components/Comments/useCommentContextMenu.tsx
+++ src/ui/components/Comments/useCommentContextMenu.tsx
@@ -1,9 +1,11 @@
 import { SerializedEditorState } from "lexical";
+import { ReactNode, useContext } from "react";
 
 import ContextMenuDivider from "replay-next/components/context-menu/ContextMenuDivider";
 import ContextMenuItem from "replay-next/components/context-menu/ContextMenuItem";
 import useContextMenu from "replay-next/components/context-menu/useContextMenu";
 import Icon from "replay-next/components/Icon";
+import { SessionContext } from "replay-next/src/contexts/SessionContext";
 import { setFocusRegionBeginTime, setFocusRegionEndTime } from "ui/actions/timeline";
 import { useAppDispatch } from "ui/setup/hooks";
 import type { Remark } from "ui/state/comments";
@@ -25,6 +27,9 @@ export default function useCommentContextMenu({
   type: "comment" | "reply";
 }) {
   const dispatch = useAppDispatch();
+  const { currentUserInfo } = useContext(SessionContext);
+
+  const isCurrentUserAuthor = currentUserInfo?.id === remark.user?.id;
 
   const { confirmDestructive } = useConfirm();
 
@@ -61,30 +66,47 @@ export default function useCommentContextMenu({
     dispatch(setFocusRegionBeginTime(remark.time!, true));
   };
 
-  const { contextMenu, onContextMenu } = useContextMenu(
-    <>
-      <ContextMenuItem onClick={editRemark}>Edit comment</ContextMenuItem>
-      <ContextMenuItem onClick={confirmDelete}>
+  const contextMenuItems: ReactNode[] = [];
+  if (isCurrentUserAuthor) {
+    contextMenuItems.push(
+      <ContextMenuItem key="edit" onClick={editRemark}>
+        Edit comment
+      </ContextMenuItem>
+    );
+    contextMenuItems.push(
+      <ContextMenuItem key="delete" onClick={confirmDelete}>
         {type === "comment" ? "Delete comment and replies" : "Delete comment"}
       </ContextMenuItem>
-      {!remark.isPublished && (
-        <ContextMenuItem onClick={publishRemark}>Publish comment</ContextMenuItem>
-      )}
-      <ContextMenuDivider />
-      <ContextMenuItem onClick={setFocusStart}>
-        <>
-          <Icon type="set-focus-start" />
-          Set focus start
-        </>
+    );
+  }
+  if (!remark.isPublished) {
+    contextMenuItems.push(
+      <ContextMenuItem key="publish" onClick={publishRemark}>
+        Publish comment
       </ContextMenuItem>
-      <ContextMenuItem onClick={setFocusEnd}>
-        <>
-          <Icon type="set-focus-end" />
-          Set focus end
-        </>
-      </ContextMenuItem>
-    </>
+    );
+  }
+  if (contextMenuItems.length > 0) {
+    contextMenuItems.push(<ContextMenuDivider key="divider" />);
+  }
+
+  contextMenuItems.push(
+    <ContextMenuItem key="focusStart" onClick={setFocusStart}>
+      <>
+        <Icon type="set-focus-start" />
+        Set focus start
+      </>
+    </ContextMenuItem>
   );
 
-  return { contextMenu, onContextMenu };
+  contextMenuItems.push(
+    <ContextMenuItem key="focusEnd" onClick={setFocusEnd}>
+      <>
+        <Icon type="set-focus-end" />
+        Set focus end
+      </>
+    </ContextMenuItem>
+  );
+
+  return useContextMenu(contextMenuItems);
 }
