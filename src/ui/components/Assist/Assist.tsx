385c12efb976694c451b7753a82939e0e69fa727
Jon Bell <github@lot23.com>
2023-03-26 19:46:37 +0000

    Putting assets in the right place

--- src/ui/components/Assist/Assist.tsx
+++ src/ui/components/Assist/Assist.tsx
@@ -53,51 +53,65 @@ const Tour: React.FC = () => {
 
   const info = useTestInfo();
 
-  const [videoUrl, setVideoUrl] = useState("/images/tour/consoleupdate.gif");
+  const [videoUrl, setVideoUrl] = useState(
+    "https://website-git-master-recordreplay.vercel.app/assist/consoleupdate.gif"
+  );
 
   const handleClick = (index: number) => {
     setSelectedItem(index);
 
     if (index === 0) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/open_devtools.gif");
     } else if (index === 1) {
-      setVideoUrl("/images/tour/focusmode.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/time_travel_in_console.gif"
+      );
     } else if (index === 2) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/set_print_statement.gif"
+      );
     } else if (index === 3) {
-      setVideoUrl("/images/tour/focusmode.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/add_a_comment.gif");
     } else if (index === 4) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/add_a_comment_to_code.gif"
+      );
     } else if (index === 5) {
-      setVideoUrl("/images/tour/focusmode.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/add_a_comment_to_network_request.gif"
+      );
     } else if (index === 6) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/add_a_comment_to_print_statement.gif"
+      );
     } else if (index === 7) {
-      setVideoUrl("/images/tour/focusmode.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/jump_to_code.gif");
     } else if (index === 8) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/unicorn_badge.gif");
     } else if (index === 9) {
-      setVideoUrl("/images/tour/focusmode.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/record_a_replay.gif");
     } else if (index === 10) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/explore_sources.gif");
     } else if (index === 11) {
-      setVideoUrl("/images/tour/focusmode.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/quick_open_a_file.gif"
+      );
     } else if (index === 12) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/launch_command_palette.gif"
+      );
     } else if (index === 13) {
-      setVideoUrl("/images/tour/focusmode.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/jump_to_an_event.gif");
     } else if (index === 14) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/inspect_an_element.gif"
+      );
     } else if (index === 15) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl(
+        "https://website-git-master-recordreplay.vercel.app/assist/inspect_an_component.gif"
+      );
     } else if (index === 16) {
-      setVideoUrl("/images/tour/focusmode.gif");
-    } else if (index === 17) {
-      setVideoUrl("/images/tour/focusmode.gif");
-    } else if (index === 18) {
-      setVideoUrl("/images/tour/focusmode.gif");
-    } else if (index === 19) {
-      setVideoUrl("/images/tour/consoleupdate.gif");
+      setVideoUrl("https://website-git-master-recordreplay.vercel.app/assist/use_focus_mode.gif");
     }
   };
 
@@ -123,24 +137,78 @@ const Tour: React.FC = () => {
 
   // Hard-coded checklist items
   const checklistItems = [
-    { label: "Open DevTools", videoUrl: "/images/tour/video1.gif" },
-    { label: "Time travel in the console", videoUrl: "/images/tour/video2.gif" },
-    { label: "Magic print statements", videoUrl: "/images/tour/video3.gif" },
-    { label: "Add a comment", videoUrl: "/images/tour/video4.gif" },
-    { label: "... to a line of code", videoUrl: "/images/tour/video5.gif" },
-    { label: "... to a network request", videoUrl: "/images/tour/video6.gif" },
-    { label: "... to a print statement", videoUrl: "/images/tour/video7.gif" },
-    { label: "Jump to code", videoUrl: "/images/tour/video8.gif" },
-    { label: "Add a unicorn badge", videoUrl: "/images/tour/video9.gif" },
-    { label: "Record a replay", videoUrl: "/images/tour/video10.gif" },
-    { label: "Explore sources", videoUrl: "/images/tour/video11.gif" },
-    { label: "Search source text", videoUrl: "/images/tour/video12.gif" },
-    { label: "Quick-open a file", videoUrl: "/images/tour/video13.gif" },
-    { label: "Launch command palette", videoUrl: "/images/tour/video14.gif" },
-    { label: "Jump to an event", videoUrl: "/images/tour/video15.gif" },
-    { label: "Inspect an element", videoUrl: "/images/tour/video16.gif" },
-    { label: "Inspect a component", videoUrl: "/images/tour/video17.gif" },
-    { label: "Use Focus Mode", videoUrl: "/images/tour/video18.gif" },
+    {
+      label: "Open DevTools",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video1.gif",
+    },
+    {
+      label: "Time travel in the console",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video2.gif",
+    },
+    {
+      label: "Magic print statements",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video3.gif",
+    },
+    {
+      label: "Add a comment",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video4.gif",
+    },
+    {
+      label: "... to a line of code",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video5.gif",
+    },
+    {
+      label: "... to a network request",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video6.gif",
+    },
+    {
+      label: "... to a print statement",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video7.gif",
+    },
+    {
+      label: "Jump to code",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video8.gif",
+    },
+    {
+      label: "Add a unicorn badge",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video9.gif",
+    },
+    {
+      label: "Record a replay",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video10.gif",
+    },
+    {
+      label: "Explore sources",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video11.gif",
+    },
+    {
+      label: "Search source text",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video12.gif",
+    },
+    {
+      label: "Quick-open a file",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video13.gif",
+    },
+    {
+      label: "Launch command palette",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video14.gif",
+    },
+    {
+      label: "Jump to an event",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video15.gif",
+    },
+    {
+      label: "Inspect an element",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video16.gif",
+    },
+    {
+      label: "Inspect a component",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video17.gif",
+    },
+    {
+      label: "Use Focus Mode",
+      videoUrl: "https://website-git-master-recordreplay.vercel.app/assist/video18.gif",
+    },
   ];
 
   return (
