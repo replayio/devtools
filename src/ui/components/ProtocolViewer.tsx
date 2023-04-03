0f52466832e07ea4950f594ffedb6f3c4e64f509
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-04-03 17:49:54 -0400

    Fix loader style/padding

--- src/ui/components/ProtocolViewer.tsx
+++ src/ui/components/ProtocolViewer.tsx
@@ -641,12 +641,18 @@ export const RecordedAppProtocolViewer = React.forwardRef(function RecordedAppPr
   let content: React.ReactNode;
 
   if (sourceDetails.length === 0) {
-    content = <i>Loading sources...</i>;
+    content = <h3 className={styles.Header}>Loading sources...</h3>;
   } else if (!isRecordingOfReplay) {
     content = <h3 className={styles.Header}>Not a recording of Replay</h3>;
   } else {
     content = (
-      <Suspense fallback={<Loader />}>
+      <Suspense
+        fallback={
+          <h3 className={styles.Header}>
+            <Loader />
+          </h3>
+        }
+      >
         <RecordedProtocolMessages sourceDetails={sourceDetails} />
       </Suspense>
     );
