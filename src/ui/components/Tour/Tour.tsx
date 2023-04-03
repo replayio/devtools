82cd28d45f5c45d96bb5f59821f1bf273d59e7a4
Jon Bell <github@lot23.com>
2023-04-03 01:29:33 +0000

    Fixing demo

--- src/ui/components/Tour/Tour.tsx
+++ src/ui/components/Tour/Tour.tsx
@@ -101,10 +101,7 @@ const Tour: React.FC = () => {
                         <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
                       </svg>
                     </div>
-                    <p>
-                      Now click the plus button on a line of code to set a print statement. Watch
-                      this!
-                    </p>
+                    <p>Now click the plus button on a line of code to set a print statement.</p>
                   </>
                 )}
 
