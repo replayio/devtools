71f4f65f5fdbf9a389909333c9749513fd2437c9
Jon Bell <github@lot23.com>
2023-04-05 23:10:36 +1200

    reverting so things don't fail

--- src/ui/components/ReplayAssistButton.tsx
+++ src/ui/components/ReplayAssistButton.tsx
@@ -3,14 +3,14 @@ import React, { FC, useEffect, useState } from "react";
 import useLocalStorage from "replay-next/src/hooks/useLocalStorage";
 import { UIThunkAction } from "ui/actions";
 import { setSelectedPrimaryPanel } from "ui/actions/layout";
-import { replayAssistToggled, showReplayAssist } from "ui/reducers/app";
+import { getReplayAssist, replayAssistToggled } from "ui/reducers/app";
 import { useAppDispatch, useAppSelector } from "ui/setup/hooks";
 import useAuth0 from "ui/utils/useAuth0";
 
 function toggleAndShowAssistPanel(): UIThunkAction {
   return (dispatch, getState) => {
     dispatch(replayAssistToggled());
-    const newValue = showReplayAssist(getState());
+    const newValue = getReplayAssist(getState());
     if (!newValue) {
       dispatch(setSelectedPrimaryPanel("events"));
     } else {
@@ -22,17 +22,17 @@ function toggleAndShowAssistPanel(): UIThunkAction {
 const ReplayAssistButton: FC = () => {
   const { isAuthenticated } = useAuth0();
   const dispatch = useAppDispatch();
-  const shouldShowReplayAssist = useAppSelector(showReplayAssist);
+  const shouldShowReplayAssist = useAppSelector(getReplayAssist);
 
   if (!isAuthenticated) {
     return null;
   }
 
   return (
-    <div className="row row-flat rounded-none border-t border-b">
+    <div className="border-t border-b rounded-none row row-flat">
       <input
         type="checkbox"
-        className="rounded-sm border-gray-400"
+        className="border-gray-400 rounded-sm"
         id="replay-assist-checkbox"
         checked={shouldShowReplayAssist}
         onChange={() => {
