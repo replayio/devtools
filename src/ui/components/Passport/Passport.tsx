bd5e27c1b2a2ff2889fda469512610c18a259f40
Jon Bell <github@lot23.com>
2023-04-11 12:32:05 +1200

    Making things look much nicer

--- src/ui/components/Passport/Passport.tsx
+++ src/ui/components/Passport/Passport.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Icon from "replay-next/components/Icon";
 import hooks from "ui/hooks";
@@ -14,11 +14,49 @@ import {
   shouldShowLaunchCommandPalette,
   shouldShowRecordReplay,
   shouldShowSearchSourceText,
-  shouldShowTour,
   shouldShowUseFocusMode,
 } from "ui/utils/onboarding";
 
 import styles from "./Passport.module.css";
+
+const useRandomPosition = (
+  bottomRange: [number, number],
+  rightRange: [number, number],
+  deps: any[]
+) => {
+  const initialState = () => {
+    const randomBottom = Math.floor(
+      Math.random() * (bottomRange[1] - bottomRange[0]) + bottomRange[0]
+    );
+    const randomRight = Math.floor(Math.random() * (rightRange[1] - rightRange[0]) + rightRange[0]);
+    return { bottom: randomBottom, right: randomRight };
+  };
+
+  const [position, setPosition] = useState(initialState);
+
+  useEffect(() => {
+    setPosition(initialState());
+  }, deps);
+
+  return position;
+};
+
+const useRandomRotation = (rotationRange: [number, number], deps: any[]) => {
+  const initialState = () => {
+    const randomRotation = Math.floor(
+      Math.random() * (rotationRange[1] - rotationRange[0]) + rotationRange[0]
+    );
+    return randomRotation;
+  };
+
+  const [rotation, setRotation] = useState(initialState());
+
+  useEffect(() => {
+    setRotation(initialState());
+  }, deps);
+
+  return rotation;
+};
 
 const Passport: React.FC = () => {
   const { nags } = hooks.useGetUserInfo();
@@ -34,8 +72,21 @@ const Passport: React.FC = () => {
   const showJumpToEvent = shouldShowJumpToEvent(nags);
   const showInspectElement = shouldShowInspectElement(nags);
   const showUseFocusMode = shouldShowUseFocusMode(nags);
+  const [stepIndex, setStepIndex] = useState(0);
 
-  const [videoVisible, setVideoVisible] = useState(false);
+  const renderCheckmarkIcon = (completed: boolean | undefined) => {
+    if (completed === false) {
+      return "checked-rounded";
+    }
+    return "unchecked-rounded";
+  };
+
+  const renderCompletedImage = (completed: boolean | undefined, imageBaseName: string) => {
+    if (completed != true) {
+      return `/images/passport/${imageBaseName}-complete.svg`;
+    }
+    return null;
+  };
 
   const getImageName = (label: string, completed: boolean) => {
     const baseName = label.toLowerCase().replace(/ /g, "_");
@@ -49,7 +100,6 @@ const Passport: React.FC = () => {
       className={`absolute top-0 left-0 w-full ${styles.itemImage}`}
     />
   );
-  const showTour = shouldShowTour(nags);
 
   const handleClick = (index: number) => {
     setStepIndex(index);
@@ -61,98 +111,118 @@ const Passport: React.FC = () => {
 
   const updatedChecklistItems = [
     {
-      label: "Tour Grad",
-      completed: showTour,
-      videoUrl: "https://vercel.replay.io/assist/set_print_statement.gif",
-    },
-    {
       label: "Time travel in the console",
       completed: showConsoleNavigate,
       videoUrl: "https://vercel.replay.io/assist/time_travel_in_console.gif",
+      imageBaseName: "time_travel_in_the_console",
     },
     {
       label: "Set a print statement",
       completed: showBreakpointEdit,
       videoUrl: "https://vercel.replay.io/assist/set_print_statement.gif",
+      imageBaseName: "set_a_print_statement",
     },
-
-    {
-      label: "Add a unicorn badge",
-      completed: showAddUnicornBadge,
-      videoUrl: "https://vercel.replay.io/assist/unicorn_badge.gif",
-    },
-
-    {
-      label: "Search source text",
-      completed: showSearchSourceText,
-      videoUrl: "https://vercel.replay.io/assist/search_source_text.gif",
-    },
-    {
-      label: "Jump to code",
-      completed: showJumpToCode,
-      videoUrl: "https://vercel.replay.io/assist/jump_to_code.gif",
-    },
-
     {
       label: "Launch command palette",
       completed: showLaunchCommandPalette,
       videoUrl: "https://vercel.replay.io/assist/launch_command_palette.gif",
+      imageBaseName: "launch_command_palette",
     },
     {
       label: "Explore sources",
       completed: showExploreSources,
       videoUrl: "https://vercel.replay.io/assist/explore_sources.gif",
+      imageBaseName: "explore_sources",
+    },
+    {
+      label: "Search source text",
+      completed: showSearchSourceText,
+      videoUrl: "https://vercel.replay.io/assist/search_source_text.gif",
+      imageBaseName: "search_source_text",
     },
     {
       label: "Jump to event",
       completed: showJumpToEvent,
       videoUrl: "https://vercel.replay.io/assist/jump_to_an_event.gif",
+      imageBaseName: "jump_to_event",
+    },
+    {
+      label: "Jump to code",
+      completed: showJumpToCode,
+      videoUrl: "https://vercel.replay.io/assist/jump_to_code.gif",
+      imageBaseName: "jump_to_code",
     },
     {
       label: "Add a comment",
       completed: showAddComment,
       videoUrl: "https://vercel.replay.io/assist/add_a_comment.gif",
+      imageBaseName: "add_a_comment",
     },
-
     {
-      label: "Inspect element",
-      completed: showInspectElement,
-      videoUrl: "https://vercel.replay.io/assist/inspect_an_element.gif",
+      label: "Add a unicorn badge",
+      completed: showAddUnicornBadge,
+      videoUrl: "https://vercel.replay.io/assist/unicorn_badge.gif",
+      imageBaseName: "add_a_unicorn_badge",
     },
-
     {
       label: "Use focus mode",
       completed: showUseFocusMode,
       videoUrl: "https://vercel.replay.io/assist/use_focus_mode.gif",
+      imageBaseName: "use_focus_mode",
+    },
+    {
+      label: "Record a replay",
+      completed: showRecordReplay,
+      videoUrl: "https://vercel.replay.io/assist/record_a_replay.gif",
+      imageBaseName: "record_a_replay",
+    },
+    {
+      label: "Inspect element",
+      completed: showInspectElement,
+      videoUrl: "https://vercel.replay.io/assist/inspect_an_element.gif",
+      imageBaseName: "inspect_element",
     },
   ];
 
-  const [stepIndex, setStepIndex] = useState(0);
   const selectedItem = updatedChecklistItems[stepIndex];
+  const randomPosition = useRandomPosition([225, 410], [-45, 10], [stepIndex]);
+  const randomRotation = useRandomRotation([-20, 20], [stepIndex]);
 
   return (
     <div className={styles.AssistBoxWrapper}>
       <div className={styles.AssistBoxGradient}>
         <div className={styles.AssistBox}>
-          <div className={styles.AssistBoxInternal}>
-            <div className={styles.checklist}>
-              <img src={`/images/passport/passportHeader.svg`} className={`my-3 w-full px-3`} />
+          <div className="p-0 pt-3">
+            <img src={`/images/passport/passportHeader.svg`} className={`mb-5 w-full px-1`} />
+            <div className={styles.AssistBoxInternal}>
+              <div className={styles.checklist}>
+                {updatedChecklistItems.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`flex ${getItemStyle(index)} ${styles.checklistItem}`}
+                    onClick={() => handleClick(index)}
+                  >
+                    <Icon className={styles.stepIcon} type={renderCheckmarkIcon(item.completed)} />
 
-              {updatedChecklistItems.map((item, index) => (
-                <div
-                  key={index}
-                  className={`flex flex-col ${styles.checklistItem}`}
-                  onClick={() => handleClick(index)}
-                >
-                  <div className={`relative ${getItemStyle(index)} ${styles.itemContainer}`}>
-                    {renderItemImage(item.label, item.completed)}
+                    <span className="ml-2">{item.label}</span>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
-        <div className="absolute bottom-32 w-full px-2">
+        {renderCompletedImage(selectedItem.completed, selectedItem.imageBaseName) && (
+          <img
+            src={renderCompletedImage(selectedItem.completed, selectedItem.imageBaseName)}
+            className={styles.largeCompletedImage}
+            style={{
+              bottom: `${randomPosition.bottom}px`,
+              right: `${randomPosition.right}%`,
+              transform: `rotate(${randomRotation}deg)`,
+            }}
+          />
+        )}
+        <div className={styles.videoExampleWrapper}>
           <img src={selectedItem.videoUrl} className={styles.videoExample} />
         </div>
       </div>
