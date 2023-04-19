87f1ddfaa464ef05bd731149c3e721e89a0a71b7
Jon Bell <github@lot23.com>
2023-04-20 11:42:39 +1200

    Fixing tour

--- src/ui/components/Tour/Tour.tsx
+++ src/ui/components/Tour/Tour.tsx
@@ -55,130 +55,168 @@ const Tour: React.FC = () => {
 
   const { dismissTourNag } = useNagDismissal();
 
+  const Intro = ({ typeOfReplay }) => (
+    <div className={styles.intro}>
+      <p className={styles.h1}>Hello and welcome!</p>
+      {typeOfReplay === "events" ? (
+        <>
+          <p>Replay is the first time-travel enabled DevTools. Let's get started!</p>
+          <p>
+            <em>Click on DevTools in the top right.</em>
+          </p>
+        </>
+      ) : null}
+    </div>
+  );
+
+  const HelloAgain = ({ typeOfReplay }) => (
+    <div className={styles.intro}>
+      <p className={styles.h1}>Hello again!</p>
+      <p>To continue, please click the DevTools toggle at the top right.</p>
+    </div>
+  );
+
+  const TimeTravel = () => (
+    <div className={styles.intro}>
+      <div className={styles.h1}>Time travel 🚀</div>
+      <p>In DevTools, look underneath the video to find the Replay console.</p>
+      <p>Hover over the console and you’ll see a fast-forward button. Click it to time travel!</p>
+    </div>
+  );
+
+  const TimeTravelGif = () => (
+    <img src="https://vercel.replay.io/tour/fast-forward.gif" className={styles.videoExample} />
+  );
+
+  const OneClickLogs = () => (
+    <div className={styles.intro}>
+      <div className={styles.h1}>
+        One-click logs
+        <svg className={styles.logPoint} viewBox="0 0 24 24" fill="currentColor">
+          <path d="M0 0h24v24H0z" fill="none"></path>
+          <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
+        </svg>
+      </div>
+
+      <p>
+        Now a file should be open in the Source Viewer. (If not, you can open a file by pressing
+        command-P or clicking Sources Explorer from the left-nav)
+      </p>
+      <p>Click the plus button on a line of code to set a print statement.</p>
+    </div>
+  );
+
+  const OneClickLogsGif = () => (
+    <img src="https://vercel.replay.io/tour/addlogs.gif" className={styles.videoExample} />
+  );
+
+  const EditLogs = () => (
+    <div className={styles.intro}>
+      <div className={styles.h1}>One-click logs 🦄</div>
+      <p>You can pass anything you want here, including objects and variables.</p>
+      <p>
+        <em>Type something, then hit enter or click the check button.</em>
+      </p>
+    </div>
+  );
+
+  const EditLogsGif = () => (
+    <img src="https://vercel.replay.io/tour/editlogs.gif" className={styles.videoExample} />
+  );
+
+  const CompletedTour = ({ setShowConfetti, setShowPassport, dismissTourNag }) => (
+    <div className={styles.intro}>
+      <div className={styles.h1}>Check it out!</div>
+      <p>Take a look at the console.</p>
+      <p>
+        Replay just re-ran your recording and retroactively added your print statement each time
+        that line of code was called. 🤯
+      </p>
+      <p>And with that, you've graduated. Happy exploring!</p>
+      <p className="mt-8">
+        <a
+          href="#"
+          onClick={e => {
+            e.stopPropagation();
+            setShowConfetti(true);
+            setShowPassport(true);
+            setTimeout(() => {
+              setShowConfetti(false);
+              dismissTourNag();
+            }, 2500);
+          }}
+          className="hover:cursor-hand whitespace-nowrap rounded-lg bg-white px-3 py-1 font-medium text-primaryAccent shadow-lg hover:bg-blue-50"
+        >
+          Thanks!
+        </a>
+        {showConfetti ? <Confetti /> : null}
+      </p>
+      <img
+        src={`/images/passport/tour_grad-default.png`}
+        className={styles.largeCompletedImage}
+        style={{
+          zIndex: 0,
+          opacity: 0.75,
+          bottom: `300px`,
+          transform: `rotate(14deg)`,
+        }}
+      />
+    </div>
+  );
+
+  const CompletedTourGif = () => (
+    <img src="https://vercel.replay.io/tour/consoleupdate.gif" className={styles.videoExample} />
+  );
+
   return (
     <div className={styles.TourBoxWrapper}>
       <div className={styles.TourBoxGradient}>
         <div className={styles.TourBox}>
           <div className="p-2 pt-3">
             {isNewUser ? (
-              <div className={styles.intro}>
-                <p className={styles.h1}>Hello and welcome!</p>
-                {typeOfReplay === "events" ? (
-                  <>
-                    <p>
-                      Replay is the first time-travel enabled DevTools. It’s designed to be
-                      familiar, futuristic, and fun.
-                    </p>
-                    <p>To get started, click on DevTools in the top right.</p>
-                  </>
-                ) : null}
-              </div>
+              <Intro typeOfReplay={typeOfReplay} />
             ) : (
-              <div className={styles.intro}>
-                {showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && (
+              <>
+                {viewMode === "non-dev" ? (
+                  <HelloAgain typeOfReplay={typeOfReplay} />
+                ) : (
                   <>
-                    <div className={styles.h1}>Time travel 🚀</div>
-                    <p>Look underneath the video to find the Replay console.</p>
-                    <p>
-                      Hover over the console and you’ll see a fast-forward button. Click it to time
-                      travel!
-                    </p>
+                    {showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && (
+                      <TimeTravel />
+                    )}
+                    {!showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && (
+                      <OneClickLogs />
+                    )}
+                    {!showConsoleNavigate && !showBreakpointAdd && showBreakpointEdit && (
+                      <EditLogs />
+                    )}
+                    {hasCompletedTour && (
+                      <CompletedTour
+                        setShowConfetti={setShowConfetti}
+                        setShowPassport={setShowPassport}
+                        dismissTourNag={dismissTourNag}
+                      />
+                    )}
                   </>
                 )}
-
-                {!showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && (
-                  <>
-                    <div className={styles.h1}>
-                      One-click logs
-                      <svg className={styles.logPoint} viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M0 0h24v24H0z" fill="none"></path>
-                        <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path>
-                      </svg>
-                    </div>
-                    <p>Now click the plus button on a line of code to set a print statement.</p>
-                  </>
-                )}
-
-                {!showConsoleNavigate && !showBreakpointAdd && showBreakpointEdit && (
-                  <>
-                    <div className={styles.h1}>One-click logs 🦄</div>
-                    <p>You can pass anything you want here, including objects and variables.</p>
-                    <p>Later we can even show you how to add a unicorn.</p>
-                    <p>Type something here and hit enter.</p>
-                  </>
-                )}
-
-                {hasCompletedTour && (
-                  <>
-                    <div className={styles.h1}>Check it out!</div>
-                    <p>Take a look at the console.</p>
-                    <p>
-                      Replay just re-ran your recording and retroactively added your print statement
-                      each time that line of code was called. 🤯
-                    </p>
-                    <p>And with that, you've graduated from the tour. Happy exploring!</p>
-                    <p className="mt-8">
-                      <a
-                        href="#"
-                        onClick={e => {
-                          e.stopPropagation();
-                          setShowConfetti(true);
-                          setShowPassport(true);
-                          setTimeout(() => {
-                            setShowConfetti(false);
-                            dismissTourNag();
-                          }, 2500);
-                        }}
-                        className="hover:cursor-hand whitespace-nowrap rounded-lg bg-white px-3 py-1 font-medium text-primaryAccent shadow-lg hover:bg-blue-50"
-                      >
-                        Thanks!
-                      </a>
-                      {showConfetti ? <Confetti /> : null}
-                    </p>
-                    <img
-                      src={`/images/passport/tour_grad-default.png`}
-                      className={styles.largeCompletedImage}
-                      style={{
-                        zIndex: 0,
-                        opacity: 0.75,
-                        bottom: `300px`,
-                        transform: `rotate(14deg)`,
-                      }}
-                    />
-                  </>
-                )}
-              </div>
+              </>
             )}
           </div>
         </div>
       </div>
       <div className="absolute bottom-28 p-3">
-        {isNewUser && (
+        {(isNewUser || viewMode === "non-dev") && (
           <div className="relative -bottom-3">
             <img src="/images/illustrations/larry_wave.png" className="z-1 w-full" />
           </div>
         )}
-
-        {!isNewUser && showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && (
-          <img
-            src="https://vercel.replay.io/tour/fast-forward.gif"
-            className={styles.videoExample}
-          />
-        )}
-
-        {!showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && (
-          <img src="https://vercel.replay.io/tour/addlogs.gif" className={styles.videoExample} />
-        )}
-
-        {!showConsoleNavigate && !showBreakpointAdd && showBreakpointEdit && (
-          <img src="https://vercel.replay.io/tour/editlogs.gif" className={styles.videoExample} />
-        )}
-
-        {hasCompletedTour && (
-          <img
-            src="https://vercel.replay.io/tour/consoleupdate.gif"
-            className={styles.videoExample}
-          />
+        {!isNewUser && viewMode !== "non-dev" && (
+          <>
+            {showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && <TimeTravelGif />}
+            {!showConsoleNavigate && showBreakpointAdd && showBreakpointEdit && <OneClickLogsGif />}
+            {!showConsoleNavigate && !showBreakpointAdd && showBreakpointEdit && <EditLogsGif />}
+            {hasCompletedTour && <CompletedTourGif />}
+          </>
         )}
       </div>
     </div>
