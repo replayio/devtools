d76648b43f1908553b387bc1b98f87719b9fa1e9
Mark Erikson <mark@isquaredsoftware.com>
2023-04-28 14:30:41 -0400

    Reimplement Cypress "Jump to Code" button display and logic

--- src/ui/components/TestSuite/views/TestItem/TestSteps/AnnotatedTestStepRow.tsx
+++ src/ui/components/TestSuite/views/TestItem/TestSteps/AnnotatedTestStepRow.tsx
@@ -1,15 +1,18 @@
-import { Suspense, memo, useContext, useMemo } from "react";
+import { Suspense, memo, useContext, useMemo, useState } from "react";
 
 import { getExecutionPoint } from "devtools/client/debugger/src/selectors";
 import Loader from "replay-next/components/Loader";
 import useSuspendAfterMount from "replay-next/src/hooks/useSuspendAfterMount";
 import { ReplayClientContext } from "shared/client/ReplayClientContext";
-import { JumpToCodeButton } from "ui/components/shared/JumpToCodeButton";
+import { seek } from "ui/actions/timeline";
+import { jumpToClickEventFunctionLocation } from "ui/components/Events/Event";
+import { PointWithEventType } from "ui/components/Events/Event";
+import { JumpToCodeButton, JumpToCodeStatus } from "ui/components/shared/JumpToCodeButton";
 import { useJumpToSource } from "ui/components/TestSuite/hooks/useJumpToSource";
 import { getConsolePropsCountSuspense } from "ui/components/TestSuite/suspense/consoleProps";
 import { AnnotatedTestStep, ProcessedTestMetadata } from "ui/components/TestSuite/types";
 import { Position } from "ui/components/TestSuite/views/TestItem/types";
-import { useAppSelector } from "ui/setup/hooks";
+import { useAppDispatch, useAppSelector } from "ui/setup/hooks";
 
 import styles from "./AnnotatedTestStepRow.module.css";
 
@@ -24,7 +27,9 @@ export default memo(function AnnotatedTestStepRow({
 }) {
   const { annotations, args, error, index, name } = testStep.data;
 
+  const dispatch = useAppDispatch();
   const executionPoint = useAppSelector(getExecutionPoint);
+  const [jumpToCodeStatus, setJumpToCodeStatus] = useState<JumpToCodeStatus>("not_checked");
 
   const argsString = useMemo(() => {
     if (args) {
@@ -33,12 +38,64 @@ export default memo(function AnnotatedTestStepRow({
     return "";
   }, [args]);
 
-  const { disabled, onClick } = useJumpToSource({ testMetadata, testStep });
+  const { disabled: jumpToTestSourceDisabled, onClick: onClickJumpToTestSource } = useJumpToSource({
+    testMetadata,
+    testStep,
+  });
+
+  const onJumpToClickEvent = async () => {
+    const onSeek = (point: string, time: number) => dispatch(seek(point, time, true));
+    // We only support clicks and keypress events for now.
+    // The rendering logic has limited the button display
+
+    const cypressStepTypesToEventTypes = {
+      click: "mousedown",
+      type: "keypress",
+    } as const;
+    const eventKind =
+      cypressStepTypesToEventTypes[testStep.data.name as keyof typeof cypressStepTypesToEventTypes];
+
+    const { start, end } = annotations;
+
+    if (!eventKind || !start) {
+      // Shouldn't happen - the rendering logic _should_ make sure the step matches
+      return;
+    }
+
+    const eventPoint: PointWithEventType = { ...start, kind: eventKind };
+
+    setJumpToCodeStatus("loading");
+    const result = await dispatch(jumpToClickEventFunctionLocation(onSeek, eventPoint, end));
+    setJumpToCodeStatus(result);
+
+    if (result === "not_loaded") {
+      // Clear this out after a few seconds since the user could change focus.
+      // Simpler than trying to watch the focus region change over time.
+      setTimeout(() => {
+        setJumpToCodeStatus("not_checked");
+      }, 5000);
+    }
+  };
 
   const showBadge = testStep.data.name === "get";
 
+  // TODO This is very Cypress-specific. Playwright steps have a `name` like `locator.click("blah")`.
+  // We only care about click events and keyboard events. Keyboard events appear to be a "type" command,
+  // as in "type this text into the input".
+  let shouldShowJumpToCode = false;
+  if ("category" in testStep.data) {
+    shouldShowJumpToCode =
+      "category" in testStep.data &&
+      testStep.data.category === "command" &&
+      (testStep.data.name === "click" || testStep.data.name === "type");
+  }
+
   return (
-    <div className={styles.Row} data-status={error ? "error" : "success"}>
+    <div
+      className={styles.Row}
+      data-status={error ? "error" : "success"}
+      onClick={jumpToTestSourceDisabled ? undefined : onClickJumpToTestSource}
+    >
       <div className={styles.Text}>
         <span className={styles.Number}>{index + 1}</span>{" "}
         <span className={`${styles.Name} ${styles.Name}`} data-name={name} title={name}>
@@ -53,13 +110,12 @@ export default memo(function AnnotatedTestStepRow({
           <Badge testStep={testStep} position={position} />
         </Suspense>
       )}
-      {position === "current" && annotations.start && (
+      {position === "current" && shouldShowJumpToCode && annotations.start && (
         <div className={styles.JumpToCodeButton}>
           <JumpToCodeButton
             currentExecutionPoint={executionPoint}
-            disabled={disabled}
-            onClick={onClick}
-            status="not_checked"
+            onClick={onJumpToClickEvent}
+            status={jumpToCodeStatus}
             targetExecutionPoint={annotations.start.point}
           />
         </div>
