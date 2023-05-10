cb0e90a0845d324a8d8862689ec31b5f8e38c9a7
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-05-10 10:12:44 -0400

    Fix replay-next CSS token error

--- packages/replay-next/next.config.js
+++ packages/replay-next/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["design"],
+  transpilePackages: ["design", "use-context-menu"],
 
   // This setting allows the Next app to import code from e.g. "packages/protocol"
   experimental: {
