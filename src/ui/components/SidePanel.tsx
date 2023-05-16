7d126cdd87c904a970bcc8d2d7965999ef043bbe
Jon Bell <github@lot23.com>
2023-05-17 08:50:46 +1200

    Improved logged out experience (NGX) (#9203)
    * Add NGX
    
    * if/elses to improve clarity

--- src/ui/components/SidePanel.tsx
+++ src/ui/components/SidePanel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import PrimaryPanes from "devtools/client/debugger/src/components/PrimaryPanes";
 import SecondaryPanes from "devtools/client/debugger/src/components/SecondaryPanes";
@@ -37,8 +37,20 @@ function useInitialPrimaryPanel() {
 
   const { nags } = hooks.useGetUserInfo();
   const showTour = shouldShowTour(nags);
-  const initialPrimaryPanel =
-    recording && isTestSuiteReplay(recording) ? "cypress" : showTour ? "tour" : "events";
+  const { isAuthenticated } = useAuth0();
+
+  const { comments } = hooks.useGetComments(recordingId);
+
+  let initialPrimaryPanel;
+  if (recording && isTestSuiteReplay(recording)) {
+    initialPrimaryPanel = "cypress";
+  } else if (showTour) {
+    initialPrimaryPanel = "tour";
+  } else if (!isAuthenticated && comments.length > 0) {
+    initialPrimaryPanel = "comments";
+  } else {
+    initialPrimaryPanel = "events";
+  }
 
   useEffect(() => {
     if (selectedPrimaryPanel == null) {
@@ -92,25 +104,44 @@ export default function SidePanel() {
   const { recording } = useGetRecording(recordingId);
   const testSuite = recording && isTestSuiteReplay(recording);
 
+  const { comments, loading } = hooks.useGetComments(recordingId);
+
+  const [isNGXHeaderVisible, setIsNGXHeaderVisible] = useState(
+    localStorage.getItem("NGXHeaderVisibility") !== "hidden"
+  );
+
+  const initialPanelRef = useRef(selectedPrimaryPanel);
+
+  useEffect(() => {
+    if (selectedPrimaryPanel !== initialPanelRef.current) {
+      setIsNGXHeaderVisible(false);
+      localStorage.setItem("NGXHeaderVisibility", "hidden");
+    }
+  }, [selectedPrimaryPanel]);
+
   return (
     <div className="flex w-full flex-col gap-2">
-      {!isAuthenticated && !testSuite && (
+      {!isAuthenticated && !testSuite && isNGXHeaderVisible && (
         <div className={styles.TourBox}>
           <h2>Welcome to Replay!</h2>
-          <p>Just getting started with time travel debugging? Check out our docs!</p>
-
-          <button
-            type="button"
-            onClick={() => launchQuickstart("https://docs.replay.io/debugging")}
-            style={{ padding: "4px 8px" }}
-          >
-            <div className="mr-1">Documentation</div>
-            <MaterialIcon style={{ fontSize: "16px" }}>arrow_forward</MaterialIcon>
-          </button>
+          {comments.length > 0 ? (
+            <p>Check out the comments below to get started:</p>
+          ) : (
+            <>
+              <p>Just getting started with time travel debugging? Check out our docs!</p>
+              <button
+                type="button"
+                onClick={() => launchQuickstart("https://docs.replay.io/debugging")}
+                style={{ padding: "4px 8px" }}
+              >
+                <div className="mr-1">Documentation</div>
+                <MaterialIcon style={{ fontSize: "16px" }}>arrow_forward</MaterialIcon>
+              </button>
+            </>
+          )}
         </div>
       )}
-
-      {!isAuthenticated && testSuite && (
+      {!isAuthenticated && testSuite && isNGXHeaderVisible && (
         <div className={styles.TourBox}>
           <h2>Welcome! 👋</h2>
           <p>We've written some docs to get the most out of Replay test suites. Check them out!</p>
