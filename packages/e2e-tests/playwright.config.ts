6372971bdf677c4b42dca7a6d998912803ae68ea
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-05-17 16:49:41 -0400

    Create separate Chromium-based Workflow for authenticated tests

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -1,7 +1,7 @@
 import { PlaywrightTestConfig, devices } from "@playwright/test";
 import { devices as replayDevices } from "@replayio/playwright";
 
-const { CI, SLOW_MO } = process.env;
+const { CHROMIUM_ONLY, CI, SLOW_MO } = process.env;
 
 const config: PlaywrightTestConfig = {
   use: {
@@ -24,31 +24,32 @@ const config: PlaywrightTestConfig = {
 
   // Limit the number of workers on CI, use default locally
   workers: CI ? 4 : undefined,
-  projects: CI
-    ? [
-        // {
-        //   name: "replay-firefox",
-        //   use: { ...(replayDevices["Replay Firefox"] as any) },
-        // },
-        // {
-        //   name: "firefox",
-        //   use: { ...devices["Desktop Firefox"] },
-        // },
-        {
-          name: "replay-chromium",
-          use: { ...(replayDevices["Replay Chromium"] as any) },
-        },
-        {
-          name: "chromium",
-          use: { ...devices["Desktop Chromium"] },
-        },
-      ]
-    : [
-        {
-          name: "chromium",
-          use: { ...devices["Desktop Chromium"] },
-        },
-      ],
+  projects:
+    CI && !CHROMIUM_ONLY
+      ? [
+          // {
+          //   name: "replay-firefox",
+          //   use: { ...(replayDevices["Replay Firefox"] as any) },
+          // },
+          // {
+          //   name: "firefox",
+          //   use: { ...devices["Desktop Firefox"] },
+          // },
+          {
+            name: "replay-chromium",
+            use: { ...(replayDevices["Replay Chromium"] as any) },
+          },
+          {
+            name: "chromium",
+            use: { ...devices["Desktop Chromium"] },
+          },
+        ]
+      : [
+          {
+            name: "chromium",
+            use: { ...devices["Desktop Chromium"] },
+          },
+        ],
 };
 
 export default config;
