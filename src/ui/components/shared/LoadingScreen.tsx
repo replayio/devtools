293a64302b1f4ec26435c36cc180b732a1f94cb8
Jon Bell <github@lot23.com>
2023-05-19 16:43:39 +1200

    Improved loading screen UX

--- src/ui/components/shared/LoadingScreen.tsx
+++ src/ui/components/shared/LoadingScreen.tsx
@@ -6,9 +6,9 @@ import { getAwaitingSourcemaps, getUploading } from "ui/reducers/app";
 import { UIState } from "ui/state";
 
 import { BubbleViewportWrapper } from "./Viewport";
+import styles from "./LoadingScreen.module.css";
 
 const colorOptions: Array<"blue" | "green" | "red"> = ["blue", "green", "red"];
-
 const Hoverboard = dynamic(() => import("./Hoverboard"), {
   ssr: false,
   loading: () => <div />,
@@ -28,10 +28,10 @@ export function LoadingScreenTemplate({ children }: { children?: ReactNode }) {
   }, [hoverboardColor]);
 
   return (
-    <BubbleViewportWrapper>
-      <div className="relative flex w-96 flex-col items-center space-y-1 rounded-lg bg-loadingBoxes p-4 py-12 shadow-sm">
+    <BubbleViewportWrapper className={styles.viewportWrapper}>
+      <div className={styles.loadingScreenWrapper}>
         <div className="flex flex-col items-center space-y-2">
-          <div className="h-32 w-32" onClick={changeHoverboardColor}>
+          <div className={styles.hoverboardWrapper} onClick={changeHoverboardColor}>
             <Hoverboard color={hoverboardColor} />
           </div>
           {children}
@@ -45,32 +45,48 @@ function LoadingScreen({
   uploading,
   awaitingSourcemaps,
   fallbackMessage,
-  stalledTimeout = 9000,
+  stalledTimeout = 15000,
 }: PropsFromRedux & { fallbackMessage: string; stalledTimeout?: number }) {
-  // The backend send events in this order: uploading replay -> uploading sourcemaps.
-  let waitingForMessage = <span>{fallbackMessage}</span>;
-  if (awaitingSourcemaps) {
-    waitingForMessage = <span>Uploading sourcemaps...</span>;
-    stalledTimeout = Infinity;
-  } else if (uploading) {
-    waitingForMessage = <span>Uploading {Math.round(+uploading.amount)}Mb</span>;
-    stalledTimeout = Infinity;
-  }
+  const phrases = [
+    "Tuning the flux capacitor...",
+    "Prepping DeLorean for time travel...",
+    "Revving up to 88 miles per hour...",
+    "Gathering 1.21 gigawatts of power...",
+    "Ensuring temporal paradox safeguards...",
+    "Being careful not to disrupt the timeline...",
+  ];
+  const longWaitMessage =
+    "<div><p>This is taking longer than expected.</p><p><a href='http://replay.io/discord' target='discord'>Report problems on Discord</a></p>";
 
-  let [stalled, setStalled] = useState<boolean>(false);
+  const [message, setMessage] = useState(fallbackMessage);
+
+  const changeMessage = () => {
+    const randomIndex = Math.floor(Math.random() * phrases.length);
+    setMessage(phrases[randomIndex]);
+  };
+
   useEffect(() => {
-    if (stalledTimeout === Infinity) {
-      return;
-    }
+    const phraseTimeout = setTimeout(changeMessage, 5000);
+    return () => clearTimeout(phraseTimeout);
+  }, []);
 
-    const timeout = setTimeout(() => setStalled(true), stalledTimeout);
+  useEffect(() => {
+    const stalledTimeoutId = setTimeout(() => {
+      setMessage(longWaitMessage);
+    }, stalledTimeout);
+    return () => clearTimeout(stalledTimeoutId);
+  }, []);
 
-    return () => clearTimeout(timeout);
-  }, [fallbackMessage, stalledTimeout]);
+  const waitingForMessage =
+    awaitingSourcemaps || uploading ? (
+      <span>Uploading {Math.round(uploading?.amount || 0)}Mb</span>
+    ) : (
+      <span dangerouslySetInnerHTML={{ __html: message }}></span>
+    );
 
   return (
     <LoadingScreenTemplate>
-      <span className="text-sm">{stalled ? "Reticulating splines..." : waitingForMessage}</span>
+      <span className={styles.messageWrapper} dangerouslySetInnerHTML={{ __html: message }}></span>
     </LoadingScreenTemplate>
   );
 }
