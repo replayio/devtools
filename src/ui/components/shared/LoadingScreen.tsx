bfc151388ef318f9e32f40faf36680739c8364e9
Jon Bell <github@lot23.com>
2023-05-19 16:46:52 +1200

    Improved loading screen

--- src/ui/components/shared/LoadingScreen.tsx
+++ src/ui/components/shared/LoadingScreen.tsx
@@ -1,100 +1,276 @@
-import dynamic from "next/dynamic";
-import React, { ReactNode, useEffect, useState } from "react";
+import Head from "next/head";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { ConnectedProps, connect } from "react-redux";
+import {
+  ImperativePanelHandle,
+  Panel,
+  PanelGroup,
+  PanelResizeHandle,
+} from "react-resizable-panels";
 
-import { getAwaitingSourcemaps, getUploading } from "ui/reducers/app";
+import InspectorContextReduxAdapter from "devtools/client/debugger/src/components/shared/InspectorContextReduxAdapter";
+import { ThreadFront } from "protocol/thread";
+import { ExpandablesContextRoot } from "replay-next/src/contexts/ExpandablesContext";
+import { PointsContextRoot } from "replay-next/src/contexts/points/PointsContext";
+import { SelectedFrameContextRoot } from "replay-next/src/contexts/SelectedFrameContext";
+import useLocalStorage from "replay-next/src/hooks/useLocalStorage";
+import usePreferredFontSize from "replay-next/src/hooks/usePreferredFontSize";
+import { clearTrialExpired, createSocket } from "ui/actions/session";
+import TerminalContextAdapter from "ui/components/SecondaryToolbox/TerminalContextAdapter";
+import { useGetRecording, useGetRecordingId } from "ui/hooks/recordings";
+import { useFeature } from "ui/hooks/settings";
+import { useTrackLoadingIdleTime } from "ui/hooks/tracking";
+import { useUserIsAuthor } from "ui/hooks/users";
+import { getViewMode } from "ui/reducers/layout";
+import { useAppSelector } from "ui/setup/hooks";
 import { UIState } from "ui/state";
+import { isTest } from "ui/utils/environment";
+import {
+  endUploadWaitTracking,
+  maybeSetGuestMixpanelContext,
+  trackEventOnce,
+} from "ui/utils/mixpanel";
+import tokenManager, { TokenState } from "ui/utils/tokenManager";
+import useAuth0 from "ui/utils/useAuth0";
 
-import { BubbleViewportWrapper } from "./Viewport";
-import styles from "./LoadingScreen.module.css";
+import { selectors } from "../reducers";
+import { CommandPaletteModal } from "./CommandPalette/CommandPaletteModal";
+import FocusContextReduxAdapter from "./FocusContextReduxAdapter";
+import Header from "./Header/index";
+import KeyboardShortcuts from "./KeyboardShortcuts";
+import { KeyModifiers } from "./KeyModifiers";
+import LayoutContextAdapter from "./LayoutContextAdapter";
+import TimelineContextAdapter from "./SecondaryToolbox/TimelineContextAdapter";
+import SelectedFrameContextAdapter from "./SelectedFrameContextAdapter";
+import SessionContextAdapter from "./SessionContextAdapter";
+import LoadingScreen from "./shared/LoadingScreen";
+import ReplayLogo from "./shared/ReplayLogo";
+import SidePanel from "./SidePanel";
+import SourcesContextAdapter from "./SourcesContextAdapter";
+import Timeline from "./Timeline/Timeline";
+import Toolbar from "./Toolbar";
+import Video from "./Video";
 
-const colorOptions: Array<"blue" | "green" | "red"> = ["blue", "green", "red"];
-const Hoverboard = dynamic(() => import("./Hoverboard"), {
-  ssr: false,
-  loading: () => <div />,
-});
+const Viewer = React.lazy(() => import("./Viewer"));
 
-export function LoadingScreenTemplate({ children }: { children?: ReactNode }) {
-  const [hoverboardColor, setHoverboardColor] = useState(colorOptions[2]);
+type DevToolsProps = PropsFromRedux & { apiKey?: string; uploadComplete: boolean };
 
-  const changeHoverboardColor = () => {
-    const randomIndex = Math.floor(Math.random() * colorOptions.length);
-    setHoverboardColor(colorOptions[randomIndex]);
+export const sidePanelStorageKey = "Replay:SidePanelCollapsed";
+
+function ViewLoader() {
+  const [showLoader, setShowLoader] = useState(false);
+  const idRef = useRef<ReturnType<typeof setTimeout>>();
+  useEffect(() => {
+    idRef.current = setTimeout(() => {
+      setShowLoader(true);
+    }, 5000);
+
+    return () => clearTimeout(idRef.current!);
+  });
+
+  if (!showLoader) {
+    return null;
+  }
+
+  return (
+    <div className="absolute flex h-full w-full items-center justify-center bg-chrome">
+      <ReplayLogo size="md" color="gray" />
+    </div>
+  );
+}
+
+function Body() {
+  const viewMode = useAppSelector(getViewMode);
+
+  const sidePanelRef = useRef<ImperativePanelHandle>(null);
+
+  const [sidePanelCollapsed, setSidePanelCollapsed] = useLocalStorage(sidePanelStorageKey, false);
+
+  const onSidePanelCollapse = (isCollapsed: boolean) => {
+    setSidePanelCollapsed(isCollapsed);
   };
 
   useEffect(() => {
-    const interval = setInterval(changeHoverboardColor, 5_000);
-    return () => clearInterval(interval);
-  }, [hoverboardColor]);
+    if (sidePanelCollapsed) {
+      sidePanelRef.current?.collapse();
+    } else {
+      sidePanelRef.current?.expand();
+    }
+  }, [sidePanelCollapsed]);
 
   return (
-    <BubbleViewportWrapper className={styles.viewportWrapper}>
-      <div className={styles.loadingScreenWrapper}>
-        <div className="flex flex-col items-center space-y-2">
-          <div className={styles.hoverboardWrapper} onClick={changeHoverboardColor}>
-            <Hoverboard color={hoverboardColor} />
-          </div>
-          {children}
-        </div>
+    <div className="vertical-panels pr-2">
+      <div className="flex h-full flex-row overflow-hidden bg-chrome">
+        <Toolbar />
+        <PanelGroup autoSaveId="DevTools-horizontal" className="split-box" direction="horizontal">
+          <Panel
+            className="flex=1 flex h-full overflow-hidden"
+            collapsible
+            defaultSize={20}
+            id="Panel-SidePanel"
+            minSize={15}
+            onCollapse={onSidePanelCollapse}
+            ref={sidePanelRef}
+          >
+            <SidePanel />
+          </Panel>
+          <PanelResizeHandle
+            className={`h-full ${sidePanelCollapsed ? "w-0" : "w-2"}`}
+            id="PanelResizeHandle-SidePanel"
+          />
+          <Panel className="flex h-full overflow-hidden" minSize={50}>
+            {viewMode === "dev" ? (
+              <React.Suspense fallback={<ViewLoader />}>
+                <Viewer />
+              </React.Suspense>
+            ) : (
+              <Video />
+            )}
+          </Panel>
+        </PanelGroup>
       </div>
-    </BubbleViewportWrapper>
+      <Timeline />
+    </div>
   );
 }
 
-function LoadingScreen({
-  uploading,
-  awaitingSourcemaps,
-  fallbackMessage,
-  stalledTimeout = 15000,
-}: PropsFromRedux & { fallbackMessage: string; stalledTimeout?: number }) {
-  const phrases = [
-    "Tuning the flux capacitor...",
-    "Prepping DeLorean for time travel...",
-    "Revving up to 88 miles per hour...",
-    "Gathering 1.21 gigawatts of power...",
-    "Ensuring temporal paradox safeguards...",
-    "Being careful not to disrupt the timeline...",
-  ];
-  const longWaitMessage =
-    "<div><p>This is taking longer than expected.</p><p><a href='http://replay.io/discord' target='discord'>Report problems on Discord</a></p>";
+function _DevTools({
+  apiKey,
+  clearTrialExpired,
+  createSocket,
+  loadedRegions,
+  loadingFinished,
+  sessionId,
+  showCommandPalette,
+  uploadComplete,
+}: DevToolsProps) {
+  const { isAuthenticated } = useAuth0();
+  const recordingId = useGetRecordingId();
+  const { recording } = useGetRecording(recordingId);
+  const { trackLoadingIdleTime } = useTrackLoadingIdleTime(uploadComplete, recording);
+  const { userIsAuthor, loading } = useUserIsAuthor();
+  const isExternalRecording = useMemo(
+    () => recording?.user && !recording.user.internal,
+    [recording]
+  );
 
-  const [message, setMessage] = useState(fallbackMessage);
-
-  const changeMessage = () => {
-    const randomIndex = Math.floor(Math.random() * phrases.length);
-    setMessage(phrases[randomIndex]);
-  };
-
-  useEffect(() => {
-    const phraseTimeout = setTimeout(changeMessage, 5000);
-    return () => clearTimeout(phraseTimeout);
-  }, []);
+  const { value: enableLargeText } = useFeature("enableLargeText");
+  usePreferredFontSize(enableLargeText);
 
   useEffect(() => {
-    const stalledTimeoutId = setTimeout(() => {
-      setMessage(longWaitMessage);
-    }, stalledTimeout);
-    return () => clearTimeout(stalledTimeoutId);
+    import("./Viewer");
   }, []);
+  useEffect(() => {
+    // We only track anonymous usage for recording by non-internal users so that
+    // test runner cases (e.g. QA Wolf) are excluded.
+    // Wait until we start rendering the DevTools component before potentially registering
+    // a user as a guest in Mixpanel. This is to avoid sending too many unique distinct guest
+    // users to Mixpanel.
+    if (!isAuthenticated && isExternalRecording) {
+      maybeSetGuestMixpanelContext();
+    }
+  }, [isAuthenticated, isExternalRecording]);
 
-  const waitingForMessage =
-    awaitingSourcemaps || uploading ? (
-      <span>Uploading {Math.round(uploading?.amount || 0)}Mb</span>
-    ) : (
-      <span dangerouslySetInnerHTML={{ __html: message }}></span>
-    );
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+
+    trackEventOnce("session.devtools_start", {
+      userIsAuthor: !!userIsAuthor,
+      workspaceUuid: recording?.workspace?.id || null,
+    });
+  });
+
+  useEffect(() => {
+    let token: Promise<TokenState | void> = Promise.resolve();
+    if (isAuthenticated) {
+      token = tokenManager.getToken();
+    }
+
+    token
+      .then(async ts => {
+        if (ts?.token) {
+          await ThreadFront.setAccessToken(ts.token);
+        }
+
+        createSocket(recordingId, ThreadFront);
+      })
+      .catch(() => {
+        console.error("Failed to create session");
+      });
+
+    return () => {
+      clearTrialExpired();
+    };
+  }, [isAuthenticated, clearTrialExpired, createSocket, recordingId]);
+
+  useEffect(() => {
+    if (uploadComplete && loadingFinished) {
+      endUploadWaitTracking(sessionId!);
+    }
+  }, [loadingFinished, uploadComplete, sessionId]);
+
+  useEffect(() => {
+    if (loadingFinished) {
+      trackLoadingIdleTime(sessionId!);
+    }
+  }, [loadingFinished, trackLoadingIdleTime, sessionId]);
+
+  if (!loadingFinished) {
+    return <LoadingScreen fallbackMessage="Loading..." />;
+  }
+
+  const title = recording?.title;
 
   return (
-    <LoadingScreenTemplate>
-      <span className={styles.messageWrapper} dangerouslySetInnerHTML={{ __html: message }}></span>
-    </LoadingScreenTemplate>
+    <SessionContextAdapter apiKey={apiKey ?? null}>
+      <SourcesContextAdapter>
+        <FocusContextReduxAdapter>
+          <PointsContextRoot>
+            <TimelineContextAdapter>
+              <SelectedFrameContextRoot SelectedFrameContextAdapter={SelectedFrameContextAdapter}>
+                <TerminalContextAdapter>
+                  <InspectorContextReduxAdapter>
+                    <ExpandablesContextRoot>
+                      <LayoutContextAdapter>
+                        <KeyModifiers>
+                          {title && (
+                            <Head>
+                              <title>{title}</title>
+                            </Head>
+                          )}
+                          <Header />
+                          <Body />
+                          {showCommandPalette ? <CommandPaletteModal /> : null}
+                          <KeyboardShortcuts />
+                        </KeyModifiers>
+                      </LayoutContextAdapter>
+                    </ExpandablesContextRoot>
+                  </InspectorContextReduxAdapter>
+                </TerminalContextAdapter>
+              </SelectedFrameContextRoot>
+            </TimelineContextAdapter>
+          </PointsContextRoot>
+        </FocusContextReduxAdapter>
+      </SourcesContextAdapter>
+    </SessionContextAdapter>
   );
 }
 
-const connector = connect((state: UIState) => ({
-  uploading: getUploading(state),
-  awaitingSourcemaps: getAwaitingSourcemaps(state),
-}));
+const connector = connect(
+  (state: UIState) => ({
+    loadingFinished: selectors.getLoadingFinished(state),
+    loadedRegions: selectors.getLoadedRegions(state),
+    sessionId: selectors.getSessionId(state),
+    showCommandPalette: selectors.getShowCommandPalette(state),
+  }),
+  {
+    createSocket,
+    clearTrialExpired,
+  }
+);
 type PropsFromRedux = ConnectedProps<typeof connector>;
+const ConnectedDevTools = connector(_DevTools);
 
-export default connector(LoadingScreen);
+export default ConnectedDevTools;
