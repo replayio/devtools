6e069c115fed815114eeb41b9dcc5d0e5251cf2c
Jon Bell <github@lot23.com>
2023-05-23 08:56:32 +1200

    Improve loading screen states (#9220)
    * Improved loading screen states
    
    * Addressing lint + TSC
    
    * Improved styles
    
    * Fixing errors
    
    * Fixing errors
    
    * Addressing code issues
    
    * Improving code
    
    * Removing debugging code
    
    * Aligning to center of screen
    
    * Improving code
    
    * Removing debugging code
    
    * Fixing timeouts and intervals
    
    * Removing debug code

--- src/ui/components/shared/LoadingScreen.tsx
+++ src/ui/components/shared/LoadingScreen.tsx
@@ -1,11 +1,12 @@
 import dynamic from "next/dynamic";
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useCallback, useEffect, useState } from "react";
 import { ConnectedProps, connect } from "react-redux";
 
 import { getAwaitingSourcemaps, getUploading } from "ui/reducers/app";
 import { UIState } from "ui/state";
 
 import { BubbleViewportWrapper } from "./Viewport";
+import styles from "./LoadingScreen.module.css";
 
 const colorOptions: Array<"blue" | "green" | "red"> = ["blue", "green", "red"];
 
@@ -17,60 +18,80 @@ const Hoverboard = dynamic(() => import("./Hoverboard"), {
 export function LoadingScreenTemplate({ children }: { children?: ReactNode }) {
   const [hoverboardColor, setHoverboardColor] = useState(colorOptions[2]);
 
-  const changeHoverboardColor = () => {
+  const changeHoverboardColor = useCallback(() => {
     const randomIndex = Math.floor(Math.random() * colorOptions.length);
     setHoverboardColor(colorOptions[randomIndex]);
-  };
+  }, []);
 
   useEffect(() => {
-    const interval = setInterval(changeHoverboardColor, 5_000);
-    return () => clearInterval(interval);
-  }, [hoverboardColor]);
+    const timeoutId = setInterval(changeHoverboardColor, 5000);
+    return () => clearInterval(timeoutId);
+  }, [changeHoverboardColor]);
 
   return (
-    <BubbleViewportWrapper>
-      <div className="relative flex w-96 flex-col items-center space-y-1 rounded-lg bg-loadingBoxes p-4 py-12 shadow-sm">
-        <div className="flex flex-col items-center space-y-2">
-          <div className="h-32 w-32" onClick={changeHoverboardColor}>
-            <Hoverboard color={hoverboardColor} />
+    <div className={styles.loadingScreenWrapper}>
+      <BubbleViewportWrapper>
+        <div className={styles.loadingScreenWrapper}>
+          <div className="flex flex-col items-center space-y-2">
+            <div className={styles.hoverboardWrapper} onClick={changeHoverboardColor}>
+              <Hoverboard color={hoverboardColor} />
+            </div>
+            {children}
           </div>
-          {children}
         </div>
-      </div>
-    </BubbleViewportWrapper>
+      </BubbleViewportWrapper>
+    </div>
   );
 }
+
+const phrases = [
+  "Tuning the flux capacitor...",
+  "Prepping DeLorean for time travel...",
+  "Revving up to 88 miles per hour...",
+  "Gathering 1.21 gigawatts of power...",
+  "Ensuring temporal paradox safeguards...",
+  "Being careful not to mess up your timeline...",
+];
 
 function LoadingScreen({
   uploading,
   awaitingSourcemaps,
   fallbackMessage,
-  stalledTimeout = 9000,
+  stalledTimeout = 15000,
 }: PropsFromRedux & { fallbackMessage: string; stalledTimeout?: number }) {
-  // The backend send events in this order: uploading replay -> uploading sourcemaps.
-  let waitingForMessage = <span>{fallbackMessage}</span>;
-  if (awaitingSourcemaps) {
-    waitingForMessage = <span>Uploading sourcemaps...</span>;
-    stalledTimeout = Infinity;
-  } else if (uploading) {
-    waitingForMessage = <span>Uploading {Math.round(+uploading.amount)}Mb</span>;
-    stalledTimeout = Infinity;
-  }
+  const longWaitMessage =
+    "<div><p>This is taking longer than expected.</p><p><a href='http://replay.io/discord' target='discord'>Contact us on Discord</a></p>";
 
-  let [stalled, setStalled] = useState<boolean>(false);
+  const [message, setMessage] = useState(fallbackMessage);
+
   useEffect(() => {
-    if (stalledTimeout === Infinity) {
-      return;
-    }
+    const changeMessage = () => {
+      const randomIndex = Math.floor(Math.random() * phrases.length);
+      setMessage(phrases[randomIndex]);
+    };
+    const phraseTimeout = setTimeout(changeMessage, 5000);
+    // swap to cutesy phrase after 5 seconds
+    // note: this should only be called a single time
+    return () => clearTimeout(phraseTimeout);
+  }, []);
 
-    const timeout = setTimeout(() => setStalled(true), stalledTimeout);
+  useEffect(() => {
+    const stalledTimeoutId = setTimeout(() => {
+      // after 15 seconds, switch to stalled message
+      setMessage(longWaitMessage);
+    }, stalledTimeout);
+    return () => clearTimeout(stalledTimeoutId);
+  }, [stalledTimeout]);
 
-    return () => clearTimeout(timeout);
-  }, [fallbackMessage, stalledTimeout]);
-
+  const waitingForMessage =
+    awaitingSourcemaps || uploading ? (
+      <span>Uploading {Math.round(uploading?.amount ? Number(uploading.amount) : 0)}Mb</span>
+    ) : (
+      <span dangerouslySetInnerHTML={{ __html: message }}></span>
+    );
   return (
     <LoadingScreenTemplate>
-      <span className="text-sm">{stalled ? "Reticulating splines..." : waitingForMessage}</span>
+      <span className={styles.messageWrapper} dangerouslySetInnerHTML={{ __html: message }}></span>
     </LoadingScreenTemplate>
   );
 }
