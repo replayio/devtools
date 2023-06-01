246154e64724fd0c29717a3ad78d50e60100deb1
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-06-01 10:35:01 -0400

    Migrate TestRun GraphQL data to new format (#9271)
    Background reasoning explained in Notion (search for "Test Suites: Library data architecture")
    
    * Migrate legacy GraphQL format to new data format
    * Detect and skip migration if data already in new format

--- src/ui/components/Library/Team/View/TestRuns/TestRunListItem.tsx
+++ src/ui/components/Library/Team/View/TestRuns/TestRunListItem.tsx
@@ -2,8 +2,7 @@ import Link from "next/link";
 import { useContext } from "react";
 
 import Icon from "ui/components/shared/Icon";
-import MaterialIcon from "ui/components/shared/MaterialIcon";
-import { TestRun } from "ui/hooks/tests";
+import { TestSuiteRun } from "ui/hooks/tests";
 
 import { TeamContext } from "../../TeamContextRoot";
 import { getTruncatedRelativeDate } from "../Recordings/RecordingListItem/RecordingListItem";
@@ -13,8 +12,9 @@ import { RunStats } from "./RunStats";
 import { TestRunsContext } from "./TestRunsContextRoot";
 import styles from "../../../Library.module.css";
 
-function Title({ testRun }: { testRun: TestRun }) {
-  const title = testRun.commitTitle || "Unknown";
+function Title({ testSuiteRun }: { testSuiteRun: TestSuiteRun }) {
+  const title = testSuiteRun.title;
+
   // TODO: This should be done in CSS.
   const formatted = title.length > 80 ? title.slice(0, 80) + "..." : title;
 
@@ -25,26 +25,34 @@ function Title({ testRun }: { testRun: TestRun }) {
   );
 }
 
-function Attributes({ testRun }: { testRun: TestRun }) {
-  const { mergeId, mergeTitle, user, date, branch } = testRun;
+function Attributes({ testSuiteRun }: { testSuiteRun: TestSuiteRun }) {
+  const { date, source, title } = testSuiteRun;
+  if (source) {
+    const { branchName, branchStatus, user } = source;
 
-  return (
-    <div className={`flex flex-row items-center text-xs font-light`}>
-      <AttributeContainer icon="schedule">{getTruncatedRelativeDate(date)}</AttributeContainer>
-      <AttributeContainer icon="person">{user || ""}</AttributeContainer>
-      {mergeId && (
-        <AttributeContainer title={mergeTitle} icon="merge_type">
-          {mergeId}
-        </AttributeContainer>
-      )}
-      {!mergeId && (
-        <AttributeContainer maxWidth="160px" icon="fork_right">
-          {branch || ""}
-        </AttributeContainer>
-      )}
-      <ModeAttribute testRun={testRun} />
-    </div>
-  );
+    return (
+      <div className="flex flex-row items-center text-xs font-light">
+        <AttributeContainer icon="schedule">{getTruncatedRelativeDate(date)}</AttributeContainer>
+        <AttributeContainer icon="person">{user}</AttributeContainer>
+        {branchStatus === "open" ? (
+          <AttributeContainer maxWidth="160px" icon="fork_right">
+            {branchName}
+          </AttributeContainer>
+        ) : (
+          <AttributeContainer title={title} icon="merge_type">
+            {branchStatus}
+          </AttributeContainer>
+        )}
+        <ModeAttribute testSuiteRun={testSuiteRun} />
+      </div>
+    );
+  } else {
+    return (
+      <div className="flex flex-row items-center text-xs font-light">
+        <AttributeContainer icon="schedule">{getTruncatedRelativeDate(date)}</AttributeContainer>
+      </div>
+    );
+  }
 }
 
 function Status({ failCount }: { failCount: number }) {
@@ -57,32 +65,30 @@ function Status({ failCount }: { failCount: number }) {
   );
 }
 
-export function TestRunListItem({ testRun }: { testRun: TestRun }) {
+export function TestRunListItem({ testSuiteRun }: { testSuiteRun: TestSuiteRun }) {
   const { focusId } = useContext(TestRunsContext);
   const { teamId } = useContext(TeamContext);
+
   // TODO Don't count flakes
-  const failCount = testRun.stats?.failed || 0;
-  const isSelected = focusId === testRun.id;
+  const failCount = testSuiteRun.results.counts.failed;
+  const isSelected = focusId === testSuiteRun.id;
 
   return (
     <Link
-      href={`/team/${teamId}/runs/${testRun.id}`}
+      href={`/team/${teamId}/runs/${testSuiteRun.id}`}
       className={`flex cursor-pointer flex-row space-x-3 rounded-sm border-b border-chrome bg-themeBase-100 px-3 py-3 ${
         styles.libraryRow
-      }     
+      }
       ${isSelected ? styles.libraryRowSelected : ""}
       `}
     >
       <Status failCount={failCount} />
       <div className="flex flex-grow flex-col space-y-1 overflow-hidden">
         <div className="flex flex-row justify-between space-x-3">
-          <Title testRun={testRun} />
-          <RunStats testRun={testRun} />
+          <Title testSuiteRun={testSuiteRun} />
+          <RunStats testSuiteRun={testSuiteRun} />
         </div>
-        <Attributes testRun={testRun} />
-        {testRun.title ? (
-          <div className="flex flex-row items-center justify-between text-xs">{testRun.title}</div>
-        ) : null}
+        <Attributes testSuiteRun={testSuiteRun} />
       </div>
     </Link>
   );
