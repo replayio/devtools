8d374af7852f69dcb7a327fb57d84caeef85c083
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-06-20 17:16:17 -0400

    Custom Test Suite parsing error fallback UI

--- src/ui/components/TestSuite/views/TestSuitePanel.tsx
+++ src/ui/components/TestSuite/views/TestSuitePanel.tsx
@@ -3,6 +3,7 @@ import { Suspense, useContext } from "react";
 import ErrorBoundary from "replay-next/components/ErrorBoundary";
 import Loader from "replay-next/components/Loader";
 import { TestSuiteContext } from "ui/components/TestSuite/views/TestSuiteContext";
+import TestSuiteErrorFallback from "ui/components/TestSuite/views/TestSuiteErrorFallback";
 
 import GroupTestCasesPanel from "./GroupedTestCases";
 import TestRecordingPanel from "./TestRecording";
@@ -11,7 +12,7 @@ import styles from "./TestSuitePanel.module.css";
 // TODO Show better/custom error fallback to failed test suites
 export default function TestSuitePanel() {
   return (
-    <ErrorBoundary>
+    <ErrorBoundary fallback={<TestSuiteErrorFallback />}>
       <Suspense
         fallback={
           <div className={styles.Loading} data-test-name="TestSuitePanel">
