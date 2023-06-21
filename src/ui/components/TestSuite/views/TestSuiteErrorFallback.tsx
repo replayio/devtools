076b998e30f0baa3ba5a800997bb73c002eb5ed5
Jon Bell <github@lot23.com>
2023-06-22 07:57:00 +1200

    Adjust the testsuites error screen (#9360)
    * Adjusted error message
    
    * Testsuite error adjustments
    
    * Cleaning up CSS

--- src/ui/components/TestSuite/views/TestSuiteErrorFallback.tsx
+++ src/ui/components/TestSuite/views/TestSuiteErrorFallback.tsx
@@ -25,15 +25,11 @@ export default function TestSuiteErrorFallback() {
         </div>
       </div>
       <div className={styles.ErrorMessage}>
-        <Icon className={styles.ErrorIcon} type="warning" />
-        Something went wrong processing test metadata.
-      </div>
-      <div className={styles.ContactUsMessage}>
-        Please{" "}
+        <h2 className={styles.ErrorMessageHeader}>Error</h2>
+        Test metadata could not be processed.
         <a className={styles.Link} href="http://replay.io/discord" target="discord">
-          contact us
-        </a>{" "}
-        on Discord.
+          Contact us on Discord
+        </a>
       </div>
     </div>
   );
