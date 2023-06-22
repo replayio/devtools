03cff40cc4d080ee5a9631efa58b6813283488e0
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-06-22 11:13:44 -0400

    Moved is-in-focus-window check from RequestTable .map() into RequestRow

--- src/ui/components/NetworkMonitor/RequestTable.tsx
+++ src/ui/components/NetworkMonitor/RequestTable.tsx
@@ -1,10 +1,8 @@
 import classNames from "classnames";
 import { Row, TableInstance } from "react-table";
 
-import { useCurrentFocusWindow } from "replay-next/src/hooks/useCurrentFocusWindow";
 import { useNag } from "replay-next/src/hooks/useNag";
 import { Nag } from "shared/graphql/types";
-import { isPointInRegion } from "shared/utils/time";
 import { trackEvent } from "ui/utils/telemetry";
 
 import { HeaderGroups } from "./HeaderGroups";
@@ -34,8 +32,6 @@ const RequestTable = ({
   table: TableInstance<RequestSummary>;
 }) => {
   const { columns, getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = table;
-
-  const focusWindow = useCurrentFocusWindow();
 
   const onSeek = (request: RequestSummary) => {
     trackEvent("net_monitor.seek_to_request");
@@ -68,16 +64,12 @@ const RequestTable = ({
               firstInFuture = true;
             }
 
-            const isInLoadedRegion =
-              focusWindow !== null && isPointInRegion(row.original.point.point, focusWindow);
-
             prepareRow(row);
 
             return (
               <RequestRow
                 currentTime={currentTime}
                 isFirstInFuture={firstInFuture}
-                isInLoadedRegion={isInLoadedRegion}
                 isInPast={inPast}
                 isSelected={selectedRequest?.id === row.original.id}
                 key={row.getRowProps().key}
