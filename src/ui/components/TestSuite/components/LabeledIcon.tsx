cade01d7c88cd004ab9aca8134ceeffb47c7b1d2
Mark Erikson <mark@isquaredsoftware.com>
2023-06-23 17:45:41 -0400

    Use absolute dates for test panel relative date hover tooltip (#9377)

--- src/ui/components/TestSuite/components/LabeledIcon.tsx
+++ src/ui/components/TestSuite/components/LabeledIcon.tsx
@@ -7,17 +7,19 @@ export default function LabeledIcon({
   icon,
   label,
   dataTestName,
+  title,
 }: {
   className?: string;
   icon: string;
   label: string;
   dataTestName?: string;
+  title?: string;
 }) {
   return (
     <div
       className={`${className} ${styles.LabeledIcon}`}
       data-test-name={dataTestName}
-      title={label}
+      title={title ?? label}
     >
       <MaterialIcon className={styles.Icon}>{icon}</MaterialIcon>
       <label className={styles.Label}>{label}</label>
