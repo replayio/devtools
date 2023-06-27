e71c3d876305ee8fc82f64daae6323b08261f1b2
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-06-27 10:28:55 -0400

    Fixed uncontrolled component DEV warning for shared/Forms/Checkbox

--- src/ui/components/shared/Forms/Checkbox.tsx
+++ src/ui/components/shared/Forms/Checkbox.tsx
@@ -11,10 +11,11 @@ export default function Checkbox({
   return (
     <input
       type="checkbox"
-      checked={checked}
+      checked={checked == true}
       disabled={disabled}
       className={classNames(
-        "m-0 h-4 w-4 cursor-pointer rounded bg-checkbox text-primaryAccent focus:ring-primaryAccent",
+        "m-0 h-4 w-4 cursor-pointer rounded bg-checkbox text-primaryAccent transition-opacity focus:ring-primaryAccent",
+        disabled && "cursor-default opacity-90",
         className
       )}
       id={id}
