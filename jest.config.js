a2fdde011e498ed98cfb329589f740439038e8f7
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-06-29 13:01:38 -0400

    Update dependencies (#9400)
    * Upgrade Babel and Prettier deps to support TypeScript "satisfies" operator
    * Update Trunk to pull in newer Babel also
    * Use @swc/jest instead of babel-jest for unit tests (for the same reason as above)
    * Fixed an issue with async-store-helper test mocking

--- jest.config.js
+++ jest.config.js
@@ -52,10 +52,20 @@ module.exports = {
     // Use babel-jest to transpile tests with the next/babel preset
     // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
     "^.+\\.(js|jsx|ts|tsx)$": [
-      "babel-jest",
+      "@swc/jest",
       {
-        presets: ["next/babel"],
-        plugins: ["babel-plugin-transform-import-meta"],
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            jsx: true,
+            tsx: true,
+          },
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
       },
     ],
 
