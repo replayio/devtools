3356b6a2ee841c867d5934d3f83db7a3bf5c7daf
Mark Erikson <mark@isquaredsoftware.com>
2023-07-03 16:15:21 -0400

    Always show recording date, and show as its own row if no user

--- src/ui/components/TestSuite/components/LabeledIcon.tsx
+++ src/ui/components/TestSuite/components/LabeledIcon.tsx
@@ -8,12 +8,14 @@ export default function LabeledIcon({
   label,
   dataTestName,
   title,
+  iconClassname = "",
 }: {
   className?: string;
   icon: string;
   label: string;
   dataTestName?: string;
   title?: string;
+  iconClassname?: string;
 }) {
   return (
     <div
@@ -21,7 +23,7 @@ export default function LabeledIcon({
       data-test-name={dataTestName}
       title={title ?? label}
     >
-      <MaterialIcon className={styles.Icon}>{icon}</MaterialIcon>
+      <MaterialIcon className={`${iconClassname} ${styles.Icon}`}>{icon}</MaterialIcon>
       <label className={styles.Label}>{label}</label>
     </div>
   );
