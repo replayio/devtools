152e041439b7e8fd02fb55d63dade3395c6e3caf
Mark Erikson <mark@isquaredsoftware.com>
2023-07-05 12:54:15 -0400

    Always show recording date, and show as its own row (#9442)

--- src/ui/components/Events/ReplayInfo.tsx
+++ src/ui/components/Events/ReplayInfo.tsx
@@ -4,10 +4,10 @@ import { ConnectedProps, connect } from "react-redux";
 import { OperationsData } from "shared/graphql/types";
 import { getDisplayedUrl } from "shared/utils/environment";
 import * as actions from "ui/actions/app";
+import { getTruncatedRelativeDate } from "ui/components/Library/Team/View/Recordings/RecordingListItem/RecordingListItem";
 import hooks from "ui/hooks";
 import { getRecordingTarget } from "ui/reducers/app";
 import { useAppSelector } from "ui/setup/hooks";
-import { formatRelativeTime } from "ui/utils/comments";
 import { getRecordingId, showDurationWarning } from "ui/utils/recording";
 import useAuth0 from "ui/utils/useAuth0";
 
@@ -16,7 +16,9 @@ import Icon from "../shared/Icon";
 import MaterialIcon from "../shared/MaterialIcon";
 import { getPrivacySummaryAndIcon } from "../shared/SharingModal/PrivacyDropdown";
 import PrivacyDropdown from "../shared/SharingModal/PrivacyDropdown";
+import LabeledIcon from "../TestSuite/components/LabeledIcon";
 import { getUniqueDomains } from "../UploadScreen/Privacy";
+import styles from "./ReplayInfo.module.css";
 
 const Row = ({ children, onClick }: { children: ReactNode; onClick?: () => void }) => {
   const classes = "flex flex-row space-x-2 p-1 px-4 items-center text-left overflow-hidden";
@@ -42,7 +44,9 @@ function ReplayInfo({ setModal }: PropsFromRedux) {
     return null;
   }
 
-  const time = formatRelativeTime(new Date(recording.date));
+  const time = getTruncatedRelativeDate(recording.date);
+  const date = new Date(recording.date);
+  const fullDateString = date.toLocaleString();
   const { summary, icon } = getPrivacySummaryAndIcon(recording);
 
   const showOperations = () => {
@@ -53,17 +57,29 @@ function ReplayInfo({ setModal }: PropsFromRedux) {
   return (
     <div className="flex-column flex items-center overflow-hidden border-splitter bg-bodyBgcolor">
       <div className="mt-.5 mb-2 flex w-full cursor-default flex-col self-stretch overflow-hidden px-1.5 pb-0 text-xs">
-        {recording.user ? (
-          <Row>
-            <AvatarImage
-              className="avatar rounded-full"
-              style={{ height: "1.25rem", width: "1.25rem" }}
-              src={recording.user.picture}
+        <Row>
+          {recording.user ? (
+            <>
+              <AvatarImage
+                className="avatar rounded-full"
+                style={{ height: "1.25rem", width: "1.25rem" }}
+                src={recording.user.picture}
+              />
+              <div>{recording.user.name}</div>
+              <div className="opacity-50" title={fullDateString}>
+                {time}
+              </div>
+            </>
+          ) : (
+            <LabeledIcon
+              iconClassname={styles.icon}
+              icon="schedule"
+              label={time}
+              title={fullDateString}
+              dataTestName="RecordingDate"
             />
-            <div>{recording.user.name}</div>
-            <div className="opacity-50">{time}</div>
-          </Row>
-        ) : null}
+          )}
+        </Row>
         <div>
           {isAuthenticated ? (
             <Row>
@@ -122,6 +138,7 @@ function ReplayInfo({ setModal }: PropsFromRedux) {
           <OperationsRow operations={recording.operations} onClick={showOperations} />
         ) : null}
         {showEnvironmentVariables ? <EnvironmentVariablesRow /> : null}
+
         {showDurationWarning(recording) ? <DurationWarningRow /> : null}
       </div>
     </div>
