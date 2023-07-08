34f3f5b4b236095d085580a595f1aadc5daa5e99
Jason Laster <jason.laster.11@gmail.com>
2023-07-07 21:37:03 -0700

    Redux - fix scrolling the contents pane

--- src/ui/components/SecondaryToolbox/ReduxDevTools.tsx
+++ src/ui/components/SecondaryToolbox/ReduxDevTools.tsx
@@ -55,11 +55,7 @@ export const ReduxDevToolsPanel = () => {
         </PanelResizeHandle>
 
         <ResizablePanel collapsible>
-          <div className="ml-1 grow">
-            {selectedPoint && (
-              <ReduxDevToolsContents point={selectedPoint} time={annotation.time} />
-            )}
-          </div>
+          {selectedPoint && <ReduxDevToolsContents point={selectedPoint} time={annotation.time} />}
         </ResizablePanel>
       </PanelGroup>
     </div>
