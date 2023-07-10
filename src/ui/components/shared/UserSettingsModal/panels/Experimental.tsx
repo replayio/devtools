13d205f3ac28d9117599fcce62e2bcfa00652c22
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2023-07-10 17:23:18 -0500

    lint

--- src/ui/components/shared/UserSettingsModal/panels/Experimental.tsx
+++ src/ui/components/shared/UserSettingsModal/panels/Experimental.tsx
@@ -18,7 +18,7 @@ export const PREFERENCES: PreferencesKey[] = [
   "feature_reduxDevTools",
   "backend_disableIncrementalSnapshots",
   "backend_disableConcurrentControllerLoading",
-  "backend_enableHasAnnotationKindQueryStorage"
+  "backend_enableHasAnnotationKindQueryStorage",
 ];
 
 export function Experimental() {
