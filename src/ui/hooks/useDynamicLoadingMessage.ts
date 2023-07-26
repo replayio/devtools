746accc2d63a2d4f66bc94959749e9676448037d
Jon Bell <github@lot23.com>
2023-07-27 11:05:07 +1200

    Lint

--- src/ui/hooks/useDynamicLoadingMessage.ts
+++ src/ui/hooks/useDynamicLoadingMessage.ts
@@ -48,32 +48,31 @@ const phrases = [
 const longWaitMessage =
   "<div><p>This is taking longer than expected.</p><p><a href='http://replay.io/discord' target='discord'>Contact us on Discord</a></p>";
 
-  export function useDynamicLoadingMessage(
-    isProcessed: boolean,
-    initialMessage: string,
-    stalledTimeout: number
-  ) {
-    const [message, setMessage] = useState(isProcessed ? initialMessage : "Processing...");
-  
-    useEffect(() => {
-      if (isProcessed) {
-        const changeMessage = () => {
-          const randomIndex = Math.floor(Math.random() * phrases.length);
-          setMessage(phrases[randomIndex]);
-        };
-        const phraseTimeout = setTimeout(changeMessage, 8000);
-  
-        const stalledTimeoutId = setTimeout(() => {
-          setMessage(longWaitMessage);
-        }, stalledTimeout);
-  
-        return () => {
-          clearTimeout(phraseTimeout);
-          clearTimeout(stalledTimeoutId);
-        };
-      }
-    }, [isProcessed, initialMessage, stalledTimeout]);
-  
-    return message;
-  }
-  
+export function useDynamicLoadingMessage(
+  isProcessed: boolean,
+  initialMessage: string,
+  stalledTimeout: number
+) {
+  const [message, setMessage] = useState(isProcessed ? initialMessage : "Processing...");
+
+  useEffect(() => {
+    if (isProcessed) {
+      const changeMessage = () => {
+        const randomIndex = Math.floor(Math.random() * phrases.length);
+        setMessage(phrases[randomIndex]);
+      };
+      const phraseTimeout = setTimeout(changeMessage, 8000);
+
+      const stalledTimeoutId = setTimeout(() => {
+        setMessage(longWaitMessage);
+      }, stalledTimeout);
+
+      return () => {
+        clearTimeout(phraseTimeout);
+        clearTimeout(stalledTimeoutId);
+      };
+    }
+  }, [isProcessed, initialMessage, stalledTimeout]);
+
+  return message;
+}
