0f8380fe6964ebf483d20f061206227ce234e4ac
Holger Benl <hbenl@evandor.de>
2023-07-27 17:36:04 +0200

    Fix ScopesInspector performance

--- packages/replay-next/components/inspector/ScopesInspector.tsx
+++ packages/replay-next/components/inspector/ScopesInspector.tsx
@@ -1,8 +1,10 @@
 import { NamedValue, PauseId } from "@replayio/protocol";
-import { Suspense } from "react";
+import { Suspense, useContext } from "react";
 
 import Expandable from "replay-next/components/Expandable";
-import Loader from "replay-next/components/Loader";
+import { objectCache } from "replay-next/src/suspense/ObjectPreviews";
+import { suspendInParallel } from "replay-next/src/utils/suspense";
+import { ReplayClientContext } from "shared/client/ReplayClientContext";
 
 import Inspector from "./Inspector";
 import { addPathSegment } from "./PropertiesRenderer";
@@ -23,23 +25,52 @@ export default function ScopesInspector({
 }) {
   return (
     <div className={styles.ScopesInspector} data-test-name="ScopesInspector">
-      <Suspense fallback={<Loader />}>
-        <Expandable
-          className={styles.Expandable}
-          children={protocolValues.map((protocolValue, index) => (
-            <Inspector
-              context="default"
-              key={index}
-              path={addPathSegment(path, protocolValue.name)}
-              pauseId={pauseId}
-              protocolValue={protocolValue}
-            />
-          ))}
-          defaultOpen={expandByDefault}
-          header={name}
-          persistenceKey={path}
-        />
-      </Suspense>
+      <Expandable
+        className={styles.Expandable}
+        defaultOpen={expandByDefault}
+        header={name}
+        persistenceKey={path}
+      >
+        <Suspense fallback="Loading...">
+          <ScopesInspectorChildren path={path} pauseId={pauseId} protocolValues={protocolValues} />
+        </Suspense>
+      </Expandable>
     </div>
+  );
+}
+
+function ScopesInspectorChildren({
+  path,
+  pauseId,
+  protocolValues,
+}: {
+  path?: string;
+  pauseId: PauseId;
+  protocolValues: NamedValue[];
+}) {
+  const client = useContext(ReplayClientContext);
+  // prefetch the scope values before trying to render them, without this
+  // we'd suspend further down in the component tree, which causes a
+  // performance issue described in FE-1500
+  suspendInParallel(
+    ...protocolValues.map(value => () => {
+      if (value.object) {
+        objectCache.read(client, pauseId, value.object, "full");
+      }
+    })
+  );
+
+  return (
+    <>
+      {protocolValues.map((protocolValue, index) => (
+        <Inspector
+          context="default"
+          key={index}
+          path={addPathSegment(path, protocolValue.name)}
+          pauseId={pauseId}
+          protocolValue={protocolValue}
+        />
+      ))}
+    </>
   );
 }
