603c3aacbf7cf31f9f6221dd189f0722bbb7c50c
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-08-02 23:44:59 +0530

    remove prototype object from assertion

--- packages/e2e-tests/tests/redux_devtools.test.ts
+++ packages/e2e-tests/tests/redux_devtools.test.ts
@@ -24,32 +24,19 @@ test("redux_devtools: Test Redux DevTools.", async ({
 
   await getReduxActions(page).nth(0).click();
 
-  await assertTabValue(
-    page,
-    "Action",
-    'payload: 2\ntype: "counter/incrementByAmount"\n[[Prototype]]: Object'
-  );
-  await assertTabValue(page, "State", "counter: {value: 2}\n[[Prototype]]: Object");
+  await assertTabValue(page, "Action", 'payload: 2\ntype: "counter/incrementByAmount"');
+  await assertTabValue(page, "State", "counter: {value: 2}");
   await assertTabValue(page, "Diff", 'counter:{"value":2}');
 
   await getReduxActions(page).nth(1).click();
 
-  await assertTabValue(
-    page,
-    "Action",
-    'payload: undefined\ntype: "counter/decrement"\n[[Prototype]]: Object'
-  );
-  await assertTabValue(page, "State", "counter: {value: 1}\n[[Prototype]]: Object");
+  await assertTabValue(page, "Action", 'payload: undefined\ntype: "counter/decrement"');
+  await assertTabValue(page, "State", "counter: {value: 1}");
   await assertTabValue(page, "Diff", "▶\ncounter\nvalue:2 => 1");
 
   await getReduxActions(page).nth(2).click();
 
-  await assertTabValue(
-    page,
-
-    "Action",
-    'payload: undefined\ntype: "decrement/increment"\n[[Prototype]]: Object'
-  );
-  await assertTabValue(page, "State", "counter: {value: 0}\n[[Prototype]]: Object");
+  await assertTabValue(page, "Action", 'payload: undefined\ntype: "counter/decrement"');
+  await assertTabValue(page, "State", "counter: {value: 0}");
   await assertTabValue(page, "Diff", "▶\ncounter\nvalue:1 => 0");
 });
