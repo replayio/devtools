344ca2b19688ff698432b29dd20b9b18ee427535
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-08-04 08:46:38 -0400

    Upload screen should hide Workspaces with non-matching "test" chriteria

--- src/ui/components/UploadScreen/SettingsPreview.tsx
+++ src/ui/components/UploadScreen/SettingsPreview.tsx
@@ -1,52 +1,21 @@
 import classNames from "classnames";
-import React from "react";
 
 import { Workspace } from "shared/graphql/types";
 import { getOrganizationSettings } from "ui/utils/org";
 
 import { MY_LIBRARY, personalWorkspace } from "./libraryConstants";
 
-const getIconAndText = (
-  isPublic: boolean,
-  selectedWorkspaceId: string,
-  workspaceName?: string
-): {
-  text: string;
-  icon: string;
-} => {
-  if (isPublic) {
-    return {
-      text: "This replay can be viewed by anyone with the link",
-      icon: "public",
-    };
-  }
-
-  if (selectedWorkspaceId === MY_LIBRARY) {
-    return {
-      text: `Only you can view this`,
-      icon: "person",
-    };
-  }
-
-  return {
-    text: `Shared privately with ${workspaceName}`,
-    icon: "groups",
-  };
-};
-
-type SettingsPreviewProps = {
-  onClick: () => void;
-  isPublic: boolean;
-  workspaces: Workspace[];
-  selectedWorkspaceId: string;
-};
-
 export default function SettingsPreview({
-  onClick,
   isPublic,
-  workspaces,
+  onClick,
   selectedWorkspaceId,
-}: SettingsPreviewProps) {
+  workspaces,
+}: {
+  isPublic: boolean;
+  onClick: () => void;
+  selectedWorkspaceId: string;
+  workspaces: Workspace[];
+}) {
   const { features } = getOrganizationSettings(workspaces);
   const canEdit = workspaces.length > 1 || features.user.library || features.recording.public;
 
@@ -54,7 +23,18 @@ export default function SettingsPreview({
     ? [...workspaces, personalWorkspace].find(w => w.id === selectedWorkspaceId)!.name
     : workspaces[0]!.name;
 
-  const { icon, text } = getIconAndText(isPublic, selectedWorkspaceId, workspaceName);
+  let icon: string;
+  let text: string;
+  if (isPublic) {
+    text = "This replay can be viewed by anyone with the link";
+    icon = "public";
+  } else if (selectedWorkspaceId === MY_LIBRARY) {
+    text = `Only you can view this`;
+    icon = "person";
+  } else {
+    text = `Shared privately with ${workspaceName}`;
+    icon = "groups";
+  }
 
   return (
     <button
