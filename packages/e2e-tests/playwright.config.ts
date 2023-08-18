c7835404c1f284ab007a4921cb44ab77f2e26fb2
Jason Laster <jason.laster.11@gmail.com>
2023-08-18 09:16:52 -0600

    Cleanup playwright config

--- packages/e2e-tests/playwright.config.ts
+++ packages/e2e-tests/playwright.config.ts
@@ -1,30 +1,18 @@
 import { PlaywrightTestConfig, devices } from "@playwright/test";
 import { devices as replayDevices } from "@replayio/playwright";
 
-const { CHROMIUM_ONLY, CI, SLOW_MO } = process.env;
+const { CI, SLOW_MO } = process.env;
 
-let projects;
-if (!CHROMIUM_ONLY) {
-  if (CI) {
-    projects = [
-      {
-        name: "replay-chromium",
-        use: { ...(replayDevices["Replay Chromium"] as any) },
-      },
-      {
-        name: "chromium",
-        use: { ...devices["Desktop Chromium"] },
-      },
-    ];
-  } else {
-    projects = [
-      {
-        name: "chromium",
-        use: { ...devices["Desktop Chromium"] },
-      },
-    ];
-  }
-}
+const projects = [
+  {
+    name: "replay-chromium",
+    use: { ...(replayDevices["Replay Chromium"] as any) },
+  },
+  {
+    name: "chromium",
+    use: { ...devices["Desktop Chromium"] },
+  },
+];
 
 const config: PlaywrightTestConfig = {
   use: {
