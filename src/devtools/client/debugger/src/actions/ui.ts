replayio/devtools

commit 6697036c24c1db5c7872bcad36a9d8d5ff93cd5a

Preferences/settings/features cleanup [draft] (#9385)
## [Loom overview](https://www.loom.com/share/234d2f38a30b4233a33de4e8caa54508)

Remove the legacy Mozilla preferences service and replace it with a strongly-typed system preferences API that synchronizes to both `localStorage` and GraphQL.

Code for managing user data– whether `localStorage`, `IndexedDB`, or GraphQL– now lives in `shared/user-data`:
```
packages/shared/user-data
├── GraphQL
│   ├── UserData.ts
│   ├── config.ts
│   ├── constants.ts
│   ├── queries.ts
│   ├── types.ts
│   └── useGraphQLUserData.ts
├── IndexedDB
│   ├── cache.ts
│   ├── config.ts
│   ├── types.ts
│   ├── useIndexedDBUserData.ts
│   └── utils.ts
└── LocalStorage
    ├── config.ts
    ├── types.ts
    ├── useLocalStorageUserData.ts
    └── utils.ts
```

This PR also adds some documentation about how to choose which of the above services to manage user data. That documentation can be found here: [packages/shared/user-data/README.md](https://github.com/replayio/devtools/blob/FE-1483/packages/shared/user-data/README.md)

src/devtools/client/debugger/src/components/PrimaryPanes/index.tsx
@@ -1,16 +1,15 @@
 import classnames from "classnames";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { ConnectedProps, connect } from "react-redux";
 
 // Add the necessary imports for nag functionality
 import { useNag } from "replay-next/src/hooks/useNag";
 import { Nag } from "shared/graphql/types";
-import { useFeature } from "ui/hooks/settings";
+import { useGraphQLUserData } from "shared/user-data/GraphQL/useGraphQLUserData";
 import { UIState } from "ui/state";
 
 import actions from "../../actions";
 import { getContext, getSelectedPrimaryPaneTab, getSourcesCollapsed } from "../../selectors";
-import { useDebuggerPrefs } from "../../utils/prefs";
 import Outline from "../SourceOutline/SourceOutline";
 import QuickOpenButton from "./QuickOpenButton";
 import SourcesTree from "./SourcesTree";
@@ -18,10 +17,11 @@ import SourcesTree from "./SourcesTree";
 import { Accordion, AccordionPane } from "@recordreplay/accordion";
 
 function PrimaryPanes(props: PropsFromRedux) {
-  const { value: outlineExpanded, update: updateOutlineExpanded } =
-    useDebuggerPrefs("outline-expanded");
-  const { value: sourcesCollapsed } = useDebuggerPrefs("sources-collapsed");
-  const { value: enableLargeText } = useFeature("enableLargeText");
+  const [outlineExpanded, setOutlineExpanded] = useGraphQLUserData(
+    "layout_debuggerOutlineExpanded"
+  );
+  const [sourcesCollapsed] = useGraphQLUserData("layout_sourcesCollapsed");
+  const [enableLargeText] = useGraphQLUserData("global_enableLargeText");
 
   // Add the useNag hook and useEffect block
   const [, dismissInspectElementNag] = useNag(Nag.INSPECT_ELEMENT);
@@ -47,7 +47,7 @@ function PrimaryPanes(props: PropsFromRedux) {
         header="Outline"
         className="outlines-pane"
         expanded={!!outlineExpanded}
-        onToggle={() => updateOutlineExpanded(!outlineExpanded)}
+        onToggle={() => setOutlineExpanded(!outlineExpanded)}
       >
         <Outline />
       </AccordionPane>

src/devtools/client/debugger/src/reducers/ui.ts
@@ -5,10 +5,10 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import type { Location } from "@replayio/protocol";
 
+import { userData } from "shared/user-data/GraphQL/UserData";
 import { SourceDetails } from "ui/reducers/sources";
 import type { UIState } from "ui/state";
 
-import { prefs } from "../utils/prefs";
 import { closeQuickOpen } from "./quick-open";
 import type { Range } from "./types";
 
@@ -41,8 +41,8 @@ export const createUIState = (): UISliceState => ({
   fullTextSearchQuery: "",
   fullTextSearchFocus: false,
   shownSource: null,
-  sourcesCollapsed: prefs.sourcesCollapsed as boolean,
-  frameworkGroupingOn: prefs.frameworkGroupingOn as boolean,
+  sourcesCollapsed: userData.get("layout_sourcesCollapsed"),
+  frameworkGroupingOn: userData.get("debugger_frameworkGroupingOn"),
   highlightedLineRange: undefined,
   viewport: null,
   cursorPosition: null,

src/ui/actions/app.ts
@@ -6,12 +6,13 @@ import {
   jumpToPreviousPause,
 } from "devtools/client/debugger/src/actions/pause/jumps";
 import { openQuickOpen } from "devtools/client/debugger/src/actions/quick-open";
-import { prefs } from "devtools/client/debugger/src/utils/prefs";
 import { ThreadFront as ThreadFrontType } from "protocol/thread";
 import { ReplayClientInterface } from "shared/client/types";
+import { getSystemColorScheme } from "shared/theme/getSystemColorScheme";
+import { userData } from "shared/user-data/GraphQL/UserData";
 import { shallowEqual } from "shared/utils/compare";
 import { CommandKey } from "ui/components/CommandPalette/CommandPalette";
-import { getEventsForType, getTheme } from "ui/reducers/app";
+import { getEventsForType } from "ui/reducers/app";
 import { Canvas, EventKind, ReplayEvent, ReplayNavigationEvent } from "ui/state/app";
 import { boundingRectsCache } from "ui/suspense/nodeCaches";
 import { compareBigInt } from "ui/utils/helpers";
@@ -23,7 +24,6 @@ import {
   setModal,
   setMouseTargetsLoading,
   setSessionId,
-  updateTheme,
 } from "../reducers/app";
 import {
   hideCommandPalette,
@@ -84,14 +84,6 @@ function onNavigationEvents(events: ReplayNavigationEvent[], store: UIStore) {
   newNavEvents.sort((a, b) => compareBigInt(BigInt(a.point), BigInt(b.point)));
 
   store.dispatch(loadReceivedEvents({ navigation: newNavEvents }));
-}
-
-export function toggleTheme(): UIThunkAction {
-  return (dispatch, getState) => {
-    const theme = getTheme(getState());
-    const newTheme = theme == "dark" ? "light" : "dark";
-    dispatch(updateTheme(newTheme));
-  };
 }
 
 export function hideModal() {
@@ -228,7 +220,7 @@ export function executeCommand(key: CommandKey): UIThunkAction {
     } else if (key === "open_outline") {
       dispatch(setViewMode("dev"));
       dispatch(setSelectedPrimaryPanel("explorer"));
-      prefs.outlineExpanded = true;
+      userData.set("layout_debuggerOutlineExpanded", true);
     } else if (key === "open_viewer") {
       dispatch(setViewMode("non-dev"));
     } else if (key === "show_comments") {
@@ -245,7 +237,11 @@ export function executeCommand(key: CommandKey): UIThunkAction {
     } else if (key === "toggle_edit_focus") {
       dispatch(toggleFocusMode());
     } else if (key === "toggle_dark_mode") {
-      dispatch(toggleTheme());
+      let theme = userData.get("global_theme");
+      if (theme === "system") {
+        theme = getSystemColorScheme();
+      }
+      userData.set("global_theme", theme === "dark" ? "light" : "dark");
     } else if (key === "pin_to_bottom") {
       dispatch(setToolboxLayout("bottom"));
     } else if (key === "pin_to_left") {

src/ui/reducers/layout.ts
@@ -1,13 +1,13 @@
 import { sourcesDisplayed } from "devtools/client/debugger/src/reducers/ui";
+import { userData } from "shared/user-data/GraphQL/UserData";
 import { LayoutAction } from "ui/actions/layout";
 import { UIState } from "ui/state";
-import { LayoutState, ViewMode } from "ui/state/layout";
-import { prefs } from "ui/utils/prefs";
+import { LayoutState } from "ui/state/layout";
 
 export const syncInitialLayoutState: LayoutState = {
   showCommandPalette: false,
   selectedPrimaryPanel: null,
-  viewMode: prefs.defaultMode as ViewMode,
+  viewMode: userData.get("layout_defaultViewMode"),
   toolboxLayout: "ide",
   selectedPanel: "console",
   localNags: [],

packages/replay-next/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useCallback } from "react";
+import { Suspense, useCallback, useTransition } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
 import CommentList from "replay-next/components/comments/CommentList";
@@ -20,8 +20,9 @@ import { SelectedFrameContextRoot } from "replay-next/src/contexts/SelectedFrame
 import { SourcesContextRoot } from "replay-next/src/contexts/SourcesContext";
 import { TerminalContextRoot } from "replay-next/src/contexts/TerminalContext";
 import { TimelineContextRoot } from "replay-next/src/contexts/TimelineContext";
-import useLocalStorage from "replay-next/src/hooks/useLocalStorage";
 import usePreferredColorScheme from "replay-next/src/hooks/usePreferredColorScheme";
+import { ReplayNextCurrentPanel } from "shared/user-data/LocalStorage/config";
+import useLocalStorageUserData from "shared/user-data/LocalStorage/useLocalStorageUserData";
 
 import styles from "./index.module.css";
 
@@ -35,10 +36,20 @@ export default function HomePage({ apiKey }: { apiKey?: string }) {
   // TODO As we finalize the client implementation to interface with Replay backend,
   // we can inject a wrapper here that also reports cache hits and misses to this UI in a debug panel.
 
-  const [panel, setPanel, isPending] = useLocalStorage<Panel>("bvaughn:panel", "sources", true);
+  const [isPending, startTransition] = useTransition();
 
-  const showCommentsPanel = useCallback(() => setPanel("comments"), [setPanel]);
-  const showSourcesPanel = useCallback(() => setPanel("sources"), [setPanel]);
+  const [panel, setPanel] = useLocalStorageUserData("replayNextCurrentPanel");
+  const setPanelTransition = useCallback(
+    (value: ReplayNextCurrentPanel) => {
+      startTransition(() => {
+        setPanel(value);
+      });
+    },
+    [setPanel]
+  );
+
+  const showCommentsPanel = useCallback(() => setPanelTransition("comments"), [setPanelTransition]);
+  const showSourcesPanel = useCallback(() => setPanelTransition("sources"), [setPanelTransition]);
 
   usePreferredColorScheme();
 
@@ -61,7 +72,7 @@ export default function HomePage({ apiKey }: { apiKey?: string }) {
                             className={panel === "comments" ? styles.TabSelected : styles.Tab}
                             data-test-id="TabButton-Comments"
                             disabled={isPending}
-                            onClick={() => setPanel("comments")}
+                            onClick={() => setPanelTransition("comments")}
                           >
                             <Icon className={styles.TabIcon} type="comments" />
                           </button>
@@ -69,7 +80,7 @@ export default function HomePage({ apiKey }: { apiKey?: string }) {
                             className={panel === "sources" ? styles.TabSelected : styles.Tab}
                             data-test-id="TabButton-Sources"
                             disabled={isPending}
-                            onClick={() => setPanel("sources")}
+                            onClick={() => setPanelTransition("sources")}
                           >
                             <Icon className={styles.TabIcon} type="source-explorer" />
                           </button>
@@ -77,7 +88,7 @@ export default function HomePage({ apiKey }: { apiKey?: string }) {
                             className={panel === "search" ? styles.TabSelected : styles.Tab}
                             data-test-id="TabButton-Search"
                             disabled={isPending}
-                            onClick={() => setPanel("search")}
+                            onClick={() => setPanelTransition("search")}
                           >
                             <Icon className={styles.TabIcon} type="search" />
                           </button>
@@ -87,7 +98,7 @@ export default function HomePage({ apiKey }: { apiKey?: string }) {
                             }
                             data-test-id="TabButton-ProtocolViewer"
                             disabled={isPending}
-                            onClick={() => setPanel("protocol-viewer")}
+                            onClick={() => setPanelTransition("protocol-viewer")}
                           >
                             <Icon className={styles.TabIcon} type="protocol-viewer" />
                           </button>

src/devtools/client/debugger/src/components/SecondaryPanes/index.tsx
@@ -1,16 +1,8 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at <http://mozilla.org/MPL/2.0/>. */
-
-//
-
-import React from "react";
-
+import { useGraphQLUserData } from "shared/user-data/GraphQL/useGraphQLUserData";
 import { getCurrentPoint } from "ui/actions/app";
 import { getCurrentTime } from "ui/reducers/timeline";
 import { useAppSelector } from "ui/setup/hooks";
 
-import { useDebuggerPrefs } from "../../utils/prefs";
 import BreakpointsPane from "./BreakpointsPane";
 import CommandBar from "./CommandBar";
 import NewFrames from "./Frames/NewFrames";
@@ -24,14 +16,12 @@ export default function SecondaryPanes() {
   const currentPoint = useAppSelector(getCurrentPoint);
   const currentTime = useAppSelector(getCurrentTime);
 
-  const { value: scopesExpanded, update: updateScopesExpanded } =
-    useDebuggerPrefs("scopes-visible");
-  const { value: callstackVisible, update: updateCallstackVisible } =
-    useDebuggerPrefs("call-stack-visible");
-  const { value: breakpointsVisible, update: updateBreakpointsVisible } =
-    useDebuggerPrefs("breakpoints-visible");
-  const { value: logpointsVisible, update: updateLogpointsVisible } =
-    useDebuggerPrefs("logpoints-visible");
+  const [scopesVisible, setScopesVisible] = useGraphQLUserData("debugger_scopesVisible");
+  const [callStackVisible, setCallStackVisible] = useGraphQLUserData("debugger_callStackVisible");
+  const [breakpointsVisible, setBreakpointsVisible] = useGraphQLUserData(
+    "debugger_breakpointsVisible"
+  );
+  const [logpointsVisible, setLogpointsVisible] = useGraphQLUserData("debugger_logpointsVisible");
 
   return (
     <div className="secondary-panes-wrapper">
@@ -42,7 +32,7 @@ export default function SecondaryPanes() {
           header="Breakpoints"
           className="breakpoints-pane"
           expanded={breakpointsVisible}
-          onToggle={() => updateBreakpointsVisible(!breakpointsVisible)}
+          onToggle={() => setBreakpointsVisible(!breakpointsVisible)}
         >
           <BreakpointsPane />
         </AccordionPane>
@@ -50,23 +40,23 @@ export default function SecondaryPanes() {
           header="Print Statements"
           className="breakpoints-pane"
           expanded={logpointsVisible}
-          onToggle={() => updateLogpointsVisible(!logpointsVisible)}
+          onToggle={() => setLogpointsVisible(!logpointsVisible)}
         >
           <LogpointsPane />
         </AccordionPane>
         <AccordionPane
           header="Call Stack"
           className="call-stack-pane"
-          expanded={callstackVisible}
-          onToggle={() => updateCallstackVisible(!callstackVisible)}
+          expanded={callStackVisible}
+          onToggle={() => setCallStackVisible(!callStackVisible)}
         >
           {currentPoint && <NewFrames point={currentPoint} time={currentTime} panel="debugger" />}
         </AccordionPane>
         <AccordionPane
           header="Scopes"
           className="scopes-pane"
-          expanded={scopesExpanded}
-          onToggle={() => updateScopesExpanded(!scopesExpanded)}
+          expanded={scopesVisible}
+          onToggle={() => setScopesVisible(!scopesVisible)}
         >
           <NewScopes />
         </AccordionPane>

src/ui/actions/layout.ts
@@ -1,5 +1,6 @@
 import { Action } from "@reduxjs/toolkit";
 
+import { ViewMode } from "shared/user-data/GraphQL/config";
 import {
   getLocalNags,
   getSelectedPanel,
@@ -12,7 +13,6 @@ import {
   SecondaryPanelName,
   ToolboxLayout,
   VIEWER_PANELS,
-  ViewMode,
 } from "ui/state/layout";
 import { trackEvent } from "ui/utils/telemetry";
 

src/ui/components/SecondaryToolbox/index.tsx
@@ -1,25 +1,17 @@
-import "ui/setup/dynamic/inspector";
 import classnames from "classnames";
-import React, {
-  FC,
-  ReactNode,
-  RefObject,
-  Suspense,
-  useContext,
-  useEffect,
-  useLayoutEffect,
-  useState,
-} from "react";
+import React, { FC, ReactNode, RefObject, Suspense, useContext, useLayoutEffect } from "react";
 import { ImperativePanelHandle } from "react-resizable-panels";
 import { useImperativeCacheValue } from "suspense";
 
 import { EditorPane } from "devtools/client/debugger/src/components/Editor/EditorPane";
 import LazyOffscreen from "replay-next/components/LazyOffscreen";
-import { recordingCapabilitiesCache } from "replay-next/src/suspense/BuildIdCache";
-import { RecordingCapabilities } from "replay-next/src/suspense/BuildIdCache";
+import {
+  RecordingCapabilities,
+  recordingCapabilitiesCache,
+} from "replay-next/src/suspense/BuildIdCache";
 import { ReplayClientContext } from "shared/client/ReplayClientContext";
+import { useGraphQLUserData } from "shared/user-data/GraphQL/useGraphQLUserData";
 import { setSelectedPanel } from "ui/actions/layout";
-import { useFeature } from "ui/hooks/settings";
 import { getSelectedPanel, getToolboxLayout } from "ui/reducers/layout";
 import { useAppDispatch, useAppSelector } from "ui/setup/hooks";
 import { SecondaryPanelName, ToolboxLayout } from "ui/state/layout";
@@ -90,7 +82,7 @@ const PanelButtons: FC<PanelButtonsProps> = ({
 }) => {
   const { supportsElementsInspector, supportsNetworkRequests, supportsRepaintingGraphics } =
     recordingCapabilities;
-  const { value: chromiumNetMonitorEnabled } = useFeature("chromiumNetMonitor");
+  const [chromiumNetMonitorEnabled] = useGraphQLUserData("feature_chromiumNetMonitor");
 
   return (
     <div className="panel-buttons theme-tab-font-size flex flex-row items-center overflow-hidden">
@@ -185,8 +177,8 @@ function SecondaryToolbox({
     REDUX_ANNOTATIONS_KIND
   );
 
+  const chromiumNetMonitorEnabled = useGraphQLUserData("feature_chromiumNetMonitor");
   const recordingCapabilities = recordingCapabilitiesCache.read(replayClient);
-  const { value: chromiumNetMonitorEnabled } = useFeature("chromiumNetMonitor");
 
   useLayoutEffect(() => {
     if (selectedPanel === "react-components" && !hasReactAnnotations) {

commit de6445a1ad08b7f12c618ccb25c5a23e37d0ccb7

Fix collapsible Sources tab in sidebar
I think this regressed in #9385. We had two places controlling this behavior- a user preference (stored in GraphQL) and a piece of Redux state (that got initialized to the user preference, but then got out of sync). This commit removes the latter since it's redundant.

src/devtools/client/debugger/src/actions/ui.ts
@@ -18,10 +18,7 @@ import {
   highlightLineRange,
   setCursorPosition,
   setPrimaryPaneTab,
-  sourcesDisplayed,
-  sourcesPanelExpanded,
   toggleActiveSearch,
-  toggleSources,
 } from "../reducers/ui";
 import { getActiveSearch, getContext, getQuickOpenEnabled } from "../selectors";
 import { selectSource } from "./sources/select";
@@ -37,11 +34,8 @@ export {
   setPrimaryPaneTab,
   setShownSource,
   setViewport,
-  sourcesDisplayed,
-  sourcesPanelExpanded,
   toggleActiveSearch,
   toggleFrameworkGrouping,
-  toggleSources,
 } from "../reducers/ui";
 
 export function setActiveSearch(activeSearch: ActiveSearchType): UIThunkAction {
@@ -60,9 +54,6 @@ export function setActiveSearch(activeSearch: ActiveSearchType): UIThunkAction {
 }
 
 // Preserve existing export names
-export const ensureSourcesIsVisible = sourcesDisplayed;
-export const toggleSourcesCollapse = toggleSources;
-export const expandSourcesPane = sourcesPanelExpanded;
 export const updateCursorPosition = setCursorPosition;
 
 export function openSourceLink(sourceId: string, line?: number, column?: number): UIThunkAction {

src/devtools/client/debugger/src/components/Editor/useTabContextMenu.tsx
@@ -3,12 +3,12 @@ import { ContextMenuDivider, ContextMenuItem, useContextMenu } from "use-context
 import { closeTab, closeTabs } from "devtools/client/debugger/src/actions/tabs";
 import {
   copyToClipboard as copySourceToClipboard,
-  ensureSourcesIsVisible,
   showSource,
 } from "devtools/client/debugger/src/actions/ui";
 import { Tab, getContext, getTabs } from "devtools/client/debugger/src/selectors";
 import { getRawSourceURL } from "devtools/client/debugger/src/utils/source";
 import { copyToClipboard as copyTextToClipboard } from "replay-next/components/sources/utils/clipboard";
+import { userData } from "shared/user-data/GraphQL/UserData";
 import { MiniSource, getSelectedSource } from "ui/reducers/sources";
 import { useAppDispatch, useAppSelector } from "ui/setup/hooks";
 
@@ -52,8 +52,9 @@ export default function useTabContextMenu({ source }: { source: MiniSource }) {
     }
   };
 
-  const onRevealInTreeClick = () => {
-    dispatch(ensureSourcesIsVisible());
+  const onRevealInTreeClick = async () => {
+    await userData.set("layout_sourcesCollapsed", false);
+
     dispatch(showSource(cx, source.id));
   };
 

src/devtools/client/debugger/src/components/PrimaryPanes/index.tsx
@@ -1,26 +1,22 @@
 import classnames from "classnames";
 import { useEffect } from "react";
-import { ConnectedProps, connect } from "react-redux";
 
 // Add the necessary imports for nag functionality
 import { useNag } from "replay-next/src/hooks/useNag";
 import { Nag } from "shared/graphql/types";
 import { useGraphQLUserData } from "shared/user-data/GraphQL/useGraphQLUserData";
-import { UIState } from "ui/state";
 
-import actions from "../../actions";
-import { getContext, getSelectedPrimaryPaneTab, getSourcesCollapsed } from "../../selectors";
 import Outline from "../SourceOutline/SourceOutline";
 import QuickOpenButton from "./QuickOpenButton";
 import SourcesTree from "./SourcesTree";
 
 import { Accordion, AccordionPane } from "@recordreplay/accordion";
 
-function PrimaryPanes(props: PropsFromRedux) {
+export default function PrimaryPanes() {
   const [outlineExpanded, setOutlineExpanded] = useGraphQLUserData(
     "layout_debuggerOutlineExpanded"
   );
-  const [sourcesCollapsed] = useGraphQLUserData("layout_sourcesCollapsed");
+  const [sourcesCollapsed, setSourcesCollapsed] = useGraphQLUserData("layout_sourcesCollapsed");
   const [enableLargeText] = useGraphQLUserData("global_enableLargeText");
 
   // Add the useNag hook and useEffect block
@@ -37,7 +33,7 @@ function PrimaryPanes(props: PropsFromRedux) {
         // ExperimentFeature: LargeText Logic
         className={classnames("sources-pane", enableLargeText ? "text-base" : "text-xs")}
         expanded={!sourcesCollapsed}
-        onToggle={() => props.toggleSourcesCollapse()}
+        onToggle={() => setSourcesCollapsed(!sourcesCollapsed)}
         initialHeight={400}
         button={<QuickOpenButton />}
       >
@@ -54,22 +50,3 @@ function PrimaryPanes(props: PropsFromRedux) {
     </Accordion>
   );
 }
-
-const mapStateToProps = (state: UIState) => {
-  return {
-    cx: getContext(state),
-    selectedTab: getSelectedPrimaryPaneTab(state),
-    sourcesCollapsed: getSourcesCollapsed(state),
-  };
-};
-
-const connector = connect(mapStateToProps, {
-  setPrimaryPaneTab: actions.setPrimaryPaneTab,
-  setActiveSearch: actions.setActiveSearch,
-  closeActiveSearch: actions.closeActiveSearch,
-  toggleSourcesCollapse: actions.toggleSourcesCollapse,
-});
-
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-export default connector(PrimaryPanes);

src/devtools/client/debugger/src/reducers/ui.ts
@@ -28,7 +28,6 @@ export interface UISliceState {
   fullTextSearchQuery: string;
   fullTextSearchFocus: boolean;
   shownSource?: SourceDetails | null;
-  sourcesCollapsed: boolean;
   frameworkGroupingOn: boolean;
   viewport?: Range | null;
   cursorPosition?: Location | null;
@@ -41,7 +40,6 @@ export const createUIState = (): UISliceState => ({
   fullTextSearchQuery: "",
   fullTextSearchFocus: false,
   shownSource: null,
-  sourcesCollapsed: userData.get("layout_sourcesCollapsed"),
   frameworkGroupingOn: userData.get("debugger_frameworkGroupingOn"),
   highlightedLineRange: undefined,
   viewport: null,
@@ -60,12 +58,6 @@ const uiSlice = createSlice({
     },
     setShownSource(state, action: PayloadAction<SourceDetails | null>) {
       state.shownSource = action.payload;
-    },
-    toggleSources(state) {
-      state.sourcesCollapsed = !state.sourcesCollapsed;
-    },
-    sourcesPanelExpanded(state) {
-      state.sourcesCollapsed = false;
     },
     highlightLineRange(state, action: PayloadAction<HighlightedRange>) {
       const { start, end, sourceId } = action.payload;
@@ -99,12 +91,6 @@ const uiSlice = createSlice({
     setCursorPosition(state, action: PayloadAction<Location | null>) {
       state.cursorPosition = action.payload;
     },
-    // Need to ensure three pieces of UI are updated:
-    // Here: pause info panel is open, sources are open
-    // Layout reducer: selected primary panel is "explorer"
-    sourcesDisplayed(state) {
-      state.sourcesCollapsed = false;
-    },
   },
   extraReducers: builder => {
     builder.addCase(closeQuickOpen, state => {
@@ -123,11 +109,8 @@ export const {
   setPrimaryPaneTab,
   setShownSource,
   setViewport,
-  sourcesDisplayed,
-  sourcesPanelExpanded,
   toggleActiveSearch,
   toggleFrameworkGrouping,
-  toggleSources,
 } = uiSlice.actions;
 
 export function closeActiveSearch() {
@@ -140,7 +123,6 @@ export const getSelectedPrimaryPaneTab = (state: UIState) => state.ui.selectedPr
 export const getActiveSearch = (state: UIState) => state.ui.activeSearch;
 export const getFrameworkGroupingState = (state: UIState) => state.ui.frameworkGroupingOn;
 export const getShownSource = (state: UIState) => state.ui.shownSource;
-export const getSourcesCollapsed = (state: UIState) => state.ui.sourcesCollapsed;
 export const getHighlightedLineRange = (state: UIState) => state.ui.highlightedLineRange;
 export const getViewport = (state: UIState) => state.ui.viewport;
 export const getCursorPosition = (state: UIState) => state.ui.cursorPosition;

src/ui/actions/app.ts
@@ -214,9 +214,6 @@ export function executeCommand(key: CommandKey): UIThunkAction {
     } else if (key === "open_sources") {
       dispatch(setViewMode("dev"));
       dispatch(setSelectedPrimaryPanel("explorer"));
-      // Someday we'll fix circular dependencies. Today is not that day.
-      const { expandSourcesPane } = await import("devtools/client/debugger/src/actions/ui");
-      dispatch(expandSourcesPane());
     } else if (key === "open_outline") {
       dispatch(setViewMode("dev"));
       dispatch(setSelectedPrimaryPanel("explorer"));

src/ui/reducers/layout.ts
@@ -1,4 +1,3 @@
-import { sourcesDisplayed } from "devtools/client/debugger/src/reducers/ui";
 import { userData } from "shared/user-data/GraphQL/UserData";
 import { LayoutAction } from "ui/actions/layout";
 import { UIState } from "ui/state";
@@ -28,10 +27,6 @@ export default function update(state = syncInitialLayoutState, action: LayoutAct
 
     case "set_selected_primary_panel": {
       return { ...state, selectedPrimaryPanel: action.panel };
-    }
-
-    case sourcesDisplayed.type: {
-      return { ...state, selectedPrimaryPanel: "explorer" };
     }
 
     case "set_view_mode": {