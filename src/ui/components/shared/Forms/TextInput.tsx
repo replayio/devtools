aec078a72fe1c490fcd463700a968863b9d2d85d
Jon Bell <github@lot23.com>
2023-08-23 10:08:41 +1200

    Use system instead of default

--- src/ui/components/shared/Forms/TextInput.tsx
+++ src/ui/components/shared/Forms/TextInput.tsx
@@ -14,7 +14,7 @@ export default React.forwardRef<
 >(function TextInput(props, ref) {
   const { textSize, center, theme, className, ...otherProps } = props;
 
-  const themeClass = theme || "default";
+  const themeClass = theme || "system";
   let textSizeClass = "text-sm"; // Default Tailwind class for textSize
 
   // Explicitly map textSize prop to Tailwind classes
