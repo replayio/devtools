cde0013064588bf8251a4629354b045025ff0abd
Mark Erikson <mark@isquaredsoftware.com>
2023-08-30 15:40:45 -0400

    Simplify LayoutApp/BoxModel component structure
    - Removed useless `showBoxModelProperties` prop
    - Removed useless "Box Model" accordion in LayoutApp
    - Converted LayoutApp to a function component

--- src/devtools/client/inspector/layout/components/LayoutApp.tsx
+++ src/devtools/client/inspector/layout/components/LayoutApp.tsx
@@ -3,53 +3,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React from "react";
-import { ConnectedProps, connect } from "react-redux";
 
-import Accordion from "devtools/client/shared/components/Accordion";
-import { userData } from "shared/user-data/GraphQL/UserData";
-import type { UIState } from "ui/state";
+import { useAppSelector } from "ui/setup/hooks";
 
 import { BoxModel } from "../../boxmodel/components/BoxModel";
 
-const mapStateToProps = (state: UIState) => ({
-  boxModel: state.boxModel,
-});
+function LayoutApp() {
+  const boxModel = useAppSelector(state => state.boxModel);
 
-const connector = connect(mapStateToProps);
-type PropsFromRedux = ConnectedProps<typeof connector>;
-
-interface LayoutAppProps {
-  showBoxModelProperties: boolean;
-}
-
-type FinalLAProps = PropsFromRedux & LayoutAppProps;
-
-class LayoutApp extends React.PureComponent<FinalLAProps> {
-  getBoxModelSection() {
-    return {
-      component: BoxModel,
-      componentProps: this.props,
-      contentClassName: "layout-content",
-      header: "Box Model",
-      id: "layout-section-boxmodel",
-      opened: userData.get("layout_inspectorBoxModelOpen"),
-      onToggle: (opened: boolean) => {
-        userData.set("layout_inspectorBoxModelOpen", opened);
-      },
-    };
-  }
-
-  render() {
-    const items = [this.getBoxModelSection()];
-
-    return (
-      <div className="layout-container">
-        <div className="h-full overflow-y-auto">
-          <Accordion items={items} style={{ overflow: "auto" }} />
-        </div>
+  return (
+    <div className="layout-container">
+      <div className="h-full overflow-y-auto">
+        <BoxModel boxModel={boxModel} />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default connector(LayoutApp);
+export default React.memo(LayoutApp);
