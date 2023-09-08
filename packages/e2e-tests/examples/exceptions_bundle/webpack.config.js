65de266d58e30cd47fcddf57978b7867c2b0a347
Mark Erikson <mark@isquaredsoftware.com>
2023-09-08 16:01:55 -0400

    Move over initial bundle examples

--- packages/e2e-tests/examples/exceptions_bundle/webpack.config.js
+++ packages/e2e-tests/examples/exceptions_bundle/webpack.config.js
@@ -5,9 +5,8 @@ module.exports = {
   entry: "./bundle_input.js",
   output: {
     filename: "exceptions_bundle.js",
-    path: path.resolve(__dirname, "../..")
+    path: path.resolve(__dirname, "./dist"),
   },
   devtool: "source-map",
-  mode: "development"
+  mode: "development",
 };
-
