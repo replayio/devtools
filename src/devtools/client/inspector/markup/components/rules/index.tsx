280474763c13dc598be1c92c70788c0d2b97c48a
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-09-14 15:08:49 -0400

    Add ErrorBoundary around Rules panel (#9733)
    * Add ErrorBoundary around Rules panel
    * Removed unused react-error-boundary dependency

--- src/devtools/client/inspector/markup/components/rules/index.tsx
+++ src/devtools/client/inspector/markup/components/rules/index.tsx
@@ -1,12 +1,19 @@
 import { Suspense } from "react";
 
 import { RulesPanelSuspends } from "devtools/client/inspector/markup/components/rules/RulesPanel";
+import { getSelectedNodeId } from "devtools/client/inspector/markup/selectors/markup";
+import ErrorBoundary from "replay-next/components/ErrorBoundary";
 import Loader from "replay-next/components/Loader";
+import { useAppSelector } from "ui/setup/hooks";
 
 export function RulesPanel() {
+  const selectedNodeId = useAppSelector(getSelectedNodeId);
+
   return (
-    <Suspense fallback={<Loader />}>
-      <RulesPanelSuspends />
-    </Suspense>
+    <ErrorBoundary name="RulesPanel" resetKey={selectedNodeId ?? undefined}>
+      <Suspense fallback={<Loader />}>
+        <RulesPanelSuspends />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
