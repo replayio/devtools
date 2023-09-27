5accaf56db8e74d19b33a454a0639895b3c1129e
Holger Benl <hbenl@evandor.de>
2023-09-27 14:51:56 +0200

    Load workspaces before showing the UploadScreen

--- pages/recording/upload.tsx
+++ pages/recording/upload.tsx
@@ -5,12 +5,14 @@ import { BlankViewportWrapper } from "ui/components/shared/Viewport";
 import UploadScreen from "ui/components/UploadScreen";
 import { useGetRecording, useGetRecordingId } from "ui/hooks/recordings";
 import { useGetUserSettings } from "ui/hooks/settings";
+import { useGetNonPendingWorkspaces } from "ui/hooks/workspaces";
 
 function UploadScreenWrapper({ onUpload }: { onUpload: () => void }) {
   const recordingId = useGetRecordingId();
   const { recording } = useGetRecording(recordingId);
-  // Make sure to get the user's settings before showing the upload screen.
+  // Make sure to get the user's settings and workspaces before showing the upload screen.
   const { userSettings, loading } = useGetUserSettings();
+  const { workspaces, loading: loading2 } = useGetNonPendingWorkspaces();
 
   useEffect(() => {
     if (recording?.isInitialized) {
@@ -21,9 +23,17 @@ function UploadScreenWrapper({ onUpload }: { onUpload: () => void }) {
   if (loading) {
     return <LoadingScreen message="Loading settings..." />;
   }
+  if (loading2) {
+    return <LoadingScreen message="Loading team info..." />;
+  }
 
   return recording ? (
-    <UploadScreen userSettings={userSettings} recording={recording} onUpload={onUpload} />
+    <UploadScreen
+      userSettings={userSettings}
+      workspaces={workspaces}
+      recording={recording}
+      onUpload={onUpload}
+    />
   ) : (
     <BlankViewportWrapper />
   );
