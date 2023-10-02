46045a3dc603bfcb467624c07263bee45fd2b401
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-10-02 11:41:10 -0400

    Fixed source search case highlight case sensitivity (#9800)

---
+++ packages/replay-next/components/search-files/findMatches.test.ts
@@ -0,0 +1,95 @@
+import { findMatches } from "replay-next/components/search-files/findMatches";
+
+describe("findMatches", () => {
+  it("should handle empty text", () => {
+    expect(findMatches("", "needle", true)).toMatchInlineSnapshot(`Array []`);
+  });
+
+  it("should handle empty needle", () => {
+    expect(findMatches("text", "", true)).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "text",
+          "",
+        ],
+      ]
+    `);
+  });
+
+  it("should handle case sensitive matches", () => {
+    expect(findMatches("The the THE thE the", "the", true)).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "The ",
+          "the",
+        ],
+        Array [
+          " THE E ",
+          "the",
+        ],
+      ]
+    `);
+  });
+
+  it("should handle case insensitive matches", () => {
+    expect(findMatches("The the THE", "the", false)).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "",
+          "The",
+        ],
+        Array [
+          " ",
+          "the",
+        ],
+        Array [
+          " ",
+          "THE",
+        ],
+      ]
+    `);
+  });
+
+  it("should handle partially incomplete matches", () => {
+    expect(findMatches("foo bar", "bark", false)).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "foo bar",
+          "",
+        ],
+      ]
+    `);
+
+    expect(findMatches("bark foo b", "bark", false)).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "",
+          "bark",
+        ],
+        Array [
+          " foo b",
+          "",
+        ],
+      ]
+    `);
+  });
+
+  it("should handle consecutive matches", () => {
+    expect(findMatches("foobarbarbaz", "bar", false)).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "foo",
+          "bar",
+        ],
+        Array [
+          "",
+          "bar",
+        ],
+        Array [
+          "z",
+          "",
+        ],
+      ]
+    `);
+  });
+});
