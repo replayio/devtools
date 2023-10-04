3447c5e3faa3989552447e7e107fda720678fb5f
Mark Erikson <mark@isquaredsoftware.com>
2023-10-04 13:20:07 -0400

    Rewrite RDT subapp sourcemaps for separate paths

--- packages/e2e-tests/examples/rdt-react-versions/updateExample.js
+++ packages/e2e-tests/examples/rdt-react-versions/updateExample.js
@@ -1,6 +1,9 @@
 const path = require("path");
 const fse = require("fs-extra");
 const { spawnSync } = require("child_process");
+const glob = require("glob");
+
+const remapping = require("@ampproject/remapping");
 
 const copyToLocalDev = process.argv.includes("dev");
 
@@ -18,11 +21,31 @@ if (copyToLocalDev) {
   fse.emptyDirSync(localPublicFolder);
 
   for (const version of reactSubVersions) {
-    const appDist = path.join(currentFolder, `react-${version}-app`, "dist");
+    const reactExampleFolderName = `react-${version}-app`;
+    const appDist = path.join(currentFolder, reactExampleFolderName, "dist");
     const targetFolder = path.join(localPublicFolder, `react${version}`);
     fse.mkdirpSync(targetFolder);
     console.log(`Copying React ${version} example to ${targetFolder}`);
     fse.copySync(appDist, targetFolder);
+
+    // Rewrite the sourcemaps to show each example's source files as coming from a different folder,
+    // so they don't stomp on each other when the backend processes them.
+    const targetAssetPath = path.join(targetFolder, "assets");
+    const sourcemapFiles = glob.sync(path.join(targetAssetPath, "*.js.map"), { absolute: true });
+
+    console.log("Rewriting sourcemaps for example: ", reactExampleFolderName);
+
+    for (const sourcemapFile of sourcemapFiles) {
+      const parsedSourcemap = JSON.parse(fse.readFileSync(sourcemapFile, "utf8"));
+      const remappedSourcemap = remapping(parsedSourcemap, (file, ctx) => {
+        if (file.startsWith("../../node_modules") || file.startsWith("../../src")) {
+          // Replace the original sourcemapped file path to pretend it was in an example-specific folder
+          ctx.source = file.replace("../../", `../../${reactExampleFolderName}/`);
+        }
+        return null;
+      });
+      fse.writeFileSync(sourcemapFile, JSON.stringify(remappedSourcemap), "utf8");
+    }
   }
 } else {
   const exampleDist = path.join(publicExamplesFolder, "rdt-react-versions/dist");
@@ -40,11 +63,11 @@ if (copyToLocalDev) {
     [
       "replay",
       "upload-sourcemaps",
-      "--dry-run",
+      // "--dry-run",
       "--group",
       "e2e-test",
       "--api-key",
-      "rwk_7XPbO5fhz0bkhANYXtN2dkm74wNQCchXf2OxVgAerTQ",
+      "rwk_7o3q05qOwAXoYHWiVLra5cuOilLIghqDRMWyd8ObPac",
       buildFolder,
     ],
     { stdio: "inherit", cwd: currentFolder }
