678fbbab3e07f9d5e6f3724c6b0942e40cb0c33d
Mark Erikson <mark@isquaredsoftware.com>
2023-10-04 13:19:33 -0400

    Add built artifacts for rdt-react-versions example

---
+++ public/test/examples/rdt-react-versions/dist/react15/assets/index-22620522.js
@@ -0,0 +1,2 @@
+import{R as e,j as t,a}from"./App-d50c903f.js";e.render(t.jsx(a,{}),document.getElementById("root"));
+//# sourceMappingURL=index-22620522.js.map
---
+++ public/test/examples/rdt-react-versions/dist/react17/assets/index-b24a144e.js
@@ -0,0 +1,2 @@
+import{R as e,j as t,a}from"./App-5443e92c.js";e.render(t.jsx(a,{}),document.getElementById("root"));
+//# sourceMappingURL=index-b24a144e.js.map
