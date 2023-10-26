5685852570af10bf1c39abf322e22363c8120d55
Ryan Duffy <ryan@replay.io>
2023-10-26 12:39:53 -0700

    revert some unnecessary changes

--- src/ui/components/Library/Team/View/TestRuns/hooks/useTestRunRecordingsSuspends.ts
+++ src/ui/components/Library/Team/View/TestRuns/hooks/useTestRunRecordingsSuspends.ts
@@ -1,31 +1,20 @@
 import { useContext } from "react";
 
-import { assert } from "protocol/utils";
 import { GraphQLClientContext } from "replay-next/src/contexts/GraphQLClientContext";
 import { TeamContext } from "ui/components/Library/Team/TeamContextRoot";
 import { testRunRecordingsCache } from "ui/components/Library/Team/View/TestRuns/suspense/TestRunsCache";
 import useToken from "ui/utils/useToken";
 
-import { TestRunsContext } from "../TestRunsContextRoot";
-
 export function useTestRunRecordingsSuspends(testRunId: string | null) {
-  const { testRuns } = useContext(TestRunsContext);
   const graphQLClient = useContext(GraphQLClientContext);
   const { teamId } = useContext(TeamContext);
 
   const accessToken = useToken();
 
-  if (testRunId) {
-    return testRunRecordingsCache.read(
-      graphQLClient,
-      accessToken?.token ?? null,
-      teamId,
-      testRunId
-    );
-  }
-
-  return {
-    groupedRecordings: null,
-    recordings: null,
-  };
+  return testRunId
+    ? testRunRecordingsCache.read(graphQLClient, accessToken?.token ?? null, teamId, testRunId)
+    : {
+        groupedRecordings: null,
+        recordings: null,
+      };
 }
