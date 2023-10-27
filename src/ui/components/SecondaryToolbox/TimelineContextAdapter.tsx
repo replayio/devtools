a5a1cc09a861da97c51e3a15cee00e5fdee0247f
Holger Benl <hbenl@evandor.de>
2023-10-27 12:11:13 +0200

    TimelineContextAdapter: pass down current point and time without delay

--- src/ui/components/SecondaryToolbox/TimelineContextAdapter.tsx
+++ src/ui/components/SecondaryToolbox/TimelineContextAdapter.tsx
@@ -1,31 +1,14 @@
 import { ExecutionPoint } from "@replayio/protocol";
-import React, {
-  PropsWithChildren,
-  useCallback,
-  useContext,
-  useLayoutEffect,
-  useMemo,
-  useState,
-  useTransition,
-} from "react";
+import React, { PropsWithChildren, useCallback, useMemo } from "react";
 
 import { getExecutionPoint } from "devtools/client/debugger/src/selectors";
 import { TimelineContext, TimelineContextType } from "replay-next/src/contexts/TimelineContext";
-import { ReplayClientContext } from "shared/client/ReplayClientContext";
 import { seek } from "ui/actions/timeline";
 import { getCurrentTime } from "ui/reducers/timeline";
 import { useAppDispatch, useAppSelector } from "ui/setup/hooks";
 
 // Adapter that reads the current execution point and time (from Redux) and passes them to the TimelineContext.
 export default function TimelineContextAdapter({ children }: PropsWithChildren) {
-  const client = useContext(ReplayClientContext);
-  const [state, setState] = useState<Omit<TimelineContextType, "isPending" | "update">>({
-    executionPoint: "0",
-    time: 0,
-  });
-
-  const [isPending, startTransition] = useTransition();
-
   const dispatch = useAppDispatch();
   const time = useAppSelector(getCurrentTime);
   const executionPoint = useAppSelector(getExecutionPoint) || "0";
@@ -37,19 +20,14 @@ export default function TimelineContextAdapter({ children }: PropsWithChildren) 
     [dispatch]
   );
 
-  useLayoutEffect(() => {
-    startTransition(() => {
-      setState({ executionPoint, time });
-    });
-  }, [executionPoint, time]);
-
   const context = useMemo<TimelineContextType>(
     () => ({
-      ...state,
-      isPending,
+      executionPoint,
+      time,
+      isPending: false,
       update,
     }),
-    [isPending, state, update]
+    [executionPoint, time, update]
   );
 
   return <TimelineContext.Provider value={context}>{children}</TimelineContext.Provider>;
