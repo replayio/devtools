eaaab0466668f96eb276a1cd30c764846aeb8285
Ryan Duffy <ryan@replay.io>
2023-10-30 13:21:59 -0700

    remove usused file

--- src/ui/components/Library/Team/View/TestRuns/utils.tsx
+++
@@ -1,7 +0,0 @@
-import { TestRunTest } from "shared/test-suites/TestRun";
-
-export function getDuration(tests: TestRunTest[]) {
-  return tests.reduce<number>((accumulated, test) => {
-    return accumulated + test.durationMs;
-  }, 0);
-}
