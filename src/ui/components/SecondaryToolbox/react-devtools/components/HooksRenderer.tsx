6e3255243cf9110fe4f9de8b151989cf10a4a884
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-11-01 16:41:55 -0400

    Disable mapped location logic (for now); fix font-family for props/state/hooks panel

--- src/ui/components/SecondaryToolbox/react-devtools/components/HooksRenderer.tsx
+++ src/ui/components/SecondaryToolbox/react-devtools/components/HooksRenderer.tsx
@@ -1,16 +1,12 @@
-import { MappedLocation, ObjectId, PauseId, Value as ProtocolValue } from "@replayio/protocol";
-import { MouseEvent, ReactNode, useContext } from "react";
+import { ObjectId, PauseId, Value as ProtocolValue } from "@replayio/protocol";
+import { ReactNode } from "react";
 
 import Expandable from "replay-next/components/Expandable";
-import Icon from "replay-next/components/Icon";
 import KeyValueRenderer from "replay-next/components/inspector/KeyValueRendererWithContextMenu";
-import { InspectorContext } from "replay-next/src/contexts/InspectorContext";
 import { objectCache } from "replay-next/src/suspense/ObjectPreviews";
 import { ReplayClientInterface } from "shared/client/types";
-import { hookNameCache } from "ui/components/SecondaryToolbox/react-devtools/suspense/hookNameCache";
 import { findProtocolObjectProperty } from "ui/components/SecondaryToolbox/react-devtools/utils/findProtocolObjectProperty";
 import { findProtocolObjectPropertyValue } from "ui/components/SecondaryToolbox/react-devtools/utils/findProtocolObjectPropertyValue";
-import { getReplayLocationFromReactDevToolsSourceSuspends } from "ui/components/SecondaryToolbox/react-devtools/utils/getReplayLocationFromReactDevToolsSourceSuspends";
 
 import styles from "./HooksRenderer.module.css";
 
@@ -25,8 +21,6 @@ export function HooksRenderer({
   pauseId: PauseId;
   replayClient: ReplayClientInterface;
 }) {
-  const { inspectFunctionDefinition } = useContext(InspectorContext);
-
   const hookObject = objectCache.read(replayClient, pauseId, `${objectId}`, "canOverflow");
 
   let hookName = findProtocolObjectPropertyValue<string>(hookObject, "name") || "Anonymous";
@@ -43,24 +37,6 @@ export function HooksRenderer({
     isCustomHook = length > 0;
   }
 
-  const hookSourceProperty = findProtocolObjectProperty(hookObject, "hookSource");
-  const hookSource = hookSourceProperty?.object
-    ? objectCache.read(replayClient, pauseId, hookSourceProperty.object, "canOverflow")
-    : null;
-
-  let hookSourceFunctionLocation: MappedLocation | null = null;
-  if (hookSource) {
-    hookSourceFunctionLocation = getReplayLocationFromReactDevToolsSourceSuspends(
-      replayClient,
-      hookSource
-    );
-  }
-
-  if (isCustomHook && hookSourceFunctionLocation) {
-    hookName =
-      hookNameCache.read(replayClient, hookSourceFunctionLocation, hookName) ?? "Anonymous";
-  }
-
   let renderedName: ReactNode = null;
   if (isCustomHook) {
     renderedName = <span className={styles.CustomHookName}>{hookName}</span>;
@@ -68,30 +44,56 @@ export function HooksRenderer({
     renderedName = <span className={styles.BuiltInHookName}>{hookName}</span>;
   }
 
-  if (hookSourceFunctionLocation) {
-    const viewFunctionSource = (event: MouseEvent) => {
-      event.preventDefault();
-      event.stopPropagation();
-
-      if (inspectFunctionDefinition !== null) {
-        inspectFunctionDefinition(hookSourceFunctionLocation!);
-      }
-    };
-
-    renderedName = (
-      <>
-        {renderedName}
-        <button
-          className={styles.ViewSourceIconButton}
-          date-test-name="JumpToDefinitionButton"
-          onClick={viewFunctionSource}
-          title="Jump to definition"
-        >
-          <Icon className={styles.ViewSourceIcon} type="view-function-source" />
-        </button>
-      </>
-    );
-  }
+  // TODO [FE-2016] Some hooks (useEffect, useLayoutEffect, useCallback, etc.) accept inline function arguments
+  //                so the jump-to-definition button will be displayed for these
+  //                For other types of hooks, we could use mapped locations to display this button,
+  //                except for an outstanding issue with invalid locations
+  //                Note that when we re-enable this we should probably only do it for hooks that don't have function arguments
+  //                else we may end up showing two jump-to-definition buttons
+  //
+  // const hookSourceProperty = findProtocolObjectProperty(hookObject, "hookSource");
+  // const hookSource = hookSourceProperty?.object
+  //   ? objectCache.read(replayClient, pauseId, hookSourceProperty.object, "canOverflow")
+  //   : null;
+  //
+  // let hookSourceFunctionLocation: MappedLocation | null = null;
+  // if (hookSource) {
+  //   hookSourceFunctionLocation = getReplayLocationFromReactDevToolsSourceSuspends(
+  //     replayClient,
+  //     hookSource
+  //   );
+  // }
+  //
+  // TODO [FE-2016]
+  // if (isCustomHook && hookSourceFunctionLocation) {
+  //   hookName =
+  //     hookNameCache.read(replayClient, hookSourceFunctionLocation, hookName) ?? "Anonymous";
+  // }
+  //
+  // if (hookSourceFunctionLocation) {
+  //   const viewFunctionSource = (event: MouseEvent) => {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //
+  //     if (inspectFunctionDefinition !== null) {
+  //       inspectFunctionDefinition(hookSourceFunctionLocation!);
+  //     }
+  //   };
+  //
+  //   renderedName = (
+  //     <>
+  //       {renderedName}
+  //       <button
+  //         className={styles.ViewSourceIconButton}
+  //         date-test-name="JumpToDefinitionButton"
+  //         onClick={viewFunctionSource}
+  //         title="Jump to definition"
+  //       >
+  //         <Icon className={styles.ViewSourceIcon} type="view-function-source" />
+  //       </button>
+  //     </>
+  //   );
+  // }
 
   let rendered: ReactNode = null;
   if (valueProperty?.object) {
