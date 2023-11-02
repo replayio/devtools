dcb96df745f180fb1df6217780f0a9d4b6257f99
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-11-02 09:18:08 -0400

    Add hook name and location parsing to new RDT

--- src/ui/components/SecondaryToolbox/react-devtools/components/HooksRenderer.tsx
+++ src/ui/components/SecondaryToolbox/react-devtools/components/HooksRenderer.tsx
@@ -1,10 +1,14 @@
-import { ObjectId, PauseId, Value as ProtocolValue } from "@replayio/protocol";
-import { ReactNode } from "react";
+import { MappedLocation, ObjectId, PauseId, Value as ProtocolValue } from "@replayio/protocol";
+import { MouseEvent, ReactNode, useContext } from "react";
 
 import Expandable from "replay-next/components/Expandable";
+import Icon from "replay-next/components/Icon";
 import KeyValueRenderer from "replay-next/components/inspector/KeyValueRendererWithContextMenu";
+import { InspectorContext } from "replay-next/src/contexts/InspectorContext";
 import { objectCache } from "replay-next/src/suspense/ObjectPreviews";
 import { ReplayClientInterface } from "shared/client/types";
+import { hookLocationCache } from "ui/components/SecondaryToolbox/react-devtools/suspense/hookLocationCache";
+import { hookNameCache } from "ui/components/SecondaryToolbox/react-devtools/suspense/hookNameCache";
 import { findProtocolObjectProperty } from "ui/components/SecondaryToolbox/react-devtools/utils/findProtocolObjectProperty";
 import { findProtocolObjectPropertyValue } from "ui/components/SecondaryToolbox/react-devtools/utils/findProtocolObjectPropertyValue";
 
@@ -21,6 +25,8 @@ export function HooksRenderer({
   pauseId: PauseId;
   replayClient: ReplayClientInterface;
 }) {
+  const { inspectFunctionDefinition } = useContext(InspectorContext);
+
   const hookObject = objectCache.read(replayClient, pauseId, `${objectId}`, "canOverflow");
 
   let hookName = findProtocolObjectPropertyValue<string>(hookObject, "name") || "Anonymous";
@@ -37,11 +43,57 @@ export function HooksRenderer({
     isCustomHook = length > 0;
   }
 
+  // Some hooks (useEffect, useLayoutEffect, useCallback, etc.) accept function arguments.
+  // Typically these are inline functions, so the jump-to-definition button will already work in this case.
+  // For other types of hooks, it's still useful to show that button,
+  // but it requires us to do extra mapping work to translate from the react-debug-tools reported "location"
+  // to a Location that Replay can work with.
+
+  const hookSourceProperty = findProtocolObjectProperty(hookObject, "hookSource");
+  const hookSource = hookSourceProperty?.object
+    ? objectCache.read(replayClient, pauseId, hookSourceProperty.object, "canOverflow")
+    : null;
+
+  let hookSourceFunctionLocation: MappedLocation | null = null;
+  if (hookSource) {
+    hookSourceFunctionLocation = hookLocationCache.read(replayClient, hookSource);
+  }
+
+  if (isCustomHook && hookSourceFunctionLocation) {
+    hookName =
+      hookNameCache.read(replayClient, hookSourceFunctionLocation, hookName) ?? "Anonymous";
+  }
+
   let renderedName: ReactNode = null;
   if (isCustomHook) {
     renderedName = <span className={styles.CustomHookName}>{hookName}</span>;
   } else {
     renderedName = <span className={styles.BuiltInHookName}>{hookName}</span>;
+  }
+
+  if (hookSourceFunctionLocation) {
+    const viewFunctionSource = (event: MouseEvent) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      if (inspectFunctionDefinition !== null) {
+        inspectFunctionDefinition(hookSourceFunctionLocation!);
+      }
+    };
+
+    renderedName = (
+      <>
+        {renderedName}
+        <button
+          className={styles.ViewSourceIconButton}
+          date-test-name="JumpToDefinitionButton"
+          onClick={viewFunctionSource}
+          title="Jump to definition"
+        >
+          <Icon className={styles.ViewSourceIcon} type="view-function-source" />
+        </button>
+      </>
+    );
   }
 
   let rendered: ReactNode = null;
---
+++ src/ui/components/SecondaryToolbox/react-devtools/suspense/hookLocationCache.ts
@@ -0,0 +1,54 @@
+import { Location, MappedLocation, Object as ProtocolObject } from "@replayio/protocol";
+import { createCache } from "suspense";
+
+import { mappedLocationCache } from "replay-next/src/suspense/MappedLocationCache";
+import { sourcesByUrlCache } from "replay-next/src/suspense/SourcesCache";
+import { ReplayClientInterface } from "shared/client/types";
+import { findProtocolObjectPropertyValue } from "ui/components/SecondaryToolbox/react-devtools/utils/findProtocolObjectPropertyValue";
+
+// React DevTools (specifically react-debug-tools) returns locations as objects with the following shape:
+// { fileName: string; lineNumber: number; columnNumber: number; }
+//
+// This method maps those locations to Replay's MappedLocations format (if possible) so it can be opened in the Source viewer
+export const hookLocationCache = createCache<
+  [replayClient: ReplayClientInterface, sourceObject: ProtocolObject],
+  MappedLocation | null
+>({
+  config: { immutable: true },
+  debugLabel: "hookLocationCache",
+  getKey: ([_, sourceObject]) => {
+    const fileName = findProtocolObjectPropertyValue<string>(sourceObject, "fileName");
+    const column = findProtocolObjectPropertyValue<number>(sourceObject, "columnNumber");
+    const line = findProtocolObjectPropertyValue<number>(sourceObject, "lineNumber");
+
+    return `${fileName}:${line}:${column}`;
+  },
+  load: async ([replayClient, sourceObject]) => {
+    const fileName = findProtocolObjectPropertyValue<string>(sourceObject, "fileName");
+    const column = findProtocolObjectPropertyValue<number>(sourceObject, "columnNumber");
+    const line = findProtocolObjectPropertyValue<number>(sourceObject, "lineNumber");
+
+    if (fileName) {
+      const sourcesByUrlMap = sourcesByUrlCache.read(replayClient);
+      const sources = sourcesByUrlMap.get(fileName);
+      if (sources) {
+        const nonGeneratedSource = sources.find(source => !source.generated.length);
+        if (nonGeneratedSource) {
+          const sourceId = nonGeneratedSource.sourceId;
+
+          if (column != null && line != null) {
+            const nonGeneratedLocation: Location = {
+              sourceId,
+              line,
+              column,
+            };
+
+            return await mappedLocationCache.readAsync(replayClient, nonGeneratedLocation);
+          }
+        }
+      }
+    }
+
+    return null;
+  },
+});
