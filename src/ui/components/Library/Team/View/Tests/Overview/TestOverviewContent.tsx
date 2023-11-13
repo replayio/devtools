494910bfd1b010ef1fc9773bd033a7b6daaf4d5f
Jaril <jarilvalenciano@gmail.com>
2023-11-12 22:03:21 -0500

    Clean up TestOverviewContent

--- src/ui/components/Library/Team/View/Tests/Overview/TestOverviewContent.tsx
+++ src/ui/components/Library/Team/View/Tests/Overview/TestOverviewContent.tsx
@@ -1,38 +1,21 @@
-import { access } from "fs";
 import orderBy from "lodash/orderBy";
-import { string } from "prop-types";
 import { useContext, useState } from "react";
-
 import { FailureRates, __EXECUTION } from "shared/test-suites/TestRun";
-import { useTestRunDetailsSuspends } from "ui/components/Library/Team/View/TestRuns/hooks/useTestRunDetailsSuspends";
-import { RunResults } from "ui/components/Library/Team/View/TestRuns/Overview/RunResults";
-
 import { TestContext } from "../TestContextRoot";
-import { RunSummary } from "./TestSummary";
 import styles from "../../../../Library.module.css";
 
 export function TestOverviewContent() {
-  // const { filterByStatus, filterByText, testRunId, testRunIdForDisplay, testRuns } =
-  const { filterByText, testId, testIdForDisplay, tests } = useContext(TestContext);
-
-  // const { recordings, durationMs } = useTestRunDetailsSuspends(testRunId);
-
-  const isPending = false; // testRunId !== testRunIdForDisplay;
-
-  // const hasFilters = filterByStatus !== "all" || filterByText !== "";
+  const { filterByText, testId, tests } = useContext(TestContext);
   const hasFilters = filterByText !== "";
   const test = tests.find(test => test.testId === testId);
 
   let children = null;
-  // if (testRun && recordings) {
 
   if (test) {
     if (!hasFilters || tests.find(test => test.testId === testId)) {
       children = (
         <div
-          className={`flex flex-col gap-1 border-b border-themeBorder py-2  px-4 ${
-            isPending ? "opacity-50" : ""
-          }`}
+          className="flex flex-col gap-1 border-b border-themeBorder py-2 px-4"
           data-test-id="TestRunSummary"
         >
           <div className="flex flex-row items-center justify-between gap-1">
@@ -42,13 +25,6 @@ export function TestOverviewContent() {
           </div>
           <Stats failureRates={test.failureRates} />
           <ErrorFrequency errorFrequency={test.errorFrequency} executions={test.executions} />
-          {/* <RunSummary
-            isPending={isPending}
-            recordings={recordings}
-            testRun={testRun}
-            durationMs={durationMs}
-          /> */}
-          {/* <RunResults isPending={isPending} /> */}
         </div>
       );
     }
@@ -74,8 +50,6 @@ function ErrorFrequency({
   const failing = executions.filter(e => e.result === "failed");
   const sortedFailing = orderBy(failing, "createdAt", "desc");
   const sortedPassing = orderBy(passing, "createdAt", "desc");
-
-  console.log({sortedFailing, sortedPassing});
 
   return (
     <div>
