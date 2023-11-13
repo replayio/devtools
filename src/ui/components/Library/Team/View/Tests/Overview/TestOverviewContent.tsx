228e3a1c959c9591f30b5dc22b2b32b9ae648b90
Jaril <jarilvalenciano@gmail.com>
2023-11-12 22:28:48 -0500

    Format test overview

--- src/ui/components/Library/Team/View/Tests/Overview/TestOverviewContent.tsx
+++ src/ui/components/Library/Team/View/Tests/Overview/TestOverviewContent.tsx
@@ -1,7 +1,10 @@
 import orderBy from "lodash/orderBy";
 import { useContext, useState } from "react";
+
 import { FailureRates, __EXECUTION } from "shared/test-suites/TestRun";
+
 import { TestContext } from "../TestContextRoot";
+import { Status } from "../TestListItem";
 import styles from "../../../../Library.module.css";
 
 export function TestOverviewContent() {
@@ -15,10 +18,10 @@ export function TestOverviewContent() {
     if (!hasFilters || tests.find(test => test.testId === testId)) {
       children = (
         <div
-          className="flex flex-col gap-1 border-b border-themeBorder py-2 px-4"
+          className="flex flex-col gap-1 border-b border-themeBorder"
           data-test-id="TestRunSummary"
         >
-          <div className="flex flex-row items-center justify-between gap-1">
+          <div className="flex flex-row items-center justify-between gap-1 border-b border-themeBorder py-2 px-4">
             <div className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-medium">
               {test.title}
             </div>
@@ -51,35 +54,51 @@ function ErrorFrequency({
   const sortedFailing = orderBy(failing, "createdAt", "desc");
   const sortedPassing = orderBy(passing, "createdAt", "desc");
 
+  // ${isSelected ? styles.libraryRowSelected : ""}
+
   return (
     <div>
-      <div>Top Errors</div>
-      <div>
-        {Object.entries(errorFrequency).map(([msg, count]) => (
-          <div
-            key={msg}
-            className="overflow-hidden overflow-ellipsis whitespace-nowrap"
-            onClick={() => setSelectedError(msg)}
-          >
-            {count}: {msg}
-          </div>
-        ))}
+      <div className="border-b border-themeBorder py-2 px-4">
+        <div className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-medium">
+          Top Errors
+        </div>
+        <div>
+          {Object.entries(errorFrequency)
+            .slice(0, 5)
+            .map(([msg, count]) => (
+              <div
+                key={msg}
+                className={`flex cursor-pointer flex-row items-center space-x-3 rounded-sm border-b border-chrome bg-themeBase-100 p-3 ${styles.libraryRow}
+            `}
+                onClick={() => setSelectedError(msg)}
+              >
+                <div className="flex h-5 w-8 shrink-0 items-center justify-center rounded-md bg-[#F02D5E] text-xs font-bold text-chrome">
+                  {count}
+                </div>
+                <div className="overflow-hidden overflow-ellipsis whitespace-nowrap">{msg}</div>
+              </div>
+            ))}
+        </div>
       </div>
       {selectedError ? (
-        <div>
-          <div className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+        <div className="flex flex-col gap-2 border-b border-themeBorder py-2 px-4">
+          {/* <div className="overflow-hidden overflow-ellipsis whitespace-nowrap">
             Selected error: {selectedError}
-          </div>
-          <div>
-            <div>Replay that contain this error</div>
+          </div> */}
+          <div className="flex flex-col gap-1">
+            <div className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-medium">
+              Replay that contain this error
+            </div>
             <div>
               {sortedFailing.slice(0, 3).map((e, i) => (
                 <div key={i}>{e.createdAt}</div>
               ))}
             </div>
           </div>
-          <div>
-            <div>Recent replays of the test passing</div>
+          <div className="flex flex-col gap-1">
+            <div className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-medium">
+              Recent replays of the test passing
+            </div>
             <div>
               {sortedPassing.slice(0, 3).map((e, i) => (
                 <div key={i}>{e.createdAt}</div>
@@ -96,25 +115,27 @@ function Stats({ failureRates }: { failureRates: FailureRates }) {
   const { hour, day, week, month } = failureRates;
 
   return (
-    <div>
+    <div className="border-b border-themeBorder py-2 px-4">
       <div>
-        <div>Historical failure rate</div>
+        <div className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-medium">
+          Historical failure rate
+        </div>
         <div className="flex">
           <div className="flex w-24 flex-col">
-            <div>{(hour * 100).toFixed(2)}%</div>
-            <div>This Hour</div>
+            <div className="text-lg font-medium">{(hour * 100).toFixed(2)}%</div>
+            <div className="uppercase">This Hour</div>
           </div>
           <div className="flex w-24 flex-col">
-            <div>{(day * 100).toFixed(2)}%</div>
-            <div>Day</div>
+            <div className="text-lg font-medium">{(day * 100).toFixed(2)}%</div>
+            <div className="uppercase">Day</div>
           </div>
           <div className="flex w-24 flex-col">
-            <div>{(week * 100).toFixed(2)}%</div>
-            <div>Week</div>
+            <div className="text-lg font-medium">{(week * 100).toFixed(2)}%</div>
+            <div className="uppercase">Week</div>
           </div>
           <div className="flex w-24 flex-col">
-            <div>{(month * 100).toFixed(2)}%</div>
-            <div>Month</div>
+            <div className="text-lg font-medium">{(month * 100).toFixed(2)}%</div>
+            <div className="uppercase">Month</div>
           </div>
         </div>
       </div>
