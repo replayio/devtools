d61a0a989e7cb15a9c6a7d01727562234cbb350d
Jon Bell <github@lot23.com>
2023-11-14 13:30:05 +1300

    Increased color contrast when buttons overlap selected rows (#9880)

--- src/ui/components/Library/Navigation/TeamButton.tsx
+++ src/ui/components/Library/Navigation/TeamButton.tsx
@@ -47,7 +47,7 @@ export function TeamButton({
       href={url}
       className={classNames(
         `${styles.teamRow} group flex flex-row justify-between space-x-2 px-4 py-2 text-left transition duration-200 hover:text-white focus:outline-none`,
-        isSelected ? `${styles.teamRowActive} cursor-auto font-bold` : "cursor-pointer"
+        isSelected ? `${styles.teamRowActive} cursor-auto` : "cursor-pointer"
       )}
       onClick={onClick}
     >
