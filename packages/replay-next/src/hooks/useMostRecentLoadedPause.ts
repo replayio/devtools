edf2c8937868c539e5558e3257593ba80ff6a861
Holger Benl <hbenl@evandor.de>
2023-11-15 14:00:19 +0100

    Fix the useMostRecentLoadedPause hook

--- packages/replay-next/src/hooks/useMostRecentLoadedPause.ts
+++ packages/replay-next/src/hooks/useMostRecentLoadedPause.ts
@@ -1,11 +1,7 @@
 import { ExecutionPoint, PauseId } from "@replayio/protocol";
 import { useContext, useRef } from "react";
-import { useImperativeCacheValue } from "suspense";
-
-import { ReplayClientContext } from "shared/client/ReplayClientContext";
 
 import { TimelineContext } from "../contexts/TimelineContext";
-import { pauseIdCache } from "../suspense/PauseCache";
 
 interface LoadedPause {
   pauseId: PauseId;
@@ -14,16 +10,9 @@ interface LoadedPause {
 }
 
 export function useMostRecentLoadedPause(): LoadedPause | null {
-  const client = useContext(ReplayClientContext);
   const pauseRef = useRef<LoadedPause | null>(null);
-  const { executionPoint: point, time } = useContext(TimelineContext);
-  const { status: pauseIdStatus, value: pauseId } = useImperativeCacheValue(
-    pauseIdCache,
-    client,
-    point ?? "0",
-    time
-  );
-  if (point && point !== pauseRef.current?.point && pauseIdStatus === "resolved") {
+  const { executionPoint: point, pauseId, time } = useContext(TimelineContext);
+  if (point && pauseId && pauseId !== pauseRef.current?.pauseId) {
     pauseRef.current = { pauseId, point, time };
   }
   return pauseRef.current;
--- src/ui/components/SecondaryToolbox/TimelineContextAdapter.tsx
+++ src/ui/components/SecondaryToolbox/TimelineContextAdapter.tsx
@@ -1,7 +1,7 @@
 import { ExecutionPoint, Location } from "@replayio/protocol";
 import React, { PropsWithChildren, useCallback, useMemo } from "react";
 
-import { getExecutionPoint, getTime } from "devtools/client/debugger/src/selectors";
+import { getExecutionPoint, getPauseId, getTime } from "devtools/client/debugger/src/selectors";
 import { TimelineContext, TimelineContextType } from "replay-next/src/contexts/TimelineContext";
 import { seek } from "ui/actions/timeline";
 import { useAppDispatch, useAppSelector } from "ui/setup/hooks";
@@ -11,6 +11,7 @@ export default function TimelineContextAdapter({ children }: PropsWithChildren) 
   const dispatch = useAppDispatch();
   const time = useAppSelector(getTime);
   const executionPoint = useAppSelector(getExecutionPoint);
+  const pauseId = useAppSelector(getPauseId) ?? null;
 
   const update = useCallback(
     async (
@@ -27,11 +28,12 @@ export default function TimelineContextAdapter({ children }: PropsWithChildren) 
   const context = useMemo<TimelineContextType>(
     () => ({
       executionPoint,
+      pauseId,
       time,
       isPending: false,
       update,
     }),
-    [executionPoint, time, update]
+    [executionPoint, pauseId, time, update]
   );
 
   return <TimelineContext.Provider value={context}>{children}</TimelineContext.Provider>;
