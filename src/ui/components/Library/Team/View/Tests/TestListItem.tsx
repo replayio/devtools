43a208d196beaecd6ec9883a52ce26a8e3094ccf
Jaril <jarilvalenciano@gmail.com>
2023-11-16 14:03:23 -0500

    Show a green check for tests that have 0% failure rate

--- src/ui/components/Library/Team/View/Tests/TestListItem.tsx
+++ src/ui/components/Library/Team/View/Tests/TestListItem.tsx
@@ -2,23 +2,37 @@ import { useContext } from "react";
 
 import { Test } from "shared/test-suites/TestRun";
 import HighlightedText from "ui/components/Library/Team/View/TestRuns/HighlightedText";
+import Icon from "ui/components/shared/Icon";
 
 import { TestContext } from "./TestContextRoot";
 import styles from "../../../Library.module.css";
 
-function Status({ failCount }: { failCount: number }) {
-  return (
-    <div className="flex h-5 w-8 shrink-0 items-center justify-center rounded-md bg-[#F02D5E] text-xs font-bold text-white">
-      {failCount}%
-    </div>
-  );
+function Status({ failureRate }: { failureRate: number }) {
+  const status = failureRate > 0 ? "fail" : "success";
+  const displayedFailureRate = Number((failureRate * 100).toFixed(0));
+
+  if (failureRate > 0) {
+    return (
+      <div
+        className="flex h-5 w-8 shrink-0 items-center justify-center rounded-md bg-[#F02D5E] text-xs font-bold text-white"
+        data-test-status={status}
+      >
+        {displayedFailureRate}
+      </div>
+    );
+  } else {
+    return (
+      <div data-test-status={status}>
+        {failureRate}
+        <Icon className={styles.testsuitesSuccess} filename={"testsuites-success"} size="medium" />
+      </div>
+    );
+  }
 }
 
 export function TestListItem({ filterByText, test }: { filterByText: string; test: Test }) {
   const { selectTestId, testIdForDisplay } = useContext(TestContext);
   const { title, failureRate } = test;
-
-  const failCount = Number((failureRate * 100).toFixed(0));
   const isSelected = testIdForDisplay === test.testId;
 
   const onClick = () => {
@@ -35,7 +49,7 @@ export function TestListItem({ filterByText, test }: { filterByText: string; tes
       `}
       onClick={onClick}
     >
-      <Status failCount={failCount} />
+      <Status failureRate={failureRate} />
       <div className="flex h-full flex-grow flex-col justify-evenly overflow-hidden">
         <div className="flex flex-row justify-between space-x-3">
           <div
