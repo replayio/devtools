b1b553dec47a9515af938f8fbe14e91566740758
Ryan Duffy <ryan@replay.io>
2023-11-22 08:15:46 -0800

    style errors

--- src/ui/components/Library/Team/View/NewTestRuns/TestRunsPage.tsx
+++ src/ui/components/Library/Team/View/NewTestRuns/TestRunsPage.tsx
@@ -163,11 +163,11 @@ function TestRunSpecDetails() {
   const failedTests = selectedSpecTests.filter(t => t.result === "failed");
 
   return (
-    <div className="flex flex-col justify-start text-sm">
+    <div className="flex h-full w-full flex-col justify-start text-sm">
       <div className="overflow-hidden overflow-ellipsis whitespace-nowrap border-b border-themeBorder px-4 py-3 font-bold">
         {spec}
       </div>
-      <div className="flex flex-col gap-3 py-3">
+      <div className="flex flex-grow flex-col gap-3 overflow-y-auto py-3">
         <div className="flex flex-col gap-2 px-3">
           <div className="overflow-hidden overflow-ellipsis whitespace-nowrap">Replays</div>
           {/* {replays.map((r, i) => (
@@ -199,14 +199,23 @@ function Errors({ failedTests }: { failedTests: any }) {
   return (
     <div className="flex flex-col gap-2 px-3">
       <div className="overflow-hidden overflow-ellipsis whitespace-nowrap">Errors</div>
-      <div>
+      <div className="flex flex-grow flex-col gap-2">
         {failedTests.map((t, i) => (
-          <div key={i}>
-            {t.errors.map((e, i) => (
-              <div className="p-2" key={i}>
-                {e.slice(0, 100)}...
-              </div>
-            ))}
+          // bg-black is wrong for light theme. should use CSS variables?
+          <div key={i} className="w-full overflow-x-auto rounded-md bg-black px-3 py-4">
+            <div className="whitespace-pre border-l border-l-2 border-red-500 px-3">
+              {t.errors.map((e, i) => (
+                <>
+                  <div className="mb-2 flex flex-row items-center gap-2 text-red-500">
+                    <Icon type="warning" className="h-4 w-4" />
+                    <span>Error</span>
+                  </div>
+                  <div key={i} className="font-mono text-xs">
+                    {e.split("\n").slice(0, 4).join("\n")}
+                  </div>
+                </>
+              ))}
+            </div>
           </div>
         ))}
       </div>
