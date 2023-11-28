5d5ba183ff8c78a1bbcf92f3eb9aee2dc25be14e
Brian Vaughn <brian.david.vaughn@gmail.com>
2023-11-28 14:23:16 -0500

    Update RDT e2e test recording

---
+++ public/test/examples/rdt-react-versions/dist/react17/assets/__federation_expose_AppInjector-3adc3cc9.js
@@ -0,0 +1,2 @@
+import{R as e,j as n,a as o}from"./App-ba7ddcc9.js";const s=t=>e.render(n.jsx(o,{}),document.getElementById(t)),c=t=>e.unmountComponentAtNode(document.getElementById(t));export{s as inject,c as unmount};
+//# sourceMappingURL=__federation_expose_AppInjector-3adc3cc9.js.map
