e78ed2eb3cf5a5c5d88a5d978cd32b3751a62543
Ryan Duffy <ryan@replay.io>
2023-12-12 17:58:32 -0800

    -> css module

--- src/ui/components/Library/Team/View/TestSuitePanelMessage.tsx
+++ src/ui/components/Library/Team/View/TestSuitePanelMessage.tsx
@@ -2,8 +2,8 @@ import styles from "./TestSuitePanelMessage.module.css";
 
 export function TestSuitePanelMessage({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full w-full items-center justify-center p-2">
-      <div className={styles.standardMessaging}>{children}</div>
+    <div className={styles.panelMessage}>
+      <div className={styles.content}>{children}</div>
     </div>
   );
 }
