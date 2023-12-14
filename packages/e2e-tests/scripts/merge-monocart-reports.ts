a5a8d024b375521d22b23924a508807a0bce2100
Mark Erikson <mark@isquaredsoftware.com>
2023-12-14 14:21:37 +0300

    Try merging reports together

---
+++ packages/e2e-tests/scripts/merge-monocart-reports.ts
@@ -0,0 +1,35 @@
+import fs from "fs";
+import path from "path";
+import glob from "glob";
+import { merge } from "monocart-reporter";
+
+// We'll pass this in from the outside
+const inputFolderPath: string = process.argv[2];
+
+if (!inputFolderPath) {
+  throw new Error("Missing input folder path");
+}
+
+const pattern = path.posix.join(inputFolderPath, "**/*.json");
+console.log("Input path: ", inputFolderPath, "pattern: ", pattern);
+const reportJsonFiles = glob.sync(pattern, { absolute: true });
+
+console.log("Report files: ", reportJsonFiles);
+
+(async () => {
+  try {
+    await merge(reportJsonFiles, {
+      name: "My Merged Report",
+      outputFile: "./test-results/merged-report.html",
+      onEnd: async (reportData: any, capability: any) => {
+        console.log("Finished merging report", reportData.summary);
+      },
+    });
+
+    process.exit(0);
+  } catch (error) {
+    console.error("Unexpected error: ", error);
+
+    process.exit(1);
+  }
+})();
