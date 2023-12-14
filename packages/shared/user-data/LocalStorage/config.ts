9d6e21860c72b1e4ae6d2986d8584e475547d6bf
Jaril <jarilvalenciano@gmail.com>
2023-12-14 06:50:29 -0800

    Turn on new runs view by default (#10023)

--- packages/shared/user-data/LocalStorage/config.ts
+++ packages/shared/user-data/LocalStorage/config.ts
@@ -10,6 +10,6 @@ export const config = {
   reactDevToolsStateExpanded: Boolean(true),
   replayNextCurrentPanel: "sources" as ReplayNextCurrentPanel,
   replayVideoPanelCollapsed: false,
-  enableTestSuitesNewRunsView: false,
+  enableTestSuitesNewRunsView: true,
   enableTestSuitesTestsView: false,
 };
