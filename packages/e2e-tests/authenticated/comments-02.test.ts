2133aed6f7a3db2b2b6f5aed89dcb53def14c45b
Mark Erikson <mark@isquaredsoftware.com>
2023-12-21 15:55:20 -0500

    Configure code coverage reporting for Playwright E2E in CI (#10003)
    * try without action-playwright
    
    * ls-r
    
    * change glob
    
    * Add monocart-reporter
    
    * Add monocart-reporter to Playwright config in CI
    
    * Upload artifact file for inspection
    
    * Try logging monocart-reporter output
    
    * Check monocart output location
    
    * Try to archive artifacts
    
    * Move reporter setup to Playwright config
    
    * Actually drop action-playwright from main E2E tests
    
    * Try collecting downloaded artifacts
    
    * Shard monocart artifacts
    
    * Try merging reports together
    
    * Try collecting code coverage
    
    * Log coverage value and shorten tests
    
    * Try including attachments
    
    * Try merging coverage
    
    * Run a few more tests
    
    * Try attaching coverage data to tests
    
    * Back to global coverage
    
    * Log more monocart coverage output
    
    * Skip bundles
    
    * Rewrite merge-reports script
    
    * Run all tests again
    
    * Try outputting Istanbul HTML report per shard
    
    * Ignore useless entries
    
    * Try outputting an Istanbul JSON report
    
    * Fill in source lookups while merging
    
    * Re-include next entries
    
    * More entry filtering and skip directory source lookups
    
    * Clean up coverage scripts
    
    * Restore delta
    
    * Wait for auth tests
    
    * Log missing JS coverage
    
    * Handle errors getting JS coverage
    
    * Add coverage for multi-page auth tests
    
    * Clean up logging
    
    * Fix test run ID env var
    
    * Remove dead e2etest-jail section
    
    ---------
    
    Co-authored-by: Ryan Duffy <ryan@replay.io>

--- packages/e2e-tests/authenticated/comments-02.test.ts
+++ packages/e2e-tests/authenticated/comments-02.test.ts
@@ -1,3 +1,5 @@
+import { addCoverageReport } from "monocart-reporter";
+
 import { openDevToolsTab, startTest } from "../helpers";
 import { E2E_USER_1_API_KEY, E2E_USER_2_API_KEY } from "../helpers/authentication";
 import {
@@ -7,15 +9,23 @@ import {
   replyToComment,
 } from "../helpers/comments";
 import { openSource } from "../helpers/source-explorer-panel";
-import test, { Page } from "../testFixtureCloneRecording";
+import test, { Page, base } from "../testFixtureCloneRecording";
 
 const url = "authenticated_comments.html";
 
 async function load(page: Page, recordingId: string, apiKey: string) {
   await startTest(page, recordingId, apiKey);
+  await page.coverage.startJSCoverage();
 
   await openDevToolsTab(page);
   await openSource(page, url);
+}
+
+async function close(page: Page) {
+  const jsCoverage = await page.coverage.stopJSCoverage();
+
+  await addCoverageReport(jsCoverage, base.info());
+  await page.close();
 }
 
 test.use({ exampleKey: url });
@@ -33,6 +43,7 @@ test(`authenticated/comments-02: Test shared comments and replies`, async ({
     // User 1
     const context = await browser.newContext();
     const page = await context.newPage();
+
     await load(page, recordingId, E2E_USER_1_API_KEY);
 
     await addSourceCodeComment(page, {
@@ -79,5 +90,10 @@ test(`authenticated/comments-02: Test shared comments and replies`, async ({
     });
 
     await deleteAllComments(page);
+  }
+
+  {
+    await close(pageOne);
+    await close(pageTwo);
   }
 });
