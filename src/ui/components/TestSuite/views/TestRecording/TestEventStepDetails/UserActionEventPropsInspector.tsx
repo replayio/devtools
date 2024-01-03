30eba4b98f5b3882d45d5daf20bf0b78fad9ccb2
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-01-03 13:40:08 -0500

    Test Suites: Step details loading state (#10080)
    - Restore #10074 (c78ff0e) but with the new tabs/elements UI removed (0964878)
    - Rebase changes from #10076 (e327e79)
    
    ---------
    
    Co-authored-by: Mark Erikson <mark@isquaredsoftware.com>

---
+++ src/ui/components/TestSuite/views/TestRecording/TestEventStepDetails/UserActionEventPropsInspector.tsx
@@ -0,0 +1,46 @@
+import { ExecutionPoint, TimeStampedPoint } from "@replayio/protocol";
+import { useMemo } from "react";
+import { Cache, STATUS_PENDING, useImperativeCacheValue } from "suspense";
+
+import PropertiesRenderer from "replay-next/components/inspector/PropertiesRenderer";
+import { InspectableTimestampedPointContext } from "replay-next/src/contexts/InspectorContext";
+import { UserActionEvent } from "shared/test-suites/RecordingTestMetadata";
+import {
+  TestEventDetailsEntry,
+  testEventDetailsResultsCache,
+} from "ui/components/TestSuite/suspense/TestEventDetailsCache";
+
+import { LoadingFailedMessage, LoadingInProgress } from "./TestEventDetailsLoadingMessages";
+
+export function UserActionEventPropsInspector({ testEvent }: { testEvent: UserActionEvent }) {
+  // Parent ensures this exists
+  const timeStampedPoint = testEvent.data.timeStampedPoints.result!;
+
+  const { status, value } = useImperativeCacheValue(
+    testEventDetailsResultsCache as unknown as Cache<
+      [executionPoint: ExecutionPoint],
+      TestEventDetailsEntry
+    >,
+    timeStampedPoint.point
+  );
+
+  const context = useMemo(
+    () => ({
+      executionPoint: timeStampedPoint.point,
+      time: timeStampedPoint.time,
+    }),
+    [timeStampedPoint]
+  );
+
+  if (status === STATUS_PENDING) {
+    return <LoadingInProgress />;
+  } else if (value?.props == null || value?.pauseId == null) {
+    return <LoadingFailedMessage />;
+  }
+
+  return (
+    <InspectableTimestampedPointContext.Provider value={context}>
+      <PropertiesRenderer pauseId={value.pauseId} object={value.props} hidePrototype={true} />
+    </InspectableTimestampedPointContext.Provider>
+  );
+}
