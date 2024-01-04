94c56095f67fca5ce5f54b8916cd9c2b6a3034d7
klochek <cklochek@replay.io>
2024-01-04 15:39:26 -0500

    Add a buildkite pipeline that can re-record and run the FE tests against a new Chromium build. (#10062)
    * Add a buildkite pipeline that can re-record and run the FE tests against a new Chromium build.
    
    * Fix pipeline key name
    
    * Fix arch string.
    
    * Add aws secret envs; add copyright header
    
    * Allow save-examples to save multiple specified files; record and run known-passing chromium tests.
    
    * Fmt; remove a test
    
    * Run runtime buildkite tests against prod devtools
    
    * For runtime e2e, use playwright chromium to navigate to app.replay
    
    * install playwright for runtime tests.
    
    * Fmt

---
+++ packages/e2e-tests/scripts/build_id_from_artifact.js
@@ -0,0 +1,18 @@
+/* Copyright 2024 Record Replay Inc. */
+
+const fs = require("fs");
+const path = require("path");
+const { execSync } = require("child_process");
+
+function build_id_from_artifact(os, arch) {
+  const buildIdPath = path.join("build_id", os, arch, "build_id");
+
+  execSync(
+    `buildkite-agent artifact download ${buildIdPath} . --build ` +
+      process.env.BUILDKITE_TRIGGERED_FROM_BUILD_ID
+  );
+
+  return fs.readFileSync(buildIdPath, "utf8").trim();
+}
+
+module.exports = build_id_from_artifact;
