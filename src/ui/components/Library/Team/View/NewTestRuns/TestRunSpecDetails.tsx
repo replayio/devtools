0a32b0ec46a8af2ce47bcfb88e624938f989482b
Jaril <jarilvalenciano@gmail.com>
2024-01-05 16:04:03 -0500

    Group errors and show count instead (#10092)
    * Group errors and show count instead
    
    * Memoize sortedErrors
    
    * Fix tests

--- src/ui/components/Library/Team/View/NewTestRuns/TestRunSpecDetails.tsx
+++ src/ui/components/Library/Team/View/NewTestRuns/TestRunSpecDetails.tsx
@@ -1,9 +1,7 @@
-import { useContext } from "react";
+import { useContext, useMemo, useState } from "react";
 
-import Icon from "replay-next/components/Icon";
 import { TestRunTestWithRecordings } from "shared/test-suites/TestRun";
 
-import { Alert } from "../shared/Alert";
 import { useTestRunDetailsSuspends } from "../TestRuns/hooks/useTestRunDetailsSuspends";
 import { TestSuitePanelMessage } from "../TestSuitePanelMessage";
 import { TestRunPanelWrapper } from "./TestRunPanelWrapper";
@@ -14,7 +12,7 @@ export function TestRunSpecDetails() {
   const { spec, filterTestsByText } = useContext(TestRunsContext);
   const { testRunId, testRuns } = useContext(TestRunsContext);
 
-  const { groupedTests, tests, testRun } = useTestRunDetailsSuspends(testRunId);
+  const { groupedTests, tests } = useTestRunDetailsSuspends(testRunId);
   const selectedSpecTests =
     // Select tests that not filtered in second panel
     tests
@@ -54,25 +52,79 @@ export function TestRunSpecDetails() {
   );
 }
 
+interface ErrorCount {
+  message: string;
+  summary: string;
+  count: number;
+}
+
+const getSummary = (message: string) => {
+  const firstLine = message.split("\n")[0];
+
+  if (firstLine.match(/^.*\dms: (.*)/)?.[1]) {
+    return firstLine.match(/^.*\dms: (.*)/)![1];
+  } else {
+    return firstLine;
+  }
+};
+
 function Errors({ failedTests }: { failedTests: TestRunTestWithRecordings[] }) {
+  const { spec } = useContext(TestRunsContext);
+
+  const sortedErrors = useMemo(() => {
+    const errors = failedTests.flatMap(t => t.errors || []);
+    const uniqueErrors = errors.reduce((acc, e) => {
+      const existingError = acc.find(a => a.message === e);
+
+      if (existingError) {
+        existingError.count += 1;
+      } else {
+        acc.push({ message: e, count: 1, summary: getSummary(e) });
+      }
+
+      return acc;
+    }, [] as ErrorCount[]);
+
+    return uniqueErrors.sort((a, b) => b.count - a.count);
+  }, [failedTests]);
+
   return (
     <div className="flex flex-col gap-2 px-3">
       <div className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-semibold">
         Errors
       </div>
-      {failedTests.map(t =>
-        t.errors?.map((e, i) => (
-          <div
-            key={`${t.id}-${i}`}
-            data-test-id="TestRunSpecDetails-Error"
-            className="w-full overflow-x-auto rounded-md bg-[color:var(--testsuites-v2-error-bg)] px-3 py-4"
-          >
-            <div className="flex flex-col gap-4 whitespace-pre-wrap break-words border-l-2 border-[color:var(--testsuites-v2-failed-header)] px-3">
-              <div className="font-mono text-xs">{e.split("\n").slice(0, 4).join("\n")}</div>
-            </div>
-          </div>
-        ))
-      )}
+      {sortedErrors.map((e, i) => (
+        <ErrorGroup key={`${spec}-${i}`} message={e.message} count={e.count} summary={e.summary} />
+      ))}
+    </div>
+  );
+}
+
+function ErrorGroup({
+  message,
+  count,
+  summary,
+}: {
+  message: string;
+  count: number;
+  summary: string;
+}) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div
+      className="flex w-full flex-col gap-2 overflow-x-auto rounded-md bg-[color:var(--testsuites-v2-error-bg)] px-3 py-4"
+      data-test-id="TestRunSpecDetails-Error"
+    >
+      <button className="flex flex-row gap-1" onClick={() => setExpanded(!expanded)}>
+        <div>({count})</div>
+        <div className="truncate">{summary}</div>
+      </button>
+      {expanded ? (
+        <div className="flex flex-col gap-4 whitespace-pre-wrap break-words border-l-2 border-[color:var(--testsuites-v2-failed-header)] px-3">
+          <div className="font-mono text-xs">{message.split("\n").slice(0, 4).join("\n")}</div>
+        </div>
+      ) : null}
     </div>
   );
 }
