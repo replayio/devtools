a1926307da6f385caf7d1e8cf25061e274825611
Jaril <jarilvalenciano@gmail.com>
2024-01-09 15:22:42 -0500

    Collapse single replay executions into just one row

--- src/ui/components/Library/Team/View/Tests/Overview/Execution.tsx
+++ src/ui/components/Library/Team/View/Tests/Overview/Execution.tsx
@@ -5,17 +5,32 @@ import Icon from "ui/components/shared/Icon";
 import MaterialIcon from "ui/components/shared/MaterialIcon";
 
 import { getTruncatedRelativeDate } from "../../Recordings/RecordingListItem/RecordingListItem";
+import { StatusIcon } from "./StatusIcon";
 import { Replay, getReplayResult } from "./TestDetails";
 
 export function Execution({ execution }: { execution: TestExecution }) {
   const title = execution.commitTitle || "<Commit title unavailable>";
   const sortedRecordings = orderBy(execution.recordings, "date", "desc");
+  const shouldCollapse = sortedRecordings.length === 1;
+
+  let icon;
+  if (shouldCollapse) {
+    const recording = sortedRecordings[0];
+    icon = (
+      <StatusIcon
+        status={getReplayResult(execution.result, 0, 1)}
+        isProcessed={recording.isProcessed}
+      />
+    );
+  } else {
+    icon = <ExecutionStatus result={execution.result} />;
+  }
 
   return (
     <div data-test-id="ExecutionItem" className="flex flex-col px-2">
       <div className="flex flex-row items-center justify-between gap-2 overflow-hidden py-2">
         <div className="flex flex-row items-center gap-2 overflow-hidden">
-          <ExecutionStatus result={execution.result} />
+          {icon}
           <div className="flex flex-col overflow-x-hidden">
             <div
               data-test-id="ExecutionTitle"
@@ -34,16 +49,18 @@ export function Execution({ execution }: { execution: TestExecution }) {
           <div data-test-id="ExecutionDate">{getTruncatedRelativeDate(execution.createdAt)}</div>
         </div>
       </div>
-      <div className="flex flex-col gap-1">
-        {sortedRecordings.map((r, i) => (
-          <Replay
-            recording={r}
-            key={i}
-            result={getReplayResult(execution.result, i, execution.recordings.length)}
-            attemptNumber={execution.recordings.length - i}
-          />
-        ))}
-      </div>
+      {!shouldCollapse ? (
+        <div className="flex flex-col gap-1">
+          {sortedRecordings.map((r, i) => (
+            <Replay
+              recording={r}
+              key={i}
+              result={getReplayResult(execution.result, i, execution.recordings.length)}
+              attemptNumber={execution.recordings.length - i}
+            />
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 }
