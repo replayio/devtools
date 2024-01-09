e45762983232fe2aa66024e9223c8087f866dff8
Jaril <jarilvalenciano@gmail.com>
2024-01-09 15:13:21 -0500

    Layout changes

--- src/ui/components/Library/Team/View/Tests/Overview/Execution.tsx
+++ src/ui/components/Library/Team/View/Tests/Overview/Execution.tsx
@@ -12,8 +12,8 @@ export function Execution({ execution }: { execution: TestExecution }) {
   const sortedRecordings = orderBy(execution.recordings, "date", "desc");
 
   return (
-    <div data-test-id="ExecutionItem" className="flex flex-col">
-      <div className="flex flex-row items-center justify-between gap-2 overflow-hidden border-b border-bodyBgcolor py-2 px-4">
+    <div data-test-id="ExecutionItem" className="flex flex-col px-2">
+      <div className="flex flex-row items-center justify-between gap-2 overflow-hidden py-2">
         <div className="flex flex-row items-center gap-2 overflow-hidden">
           <ExecutionStatus result={execution.result} />
           <div className="flex flex-col overflow-x-hidden">
@@ -27,21 +27,23 @@ export function Execution({ execution }: { execution: TestExecution }) {
             {execution.commitAuthor ? <CommitAuthor author={execution.commitAuthor} /> : null}
           </div>
         </div>
-        <div className="flex flex-shrink-0 flex-row items-center gap-1">
+        <div className="flex flex-shrink-0 flex-row items-center gap-1 text-xs">
           <div className="flex w-4 items-center">
             <MaterialIcon>schedule</MaterialIcon>
           </div>
           <div data-test-id="ExecutionDate">{getTruncatedRelativeDate(execution.createdAt)}</div>
         </div>
       </div>
-      {sortedRecordings.map((r, i) => (
-        <Replay
-          recording={r}
-          key={i}
-          result={getReplayResult(execution.result, i, execution.recordings.length)}
-          attemptNumber={execution.recordings.length - i}
-        />
-      ))}
+      <div className="flex flex-col gap-1">
+        {sortedRecordings.map((r, i) => (
+          <Replay
+            recording={r}
+            key={i}
+            result={getReplayResult(execution.result, i, execution.recordings.length)}
+            attemptNumber={execution.recordings.length - i}
+          />
+        ))}
+      </div>
     </div>
   );
 }
@@ -77,7 +79,7 @@ export function CommitAuthor({ author }: { author: string }) {
   return (
     <div
       data-test-id="CommitAuthor"
-      className="flex flex-row gap-4 overflow-hidden overflow-ellipsis whitespace-nowrap"
+      className="flex flex-row gap-4 overflow-hidden overflow-ellipsis whitespace-nowrap text-xs"
     >
       <div className="flex flex-row items-center gap-1">
         <div className="flex w-4 items-center">
