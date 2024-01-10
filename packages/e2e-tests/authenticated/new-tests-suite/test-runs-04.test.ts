1d8f1f94d9d8152481bf13ea3d5c1d7e83db64f6
Jaril <jarilvalenciano@gmail.com>
2024-01-10 16:32:09 -0500

    Simplify test

--- packages/e2e-tests/authenticated/new-tests-suite/test-runs-04.test.ts
+++ packages/e2e-tests/authenticated/new-tests-suite/test-runs-04.test.ts
@@ -4,16 +4,11 @@ import {
   TEST_RUN_WORKSPACE_TEAM_ID,
 } from "../../helpers/authentication";
 import test, { expect } from "../../testFixtureTestRuns";
-import {
-  filterRunsByText,
-  testRecordings,
-  testRunSummary,
-  testRunsItems,
-} from "./test-suite.utils";
+import { testRecordings, testRunSummary, testRunsItems } from "./test-suite.utils";
 
 test.use({ testRunState: "FLAKY_IN_MAIN_WITH_SOURCE" });
 
-test(`authenticated/new-test-suites/test-runs-03: flaky run in main branch with source`, async ({
+test(`authenticated/new-test-suites/test-runs-04: test ID in the URL`, async ({
   pageWithMeta: { page, clientKey, testRunId },
 }) => {
   await startLibraryTest(
@@ -27,19 +22,14 @@ test(`authenticated/new-test-suites/test-runs-03: flaky run in main branch with 
 
   //#region > Selected test run
 
-  //#region >>> Opens test run overview and match the title
-  // correct title in the overview
-  await filterRunsByText(page, clientKey);
-
-  expect(await testRunsItems(page).count()).toBe(1);
-  expect(await testRunSummary(page).count()).toBe(1);
+  //#region >>> Opens test run overview and make sure that the selected test run is the correct one
   expect(await testRunSummary(page).innerText()).toContain(clientKey);
   //#endregion
   //#endregion
 
   //#region > Replay List
 
-  //#region >>> Replay list should be displayed with both passed and flaky recording
+  //#region >>> Replay list should be displayed with the 1 passed recording
   expect(await testRecordings(page).count()).toBe(1);
   expect(await testRecordings(page).nth(0).getAttribute("data-test-status")).toBe("passed");
   //#endregion
