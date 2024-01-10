3340a80d4086ce6812ed469c64cb1582c8316476
Jaril <jarilvalenciano@gmail.com>
2024-01-10 12:23:46 -0500

    Grouping design polish (#10120)
    * Layout changes
    
    * Collapse single replay executions into just one row
    
    * Rename initial attempt to attempt 1

--- src/ui/components/Library/Team/View/Tests/Overview/Execution.tsx
+++ src/ui/components/Library/Team/View/Tests/Overview/Execution.tsx
@@ -5,17 +5,32 @@ import Icon from "ui/components/shared/Icon";
 import MaterialIcon from "ui/components/shared/MaterialIcon";
 
 import { getTruncatedRelativeDate } from "../../Recordings/RecordingListItem/RecordingListItem";
+import { StatusIcon } from "./StatusIcon";
 import { Replay, getReplayResult } from "./TestDetails";
 
 export function Execution({ execution }: { execution: TestExecution }) {
   const title = execution.commitTitle || "<Commit title unavailable>";
   const sortedRecordings = orderBy(execution.recordings, "date", "desc");
+  const shouldCollapse = sortedRecordings.length === 1;
+
+  let icon;
+  if (shouldCollapse) {
+    const recording = sortedRecordings[0];
+    icon = (
+      <StatusIcon
+        status={getReplayResult(execution.result, 0, 1)}
+        isProcessed={recording.isProcessed}
+      />
+    );
+  } else {
+    icon = <ExecutionStatus result={execution.result} />;
+  }
 
   return (
-    <div data-test-id="ExecutionItem" className="flex flex-col">
-      <div className="flex flex-row items-center justify-between gap-2 overflow-hidden border-b border-bodyBgcolor py-2 px-4">
+    <div data-test-id="ExecutionItem" className="flex flex-col px-2">
+      <div className="flex flex-row items-center justify-between gap-2 overflow-hidden py-2">
         <div className="flex flex-row items-center gap-2 overflow-hidden">
-          <ExecutionStatus result={execution.result} />
+          {icon}
           <div className="flex flex-col overflow-x-hidden">
             <div
               data-test-id="ExecutionTitle"
@@ -27,21 +42,25 @@ export function Execution({ execution }: { execution: TestExecution }) {
             {execution.commitAuthor ? <CommitAuthor author={execution.commitAuthor} /> : null}
           </div>
         </div>
-        <div className="flex flex-shrink-0 flex-row items-center gap-1">
+        <div className="flex flex-shrink-0 flex-row items-center gap-1 text-xs">
           <div className="flex w-4 items-center">
             <MaterialIcon>schedule</MaterialIcon>
           </div>
           <div data-test-id="ExecutionDate">{getTruncatedRelativeDate(execution.createdAt)}</div>
         </div>
       </div>
-      {sortedRecordings.map((r, i) => (
-        <Replay
-          recording={r}
-          key={i}
-          result={getReplayResult(execution.result, i, execution.recordings.length)}
-          attemptNumber={execution.recordings.length - i}
-        />
-      ))}
+      {!shouldCollapse ? (
+        <div className="flex flex-col gap-1">
+          {sortedRecordings.map((r, i) => (
+            <Replay
+              recording={r}
+              key={i}
+              result={getReplayResult(execution.result, i, execution.recordings.length)}
+              attemptNumber={execution.recordings.length - i}
+            />
+          ))}
+        </div>
+      ) : null}
     </div>
   );
 }
@@ -77,7 +96,7 @@ export function CommitAuthor({ author }: { author: string }) {
   return (
     <div
       data-test-id="CommitAuthor"
-      className="flex flex-row gap-4 overflow-hidden overflow-ellipsis whitespace-nowrap"
+      className="flex flex-row gap-4 overflow-hidden overflow-ellipsis whitespace-nowrap text-xs"
     >
       <div className="flex flex-row items-center gap-1">
         <div className="flex w-4 items-center">
