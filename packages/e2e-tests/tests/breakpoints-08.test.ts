537b2f46c37393fd38f11d3a48ac37182f6e00ac
Holger Benl <hbenl@evandor.de>
2024-01-11 16:23:14 +0100

    Re-record doc_navigate with Chromium (#10139)

--- packages/e2e-tests/tests/breakpoints-08.test.ts
+++ packages/e2e-tests/tests/breakpoints-08.test.ts
@@ -18,23 +18,23 @@ test(`breakpoints-08: should be temporarily disabled`, async ({
   await openDevToolsTab(page);
 
   // Add breakpoint and verify text in console
-  await addBreakpoint(page, { lineNumber: 5, url: exampleKey });
+  await addBreakpoint(page, { lineNumber: 4, url: exampleKey });
 
   // Find the newly added point in the side panel
   await openPauseInformationPanel(page);
   await openPrintStatementsAccordionPane(page);
-  const breakpoints = findPoints(page, "breakpoint", { lineNumber: 5 });
-  await expect(await breakpoints.count()).toBe(1);
+  const breakpoints = findPoints(page, "breakpoint", { lineNumber: 4 });
+  expect(await breakpoints.count()).toBe(1);
   const breakpoint = breakpoints.first();
 
   // Temporarily disable breakpoint and verify that it's still in the panel
   await togglePoint(page, breakpoint, false);
-  await expect(await breakpoints.count()).toBe(1);
+  expect(await breakpoints.count()).toBe(1);
 
   // Re-enable breakpoint
   await togglePoint(page, breakpoint, true);
 
   // Delete the breakpoint and verify that it's no longer in the side panel
-  await removeBreakpoint(page, { lineNumber: 5, url: exampleKey });
-  await expect(await breakpoints.count()).toBe(0);
+  await removeBreakpoint(page, { lineNumber: 4, url: exampleKey });
+  expect(await breakpoints.count()).toBe(0);
 });
