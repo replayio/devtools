aa41ad7edfb4fb2654904d0a8fc55b792dae2812
D. Seifert <Domiii@users.noreply.github.com>
2024-01-17 01:57:17 +0800

    Reduce examples, because having too many screws up  tests due to BAC-4470

--- packages/e2e-tests/scripts/buildkite_run_fe_tests.js
+++ packages/e2e-tests/scripts/buildkite_run_fe_tests.js
@@ -55,8 +55,14 @@ function run_fe_tests(CHROME_BINARY_PATH) {
     process.env.AUTHENTICATED_TESTS_WORKSPACE_API_KEY = process.env.RECORD_REPLAY_API_KEY;
     process.env.PLAYWRIGHT_TEST_BASE_URL = "https://app.replay.io";
 
+    const htmlFiles = [
+      "doc_rr_objects.html"
+    ];
+
     execSync(
-      `xvfb-run ./packages/e2e-tests/scripts/save-examples.ts --runtime=chromium --target=browser --project=replay-chromium-local`,
+      `xvfb-run ./packages/e2e-tests/scripts/save-examples.ts --runtime=chromium --target=browser --project=replay-chromium-local --example=${htmlFiles.join(
+        ","
+      )}`,
       { stdio: "inherit", env: process.env }
     );
 
@@ -71,45 +77,6 @@ function run_fe_tests(CHROME_BINARY_PATH) {
   {
     // Run the known-passing tests.
     const testNames = [
-      "breakpoints-01",
-      "breakpoints-03",
-      "breakpoints-04",
-      "breakpoints-05",
-      "breakpoints-06",
-      "comments-01",
-      "comments-02",
-      "comments-03",
-      "console_async_eval",
-      "console_dock",
-      "console_eval",
-      "console-expressions-01",
-      "cypress-01",
-      "cypress-02",
-      "cypress-03",
-      "elements-search",
-      "focus_mode-01",
-      "logpoints-01",
-      "logpoints-02",
-      "logpoints-03_chromium",
-      "logpoints-05",
-      "logpoints-06",
-      "logpoints-07",
-      "logpoints-08",
-      "logpoints-09",
-      "network-0",
-      "object_preview-03",
-      "object_preview-04",
-      "object_preview-05",
-      "passport-01",
-      "passport-03",
-      "passport-04",
-      "react_devtools-01-basic.test",
-      "react_devtools-03-multiple-versions",
-      "react_devtools-04-seeking",
-      "scopes_renderer",
-      "stacking_chromium",
-      "stepping-01",
-      "stepping-05_chromium",
       "object_preview-01",
       "object_preview-02",
       "object_preview-06",
