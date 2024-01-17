ac09a186fb0179aef94feb8ebe96ea577f994d04
Aditya Pandey <aditya@autospace.co>
2024-01-18 01:17:34 +0530

    Don't add listener inside function

--- packages/e2e-tests/scripts/loadRecording.ts
+++ packages/e2e-tests/scripts/loadRecording.ts
@@ -11,7 +11,23 @@ const callbacks: any = {
   onError: console.log,
 };
 
+const onProgress = ({
+  progressPercent,
+  recordingId,
+}: {
+  progressPercent: number;
+  recordingId: string;
+}) => {
+  console.log(`    ⏳ Processing recording ${recordingId} ${progressPercent}%`);
+};
+
 const client = new SimpleProtocolClient(new WebSocket(DISPATCH_URL), callbacks, console.log);
+
+client.addEventListener(
+  // @ts-expect-error - when we update protocol client to 0.68, it will throw an error - just remove this comment
+  "Recording.processRecordingProgress",
+  onProgress
+);
 
 export const loadRecording = async (recordingId: string) => {
   const { sessionId } = await client.sendCommand("Recording.createSession", {
@@ -19,19 +35,8 @@ export const loadRecording = async (recordingId: string) => {
   });
   console.log(`    ⏳ Processing recording ${recordingId} with session ${sessionId}`);
 
-  try {
-    client.addEventListener(
-      // @ts-expect-error - when we update protocol client to 0.68, it will throw an error - just remove this comment
-      "Recording.processRecordingProgress",
-      ({ progressPercent }: { progressPercent: number }) => {
-        console.log(`    ⏳ Processing recording ${recordingId} ${progressPercent}%`);
-      }
-    );
-
-    await client.sendCommand("Recording.processRecording", {
-      recordingId,
-    });
-  } finally {
-    console.log(`    ✅ Loaded recording ${recordingId}`);
-  }
+  await client.sendCommand("Recording.processRecording", {
+    recordingId,
+  });
+  console.log(`    ✅ Loaded recording ${recordingId}`);
 };
