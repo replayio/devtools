29cee2d0379d678e8d011b4df74274a206381ba4
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-01-18 08:44:56 -0500

    Improve e2e test scripts (#10182)
    We had a lot of redundant, overlapping data between examples.json and the hardcoded knownExamples array in save-examples. I got tired of trying to update an example that was missing from knownExamples. There's no reason to have both.

--- packages/e2e-tests/print-test-stats.ts
+++ packages/e2e-tests/print-test-stats.ts
@@ -15,3 +15,13 @@ console.table(browserSummaryStats);
 console.table(osSummaryStats);
 console.table(releaseYearStats);
 console.table(testFileToInfoMap);
+
+/* Log data in a format easily imported by Google Sheets
+console.log(
+  Object.entries(testFileToInfoMap)
+    .map(([key, { recordingId, runtime, runtimeOS, runtimeReleaseDate }]) => {
+      return [runtimeReleaseDate, runtimeOS, runtime, key, recordingId].join(",");
+    })
+    .join("\n")
+);
+*/
