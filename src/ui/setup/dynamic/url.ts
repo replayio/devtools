cd4e9f8ad600521525e7cbe705b10995e73ab777
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-01-24 10:44:37 -0500

    Refactored Redux-to-URL sync logic (#10217)
    1. Consolidate Redux-to-URL sync logic and cleaned up some naming/typing around URL params
    2. Add new params for: view mode, primary and secondary panel, and selected comment
    3. Initialize Redux state with new param values
    4. Delete a bunch of legacy cruft

---
+++ src/ui/setup/dynamic/url.ts
@@ -0,0 +1,107 @@
+import { ExecutionPoint, TimeStampedPointRange } from "@replayio/protocol";
+import throttle from "lodash/throttle";
+
+import { getExecutionPoint, getTime } from "devtools/client/debugger/src/selectors";
+import { ReplayClientInterface } from "shared/client/types";
+import type { ViewMode } from "shared/user-data/GraphQL/config";
+import { decodeBase64FromURL, encodeObjectToURL } from "shared/utils/environment";
+import { getSelectedCommentId } from "ui/reducers/app";
+import { getSelectedPanel, getSelectedPrimaryPanel, getViewMode } from "ui/reducers/layout";
+import type { AppStore } from "ui/setup/store";
+import type { PrimaryPanelName, SecondaryPanelName } from "ui/state/layout";
+
+export type MutableURLParams = {
+  commentId: string | null;
+  focusWindow: TimeStampedPointRange | null;
+  point: ExecutionPoint | null;
+  primaryPanel: PrimaryPanelName | null;
+  secondaryPanel: SecondaryPanelName | null;
+  time: number | null;
+  viewMode: ViewMode | null;
+};
+
+// This method returns a subset of URL parameters, onces that change to mirror Redux state;
+// Read-only parameters are managed by shared/utils/environment
+export function getMutableParamsFromURL(): MutableURLParams {
+  const { searchParams } = getURL();
+
+  let focusWindowParam = searchParams.get("focusWindow") ?? null;
+  let focusWindow: TimeStampedPointRange | null = null;
+  if (focusWindowParam) {
+    try {
+      focusWindow = decodeBase64FromURL(focusWindowParam) as TimeStampedPointRange;
+    } catch (error) {}
+  }
+
+  let time: number | null = null;
+  const timeParam = searchParams.get("time");
+  if (timeParam) {
+    const maybeTime = +timeParam;
+    if (!isNaN(maybeTime)) {
+      time = maybeTime;
+    }
+  }
+
+  return {
+    commentId: searchParams.get("commentId"),
+    focusWindow,
+    point: searchParams.get("point"),
+    primaryPanel: searchParams.get("primaryPanel") as PrimaryPanelName | null,
+    secondaryPanel: searchParams.get("secondaryPanel") as SecondaryPanelName | null,
+    time,
+    viewMode: searchParams.get("viewMode") as ViewMode | null,
+  };
+}
+
+export function getURL(): URL {
+  const url = typeof window !== "undefined" ? window.location.href : "https://app.replay.io";
+  return new URL(url);
+}
+
+// WARNING
+// Do not call this method until all reducers have been initialized.
+export function setUpUrlParamsListener(store: AppStore, replayClient: ReplayClientInterface) {
+  // Subscribe to Redux changes so we can sync them to the URL
+  //
+  // We could subscribe to store changes using startAppListening()
+  // but since there are multiple things we're checking,
+  // it seems more straightforward to just subscribe to the store directly
+  // and throttle the updates.
+  store.subscribe(
+    throttle(function onChange() {
+      const state = store.getState();
+
+      const commentId = getSelectedCommentId(state);
+      const point = getExecutionPoint(state);
+      const primaryPanel = getSelectedPrimaryPanel(state);
+      const secondaryPanel = getSelectedPanel(state);
+      const time = getTime(state);
+      const viewMode = getViewMode(state);
+
+      // This action signals a change in the focus window,
+      // but the payload only contains times (numbers).
+      // ReplayClient contains the higher resolution execution points.
+      // The timeline actions awaits ReplayClient before finishing.
+      const focusWindow = replayClient.getCurrentFocusWindow();
+
+      const params: { [key in keyof MutableURLParams]: string | undefined } = {
+        commentId: commentId != null ? commentId : undefined,
+        focusWindow: focusWindow != null ? encodeObjectToURL(focusWindow) : undefined,
+        point: point != null ? point : undefined,
+        primaryPanel: primaryPanel != null ? primaryPanel : undefined,
+        secondaryPanel: secondaryPanel != null ? secondaryPanel : undefined,
+        time: time != null ? `${time}` : undefined,
+        viewMode: viewMode != null ? viewMode : undefined,
+      };
+
+      const newUrl = getURL();
+      Object.entries(params).forEach(([key, value]) => {
+        newUrl.searchParams.set(key, value ?? "");
+      });
+
+      if (getURL().toString() !== newUrl.toString()) {
+        window.history.replaceState({}, "", newUrl.toString());
+      }
+    }, 1_000)
+  );
+}
--- src/ui/setup/listenerMiddleware.ts
+++ src/ui/setup/listenerMiddleware.ts
@@ -1,8 +1,6 @@
-import { addListener, createListenerMiddleware, nanoid } from "@reduxjs/toolkit";
-import type { TypedAddListener, TypedStartListening } from "@reduxjs/toolkit";
-import type { AnyListenerPredicate } from "@reduxjs/toolkit/dist/listenerMiddleware/types";
+import type { TypedStartListening } from "@reduxjs/toolkit";
+import { createListenerMiddleware } from "@reduxjs/toolkit";
 
-import type { UIThunkAction } from "ui/actions";
 import type { UIState } from "ui/state";
 import { ThunkExtraArgs, extraThunkArgs } from "ui/utils/thunk";
 
@@ -15,42 +13,3 @@ export const listenerMiddleware = createListenerMiddleware({
 export type AppStartListening = TypedStartListening<UIState, AppDispatch, ThunkExtraArgs>;
 
 export const startAppListening = listenerMiddleware.startListening as AppStartListening;
-
-export const addAppListener = addListener as TypedAddListener<UIState, AppDispatch, ThunkExtraArgs>;
-
-/**
- * A hacky but useful utility to add a one-shot RTK listener that
- * returns a promise that resolves when the given `predicate` returns true.
- * This allows for scenarios like "wait for this action" or "wait for this
- * state value" from inside a thunk.
- *
- * Usage:
- * ```ts
- * await dispatch(
- *   listenForCondition(
- *     (action, currState, prevState) => state.counter.value > 3
- *   )
- * )
- * ```
- */
-export const listenForCondition =
-  (predicate: AnyListenerPredicate<UIState>, timeout?: number): UIThunkAction =>
-  dispatch => {
-    // Listeners can only start in response to an action.
-    // Define a unique and useless action type we can dispatch to trigger this.
-    const dummyTriggeringActionType = `conditionListenerTrigger-${nanoid()}`;
-
-    return new Promise<boolean>((resolve, reject) => {
-      dispatch(
-        addListener({
-          type: dummyTriggeringActionType,
-          effect: (_, { condition, unsubscribe }) =>
-            condition(predicate as AnyListenerPredicate<unknown>, timeout)
-              .then(resolve)
-              .catch(reject)
-              .finally(unsubscribe),
-        })
-      );
-      dispatch({ type: dummyTriggeringActionType });
-    });
-  };
