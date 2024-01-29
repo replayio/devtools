47c97d195a950b8968bf97c154e19130aa3d31f1
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-01-29 13:25:39 -0500

    Add new Advanced user setting: feature_showLogPointPanelAboveLine

--- packages/replay-next/components/sources/SourceListRow.tsx
+++ packages/replay-next/components/sources/SourceListRow.tsx
@@ -24,6 +24,7 @@ import { bucketVisibleLines } from "replay-next/src/utils/source";
 import { ParsedToken } from "replay-next/src/utils/syntax-parser";
 import { ReplayClientContext } from "shared/client/ReplayClientContext";
 import { LineHitCounts, POINT_BEHAVIOR_DISABLED, Point } from "shared/client/types";
+import { useGraphQLUserData } from "shared/user-data/GraphQL/useGraphQLUserData";
 import { toPointRange } from "shared/utils/time";
 
 import LogPointPanel from "./log-point-panel/LogPointPanel";
@@ -80,6 +81,8 @@ export default function SourceListRow({
   const client = useContext(ReplayClientContext);
   const [{ enabled: searchEnabled, index: searchResultIndex, results: searchResults }] =
     useContext(SourceSearchContext);
+
+  const [logPointPanelAbove] = useGraphQLUserData("feature_showLogPointPanelAboveLine");
 
   const visibleSearchResults = useMemo<SourceSearchResult[]>(
     () => (searchEnabled ? searchResults.filter(result => result.lineIndex == lineIndex) : []),
@@ -154,6 +157,7 @@ export default function SourceListRow({
   return (
     <div
       className={styles.Row}
+      data-log-point-placement={logPointPanelAbove ? "above" : "below"}
       data-test-hitcounts-loaded={hitCounts != null ? true : undefined}
       data-test-line-has-hits={lineHitCounts != null ? hitCount > 0 : undefined}
       data-test-line-number={lineNumber}
@@ -217,7 +221,7 @@ export default function SourceListRow({
       )}
       {showPointPanel && (
         <LogPointPanel
-          className={styles.PointPanel}
+          className={styles.LogPointPanel}
           pointWithPendingEdits={pointWithPendingEdits}
           pointForSuspense={pointForSuspense!}
         />
