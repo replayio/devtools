replayio/devtools

commit 145c776870b6e85e8e65cc3925e57abe92d6d810

Re-add "events" info to Playwright tests (#10206)
Playwright recording data isn't rich enough. Doesn't even seem to have execution points anymore so I can't use test steps to seek to the a point in the recording. We need the events back to debug failing e2e tests.

src/ui/components/Toolbar.tsx
@@ -403,23 +403,22 @@ export default function Toolbar() {
             onClick={handleButtonClick}
           />
         ) : null}
-        {testRunner !== null ? (
-          testRunner === "cypress" ? (
-            <ToolbarButton
-              icon="cypress"
-              label="Cypress Panel"
-              name="cypress"
-              onClick={handleButtonClick}
-            />
-          ) : (
-            <ToolbarButton
-              icon="playwright"
-              label="Test Info"
-              name="cypress"
-              onClick={handleButtonClick}
-            />
-          )
+        {testRunner === "cypress" ? (
+          <ToolbarButton
+            icon="cypress"
+            label="Cypress Panel"
+            name="cypress"
+            onClick={handleButtonClick}
+          />
         ) : (
+          <ToolbarButton
+            icon="playwright"
+            label="Test Info"
+            name="cypress"
+            onClick={handleButtonClick}
+          />
+        )}
+        {testRunner !== "cypress" && (
           <ToolbarButton
             icon="info"
             label="Replay Info"

commit 214abdde18614978e42db40b1c9fe58ea512be2d

Don't show Playwright toolbar button for non-test recordings (#10257)
This regressed in #10206

src/ui/components/Toolbar.tsx
@@ -403,14 +403,15 @@ export default function Toolbar() {
             onClick={handleButtonClick}
           />
         ) : null}
-        {testRunner === "cypress" ? (
+        {testRunner === "cypress" && (
           <ToolbarButton
             icon="cypress"
             label="Cypress Panel"
             name="cypress"
             onClick={handleButtonClick}
           />
-        ) : (
+        )}
+        {testRunner === "playwright" && (
           <ToolbarButton
             icon="playwright"
             label="Test Info"