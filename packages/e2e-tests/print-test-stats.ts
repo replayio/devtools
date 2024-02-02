68f79958c1170df3945a1b26c42e6d5e8f328770
D. Seifert <Domiii@users.noreply.github.com>
2024-02-03 00:10:37 +0800

    lint fix

--- packages/e2e-tests/print-test-stats.ts
+++ packages/e2e-tests/print-test-stats.ts
@@ -1,4 +1,5 @@
 import mapValues from "lodash/mapValues";
+
 import { getStats } from "./scripts/get-stats";
 
 const {
@@ -16,17 +17,18 @@ console.table(browserSummaryStats);
 console.table(osSummaryStats);
 console.table(releaseYearStats);
 console.table(
-    mapValues(testFileToInfoMap,
-      ({ exampleName, recordingId, runtime, runtimeOS, runtimeReleaseDate }) => {
-        return {
-          exampleName,
-          runtimeReleaseDate: runtimeReleaseDate.toISOString().slice(0, 10) as any,
-          runtimeOS,
-          runtime,
-          recordingId,
-        };
-      }
-    )
+  mapValues(
+    testFileToInfoMap,
+    ({ exampleName, recordingId, runtime, runtimeOS, runtimeReleaseDate }) => {
+      return {
+        exampleName,
+        runtimeReleaseDate: runtimeReleaseDate.toISOString().slice(0, 10) as any,
+        runtimeOS,
+        runtime,
+        recordingId,
+      };
+    }
+  )
 );
 
 /* Log data in a format easily imported by Google Sheets
