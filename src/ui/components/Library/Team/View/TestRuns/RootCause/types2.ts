9e333cc1a01af9ac6d730db0e2160b4fb2fcfcf0
Jaril <jarilvalenciano@gmail.com>
2024-02-05 15:38:18 -0800

    Add better types

---
+++ src/ui/components/Library/Team/View/TestRuns/RootCause/types2.ts
@@ -0,0 +1,171 @@
+import { ExecutionPoint, MappedLocation, RequestInitiator, TimeStampedPoint } from "@replayio/protocol";
+
+type Data = {
+  result: string; // AnalysisResult
+  discrepancies: Array<RootCauseAnalysisResult> | undefined;
+};
+
+export interface RootCauseAnalysisResult {
+  // The failed test run which was analyzed.
+  failedRun: TestRunInfo;
+
+  // A particular successful run which the discrepancies will be in relation to.
+  successRun: TestRunInfo;
+
+  // Additional successful runs which were analyzed.
+  additionalSuccessRuns: TestRunInfo[];
+
+  // Discrepancies found while analyzing the failure.
+  discrepancies: AnyDiscrepancy[];
+}
+
+interface TestRunInfo {
+  id: TestId;
+  start: TimeStampedPoint;
+  end: TimeStampedPoint;
+}
+
+export interface TestId {
+  recordingId: string;
+  testId: number;
+  attempt: number;
+}
+
+type AnyDiscrepancy =
+  | ExecutedStatementDiscrepancy
+  | ReactComponentDiscrepancy
+  | NetworkEventDiscrepancy;
+
+type ExecutedStatementDiscrepancy = Discrepancy<ExecutedStatementWithDescription>;
+type ReactComponentDiscrepancy = Discrepancy<ReactComponent>;
+type NetworkEventDiscrepancy = Discrepancy<NetworkEventWithAlternate>;
+
+// Information about a discrepancy identified in a test failure.
+export interface Discrepancy<T extends DiscrepancyEvent> {
+  // The kind of discrepancy.
+  kind: DiscrepancyKind;
+
+  // The kind of underlying event.
+  eventKind: string;
+
+  // Any ID for a sequence of discrepancies this is associated with.
+  sequenceId: string;
+
+  // Information about the discrepancy. For Extra this describes an event that
+  // happened in the failing run, for Missing this describes an event that happened
+  // in a particular successful run.
+  event: T;
+
+  // Point for the most recent event in the failed run that also matches up with an
+  // event in the successful run.
+  commonPoint: ExecutionPoint;
+}
+
+// Information that must be present in events associated with discrepancies.
+export interface DiscrepancyEvent {
+  // Point in the associated recording where the event occurred.
+  point: ExecutionPoint;
+  time: number;
+
+  // Key identifying the event. All events in a run must have distinct keys,
+  // and keys between different runs must be comparable.
+  key: string;
+}
+
+// Kinds of discrepancies that can happen in a test failure.
+export enum DiscrepancyKind {
+  // Something extra happened in the failure but not in any successful run.
+  Extra = "Extra",
+
+  // Something didn't happen in the failure but happened in every successful run.
+  Missing = "Missing",
+}
+
+// Reported discrepancies for executed statements include a description of the
+// statement which is either extra or missing in the failed run.
+interface ExecutedStatementWithDescription extends ExecutedStatement {
+  description?: LocationDescription;
+}
+
+// Reported discrepancies for the contents of network requests/responses include
+// a description of what the corresponding content is in the other run.
+interface NetworkEventWithAlternate extends NetworkEvent {
+  alternate?: NetworkEventContents;
+}
+
+// Information about a statement that executed within a recording.
+export interface ExecutedStatement extends DiscrepancyEvent {
+  // Location of the statement.
+  location: MappedLocation;
+}
+
+export interface NetworkEvent extends DiscrepancyEvent {
+  requestId: string;
+  data: NetworkEventContents;
+}
+
+export type NetworkEventContents =
+  | NetworkEventContentsRequest
+  | NetworkEventContentsResponseJSON;
+
+interface NetworkEventContentsRequest {
+  kind: "Request";
+  requestUrl: string;
+  requestMethod: string;
+  requestTag: string | undefined;
+  responseCode: number | undefined;
+  initiator: RequestInitiator | undefined;
+}
+
+interface NetworkEventContentsResponseJSON {
+  kind: "ResponseJSON";
+
+  // Information about the associated request.
+  requestUrl: string;
+  requestTag: string | undefined;
+
+  // Path to the JSON value being described.
+  path: string;
+
+  // Value in the JSON at the associated path.
+  value: ComparableValue;
+}
+
+export type ComparableValue =
+  | ComparableValueLiteral
+  | ComparableValueType
+  | ComparableValueAny;
+
+// A specific literal value.
+interface ComparableValueLiteral {
+  kind: "Literal";
+  v: ValueLiteral;
+}
+
+type ValueType = "number" | "boolean" | "string" | "array";
+
+// A type of value.
+interface ComparableValueType {
+  kind: "Type";
+  type: ValueType;
+}
+
+// Topmost element in the partial order, matches any other value.
+interface ComparableValueAny {
+  kind: "Any";
+}
+
+type ValueArrayLiteral = Array<number | boolean | string>;
+type ValueLiteral = number | boolean | string | ValueArrayLiteral;
+
+export interface LocationDescription {
+  url: string | undefined;
+  line: number;
+  column: number;
+  text?: string;
+}
+
+// Information about a react component that was added.
+export interface ReactComponent extends DiscrepancyEvent {
+  nodeName: string;
+}
