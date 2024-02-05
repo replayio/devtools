82f1f2c98ec7ecf283be717b7e3a9352b4380762
Jaril <jarilvalenciano@gmail.com>
2024-02-05 15:13:25 -0800

    Root cause analysis UI prototype (#10260)
    * Display ReactComponent discrepancies
    
    * Display ExecutedStatement discrepancies
    
    * Reformat the collapsible component
    
    * Display NetworkEvent discrepancies
    
    * Fix linking to point/time
    
    * Lint

---
+++ src/ui/components/Library/Team/View/TestRuns/RootCause/ExecutedStatement.tsx
@@ -0,0 +1,69 @@
+import { useContext } from "react";
+
+import { Collapsible } from "./Collapsible";
+import { ReplayLink } from "./ReplayLink";
+import { RootCauseContext } from "./RootCause";
+import { ExecutedStatementDiscrepancyType, Sequence } from "./types";
+
+let UNKNOWN_SOURCE = "Unknown source";
+
+export function ExecutedStatementSequences({
+  sequences,
+}: {
+  sequences: Sequence<ExecutedStatementDiscrepancyType>[];
+}) {
+  return (
+    <div className="flex flex-col gap-2 pl-4">
+      {sequences.map((d, i) => (
+        <ExecutedStatementSequence group={d} key={i} />
+      ))}
+    </div>
+  );
+}
+function ExecutedStatementSequence({
+  group,
+}: {
+  group: Sequence<ExecutedStatementDiscrepancyType>;
+}) {
+  return (
+    <Collapsible label={`${group.kind} ${group.sequenceId}`}>
+      <div className="pl-4">
+        <div className="flex flex-col gap-2">
+          {group.discrepancies.map((d, i) => (
+            <ExecutedStatementDiscrepancy key={i} discrepancy={d} />
+          ))}
+        </div>
+      </div>
+    </Collapsible>
+  );
+}
+function ExecutedStatementDiscrepancy({
+  discrepancy,
+}: {
+  discrepancy: ExecutedStatementDiscrepancyType;
+}) {
+  const { failedId, successId } = useContext(RootCauseContext);
+  const source = getSource(discrepancy.event.description.url);
+  const { kind, event } = discrepancy;
+  const recordingId = kind === "Extra" ? failedId : successId;
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row justify-between gap-4">
+        <div className="truncate font-mono">{discrepancy.event.key}</div>
+        <div className="whitespace-pre">{source}</div>
+      </div>
+      <ReplayLink
+        id={recordingId}
+        kind={kind.toLowerCase() as "extra" | "missing"}
+        result={kind == "Extra" ? "failing" : "passing"}
+        point={event.point}
+        time={event.time}
+      />
+    </div>
+  );
+}
+
+function getSource(url: string) {
+  return url ? url.split("/").pop() : UNKNOWN_SOURCE;
+}
---
+++ src/ui/components/Library/Team/View/TestRuns/RootCause/ReactComponent.tsx
@@ -0,0 +1,50 @@
+import { useContext } from "react";
+
+import { Collapsible } from "./Collapsible";
+import { ReplayLink } from "./ReplayLink";
+import { RootCauseContext } from "./RootCause";
+import { ReactComponentDiscrepancyType, Sequence } from "./types";
+
+export function ReactComponentSequences({
+  sequences,
+}: {
+  sequences: Sequence<ReactComponentDiscrepancyType>[];
+}) {
+  return (
+    <div className="flex flex-col gap-2 pl-4">
+      {sequences.map((d, i) => (
+        <ReactComponentSequence group={d} key={i} />
+      ))}
+    </div>
+  );
+}
+function ReactComponentSequence({ group }: { group: Sequence<ReactComponentDiscrepancyType> }) {
+  const { failedId, successId } = useContext(RootCauseContext);
+  const recordingId = group.kind === "Extra" ? failedId : successId;
+
+  return (
+    <Collapsible label={`${group.kind} ${group.sequenceId}`}>
+      <div className="pl-4">
+        <div>
+          {group.discrepancies.map((d, i) => (
+            <ReactComponentDiscrepancy key={i} discrepancy={d} />
+          ))}
+        </div>
+        <ReplayLink
+          id={recordingId}
+          kind={group.kind.toLowerCase() as "extra" | "missing"}
+          result={group.kind == "Extra" ? "failing" : "passing"}
+          point={group.discrepancies[0].event.point}
+          time={group.discrepancies[0].event.time}
+        />
+      </div>
+    </Collapsible>
+  );
+}
+function ReactComponentDiscrepancy({
+  discrepancy,
+}: {
+  discrepancy: ReactComponentDiscrepancyType;
+}) {
+  return <div>nodeName: {discrepancy.event.nodeName}</div>;
+}
