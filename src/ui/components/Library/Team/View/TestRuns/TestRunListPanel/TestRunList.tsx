replayio/devtools

commit 299424c5a6b56d65f71f6d6ac56b25d2c28cf2c7

Fix native browser navigation with test suite dashboard (#10158)
* Fix native browser navigation with test suite dashboard

* Re-add "events" info to Playwright tests (#10206)

Playwright recording data isn't rich enough. Doesn't even seem to have execution points anymore so I can't use test steps to seek to the a point in the recording. We need the events back to debug failing e2e tests.

* Add missing doc_control_flow_firefox.html (#10207)

* Make e2e browser recording uploads "strict": fail them if any part of the upload fails. (#10208)

* Don't fail save-examples if a recording fails to process; RUN: add debug CLI logging for buildkite. (#10209)

* Don't fail save-examples if a recording fails to process; RUN: add debug CLI logging for buildkite.

* Add message for unreferenced test files.

* RUN: Fix bad env var setting (#10212)

* tighten parseQueryParams return value

* refactor ExecuctionItem to avoid invalid DOM tree

* collapse testRun.id into testRunId

* cleanup

* Consolidate non-apollo GraphQL clients (#10213)

* Update save-examples to support --build flag (#10214)

* Add new --build flag to the save-examples script so that we can more easily target batches of recordings
* Remove (broken) Playwright dynamic import; this didn't work and we won't be updating Gecko recordings anyway

* Re-record cypress-realword example (#10216)

* Remove REPLAY_DISABLE_CLONE for tests; add pre-processing to the test startup. (#10215)

* Remove REPLAY_DISABLE_CLONE for tests; add pre-processing to the test.

* Add back pre-processing of golden recordings.

* Re-use loadRecording

* use startTransition

* Update example recordings to latest Chromium release (2024-01-23) (#10218)

* Update doc_minified_chromium and rdt-react-versions examples (#10219)

* Refactored Redux-to-URL sync logic (#10217)

1. Consolidate Redux-to-URL sync logic and cleaned up some naming/typing around URL params

src/ui/components/Library/Team/View/TestRuns/TestRunListPanel/TestRunList.tsx
@@ -2,9 +2,10 @@ import { useContext, useMemo } from "react";
 import ReactVirtualizedAutoSizer from "react-virtualized-auto-sizer";
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 
-import { TestRun } from "shared/test-suites/TestRun";
+import { TestRun, filterTestRun, getTestRunTitle } from "shared/test-suites/TestRun";
 
 import { TestSuitePanelMessage } from "../../TestSuitePanelMessage";
+import { useTestRunSuspends } from "../hooks/useTestRunSuspends";
 import { TestRunsContext } from "../TestRunsContextRoot";
 import { TestRunListItem } from "./TestRunListItem";
 import styles from "./TestRunList.module.css";
@@ -15,17 +16,28 @@ type ItemData = {
 };
 
 export function TestRunList() {
-  const { filterByText, testRunsLoading, testRuns, testRunCount } = useContext(TestRunsContext);
+  const { filterByText, filterByBranch, filterByStatus } = useContext(TestRunsContext);
+  const { testRuns } = useTestRunSuspends();
+  const itemData = useMemo<ItemData>(() => {
+    let filteredTestRuns = testRuns;
 
-  const itemData = useMemo<ItemData>(
-    () => ({
+    if (filterByBranch === "primary" || filterByStatus === "failed" || filterByText !== "") {
+      filteredTestRuns = filteredTestRuns.filter(testRun =>
+        filterTestRun(testRun, {
+          branch: filterByBranch,
+          text: filterByText,
+          status: filterByStatus,
+        })
+      );
+    }
+
+    return {
       filterByText,
-      testRuns,
-    }),
-    [filterByText, testRuns]
-  );
+      testRuns: filteredTestRuns,
+    };
+  }, [filterByBranch, filterByStatus, filterByText, testRuns]);
 
-  if (!testRunsLoading && testRuns.length === 0 && testRunCount > 0) {
+  if (testRuns.length > 0 && itemData.testRuns.length === 0) {
     return (
       <TestSuitePanelMessage data-test-id="NoTestRuns" className={styles.message}>
         No test runs match the current filters
@@ -40,7 +52,7 @@ export function TestRunList() {
           children={TestRunListRow}
           className="text-sm"
           height={height}
-          itemCount={testRuns.length}
+          itemCount={itemData.testRuns.length}
           itemData={itemData}
           itemSize={28}
           width={width}

src/ui/components/Library/Team/View/TestRuns/TestRunListPanel/TestRunsStats.tsx
@@ -8,6 +8,7 @@ import { useTheme } from "shared/theme/useTheme";
 import { TestRunsContext } from "ui/components/Library/Team/View/TestRuns/TestRunsContextRoot";
 import { TimeFilterContext } from "ui/components/Library/Team/View/TimeFilterContextRoot";
 
+import { useTestRunSuspends } from "../hooks/useTestRunSuspends";
 import styles from "./TestRunsStats.module.css";
 
 type ChartDataType = { failureRate: number; label: string };
@@ -17,7 +18,7 @@ const PADDING = 4;
 const POINT_RADIUS = 4;
 
 export function TestRunsStats() {
-  const { testRuns } = useContext(TestRunsContext);
+  const { testRuns } = useTestRunSuspends();
 
   if (!testRuns.length) {
     return null;
@@ -44,7 +45,7 @@ export function TestRunsStats() {
 }
 
 function ChartWithDimensions({ height, width }: { height: number; width: number }) {
-  const { testRuns } = useContext(TestRunsContext);
+  const { testRuns } = useTestRunSuspends();
   const { startTime, endTime } = useContext(TimeFilterContext);
 
   const ref = useRef<HTMLCanvasElement>(null);

src/ui/components/Library/Team/View/TestRuns/TestRunsContextRoot.tsx
@@ -5,18 +5,13 @@ import {
   SetStateAction,
   createContext,
   useDeferredValue,
-  useEffect,
-  useMemo,
+  useLayoutEffect,
   useState,
+  useTransition,
 } from "react";
-import { STATUS_PENDING } from "suspense";
 
-import { TestRun, getTestRunTitle } from "shared/test-suites/TestRun";
 import { useGetTeamRouteParams } from "ui/components/Library/Team/utils";
 import { trackEvent } from "ui/utils/telemetry";
-
-import { useSyncTestStateToUrl } from "./hooks/useSyncTestStateToUrl";
-import { useTestRuns } from "./hooks/useTestRuns";
 
 type TestRunsContextType = {
   filterByBranch: "all" | "primary";
@@ -29,11 +24,9 @@ type TestRunsContextType = {
   setFilterByStatus: Dispatch<SetStateAction<"all" | "failed">>;
   setFilterByText: Dispatch<SetStateAction<string>>;
   setFilterTestsByText: Dispatch<SetStateAction<string>>;
-  testRunsLoading: boolean;
-  testRuns: TestRun[];
-  testRunCount: number;
+  testRunIdForSuspense: string | null;
   testRunId: string | null;
-  testRunIdForDisplay: string | null;
+  testRunPending: boolean;
   testId: string | null;
   setTestId: React.Dispatch<React.SetStateAction<string | null>>;
 };
@@ -41,79 +34,27 @@ type TestRunsContextType = {
 export const TestRunsContext = createContext<TestRunsContextType>(null as any);
 
 export function TestRunsContextRoot({ children }: { children: ReactNode }) {
-  const { teamId, testRunId: defaultTestRunId } = useGetTeamRouteParams();
-
-  const { testRuns, status } = useTestRuns();
-
-  const [testRunId, setTestRunId] = useState<string | null>(defaultTestRunId ?? null);
-
+  const { teamId, testRunId, testId } = useGetTeamRouteParams();
+  const [testRunIdForSuspense, setTestRunIdForSuspense] = useState<string | null>(
+    testRunId ?? null
+  );
   const [filterByBranch, setFilterByBranch] = useState<"all" | "primary">("all");
   const [filterByStatus, setFilterByStatus] = useState<"all" | "failed">("all");
 
   const [filterByText, setFilterByText] = useState("");
   const filterByTextDeferred = useDeferredValue(filterByText);
   const [filterTestsByText, setFilterTestsByText] = useState("");
-
-  const [testId, setTestId] = useState<string | null>(null);
-
   const router = useRouter();
 
-  const filteredTestRuns = useMemo(() => {
-    let filteredTestRuns = testRuns;
-
-    if (filterByBranch === "primary" || filterByStatus === "failed" || filterByText !== "") {
-      const lowerCaseText = filterByText.toLowerCase();
-
-      filteredTestRuns = filteredTestRuns.filter(testRun => {
-        if (filterByStatus === "failed") {
-          if (testRun.results.counts.failed === 0) {
-            return false;
-          }
-        }
-
-        const branchName = testRun.source?.branchName ?? "";
-
-        if (filterByBranch === "primary") {
-          // TODO This should be configurable by Workspace
-          if (branchName !== "main" && branchName !== "master") {
-            return false;
-          }
-        }
-
-        if (filterByText !== "") {
-          const user = testRun.source?.user ?? "";
-          const title = getTestRunTitle(testRun);
-
-          if (
-            !branchName.toLowerCase().includes(lowerCaseText) &&
-            !user.toLowerCase().includes(lowerCaseText) &&
-            !title.toLowerCase().includes(lowerCaseText)
-          ) {
-            return false;
-          }
-        }
-
-        return true;
+  const [isTestRunPending, startTestRunTransition] = useTransition();
+  useLayoutEffect(() => {
+    if (testRunId) {
+      setFilterTestsByText("");
+      startTestRunTransition(() => {
+        setTestRunIdForSuspense(testRunId);
       });
     }
-
-    return filteredTestRuns;
-  }, [filterByBranch, filterByStatus, filterByText, testRuns]);
-
-  useEffect(() => {
-    if (testRunId == null) {
-      // Select the first test run by default if nothing is selected.
-      setTestRunId(testRuns[0]?.id ?? null);
-    }
-  }, [router, teamId, testRunId, testRuns]);
-
-  useEffect(() => {
-    setFilterTestsByText("");
   }, [testRunId]);
-
-  useSyncTestStateToUrl(teamId, testRunId, setTestRunId, testId, setTestId);
-
-  const deferredTestRunId = useDeferredValue(testRunId);
 
   return (
     <TestRunsContext.Provider
@@ -124,22 +65,23 @@ export function TestRunsContextRoot({ children }: { children: ReactNode }) {
         filterByTextForDisplay: filterByText,
         filterTestsByText,
         selectTestRun: runId => {
-          setTestRunId(runId);
           trackEvent("test_dashboard.select_run", { view: "runs" });
+          router.push({ pathname: `/team/${teamId}/runs/${runId}`, query: router.query });
         },
         setFilterByBranch,
         setFilterByStatus,
         setFilterByText,
         setFilterTestsByText,
-        testRunId: deferredTestRunId,
-        testRunIdForDisplay: testRunId,
-        testRunsLoading: status === STATUS_PENDING,
-        testRuns: filteredTestRuns,
-        testRunCount: status === STATUS_PENDING ? 0 : testRuns.length,
-        testId,
+        testRunIdForSuspense: testRunIdForSuspense,
+        testRunId: testRunId ?? null,
+        testRunPending: isTestRunPending,
+        testId: testId ?? null,
         setTestId: testId => {
-          setTestId(testId);
           trackEvent("test_dashboard.select_test", { view: "runs" });
+          router.push({
+            pathname: `/team/${teamId}/runs/${testRunId}/tests/${testId}`,
+            query: router.query,
+          });
         },
       }}
     >

src/ui/components/Library/Team/View/TestRuns/hooks/useTestRunSuspends.ts
@@ -11,33 +11,24 @@ import { testRunsIntervalCache } from "../suspense/TestRunsCache";
 
 const EMPTY_ARRAY: any[] = [];
 
-export function useTestRuns(): { testRuns: TestRun[]; status: Status } {
+export function useTestRunSuspends(): { testRuns: TestRun[] } {
   const graphQLClient = useContext(GraphQLClientContext);
   const { teamId } = useContext(TeamContext);
   const { startTime, endTime } = useContext(TimeFilterContext);
-  const [testRuns, setTestRuns] = useState<TestRun[]>([]);
 
   const accessToken = useToken();
 
-  const { value = EMPTY_ARRAY, status } = useImperativeIntervalCacheValues(
-    testRunsIntervalCache,
-    startTime.getTime(),
-    endTime.getTime(),
-    graphQLClient,
-    accessToken?.token ?? null,
-    teamId
-  );
-
-  useEffect(() => {
-    if (status === STATUS_PENDING) {
-      // To avoid resetting the test runs to an empty array when loading
-      return;
-    }
-
-    setTestRuns(value);
-  }, [value, status]);
+  const testRuns = teamId
+    ? testRunsIntervalCache.read(
+        startTime.getTime(),
+        endTime.getTime(),
+        graphQLClient,
+        accessToken?.token ?? null,
+        teamId
+      )
+    : EMPTY_ARRAY;
 
   const testRunsDesc = useMemo(() => [...testRuns].reverse(), [testRuns]);
 
-  return { testRuns: testRunsDesc, status };
+  return { testRuns: testRunsDesc };
 }

commit cef19cd6537997b1c4b68ee07f10c8c7faeaaea3

Test runs chart updates when filters options change (#10268)
This worked originally but regressed in #10158 when test runs loading was moved from the context root (centralized, pre-filtered place) into the leaf nodes (TestRunList and TestRunsStats).

src/ui/components/Library/Team/View/TestRuns/TestRunListPanel/TestRunList.tsx
@@ -2,10 +2,10 @@ import { useContext, useMemo } from "react";
 import ReactVirtualizedAutoSizer from "react-virtualized-auto-sizer";
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 
-import { TestRun, filterTestRun, getTestRunTitle } from "shared/test-suites/TestRun";
+import { TestRun } from "shared/test-suites/TestRun";
 
 import { TestSuitePanelMessage } from "../../TestSuitePanelMessage";
-import { useTestRunSuspends } from "../hooks/useTestRunSuspends";
+import { useTestRunsSuspends } from "../hooks/useTestRunsSuspends";
 import { TestRunsContext } from "../TestRunsContextRoot";
 import { TestRunListItem } from "./TestRunListItem";
 import styles from "./TestRunList.module.css";
@@ -16,26 +16,15 @@ type ItemData = {
 };
 
 export function TestRunList() {
-  const { filterByText, filterByBranch, filterByStatus } = useContext(TestRunsContext);
-  const { testRuns } = useTestRunSuspends();
-  const itemData = useMemo<ItemData>(() => {
-    let filteredTestRuns = testRuns;
-
-    if (filterByBranch === "primary" || filterByStatus === "failed" || filterByText !== "") {
-      filteredTestRuns = filteredTestRuns.filter(testRun =>
-        filterTestRun(testRun, {
-          branch: filterByBranch,
-          text: filterByText,
-          status: filterByStatus,
-        })
-      );
-    }
-
-    return {
+  const { filterByText } = useContext(TestRunsContext);
+  const { testRuns } = useTestRunsSuspends();
+  const itemData = useMemo<ItemData>(
+    () => ({
       filterByText,
-      testRuns: filteredTestRuns,
-    };
-  }, [filterByBranch, filterByStatus, filterByText, testRuns]);
+      testRuns,
+    }),
+    [filterByText, testRuns]
+  );
 
   if (testRuns.length > 0 && itemData.testRuns.length === 0) {
     return (

src/ui/components/Library/Team/View/TestRuns/TestRunListPanel/TestRunsStats.tsx
@@ -5,10 +5,9 @@ import AutoSizer from "react-virtualized-auto-sizer";
 import useTooltip from "replay-next/src/hooks/useTooltip";
 import { TestRun } from "shared/test-suites/TestRun";
 import { useTheme } from "shared/theme/useTheme";
-import { TestRunsContext } from "ui/components/Library/Team/View/TestRuns/TestRunsContextRoot";
 import { TimeFilterContext } from "ui/components/Library/Team/View/TimeFilterContextRoot";
 
-import { useTestRunSuspends } from "../hooks/useTestRunSuspends";
+import { useTestRunsSuspends } from "../hooks/useTestRunsSuspends";
 import styles from "./TestRunsStats.module.css";
 
 type ChartDataType = { failureRate: number; label: string };
@@ -18,7 +17,7 @@ const PADDING = 4;
 const POINT_RADIUS = 4;
 
 export function TestRunsStats() {
-  const { testRuns } = useTestRunSuspends();
+  const { testRuns } = useTestRunsSuspends();
 
   if (!testRuns.length) {
     return null;
@@ -45,7 +44,7 @@ export function TestRunsStats() {
 }
 
 function ChartWithDimensions({ height, width }: { height: number; width: number }) {
-  const { testRuns } = useTestRunSuspends();
+  const { testRuns } = useTestRunsSuspends();
   const { startTime, endTime } = useContext(TimeFilterContext);
 
   const ref = useRef<HTMLCanvasElement>(null);

src/ui/components/Library/Team/View/TestRuns/TestRunsContextRoot.tsx
@@ -13,15 +13,18 @@ import {
 import { useGetTeamRouteParams } from "ui/components/Library/Team/utils";
 import { trackEvent } from "ui/utils/telemetry";
 
+export type FilterByBranch = "all" | "primary";
+export type FilterByStatus = "all" | "failed";
+
 type TestRunsContextType = {
-  filterByBranch: "all" | "primary";
-  filterByStatus: "all" | "failed";
+  filterByBranch: FilterByBranch;
+  filterByStatus: FilterByStatus;
   filterByText: string;
   filterByTextForDisplay: string;
   filterTestsByText: string;
   selectTestRun: Dispatch<SetStateAction<string | null>>;
-  setFilterByBranch: Dispatch<SetStateAction<"all" | "primary">>;
-  setFilterByStatus: Dispatch<SetStateAction<"all" | "failed">>;
+  setFilterByBranch: Dispatch<SetStateAction<FilterByBranch>>;
+  setFilterByStatus: Dispatch<SetStateAction<FilterByStatus>>;
   setFilterByText: Dispatch<SetStateAction<string>>;
   setFilterTestsByText: Dispatch<SetStateAction<string>>;
   testRunIdForSuspense: string | null;
@@ -38,8 +41,8 @@ export function TestRunsContextRoot({ children }: { children: ReactNode }) {
   const [testRunIdForSuspense, setTestRunIdForSuspense] = useState<string | null>(
     testRunId ?? null
   );
-  const [filterByBranch, setFilterByBranch] = useState<"all" | "primary">("all");
-  const [filterByStatus, setFilterByStatus] = useState<"all" | "failed">("all");
+  const [filterByBranch, setFilterByBranch] = useState<FilterByBranch>("all");
+  const [filterByStatus, setFilterByStatus] = useState<FilterByStatus>("all");
 
   const [filterByText, setFilterByText] = useState("");
   const filterByTextDeferred = useDeferredValue(filterByText);

src/ui/components/Library/Team/View/TestRuns/hooks/useTestRunSuspends.ts
@@ -1,34 +1,0 @@
-import { useContext, useEffect, useMemo, useState } from "react";
-import { STATUS_PENDING, Status, useImperativeIntervalCacheValues } from "suspense";
-
-import { GraphQLClientContext } from "replay-next/src/contexts/GraphQLClientContext";
-import { TestRun } from "shared/test-suites/TestRun";
-import { TeamContext } from "ui/components/Library/Team/TeamContextRoot";
-import useToken from "ui/utils/useToken";
-
-import { TimeFilterContext } from "../../TimeFilterContextRoot";
-import { testRunsIntervalCache } from "../suspense/TestRunsCache";
-
-const EMPTY_ARRAY: any[] = [];
-
-export function useTestRunSuspends(): { testRuns: TestRun[] } {
-  const graphQLClient = useContext(GraphQLClientContext);
-  const { teamId } = useContext(TeamContext);
-  const { startTime, endTime } = useContext(TimeFilterContext);
-
-  const accessToken = useToken();
-
-  const testRuns = teamId
-    ? testRunsIntervalCache.read(
-        startTime.getTime(),
-        endTime.getTime(),
-        graphQLClient,
-        accessToken?.token ?? null,
-        teamId
-      )
-    : EMPTY_ARRAY;
-
-  const testRunsDesc = useMemo(() => [...testRuns].reverse(), [testRuns]);
-
-  return { testRuns: testRunsDesc };
-}

src/ui/components/Library/Team/View/TestRuns/hooks/useTestRunsSuspends.ts
@@ -1,0 +1,49 @@
+import { useContext, useMemo } from "react";
+
+import { GraphQLClientContext } from "replay-next/src/contexts/GraphQLClientContext";
+import { TestRun, filterTestRun } from "shared/test-suites/TestRun";
+import { TeamContext } from "ui/components/Library/Team/TeamContextRoot";
+import { TestRunsContext } from "ui/components/Library/Team/View/TestRuns/TestRunsContextRoot";
+import useToken from "ui/utils/useToken";
+
+import { TimeFilterContext } from "../../TimeFilterContextRoot";
+import { testRunsIntervalCache } from "../suspense/TestRunsCache";
+
+const EMPTY_ARRAY: any[] = [];
+
+export function useTestRunsSuspends(): { testRuns: TestRun[] } {
+  const graphQLClient = useContext(GraphQLClientContext);
+  const { teamId } = useContext(TeamContext);
+  const { filterByText, filterByBranch, filterByStatus } = useContext(TestRunsContext);
+  const { startTime, endTime } = useContext(TimeFilterContext);
+
+  const accessToken = useToken();
+
+  const testRuns = teamId
+    ? testRunsIntervalCache.read(
+        startTime.getTime(),
+        endTime.getTime(),
+        graphQLClient,
+        accessToken?.token ?? null,
+        teamId
+      )
+    : EMPTY_ARRAY;
+
+  const filteredSortedTestRuns = useMemo(() => {
+    let filteredTestRuns = testRuns;
+
+    if (filterByBranch === "primary" || filterByStatus === "failed" || filterByText !== "") {
+      filteredTestRuns = filteredTestRuns.filter(testRun =>
+        filterTestRun(testRun, {
+          branch: filterByBranch,
+          text: filterByText,
+          status: filterByStatus,
+        })
+      );
+    }
+
+    return [...filteredTestRuns].reverse();
+  }, [filterByBranch, filterByStatus, filterByText, testRuns]);
+
+  return { testRuns: filteredSortedTestRuns };
+}