1ce2cda5b4d829b1be21c75a55ea0a3461346a87
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-02-05 16:07:05 -0500

    Remove monocart-reporter

--- packages/e2e-tests/testFixtureCloneRecording.ts
+++ packages/e2e-tests/testFixtureCloneRecording.ts
@@ -1,7 +1,6 @@
 import crypto from "crypto";
 import { Page, test as base } from "@playwright/test";
 import axios from "axios";
-import { addCoverageReport } from "monocart-reporter";
 
 import { TestRecordingKey } from "./helpers";
 import { TestUser } from "./helpers/authentication";
@@ -68,14 +67,6 @@ const testWithCloneRecording = base.extend<TestIsolatedRecordingFixture>({
         console.error("Error stopping JS coverage: ", err);
       }
 
-      // A couple of our tests don't use the default page object, like `auth/comments-02`
-      // and `auth/logpoints-01`. Handle missing coverage without erroring.
-      if (!jsCoverage || Object.keys(jsCoverage).length === 0) {
-        console.error("No JS coverage: ", exampleKey);
-      } else {
-        await addCoverageReport(jsCoverage, base.info());
-      }
-
       if (newRecordingId) {
         await deleteTestRecording(newRecordingId);
       }
@@ -86,6 +77,6 @@ const testWithCloneRecording = base.extend<TestIsolatedRecordingFixture>({
   },
 });
 export default testWithCloneRecording;
-export { testWithCloneRecording as test };
 export { expect } from "@playwright/test";
 export type { Page } from "@playwright/test";
+export { testWithCloneRecording as test };
