fd93a0cfd42354610711cae45e3662f06694c968
Aditya Pandey <aditya@autospace.co>
2024-02-08 11:29:01 -0700

    Move deleted recording id into examples

--- packages/e2e-tests/tests/deleted-recording.test.ts
+++ packages/e2e-tests/tests/deleted-recording.test.ts
@@ -1,13 +1,12 @@
 import test, { expect } from "@playwright/test";
 
+import examples from "../examples.json";
 import { startTest } from "../helpers";
 
-const deletedRecordingId = "94f1729a-3aab-449b-98d6-10865a5e83c5";
-
 test("deleted-recording: Show error message for deleted recording", async ({ page }) => {
-  await startTest(page, deletedRecordingId, undefined, undefined, false);
+  await startTest(page, examples["deleted-replay"].recording, undefined, undefined, false);
 
-  const error = page.locator('[data-test-id="Error-RecordingDeleted"]');
+  const error = page.locator('[data-test-id="ExpectedError"]');
   await error.waitFor({ timeout: 10_000 });
   expect(await error.isVisible()).toBe(true);
 });
