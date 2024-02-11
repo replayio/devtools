9ccaaab7c1d8512560ff0e246d3d558dc38565bd
D. Seifert <Domiii@users.noreply.github.com>
2024-02-11 21:13:41 +0800

    Separate "test logic" from "clone logic" in `testWithCloneRecording`

--- packages/e2e-tests/testFixtureCloneRecording.ts
+++ packages/e2e-tests/testFixtureCloneRecording.ts
@@ -1,5 +1,5 @@
 import crypto from "crypto";
-import { Page, test as base } from "@playwright/test";
+import { Page, test as base, expect } from "@playwright/test";
 import axios from "axios";
 
 import { TestRecordingKey } from "./helpers";
@@ -31,34 +31,38 @@ const testWithCloneRecording = base.extend<TestIsolatedRecordingFixture>({
     const testScope = crypto.randomUUID();
     let newRecordingId: string | undefined = undefined;
     try {
-      const { recording } = exampleRecordings[exampleKey];
-      console.log("Cloning recording");
-      newRecordingId = await cloneTestRecording(recording);
-
       try {
-        await loadRecording(newRecordingId);
-      } catch (e) {
-        console.warn("Error processing recording; ignoring.");
+        const { recording } = exampleRecordings[exampleKey];
+        console.log("Cloning recording");
+        newRecordingId = await cloneTestRecording(recording);
+
+        try {
+          await loadRecording(newRecordingId);
+        } catch (e) {
+          console.warn("Error processing recording; ignoring.");
+        }
+      } catch (err: any) {
+        if (axios.isAxiosError(err)) {
+          console.error("Axios error cloning recording: ", {
+            errors: err.response?.data?.errors,
+            details: err.toJSON(),
+          });
+        } else {
+          console.error("Error cloning recording: ", err);
+        }
+        throw err;
       }
 
       await page.coverage.startJSCoverage({
         resetOnNavigation: false,
       });
+
+      // Run Test:
       await use({
         page,
         recordingId: newRecordingId,
         testScope,
       });
-    } catch (err: any) {
-      if (axios.isAxiosError(err)) {
-        console.error("Axios error cloning recording: ", {
-          errors: err.response?.data?.errors,
-          details: err.toJSON(),
-        });
-      } else {
-        console.error("Error cloning recording: ", err);
-      }
-      throw err;
     } finally {
       let jsCoverage: Awaited<ReturnType<Page["coverage"]["stopJSCoverage"]>> | undefined;
       try {
