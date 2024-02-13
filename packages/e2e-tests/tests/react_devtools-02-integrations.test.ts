e4c1d5f3cbf933b12deb511894c84ee2dd25169b
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-02-13 13:03:24 -0500

    Update breakpoints-01 example

--- packages/e2e-tests/tests/react_devtools-02-integrations.test.ts
+++ packages/e2e-tests/tests/react_devtools-02-integrations.test.ts
@@ -232,7 +232,7 @@ test("react_devtools-02: RDT integrations (Chromium)", async ({
   await searchComponents(page, "Suspense");
   await verifySearchResults(page, {
     currentNumber: 1,
-    totalNumber: 15,
+    totalNumber: 16,
   });
 
   list.evaluate(el => (el.scrollTop = 0));
