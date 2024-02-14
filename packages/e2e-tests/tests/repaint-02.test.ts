0088075b4c832fb2d0cd35544c00d9e23f9a607d
Mark Erikson <mark@isquaredsoftware.com>
2024-02-13 19:09:50 -0500

    Update packages/e2e-tests/tests/repaint-02.test.ts

--- packages/e2e-tests/tests/repaint-02.test.ts
+++ packages/e2e-tests/tests/repaint-02.test.ts
@@ -6,7 +6,7 @@ import test, { expect } from "../testFixtureCloneRecording";
 
 test.use({ exampleKey: "playwright/breakpoints-05" });
 
-test("playwright/breakpoints-05: repaints on hover", async ({
+test("repaint-02: repaints on hover", async ({
   pageWithMeta: { page, recordingId },
 }) => {
   await startTest(page, recordingId);
