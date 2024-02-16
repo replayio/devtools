243b5d607368f7c848155e5386b546cda73e4517
Chris Toshok <toshok@gmail.com>
2024-02-15 16:16:08 -0800

    accept RUNTIME_BUILD_ID as an environment variable before pulling it from the triggering buildkite build

--- packages/e2e-tests/scripts/buildkite_run_fe_tests_from_artifact.ts
+++ packages/e2e-tests/scripts/buildkite_run_fe_tests_from_artifact.ts
@@ -7,7 +7,10 @@ import run_fe_tests from "./buildkite_run_fe_tests";
 export default function run_fe_tests_from_artifact(PLATFORM, ARCH) {
   console.group("BUILD PREP");
   console.time("BUILD PREP");
-  let RUNTIME_BUILD_ID = build_id_from_artifact(PLATFORM, ARCH);
+  let RUNTIME_BUILD_ID = process.env.RUNTIME_BUILD_ID;
+  if (!RUNTIME_BUILD_ID) {
+    RUNTIME_BUILD_ID = build_id_from_artifact(PLATFORM, ARCH);
+  }
   let CHROME_BINARY_PATH = install_build_products(RUNTIME_BUILD_ID, PLATFORM, ARCH);
   console.timeEnd("BUILD PREP");
   console.groupEnd();
