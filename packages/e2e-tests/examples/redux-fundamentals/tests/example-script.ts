b8592e2b2f1c1fcd15a7281c5833880aa8ab20b8
Holger Benl <hbenl@evandor.de>
2024-02-22 12:09:12 +0100

    Add comment explaining the delay in the redux-fundamentals example

--- packages/e2e-tests/examples/redux-fundamentals/tests/example-script.ts
+++ packages/e2e-tests/examples/redux-fundamentals/tests/example-script.ts
@@ -141,5 +141,7 @@ export default async function testFunction(
   await filtersPanel.getByText('Green').click()
   await waitForListItemsCount(2)
 
+  // add a little delay to ensure that the last click was added
+  // to the recording, see [FE-2286] and [RUN-3258]
   await delay(500);
 }
