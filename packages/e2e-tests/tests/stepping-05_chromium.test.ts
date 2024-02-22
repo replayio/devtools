4d96fe6cc88c1e1f3e50616505647bd6006327c8
Chris Klochek <cklochek@replay.io>
2024-02-22 15:37:25 -0500

    WIP 2

--- packages/e2e-tests/tests/stepping-05_chromium.test.ts
+++ packages/e2e-tests/tests/stepping-05_chromium.test.ts
@@ -33,19 +33,14 @@ test(`stepping-05_chromium: Test stepping in pretty-printed code`, async ({
   await resumeToLine(page, 8);
   await stepOverToLine(page, 8);
 
-  // TODO Stepping here hangs
-  // await stepOverToLine(page, 9);
+  // TODO: extra test needed.
+  await stepOverToLine(page, 8);
+  await stepOverToLine(page, 9);
 
   await openConsolePanel(page);
   await addEventListenerLogpoints(page, [{ eventType: "click", categoryKey: "mouse" }]);
   await warpToMessage(page, "(click)", 15);
 
   await stepInToLine(page, 15);
-  await stepOutToLine(page, 12);
-
-  // TODO Stepping here hangs
-  // await stepInToLine(page, 10);
-  // await stepOutToLine(page, 15);
-  // await stepInToLine(page, 5);
-  // await stepOutToLine(page, 15);
+  await stepOutToLine(page, 9);
 });
