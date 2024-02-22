522ac82fd8913ea029b0055d73f4ab11c0b9e64c
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-02-22 17:53:39 -0500

    Update stepping-05_chromium.test to match new (fixed) BAC stepping behavior (#10356)

--- packages/e2e-tests/tests/stepping-05_chromium.test.ts
+++ packages/e2e-tests/tests/stepping-05_chromium.test.ts
@@ -32,20 +32,17 @@ test(`stepping-05_chromium: Test stepping in pretty-printed code`, async ({
   await addBreakpoint(page, { url: exampleKey, lineNumber: 8 });
   await resumeToLine(page, 8);
   await stepOverToLine(page, 8);
-
-  // TODO Stepping here hangs
-  // await stepOverToLine(page, 9);
+  await stepOverToLine(page, 8); // Chromium requires an extra step than Gecko
+  await stepOverToLine(page, 9);
+  await stepOverToLine(page, 9); // Chromium requires an extra step than Gecko
+  await stepOverToLine(page, 10);
 
   await openConsolePanel(page);
   await addEventListenerLogpoints(page, [{ eventType: "click", categoryKey: "mouse" }]);
   await warpToMessage(page, "(click)", 15);
 
   await stepInToLine(page, 15);
-  await stepOutToLine(page, 12);
 
-  // TODO Stepping here hangs
-  // await stepInToLine(page, 10);
-  // await stepOutToLine(page, 15);
-  // await stepInToLine(page, 5);
-  // await stepOutToLine(page, 15);
+  await stepInToLine(page, 2);
+  await stepOutToLine(page, 15);
 });
