b9c0f8a5e577c2746308842380fa715d6948d82f
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-02-28 10:57:06 -0500

    Show stalled processing message if stuck for > 30s

--- src/ui/components/DevToolsProcessingScreen.tsx
+++ src/ui/components/DevToolsProcessingScreen.tsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from "react";
+
 import LoadingScreen from "ui/components/shared/LoadingScreen";
 import { getProcessingProgress } from "ui/reducers/app";
 import { useAppSelector } from "ui/setup/hooks";
 
+const SHOW_STALLED_MESSAGE_AFTER_MS = 30_000;
+
 export function DevToolsProcessingScreen() {
   const processingProgress = useAppSelector(getProcessingProgress);
 
-  return (
-    <LoadingScreen
-      message={
-        processingProgress == null
-          ? "Processing..."
-          : `Processing... (${Math.round(processingProgress)}%)`
-      }
-      secondaryMessage="This could take a while, depending on the complexity and length of the replay."
-    />
-  );
+  const [showStalledMessage, setShowStalledMessage] = useState(false);
+
+  useEffect(() => {
+    if (processingProgress == null) {
+      const timeout = setTimeout(() => {
+        setShowStalledMessage(true);
+      }, SHOW_STALLED_MESSAGE_AFTER_MS);
+
+      return () => {
+        clearTimeout(timeout);
+      };
+    } else {
+      setShowStalledMessage(false);
+    }
+  }, [processingProgress]);
+
+  let message = "Determining length of replay...";
+  let secondaryMessage =
+    "This could take a while, depending on the complexity and length of the replay.";
+  if (showStalledMessage) {
+    secondaryMessage = "There may be a problem. This is taking much longer than expected.";
+  } else if (processingProgress != null) {
+    message = `Processing... (${Math.round(processingProgress)}%)`;
+  }
+
+  return <LoadingScreen message={message} secondaryMessage={secondaryMessage} />;
 }
