eb74b1620355fd4880431be92d13f4b3bde3bffa
Holger Benl <hbenl@evandor.de>
2024-03-07 17:28:18 +0100

    Show picker loading state until bounding rects are loaded

--- src/ui/components/NodePickerContext.tsx
+++ src/ui/components/NodePickerContext.tsx
@@ -28,7 +28,7 @@ export type NodePickerOptions = {
 };
 
 export type NodePickerContextType = {
-  enable: (options: NodePickerOptions, initializer: () => Promise<void>) => Promise<void>;
+  enable: (options: NodePickerOptions, initializer?: () => Promise<void>) => Promise<void>;
   disable: () => void;
   status: NodePickerStatus;
   type: NodePickerType | null;
@@ -68,7 +68,11 @@ export function NodePickerContextRoot({ children }: PropsWithChildren<{}>) {
   });
 
   const enable = useCallback(
-    async (options: NodePickerOptions, initializer: () => Promise<void>) => {
+    async (options: NodePickerOptions, initializer?: () => Promise<void>) => {
+      if (!pauseId) {
+        return;
+      }
+
       const { status } = committedStateRef.current;
 
       switch (status) {
@@ -89,7 +93,11 @@ export function NodePickerContextRoot({ children }: PropsWithChildren<{}>) {
       });
 
       try {
-        await initializer();
+        await Promise.all([
+          // Start loading rects eagerly; we'll need them in the hover/click handlers
+          boundingRectsCache.readAsync(replayClient, pauseId),
+          initializer?.(),
+        ]);
 
         setState({
           options,
@@ -104,7 +112,7 @@ export function NodePickerContextRoot({ children }: PropsWithChildren<{}>) {
         });
       }
     },
-    []
+    [pauseId]
   );
 
   const disable = useCallback(() => {
@@ -151,9 +159,6 @@ export function NodePickerContextRoot({ children }: PropsWithChildren<{}>) {
       console.error("Canvas not found");
       return;
     }
-
-    // Start loading rects eagerly; we'll need them in the hover/click handlers
-    boundingRectsCache.prefetch(replayClient, pauseId);
 
     const { limitToNodeIds, onSelected } = options;
 
