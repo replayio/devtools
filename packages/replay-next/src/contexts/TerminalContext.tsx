883e1ea878c4a6dfc93355d8dc58c29c6ef3d4e9
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-03-13 10:55:53 -0400

    Delete unused replay-next test harness code

--- packages/replay-next/src/contexts/TerminalContext.tsx
+++ packages/replay-next/src/contexts/TerminalContext.tsx
@@ -1,12 +1,5 @@
 import { ExecutionPoint, FrameId, PauseId } from "@replayio/protocol";
-import {
-  PropsWithChildren,
-  createContext,
-  useCallback,
-  useMemo,
-  useState,
-  useTransition,
-} from "react";
+import { createContext } from "react";
 
 // This context stores (in memory) messages logged to the Replay Console terminal while viewing a recording.
 
@@ -29,36 +22,4 @@ export type TerminalContextType = {
   messages: TerminalExpression[];
 };
 
-let idCounter: number = 0;
-
 export const TerminalContext = createContext<TerminalContextType>(null as any);
-
-export function TerminalContextRoot({ children }: PropsWithChildren<{}>) {
-  const [messages, setMessages] = useState<TerminalExpression[]>([]);
-
-  const [isPending, startTransition] = useTransition();
-
-  const addMessage = useCallback(
-    (partialTerminalExpression: Omit<TerminalExpression, "id" | "type">) => {
-      startTransition(() => {
-        const message: TerminalExpression = {
-          ...partialTerminalExpression,
-          id: idCounter++,
-          type: "TerminalExpression",
-        };
-
-        setMessages(prevMessages => [...prevMessages, message]);
-      });
-    },
-    []
-  );
-
-  const clearMessages = useCallback(() => setMessages([]), []);
-
-  const context = useMemo(
-    () => ({ addMessage, clearMessages, isPending, messages }),
-    [addMessage, clearMessages, isPending, messages]
-  );
-
-  return <TerminalContext.Provider value={context}>{children}</TerminalContext.Provider>;
-}
