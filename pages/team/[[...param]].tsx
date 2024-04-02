c08da693756207ac6e999d0e533d8162732742e0
Holger Benl <hbenl@evandor.de>
2024-04-02 15:57:35 +0200

    Initialize UserData in the Library

--- pages/team/[[...param]].tsx
+++ pages/team/[[...param]].tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 
+import { userData } from "shared/user-data/GraphQL/UserData";
 import { setLoadingFinished } from "ui/actions/app";
 import Library from "ui/components/Library";
 import Login from "ui/components/shared/Login/Login";
@@ -11,8 +12,9 @@ export default function TeamIndex() {
   const { isAuthenticated } = useAuth0();
 
   useEffect(() => {
+    userData.initialize(isAuthenticated);
     dispatch(setLoadingFinished(true));
-  }, [dispatch]);
+  }, [dispatch, isAuthenticated]);
 
   if (!isAuthenticated) {
     return <Login returnToPath={window.location.pathname + window.location.search} />;
