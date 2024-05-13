a8fb98692b6d67c7d1f31822185051e67359561c
Chris Toshok <ctoshok@replay.io>
2024-05-13 13:38:08 -0700

    [TT-1009][TT-771] add build reason support (#10529)
    Use the build-reason machinery to allow iterating against the buildkite pipeline from the devtools repo.
    
    See the description [here](https://github.com/replayio/backend/pull/9770) for a full explanation of how things works, but the most immediately useful thing here is:
    
    When manually kicking off a new E2E test run (after making a devtools change, e.g.), you can pick the chromium build to test against by specifying one of these env vars in the buildkite UI.
    
    `RUNTIME_CHROMIUM_BUILDKITE_BUILD=<the buildkite number of the build to fetch artifacts from>`
    `RUNTIME_CHROMIUM_BUILDKITE_BRANCH=<the branch to pull from the chromium-build pipeline.  fetches the newest build on that branch, every time you rebuild>`
    `RUNTIME_CHROMIUM_BUILD_ID=<the build id corresponding to the uploaded chromium packages>`
    
    `RUNTIME_CHROMIUM_BUILDKITE_BRANCH` is probably the most useful one.  You set that to the chromium branch name, and the script will find the newest buildkite build for that branch and test against it.
    
    The env vars are all optional, and if none are present the scripts will act as if `RUNTIME_CHROMIUM_BUILDKITE_BRANCH=master` was used.

---
+++ packages/e2e-tests/scripts/buildkite_run_tests_from_reason-macos-arm64.ts
@@ -0,0 +1,5 @@
+/* Copyright 2020-2024 Record Replay Inc. */
+
+import { build_from_reason } from "./buildkite_run_tests_from_reason";
+
+build_from_reason("macOS", "arm64");
---
+++ packages/e2e-tests/scripts/buildkite_run_tests_from_reason.ts
@@ -0,0 +1,109 @@
+/* Copyright 2020-2024 Record Replay Inc. */
+
+import { execSync } from "child_process";
+import * as path from "path";
+
+import { getSecret } from "./aws_secrets";
+import { install_build_products } from "./build_products";
+import run_fe_tests from "./buildkite_run_fe_tests";
+import {
+  detectAndRecordBuildReason,
+  fetchLatestChromiumBuildOnBranch,
+  fetchRuntimeBuildIdFromChromiumBuildId,
+  fetchRuntimeBuildIdFromChromiumBuildNumber,
+} from "./detect_and_record_build_reason";
+
+function run_fe_tests_from_build_id(os: string, arch: string, runtimeBuildId: string) {
+  console.group("BUILD PREP");
+  console.time("BUILD PREP");
+  process.env.RUNTIME_BUILD_ID = runtimeBuildId;
+  let CHROME_BINARY_PATH = install_build_products(runtimeBuildId, os, arch);
+  console.timeEnd("BUILD PREP");
+  console.groupEnd();
+
+  run_fe_tests(CHROME_BINARY_PATH);
+}
+
+function buildUrl(pipelineSlug, buildNumber) {
+  return `https://buildkite.com/replay/${pipelineSlug}/builds/${buildNumber}`;
+}
+
+function describeReason(markdownMessage, rebuild) {
+  // do something with `rebuild` here...
+  execSync(`buildkite-agent annotate --style info "${markdownMessage}"`, {
+    stdio: "inherit",
+  });
+}
+
+export async function build_from_reason(os, arch) {
+  const buildReason = await detectAndRecordBuildReason();
+
+  if (!process.env.BUILDKITE_TOKEN) {
+    process.env.BUILDKITE_TOKEN = getSecret("prod/buildkite-retry-graphql-token", "us-east-2");
+  }
+
+  switch (buildReason.reason) {
+    case "runtime-chromium-build-id": {
+      describeReason(
+        `Testing against chromium buildid ${buildReason.chromiumBuildId}`,
+        buildReason.rebuild
+      );
+      run_fe_tests_from_build_id(os, arch, buildReason.chromiumBuildId);
+      break;
+    }
+    case "chromium-build-number": {
+      describeReason(
+        `Testing against chromium build number [#${buildReason.buildNumber}](${buildUrl(
+          "chromium-build",
+          buildReason.buildNumber
+        )})`,
+        buildReason.rebuild
+      );
+      const runtimeBuildId = await fetchRuntimeBuildIdFromChromiumBuildNumber(
+        os,
+        arch,
+        buildReason.buildNumber
+      );
+      run_fe_tests_from_build_id(os, arch, runtimeBuildId);
+      break;
+    }
+    case "chromium-branch": {
+      const buildkiteToken = process.env.BUILDKITE_TOKEN;
+      if (!buildkiteToken) {
+        throw new Error("Missing BUILDKITE_TOKEN");
+      }
+
+      const { buildId, buildNumber } = await fetchLatestChromiumBuildOnBranch(
+        buildkiteToken,
+        buildReason.branch
+      );
+      describeReason(
+        `Testing against latest chromium build on branch \\\`${
+          buildReason.branch
+        }\\\`: [#${buildNumber}](${buildUrl("chromium-build", buildNumber)})`,
+        buildReason.rebuild
+      );
+      const runtimeBuildId = await fetchRuntimeBuildIdFromChromiumBuildId(os, arch, buildId);
+      run_fe_tests_from_build_id(os, arch, runtimeBuildId);
+      break;
+    }
+    case "triggered": {
+      describeReason(
+        `Testing triggered from [${buildReason.pipelineSlug}#${buildReason.buildNumber}](${buildUrl(
+          buildReason.pipelineSlug,
+          buildReason.buildNumber
+        )})`,
+        buildReason.rebuild
+      );
+      const runtimeBuildId = await fetchRuntimeBuildIdFromChromiumBuildId(
+        os,
+        arch,
+        buildReason.buildId
+      );
+      run_fe_tests_from_build_id(os, arch, runtimeBuildId);
+      break;
+    }
+    default:
+      throw new Error(`Unknown build reason: ${JSON.stringify(buildReason)}`);
+  }
+}
