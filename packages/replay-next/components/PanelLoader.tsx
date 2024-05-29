19d4eb0cbecd23d7e0851f523973817b8801b795
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-05-29 08:37:35 -0400

    Warn about possible timeout loading React annotations (#10546)

--- packages/replay-next/components/PanelLoader.tsx
+++ packages/replay-next/components/PanelLoader.tsx
@@ -1,15 +1,23 @@
-import { CSSProperties } from "react";
+import { CSSProperties, ReactNode } from "react";
 
 import Loader from "replay-next/components/Loader";
 import { LoadingProgressBar } from "replay-next/components/LoadingProgressBar";
 
 import styles from "./PanelLoader.module.css";
 
-export function PanelLoader({ className, style }: { className?: string; style?: CSSProperties }) {
+export function PanelLoader({
+  className,
+  message,
+  style,
+}: {
+  className?: string;
+  message?: ReactNode;
+  style?: CSSProperties;
+}) {
   return (
     <div className={`${styles.Wrapper} ${className}`} style={style}>
       <LoadingProgressBar />
-      <Loader className={styles.Loader} />
+      <Loader className={styles.Loader} message={message} />
     </div>
   );
 }
