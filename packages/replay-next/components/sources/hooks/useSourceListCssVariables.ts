2fbd6290337b21fe5df4912d5b40e3e9a658bfea
Brian Vaughn <brian.david.vaughn@gmail.com>
2024-06-21 11:20:27 -0700

    Add ref to dependencies array

--- packages/replay-next/components/sources/hooks/useSourceListCssVariables.ts
+++ packages/replay-next/components/sources/hooks/useSourceListCssVariables.ts
@@ -48,7 +48,7 @@ export function useSourceListCssVariables({
     return () => {
       observer.disconnect();
     };
-  }, []);
+  }, [elementRef]);
 
   useLayoutEffect(() => {
     const element = elementRef.current;
