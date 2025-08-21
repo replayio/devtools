593a8fdd3989e9a222248981bb0792f0a7de4dec
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-08-21 12:15:53 +0200

    skip it for v2 too

--- packages/shared/test-suites/RecordingTestMetadata.ts
+++ packages/shared/test-suites/RecordingTestMetadata.ts
@@ -921,7 +921,7 @@ export async function processPlaywrightTestRecording(
       id,
       result,
       source,
-      timeStampedPointRange: getPlaywrightTestTimeStampedPointRange(events),
+      timeStampedPointRange: null
     };
   } else if (isTestRecordingV3(testRecording)) {
     return testRecording;
